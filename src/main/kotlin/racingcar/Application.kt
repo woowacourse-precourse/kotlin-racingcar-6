b21003ef9ea43531d04b6f94419ba1ebb714ce2f
yeji0214 <1225yj50@naver.com>
2023-10-26 20:54:35 +0900

    시도할 횟수 입력받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,7 @@
 package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console.readLine
+import kotlin.reflect.typeOf
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.")
@@ -11,6 +12,12 @@ fun main() {
             throw IllegalArgumentException("모든 자동차 이름은 1글자 이상, 5글자 이하여야 합니다.")
         carNamesAndResults[i] = ""
     }
+
+    println("시도할 횟수는 몇 회인가요?")
+    val timeInput = readLine()
+
+
+    //print(time is Char)
 //    for (i in carNamesAndResults)
 //        println(i)
     //print(carNamesAndResults)
