be81a7a59a06080105ddb667c03fb1c9c2216400
Sheep1sik <bohg6799@naver.com>
2023-10-26 22:46:31 +0900

    feat(racingcar-Validator): validateUserInput 추가
    자동차의 이름이 5자 이하인지 검증기능 구현

---
+++ src/main/kotlin/racingcar/Validator.kt
@@ -0,0 +1,19 @@
+package racingcar
+
+class Validator {
+    // 요구사항 검증, 전진 조건 검증을 담당
+    fun validateUserInput(carNames: List<String>) {
+        // 자동차의 이름이 5자 이하인지 검증 기능
+        for (i in 0..carNames.size) {
+            if (carNames[i].length > 5) {
+                throw IllegalArgumentException("이름은 5자 이하여야 합니다.")
+            }
+        }
+
+        // 시도 횟수가 숫자가 맞는지 검증 기능
+
+    }
+
+    // 랜덤값이 전진하는 조건에 일지하는지 검증 기능
+
+}
