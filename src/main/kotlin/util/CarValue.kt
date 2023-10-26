bc1485814d5cea43b4b96df98c882560ceb654f0
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-26 17:18:35 +0900

    feat: 자동차의 기본 속성 값 작성

---
+++ src/main/kotlin/util/CarValue.kt
@@ -0,0 +1,8 @@
+package util
+
+object CarValue {
+    const val MIN_RANDOM_VALUE = 0
+    const val MAX_RANDOM_VALUE = 9
+    const val MIN_FORWARD_VALUE = 4
+    const val DISTANCE_SYMBOL = "-"
+}
