1977992e1a42887313667faec298909929bf7eeb
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-26 17:47:55 +0900

    feat: 자동차 이름 검사 기능 작성

---
+++ src/main/kotlin/util/Validator.kt
@@ -0,0 +1,19 @@
+package util
+
+import util.CarValue.CAR_NAME_MAX_LENGTH
+
+object Validator {
+    fun inputCarContentCheck(input: String): List<String> {
+        val carList = input.split(",")
+        carNameLengthCheck(carList)
+        return carList
+    }
+
+    private fun carNameLengthCheck(carList: List<String>) {
+        carList.forEach {
+            if (it.length > CAR_NAME_MAX_LENGTH) {
+                throw IllegalArgumentException()
+            }
+        }
+    }
+}
