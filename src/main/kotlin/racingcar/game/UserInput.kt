8ebf343377bff1e94288931116024e5a5514b57c
민대인 <bamin0422@gmail.com>
2023-10-27 00:25:03 +0900

    feat: getRacingCars의 잘못된 값이 입력되었을 때 검사하고 예외처리하는 로직 구현(자동차 없음, 중복 이름 발생, 이름 길이 유효하지 않음)

--- src/main/kotlin/racingcar/game/UserInput.kt
+++ src/main/kotlin/racingcar/game/UserInput.kt
@@ -1,12 +1,36 @@
 package racingcar.game
 
 import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
 
 object UserInput {
-    fun getRacingCars(): List<Car> =
-        Console.readLine().split(", ").map {_name ->
-            Car(_name)
+    private const val ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_EMPTY_DATA = "잘못된 입력 값입니다.(자동차 없음)"
+    private const val ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_SAME_NAME = "잘못된 입력 값입니다.(중복 이름 발생)"
+    private const val ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_NAME_LENGTH = "잘못된 입력 값입니다.(이름의 길이가 유효하지 않음)"
+    private val NAME_LENGTH_RANGE = 1..5
+
+    fun getRacingCars(): List<Car> {
+        val racingCars = Console.readLine().split(", ").map {_name ->
+            if (_name.length in NAME_LENGTH_RANGE) {
+                Car(_name)
+            }
+            else {
+                throw IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_NAME_LENGTH)
+            }
         }
+        checkRacingCarsAvailable(racingCars)
+        return racingCars
+    }
+
+    private fun checkRacingCarsAvailable(racingCars: List<Car>) {
+        if (racingCars.isEmpty()) {
+            throw IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_EMPTY_DATA)
+        }
+
+        if (racingCars.size != racingCars.toSet().size) {
+            throw IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_SAME_NAME)
+        }
+    }
 
     fun getAttempts(): Int = Console.readLine()!!.toInt()
 }
