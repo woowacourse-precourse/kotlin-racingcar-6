56a03c32eb41a06a11aed008b71a25ee836e2be7
Sheep1sik <bohg6799@naver.com>
2023-10-26 21:42:16 +0900

    feat(racingcar-CarGameManager): getInputUser 추가
    사용자의 자동차 이름, 시도 횟수를 입력받는 기능

--- src/main/kotlin/racingcar/CarGameManager.kt
+++ src/main/kotlin/racingcar/CarGameManager.kt
@@ -1,12 +1,23 @@
 package racingcar
+
 import camp.nextstep.edu.missionutils.Console
 import racingcar.Message.Companion.INPUT_PROMPT_MESSAGE
-class CarGameManager {
-    fun play(){
-        println(INPUT_PROMPT_MESSAGE)
 
-    }
-    private fun getInputUser(): String? {
+class CarGameManager {
+
+    // 전반적인 게임의 구동을 담당.
+
+
+    // 사용자의 자동차 이름, 시도횟수를 입력 받는 기능
+    private fun getInputUser(): String {
         return Console.readLine()
     }
+
+    // 0 - 9 사이의 무작위 값을 부여하는 기능
+
+    // 전진하는 자동차 이름과 함께 출력하는 기능
+
+    // 우승자 판별 후 출력 기능 (공동 우승자일시 쉼표(,)구분)
+
+
 }
