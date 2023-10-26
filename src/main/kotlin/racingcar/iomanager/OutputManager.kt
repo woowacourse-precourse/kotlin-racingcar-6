f09f88f324978199d778534f783cd21efaf5980b
jiwon2724 <stopone3119@naver.com>
2023-10-27 02:50:26 +0900

    [feat] : 출력 담당 클래스 구현 완료

---
+++ src/main/kotlin/racingcar/iomanager/OutputManager.kt
@@ -0,0 +1,21 @@
+package racingcar.iomanager
+
+class OutputManager {
+    fun racingCarNames() {
+        println(INPUT_RACING_CAR_NAMES)
+    }
+
+    fun attemptNumber() {
+        println(INPUT_ATTEMPT_NUMBER)
+    }
+
+    fun racingResult() {
+        println(RACING_RESULT)
+    }
+
+    companion object {
+        private const val INPUT_RACING_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        private const val INPUT_ATTEMPT_NUMBER = "시도할 횟수는 몇 회인가요?"
+        private const val RACING_RESULT = "실행 결과"
+    }
+}
