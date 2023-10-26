c6e54b05b48740bcb2141b668793e87956e1d7bb
yeji0214 <1225yj50@naver.com>
2023-10-26 20:30:39 +0900

    자동차 이름 예외처리(빈 문자열이거나 5자 초과인 경우)

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,8 +6,11 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.")
     val carNames = readLine().split(',')
     val carNamesAndResults = mutableMapOf<String, String>()
-    for (i in carNames)
+    for (i in carNames) {
+        if (i == "" || i.length > 5)
+            throw IllegalArgumentException("모든 자동차 이름은 1글자 이상, 5글자 이하여야 합니다.")
         carNamesAndResults[i] = ""
+    }
 //    for (i in carNamesAndResults)
 //        println(i)
     //print(carNamesAndResults)
