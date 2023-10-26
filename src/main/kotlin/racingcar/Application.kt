e8bcd50d4cfd027676f0c74be9284f266a4e8018
HYUNJIN LEE <dlguswls456@naver.com>
2023-10-26 18:46:05 +0900

    feat: 자동차 이름 입력 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,23 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+
+    val carList = getCars()
+
+}
+
+private fun getCars(): MutableList<Car> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val inputNames = Console.readLine().split(",")
+    val carList = mutableListOf<Car>()
+    for (carName in inputNames) {
+        if (carName.isEmpty() || carName.length > 5) {
+            throw IllegalArgumentException()
+        }
+        carList.add(Car(name = carName))
+    }
+
+    return carList
 }
