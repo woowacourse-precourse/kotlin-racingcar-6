c7ec45d3b6a428c6728e20074a1896fbbd9e3dca
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-26 21:11:19 +0900

    refactor: 주행거리 자료형 통일, BigInteger

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,19 +1,22 @@
 import util.CarValue.CAR_INIT_DISTANCE_VALUE
 import util.CarValue.DISTANCE_SYMBOL
+import java.math.BigInteger
 
 class Car(name: String) : Acceleration {
     private var carName = name
-    private var length = CAR_INIT_DISTANCE_VALUE
+    private var distanceTraveled = CAR_INIT_DISTANCE_VALUE.toBigInteger()
 
     fun getName() = carName
 
     fun printCarInfo() {
         print("$carName : ")
-        repeat(length) {
+        var length = BigInteger.ZERO
+        while (length < distanceTraveled) {
             print(DISTANCE_SYMBOL)
+            length++
         }
         println()
     }
 
-    fun lengthPlus() = ++length
+    fun lengthPlus() = ++distanceTraveled
 }
