b80d4d7d0aae35ff69c05eef0ad257978a488f59
Tnalxmsk <alstnwkd990@naver.com>
2023-10-27 02:01:35 +0900

    feat: Racing class 거리 저장, 출력 메서드 추가
    선수가 이동한 거리를 저장하는 saveDistance() 메서드 추가
    선수들이 이동한 거리를 보여주는 printDistance() 메서드 추가

--- src/main/kotlin/racingcar/service/Racing.kt
+++ src/main/kotlin/racingcar/service/Racing.kt
@@ -3,13 +3,31 @@ package racingcar.service
 import camp.nextstep.edu.missionutils.Randoms
 import racingcar.domain.Car
 
+var COUNT = 0
+const val GO = 1
+const val STOP = 0
+const val GO_STRING = "-"
+const val STOP_STRING = ""
+
 class Racing {
     init {
         startRacing()
     }
+
     private fun startRacing() {
         val playerList = choicePlayer()
         val cycle = Input().inputCycle()
+        val distanceList = MutableList(playerList.size) { "" }
+
+        print("\n실행결과\n")
+
+        while (COUNT < cycle) {
+            repeat(playerList.size) {
+                saveDistance(decideMove(playerList[it]), distanceList, it)
+            }
+            printDistance(playerList, distanceList)
+            COUNT++
+        }
     }
 
     private fun choicePlayer(): List<Car> {
@@ -17,11 +35,25 @@ class Racing {
         return nameList.map { name -> Car(name) }
     }
 
-    fun decideMove(player: Car): Int {
+    private fun decideMove(player: Car): Int {
         return if (Randoms.pickNumberInRange(0, 9) > 3) {
             player.moveForward()
         } else {
             player.stop()
         }
+    }
+
+    private fun saveDistance(move: Int, distance: MutableList<String>, index: Int) {
+        when (move) {
+            GO -> distance[index] += GO_STRING
+            STOP -> distance[index] += STOP_STRING
+        }
+    }
+
+    private fun printDistance(playerList: List<Car>, distanceList: List<String>) {
+        repeat(playerList.size) {
+            println("${playerList[it].name} : ${distanceList[it]}")
+        }
+        println()
     }
 }
