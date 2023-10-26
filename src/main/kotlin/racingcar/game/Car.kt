782b26837fe9d15c16e7d62c7827bffaf5797bfe
민대인 <bamin0422@gmail.com>
2023-10-26 22:54:44 +0900

    feat: Car 클래스 파라미터로 name 추가 및 toString 메서드 재정의

--- src/main/kotlin/racingcar/game/Car.kt
+++ src/main/kotlin/racingcar/game/Car.kt
@@ -1,7 +1,11 @@
 package racingcar.game
 
-class Car {
+class Car (private val name: String) {
     private var distance = STARTING_POINT
+
+    override fun toString(): String {
+        return "$name : ${"-".repeat(distance)}"
+    }
 
     fun getDistance() = distance
 
