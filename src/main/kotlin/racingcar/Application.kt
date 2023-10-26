16bc54404f63ccaaabf1e349188a011f3d3363aa
Chankyu Lee <blueriz@naver.com>
2023-10-26 20:31:10 +0900

    경주할 자동차 및 시도 횟수 입력받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,9 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.")
+    val carname = readLine()
+    val cars = carname?.split(",")
+    println("시도할 횟수는 몇 회인가요?")
+    val numoftry = readLine()
 }
