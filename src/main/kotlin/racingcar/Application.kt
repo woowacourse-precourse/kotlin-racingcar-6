72c0a43edb3819a7d20008ff5c6e5d944c3834fd
Chankyu Lee <blueriz@naver.com>
2023-10-26 20:59:13 +0900

    랜덤 숫자 생성 및 출력

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,30 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("경주할 자동차 이름을 입력하세요.")
     val carname = readLine()
     val cars = carname?.split(",")
     println("시도할 횟수는 몇 회인가요?")
-    val numoftry = readLine()
+    val numoftry = readLine()?.toInt()
+    if (numoftry != null) {
+        println("실행 결과")
+        for (i in 0..numoftry){
+
+        }
+    }
+
+}
+
+fun generateRandomNumber(cars : List<String>)
+{
+    for (i in 0..cars.size)
+    {
+        for (car in cars){
+            val randomNumber = Randoms.pickNumberInRange(0, 9)
+            val dash = "-".repeat(randomNumber)
+            println("${car} : ${dash}")
+        }
+    }
 }
