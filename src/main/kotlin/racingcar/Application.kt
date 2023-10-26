428bd8e26636a1d8ce7b1f4d856c19d7383cdf58
Chankyu Lee <blueriz@naver.com>
2023-10-26 22:20:21 +0900

    자동차 전진조건 설정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,27 +4,40 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.")
-    val carname = readLine()
-    val cars = carname?.split(",")
-    println("시도할 횟수는 몇 회인가요?")
-    val numoftry = readLine()?.toInt()
-    if (numoftry != null) {
-        println("실행 결과")
-        for (i in 0..numoftry){
-
+    val carname = readlnOrNull() // 자동차 이름을 입력받음
+    val cars = carname?.split(",") // ,를 기준으로 나눔
+    val numofcars = cars?.size
+    if (numofcars != null)
+    {
+        var moveCar = Array(size = numofcars) {0}
+        println("시도할 횟수는 몇 회인가요?")
+        val numoftry = readlnOrNull()?.toInt()
+        if (numoftry != null) {
+            println("실행 결과")
+            for (i in 0 until numoftry){
+                generateRandomNumberAndCheckMove(cars, moveCar)
+                println("")
+            }
+            for (j in 0 until numofcars)
+            {
+                val name = cars[j]
+                val total = moveCar[j]
+                println("$name : $total")
+            }
         }
     }
-
 }
 
-fun generateRandomNumber(cars : List<String>)
+fun generateRandomNumberAndCheckMove(cars : List<String>, carmove : Array<Int>)
 {
-    for (i in 0..cars.size)
+    for (i in cars.indices)
     {
-        for (car in cars){
-            val randomNumber = Randoms.pickNumberInRange(0, 9)
-            val dash = "-".repeat(randomNumber)
-            println("${car} : ${dash}")
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        if (randomNumber >= 4) {
+            carmove[i] += randomNumber
         }
+        val dash = "-".repeat(randomNumber)
+        val carname = cars[i]
+        println("$carname : $dash")
     }
 }
