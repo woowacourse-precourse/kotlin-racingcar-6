16fa184e210fb5803d1bd95b5d1416130b003442
민대인 <bamin0422@gmail.com>
2023-10-26 22:46:52 +0900

    feat: inviteRacer 메서드 구현 및 racingCars 프로퍼티 추가 및 Car 클래스 STARTING_POINT 상수화

--- src/main/kotlin/racingcar/game/Car.kt
+++ src/main/kotlin/racingcar/game/Car.kt
@@ -1,11 +1,15 @@
 package racingcar.game
 
 class Car {
-    private var distance = 0
+    private var distance = STARTING_POINT
 
     fun getDistance() = distance
 
     fun move() {
         distance++
+    }
+
+    companion object {
+        private const val STARTING_POINT = 0
     }
 }
--- src/main/kotlin/racingcar/game/RacingGame.kt
+++ src/main/kotlin/racingcar/game/RacingGame.kt
@@ -1,7 +1,15 @@
 package racingcar.game
 
 class RacingGame {
+    private lateinit var racingCars: MutableList<Car>
     fun start() {
         // TODO : startGame 메서드 구현
+    }
+
+    private fun inviteRacer(n: Int) {
+        racingCars = mutableListOf()
+        repeat(n) {
+            racingCars.add(Car())
+        }
     }
 }
