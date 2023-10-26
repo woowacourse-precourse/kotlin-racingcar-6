cac274960df177142b292bae71ca47a595943dac
민대인 <bamin0422@gmail.com>
2023-10-27 00:32:13 +0900

    feat: 이동횟수에 자연수가 아닌 입력이 들어왔을 때 예외처리

--- src/main/kotlin/racingcar/game/UserInput.kt
+++ src/main/kotlin/racingcar/game/UserInput.kt
@@ -1,12 +1,13 @@
 package racingcar.game
 
 import camp.nextstep.edu.missionutils.Console
-import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 object UserInput {
     private const val ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_EMPTY_DATA = "잘못된 입력 값입니다.(자동차 없음)"
     private const val ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_SAME_NAME = "잘못된 입력 값입니다.(중복 이름 발생)"
     private const val ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_NAME_LENGTH = "잘못된 입력 값입니다.(이름의 길이가 유효하지 않음)"
+    private const val ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_NOT_NATURAL_NUMBER = "잘못된 입력 값입니다. (시도횟수에 자연수가 아닌 값 입력)"
     private val NAME_LENGTH_RANGE = 1..5
 
     fun getRacingCars(): List<Car> {
@@ -32,5 +33,11 @@ object UserInput {
         }
     }
 
-    fun getAttempts(): Int = Console.readLine()!!.toInt()
+    fun getAttempts(): Int =
+        try {
+            Console.readLine()!!.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_NOT_NATURAL_NUMBER)
+        }
+
 }
