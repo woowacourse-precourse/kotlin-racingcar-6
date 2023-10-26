3b305e1218a200c867b37bfb12523a22870771b0
민대인 <bamin0422@gmail.com>
2023-10-27 07:07:42 +0900

    refactor: PrintUtil 내부 상수 네이밍 변경

--- src/main/kotlin/racingcar/game/PrintUtil.kt
+++ src/main/kotlin/racingcar/game/PrintUtil.kt
@@ -2,19 +2,19 @@ package racingcar.game
 
 object PrintUtil {
     private const val START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    private const val ASK_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
-    private const val EXECUTION_RESULT = "실행 결과"
+    private const val ASK_ATTEMPTS_MESSAGE = "시도할 횟수는 몇 회인가요?"
+    private const val EXECUTION_RESULT_MESSAGE = "실행 결과"
 
     fun printStartMessage() {
         println(START_MESSAGE)
     }
 
     fun printAskAttempts() {
-        println(ASK_ATTEMPTS)
+        println(ASK_ATTEMPTS_MESSAGE)
     }
 
     fun printExecutionResult() {
-        println(EXECUTION_RESULT)
+        println(EXECUTION_RESULT_MESSAGE)
     }
 
     fun printRacingCars(racingCars: List<Car>) {
