80af39fa10f3fd8ea86f416482ddb1c9717b8dc4
yeji0214 <1225yj50@naver.com>
2023-10-26 20:21:47 +0900

    자동차 이름을 쉼표 기준으로 구분해서 맵에 추가(key: 자동차 이름, value: 초기값())

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.")
+    val carNames = readLine().split(',')
+    val carNamesAndResults = mutableMapOf<String, String>()
+    for (i in carNames)
+        carNamesAndResults[i] = ""
+//    for (i in carNamesAndResults)
+//        println(i)
+    //print(carNamesAndResults)
 }
