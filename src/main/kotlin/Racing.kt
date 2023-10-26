d94d77ca30dd41240a14ebce6c4a41328a86900a
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-26 20:13:26 +0900

    feat: 우승자 판별 기능 작성

--- src/main/kotlin/Racing.kt
+++ src/main/kotlin/Racing.kt
@@ -1,6 +1,9 @@
 import java.math.BigInteger
 
 object Racing {
+
+    private var maxLength = 0
+
     fun racingSetting(carList: List<Car>, round: BigInteger) {
         var count = BigInteger.ZERO
         println("실행 결과")
@@ -12,12 +15,16 @@ object Racing {
 
     private fun racingStart(carList: List<Car>) {
         for (car in carList) {
-            if (car.isGo()) {
-                car.lengthPlus()
-            }
-            car.printName()
-            car.printLength()
+            if (car.isGo()) maxLength = Math.max(car.lengthPlus(), maxLength)
+            car.printCarInfo()
         }
         println()
+    }
+
+    fun racingResult(carList: List<Car>) {
+        val winnerList = mutableListOf<String>()
+        carList.forEach {
+            if (it.lengthPlus() == maxLength + 1) winnerList.add(it.getName())
+        }
     }
 }
