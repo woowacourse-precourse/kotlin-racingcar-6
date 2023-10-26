aad1d3e97524868f2b6d448ef86430c1a6e4a9bd
lh99j <grand2181@gmail.com>
2023-10-26 19:07:07 +0900

    feat: 자동차 이름 입력받는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,15 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     printGameStartMessage()
 }
 
 fun printGameStartMessage() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+}
+
+fun inputCarNames(): String {
+    return Console.readLine()
 }
