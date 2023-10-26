9d0725f721f1385287b3eacbc4603a4962c8e71e
Sheep1sik <bohg6799@naver.com>
2023-10-26 19:14:41 +0900

    feat: Message Class 추가
    - 자동차 경주 게임의 메세지 및 값 관리를 위한 Message Class 추가

---
+++ src/main/kotlin/racingcar/Message.kt
@@ -0,0 +1,12 @@
+package racingcar
+
+class Message{
+    companion object{
+        const val INPUT_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        const val TRY_COUNT_PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+        const val EXECUTION_RESULT_MESSAGE = "실행 결과"
+        const val MIN_NUMBER = 0
+        const val MAX_NUMBER = 9
+        const val FORWARD_CONDITION = 4
+    }
+}
