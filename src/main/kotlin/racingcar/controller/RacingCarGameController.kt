8a53084abc04d0b9c3d27e0962df7fad7b2f2082
JunJangE <wnswkd486@gmail.com>
2023-10-27 21:14:54 +0900

    feat: 레이싱 카 레이스 진행 기능

--- src/main/kotlin/racingcar/controller/RacingCarGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarGameController.kt
@@ -13,6 +13,7 @@ class RacingCarGameController(
     fun playGame() {
         val racingCarNames = getRacingCarNames()
         val attemptsNum = getAttemptsNum()
+        val racingCars = race(racingCarNames = racingCarNames, attemptsNum = attemptsNum)
     private fun getRacingCarNames(): List<String> {
         outputView.printCarNamePrompt()
         return inputView.inputRacingCarNames()
@@ -24,5 +25,16 @@ class RacingCarGameController(
         outputView.printEmptyLine()
         return attemptsNum
     }
+
+    private fun race(racingCarNames: List<String>, attemptsNum: Int): List<RacingCar> {
+        var racingCars = racingCarGame.createRacingCars(racingCars = racingCarNames)
+        outputView.printGameResult()
+
+        repeat(attemptsNum) {
+            racingCars = racingCarGame.moveRacingCars(racingCars = racingCars)
+            outputView.printCarRaceResults(racingCars = racingCars)
+        }
+
+        return racingCars
     }
 }
