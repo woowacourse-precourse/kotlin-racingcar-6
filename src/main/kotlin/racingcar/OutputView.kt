7b1413a0c11a3fd0c0d9f8c3cc02360db5d80a5d
Hogu59 <dbstjdguswkd@naver.com>
2023-10-27 14:19:00 +0900

    feat: OutputView, 줄 간격 수정

--- src/main/kotlin/racingcar/OutputView.kt
+++ src/main/kotlin/racingcar/OutputView.kt
@@ -2,14 +2,13 @@ package racingcar
 
 class OutputView {
     companion object {
-        fun printExecutionResultMention() = println("실행 결과")
+        fun printExecutionResultMention() = println("\n실행 결과")
 
         fun printExecutionResult(list : List<Car>) {
             val sb = StringBuilder()
             list.forEach {
                  sb.append("${it.name} : ${printCurrentPosition(it.getPosition())}\n")
             }
-            sb.append("\n")
             println(sb)
         }
 
@@ -20,9 +19,7 @@ class OutputView {
         }
 
         fun printWinners(nameList : List<String>) {
-            println("최종 우승자 : ${nameList.joinToString(", ")}")
+            print("최종 우승자 : ${nameList.joinToString(", ")}")
         }
-
-
     }
 }
