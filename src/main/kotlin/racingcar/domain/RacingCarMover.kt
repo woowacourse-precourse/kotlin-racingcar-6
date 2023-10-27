0c86816557e44b203c78454e5f1e9e8518cf4ad0
posite <su84053@gmail.com>
2023-10-27 17:30:53 +0900

    feat: 차 전진시키기 기능 구현

---
+++ src/main/kotlin/racingcar/domain/RacingCarMover.kt
@@ -0,0 +1,23 @@
+package racingcar.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class RacingCarMover {
+
+    fun moveRacingCars(racingCars: RacingCars): RacingCars {
+        racingCars.loadRacingCars().forEach {
+            if (generateRandomNumber() > BOUNDARY_NUMBER) {
+                it.addForwardCount()
+            }
+        }
+        return racingCars
+    }
+
+    private fun generateRandomNumber(): Int = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)
+
+    companion object {
+        private const val BOUNDARY_NUMBER = 4
+        private const val MIN_NUMBER = 0
+        private const val MAX_NUMBER = 9
+    }
+}
