2bbf5c69e051fff9c1175708d597c365c3120f83
sunkk8482 <sunkk8482@naver.com>
2023-10-27 21:48:53 +0900

    feat :

--- src/main/kotlin/racingcar/CheckError.kt
+++ src/main/kotlin/racingcar/CheckError.kt
@@ -5,6 +5,9 @@ fun nameErrorCheck(name: String) {
     for (i in name.indices) {
         if (name[i] in '0'..'9') throw IllegalArgumentException("이름에 숫자가 섞여있습니다")
     }
+    if (name.isEmpty()) {
+        throw IllegalArgumentException("공백은 안됩니다!")
+    }
 }
 
 fun redundancyErrorCheck(names: List<String>) {
@@ -14,6 +17,9 @@ fun redundancyErrorCheck(names: List<String>) {
 }
 
 fun numberTimesErrorCheck(number: String) {
+    if (number.isEmpty()) {
+        throw IllegalArgumentException("공백은 안됩니다!")
+    }
     for (i in number.indices) {
         if (number[i] > '9' || number[i] < '0') {
             throw IllegalArgumentException("입력값에 숫자가 아닌 값이 있습니다.")
