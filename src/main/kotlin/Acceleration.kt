3192ecab3054490bf0c7425397d3132698642d31
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-27 17:56:51 +0900

    refactor: 인터페이스 -> 추상클래스 변경

--- src/main/kotlin/Acceleration.kt
+++ src/main/kotlin/Acceleration.kt
@@ -3,7 +3,7 @@ import util.CarValue.MAX_RANDOM_VALUE
 import util.CarValue.MIN_FORWARD_VALUE
 import util.CarValue.MIN_RANDOM_VALUE
 
-interface Acceleration {
+abstract class Acceleration {
     fun isGo(): Boolean {
         return Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE) >= MIN_FORWARD_VALUE
     }
