b02eaf493a986d900cc43408755a157f85ab2173
okm1172 <okm1172@naver.com>
2023-10-27 10:18:56 +0900

    feat : 자동차 이름 입력 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,15 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+fun enterCarName(){
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+
+    val initCarName = Console.readLine()
+
+}
+
 fun main() {
-    // TODO: 프로그램 구현
+    enterCarName()
 }
