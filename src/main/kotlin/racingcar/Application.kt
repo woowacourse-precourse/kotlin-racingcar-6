7b9540fd754e79d593eaeac1308d23c5418d02cd
okm1172 <okm1172@naver.com>
2023-10-27 11:47:32 +0900

    코드 리팩토링 & 모호한 변수/함수 이름 변경

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,7 +3,7 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
-fun checkAvailableCarName(carName: List<String>) {
+fun checkExceptionCarName(carName: List<String>) {
     for(name in carName) {
         if(name.length > 5) {
             throw IllegalArgumentException("Car name must be at most 5 letters")
@@ -11,25 +11,33 @@ fun checkAvailableCarName(carName: List<String>) {
     }
 }
 
+fun checkExceptionTryCount(tryCount: Int) {
+    if(tryCount <= 0) {
+        throw IllegalArgumentException("Count must be natural number")
+    }
+}
+
 fun splitCarName(initCarName: String): List<String> {
     return initCarName.split(",")
 }
 
-fun enterCarName(): List<String> {
+fun getCarName(): List<String> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 
     val initCarName = Console.readLine()
     val carName = splitCarName(initCarName)
 
-    checkAvailableCarName(carName)
+    checkExceptionCarName(carName)
 
     return carName
 }
 
-fun enterTryCount(): Int {
+fun getTryCount(): Int {
     println("시도할 횟수는 몇 회인가요?")
 
     val tryCount = Console.readLine().toInt()
+
+    checkExceptionTryCount(tryCount)
 
     return tryCount
 }
@@ -39,69 +47,77 @@ fun initCarForwardCount(carForwardCount: MutableList<Int>, tryCount: Int) {
         carForwardCount.add(0)
 }
 
-fun decideNumCount(carForwardCount: MutableList<Int>, carCount: Int, randomNum: Int) {
+fun saveForwardCountEachCar(carForwardCount: MutableList<Int>, carCount: Int, randomNum: Int) {
     if(randomNum>=4) {
         carForwardCount[carCount]++
     }
 }
 
-fun randomNumEachGame(carForwardCount: MutableList<Int>) {
-
+fun saveForwardCountEachGame(carForwardCount: MutableList<Int>) {
     for(carCount in carForwardCount.indices) {
-
         val randomNum = Randoms.pickNumberInRange(0,9)
 
-        decideNumCount(carForwardCount, carCount, randomNum)
+        saveForwardCountEachCar(carForwardCount, carCount, randomNum)
     }
 }
 
 fun printProgress(carName: List<String>, carForwardCount: MutableList<Int>) {
     for( i in carName.indices ) {
-
         print("${carName[i]} : ")
-
-        printProgressCount(carForwardCount[i])
+        printProgressForwardCount(carForwardCount[i])
     }
     println("")
 }
 
-fun printProgressCount(forwardCount: Int) {
-
+fun printProgressForwardCount(forwardCount: Int) {
     for(i in 1..forwardCount) {
         print("-")
     }
     println("")
 }
 
-fun startGame(carName: List<String>, tryCount: Int) {
+fun playGame(carName: List<String>, tryCount: Int) {
     //자동차 별 전진 횟수 저장
     val carForwardCount = mutableListOf<Int>()
 
     initCarForwardCount(carForwardCount,carName.size)
 
+    printRunningResult(carName, carForwardCount, tryCount)
+
+    printVictoryCar(carForwardCount,carName)
+}
+
+fun printRunningResult(carName: List<String>, carForwardCount: MutableList<Int>, tryCount: Int) {
+
     println("")
     println("실행 결과")
-    
+
     for(gameCount in 0..(tryCount-1)) {
-        randomNumEachGame(carForwardCount)
+        saveForwardCountEachGame(carForwardCount)
         printProgress(carName, carForwardCount)
     }
+}
 
+fun printVictoryCar(carForwardCount: MutableList<Int>, carName: List<String>) {
     val finalVictoryCar = mutableListOf<String>()
 
+    checkFinalVictoryCar(finalVictoryCar, carForwardCount, carName)
+    printFinalVictoryCarWithSeparator(finalVictoryCar)
+}
+
+fun checkFinalVictoryCar(finalVictoryCar: MutableList<String>, carForwardCount: MutableList<Int>, carName: List<String>) {
     for(i in carForwardCount.indices) {
         if(carForwardCount.max() == carForwardCount[i]) {
             finalVictoryCar.add(carName[i])
         }
     }
-    printFinalVictoryCar(finalVictoryCar)
 }
 
-fun printFinalVictoryCar(finalVictoryCar: MutableList<String>) {
+fun printFinalVictoryCarWithSeparator(finalVictoryCar: MutableList<String>) {
     print("최종 우승자 : ")
     print(finalVictoryCar.joinToString(separator = ", "))
-
 }
+
 fun main() {
-    startGame(enterCarName(),enterTryCount())
+    playGame(getCarName(),getTryCount())
 }
