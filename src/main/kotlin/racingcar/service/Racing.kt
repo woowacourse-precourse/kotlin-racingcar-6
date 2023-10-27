7be8dff072a428de65678fb03bb0461c03ad7b3e
Tnalxmsk <alstnwkd990@naver.com>
2023-10-27 17:54:28 +0900

    refactor: Winner, Racing 메서드 분리
    distance와 winner 와 관련된 함수를 Winner, Distance 클래스로 분리하여 Racing 클래스의 역할을 확실히 하도록 수정

--- src/main/kotlin/racingcar/service/Racing.kt
+++ src/main/kotlin/racingcar/service/Racing.kt
@@ -5,25 +5,25 @@ import racingcar.domain.Car
 
 class Racing {
     init {
-        startRacing(Input())
+        startRacing(Input(), Winner(), Distance())
     }
 
-    private fun startRacing(input: Input) {
+    private fun startRacing(input: Input, winner: Winner, distance: Distance) {
         val playerList = choicePlayer()
         val cycle = input.inputCycle()
         val distanceList = MutableList(playerList.size) { "" }
-
         print("\n실행결과\n")
 
         while (COUNT < cycle) {
             repeat(playerList.size) { index ->
-                saveDistance(decideMove(playerList[index]), distanceList, index)
+                val move = distance.decideDistance(playerList[index])
+                distance.saveDistance(move, distanceList, index)
             }
-            printDistance(playerList, distanceList)
+            distance.printDistance(playerList, distanceList)
             COUNT++
         }
-        val winner = decideWinner(playerList, distanceList)
-        printWinner(winner)
+        val winnerList = winner.decideWinner(playerList, distanceList)
+        winner.printWinner(winnerList)
     }
 
     private fun choicePlayer(): List<Car> {
@@ -32,54 +32,7 @@ class Racing {
         return nameList.map { name -> Car(name) }
     }
 
-    private fun decideMove(player: Car): Int {
-        return if (Randoms.pickNumberInRange(0, 9) > 3) {
-            player.moveForward()
-        } else {
-            player.stop()
-        }
-    }
-
-    private fun saveDistance(move: Int, distance: MutableList<String>, index: Int) {
-        when (move) {
-            GO -> distance[index] += GO_STRING
-            STOP -> distance[index] += STOP_STRING
-        }
-    }
-
-    private fun printDistance(playerList: List<Car>, distanceList: List<String>) {
-        repeat(playerList.size) {index ->
-            println("${playerList[index].name} : ${distanceList[index]}")
-        }
-        println()
-    }
-
-    private fun decideWinner(playerList: List<Car>, distanceList: List<String>): List<String> {
-        val playerMap = changeListToMap(playerList, distanceList)
-        val longDistance = distanceList.sorted().reversed()[0]
-
-        return playerMap.filterValues { it == longDistance }.keys.toList()
-    }
-
-    private fun changeListToMap(playerList: List<Car>, distanceList: List<String>) : Map<String, String> {
-        val playerMap = mutableMapOf<String, String>()
-        repeat(playerList.size) {
-            playerMap[playerList[it].name] = distanceList[it]
-        }
-        return playerMap
-    }
-
-    private fun printWinner(winnerList: List<String>) {
-        print("최종 우승자 : ")
-        val winner = winnerList.joinToString(", ")
-        println(winner)
-    }
-
     companion object {
         var COUNT = 0
-        private const val GO = 1
-        private const val STOP = 0
-        private const val GO_STRING = "-"
-        private const val STOP_STRING = ""
     }
 }
