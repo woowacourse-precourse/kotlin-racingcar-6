3aaee3d8799e74e391ef44c1d66adcee39bf529d
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-27 12:00:24 +0900

    refactor: 출력 메세지 상수 처리

---
+++ src/main/kotlin/MessageManager.kt
@@ -0,0 +1,4 @@
+object MessageManager {
+    const val INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val INPUT_RACING_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?"
+}
--- src/main/kotlin/RacingManger.kt
+++ src/main/kotlin/RacingManger.kt
@@ -1,13 +1,15 @@
 import CarFactory.makeCar
+import MessageManager.INPUT_CAR_NAME_MESSAGE
+import MessageManager.INPUT_RACING_ROUND_MESSAGE
 import Racing.racingSetting
 import camp.nextstep.edu.missionutils.Console
 import util.Validator.inputRacingRoundContentCheck
 
 class RacingManger {
     fun racingInfoInput() {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)") // 추후 특정 객체에서 메시지 관리
+        println(INPUT_CAR_NAME_MESSAGE)
         val racingCarList = makeCar(Console.readLine())
-        println("시도할 횟수는 몇 회인가요?")
+        println(INPUT_RACING_ROUND_MESSAGE)
         val raceRound = inputRacingRoundContentCheck(Console.readLine())
         racingSetting(racingCarList, raceRound)
     }
