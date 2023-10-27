40e85e08e70fefa9394968db14a6931b13e1951f
Hogu59 <dbstjdguswkd@naver.com>
2023-10-27 13:55:41 +0900

    feat: Racing class 구현

---
+++ src/main/kotlin/racingcar/Racing.kt
@@ -0,0 +1,52 @@
+package racingcar
+
+class Racing() {
+    private var round = 0
+    private lateinit var cars : List<Car>
+    fun run(){
+        cars = getCarList(InputView.getNameList())
+        round = InputView.getRoundNumber()
+
+        allRounds()
+    }
+
+    private fun getCarList(list : List<String>) : List<Car> {
+        val carList = mutableListOf<Car>()
+        list.forEach {
+            carList.add(Car(it))
+        }
+        return carList
+    }
+
+    private fun allRounds() {
+        repeat(round) { oneRound() }
+        OutputView.printWinners(getWinnerList())
+    }
+
+    private fun oneRound() {
+        cars.forEach { it.goOrStop() }
+        OutputView.printExecutionResult(cars)
+    }
+
+    fun getWinnerList() : List<String> {
+        val nameList = mutableListOf<String>()
+        val farthest = getFarthestPosition()
+
+        cars.forEach {
+            if(it.getPosition() == farthest) nameList.add(it.name)
+        }
+        return nameList
+    }
+
+    private fun getFarthestPosition() : Int {
+        var farthest = 0
+        cars.forEach {
+            if(it.getPosition() > farthest) farthest = it.getPosition()
+            //farthest = kotlin.math.max(farthest, it.getPosition())
+        }
+        return farthest
+    }
+
+
+
+}
