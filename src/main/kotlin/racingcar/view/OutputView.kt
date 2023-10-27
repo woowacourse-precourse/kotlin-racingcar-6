16f37c0a0cc898a2117ef4cf1c97534b162a3339
JunJangE <wnswkd486@gmail.com>
2023-10-27 20:52:27 +0900

    feat: 사용자에게 시도 횟수 입력을 요청하는 기능

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,7 +1,9 @@
 package racingcar.view
 
+import racingcar.utils.Constant.ATTEMPTS_NUM_PROMPT_MESSAGE
 import racingcar.utils.Constant.CAR_NAME_PROMPT_MESSAGE
 
 class OutputView {
     fun printCarNamePrompt() = println(CAR_NAME_PROMPT_MESSAGE)
+    fun printAttemptsPrompt() = println(ATTEMPTS_NUM_PROMPT_MESSAGE)
 }
