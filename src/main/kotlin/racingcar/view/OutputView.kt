383a5618d04efdf4814244af013bab7141d9d691
JunJangE <wnswkd486@gmail.com>
2023-10-27 21:12:55 +0900

    feat: 레이싱 카 경주 결과를 출력하는 기능

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -6,5 +6,12 @@ import racingcar.utils.Constant.CAR_NAME_PROMPT_MESSAGE
 class OutputView {
     fun printCarNamePrompt() = println(CAR_NAME_PROMPT_MESSAGE)
     fun printAttemptsPrompt() = println(ATTEMPTS_NUM_PROMPT_MESSAGE)
+    fun printCarRaceResults(racingCars: List<RacingCar>) {
+        racingCars.forEach { racingCar ->
+            println(RACING_CAR_RESULT_MESSAGE.format(racingCar.name, racingCar.raceRecord))
+        }
+        printEmptyLine()
+    }
+
     fun printEmptyLine() = println()
 }
