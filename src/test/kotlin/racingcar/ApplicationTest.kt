5b4a3a8c89ccac917e64267db56ba0b1a6404fb8
kimhm0728 <kimhm0728@naver.com>
2023-10-27 12:32:04 +0900

    test: 문자열을 쉼표 기준으로 분리하는 기능 테스트

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import racingcar.util.toCarNameList
 
 class ApplicationTest : NsTest() {
     @Test
@@ -24,6 +25,30 @@ class ApplicationTest : NsTest() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
         }
+    }
+
+    @Test
+    fun `문자열을 쉼표 기준으로 분리한다`() {
+        // given
+        val case = "pobi,woni,jun"
+
+        // when
+        val result = case.toCarNameList()
+
+        // then
+        assertThat(result).containsExactly("pobi", "woni", "jun")
+    }
+
+    @Test
+    fun `쉼표가 없는 문자열을 쉼표 기준으로 분리하는 경우 기존 문자열을 반환`() {
+        // given
+        val case = "pobi"
+
+        // when
+        val result = case.toCarNameList()
+
+        // then
+        assertThat(result).isEqualTo("pobi")
     }
 
     public override fun runMain() {
