6b2ddbd6d276c787cf485f19ad783f7a7f47641d
dpcks0509 <dpcks0509@gmail.com>
2023-10-27 23:09:09 +0900

    feat : 기능1. 자동차 이름 입력받기 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,15 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val carNameList = getCarName()
+}
+
+// 기능 1. 자동차 이름 입력받기
+fun getCarName(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNameInputList = Console.readLine()
+
+    return carNameInputList.split(",")
 }
