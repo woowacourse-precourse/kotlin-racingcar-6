a5e2efd076f57161bc458b5f9b79b961aa58d04f
jiwon2724 <stopone3119@naver.com>
2023-10-27 22:26:08 +0900

    [feat] : ValidationManager 구현

---
+++ src/main/kotlin/racingcar/manager/ValidationManager.kt
@@ -0,0 +1,31 @@
+package racingcar.manager
+
+class ValidationManager {
+    fun getValidCarNames(userInput: String): String {
+        return userInput.trim().let { input ->
+            isEmptyString(input)
+            isStartAndEndWithComma(input)
+            isLengthFive(input.split(",").map { it.trim() })
+            userInput
+        }
+    }
+
+    private fun isEmptyString(userInput: String): Boolean {
+        return if (userInput != EMPTY_STRING) true else throw IllegalArgumentException()
+    }
+
+    private fun isStartAndEndWithComma(carName: String): Boolean {
+        val isCheckStartEndWithComma = carName.startsWith(",") || carName.endsWith(",")
+        return if (!isCheckStartEndWithComma) true else throw IllegalArgumentException()
+    }
+
+    private fun isLengthFive(carNameList: List<String>): Boolean {
+        carNameList.forEach { if (it.length >= CAR_NAME_SIZE) throw IllegalArgumentException() }
+        return true
+    }
+
+    companion object {
+        private const val CAR_NAME_SIZE = 5
+        private const val EMPTY_STRING = ""
+    }
+}
