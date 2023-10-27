9adb38d22383774555046c383d41904b0f33991b
blueme0 <ksytpdus0304@gmail.com>
2023-10-27 23:24:12 +0900

    fix: 최종 우승자 다수가 가능하도록 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,6 +4,18 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
+    /*
+    경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)
+pobi,woni,jun
+시도할 횟수는 몇 회인가요?
+5
+
+실행 결과
+pobi : -
+woni :
+jun : -
+
+     */
     val getNames = Console.readLine().split(",")
     val peopleNum = getNames.size
     var trialCount = 0
@@ -28,9 +40,17 @@ fun main() {
         }
     }
 
-    var winner: String = cars.keys.first()
+    var winner = mutableListOf<String>()
+    var winnerScore: Int = 0
     for (name in cars.keys) {
-        if (cars[name]!! > cars[winner]!!) winner = name
+        if (cars[name]!! == winnerScore) {
+            winner.add(name)
+        }
+        if (cars[name]!! > winnerScore) {
+            winnerScore = cars[name]!!
+            winner = mutableListOf<String>()
+            winner.add(name)
+        }
     }
 
 }
