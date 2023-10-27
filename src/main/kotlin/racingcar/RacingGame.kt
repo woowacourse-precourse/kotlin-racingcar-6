82c4d33b594f438d2a0addc21bcec4b801dd09ed
Kim Gimun <kmkim7575@gmail.com>
2023-10-27 21:49:00 +0900

    feat: add function to determine whether each car can move or not and return the result

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -4,6 +4,18 @@ import camp.nextstep.edu.missionutils.Randoms
 
 object RacingGame {
 
+    fun getResult(racingCars: List<RacingCar>): List<RacingCar> {
+        val numbers = pickNumbers(racingCars.size)
+
+        racingCars.zip(numbers) { car, number ->
+            if (number >= 4) {
+                car.moves += 1
+            }
+        }
+
+        return racingCars
+    }
+
     fun pickNumbers(players: Int): List<Int> {
         val numbers = mutableListOf<Int>()
         repeat(players) {
