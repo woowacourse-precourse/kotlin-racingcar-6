0b675771efe71f2e67425a5b63deff94928f028f
mk43313@gmail.com <mk43313@gmail.com>
2023-10-27 20:38:38 +0900

    FEAT checkCar

--- src/main/kotlin/race/Race.kt
+++ src/main/kotlin/race/Race.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 import kotlin.IllegalArgumentException
 
 class Race {
-    private val garage = mutableMapOf<String, Int>()
+    public val garage = mutableMapOf<String, Int>()
 
     companion object {
         fun run() {
@@ -14,11 +14,26 @@ class Race {
         }
     }
     fun inputCar() {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼포(,) 기준으로 구분")
-        val cars = Console.readLine().split(',')
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+        val carString = Console.readLine()
+        checkCar(carString)
+    }
+
+    private fun checkCar(carString:String) {
+        if (carString.isEmpty()) {
+            throw IllegalArgumentException("자동차 이름을 입력해주세요.")
+        } else if (carString.contains(",,") || carString.startsWith(',') || carString.endsWith(',')) {
+            throw IllegalArgumentException("올바른 자동차 이름을 입력해 주세요.")
+        }
+        val cars = carString.split(',')
         for (car in cars) {
-            val trimmedCar = car.trim()
-            garage[trimmedCar] = 0
+            if (car.trim().isEmpty() || car.contains(" ")) {
+                throw IllegalArgumentException("잘못된 입력 형식 입니다.")
+            } else if (car.length > 5) {
+                throw IllegalArgumentException("자동차 이름의 길이는 1에서 5사이여야 합니다.")
+            } else {
+                garage[car] = 0
+            }
         }
     }
 }
