3d6a8dd9ba85075c6b4b03cb7c82db40404c7044
sunkk8482 <sunkk8482@naver.com>
2023-10-27 14:48:52 +0900

    feat : 입력 및 출력 View

---
+++ src/main/kotlin/racingcar/View.kt
@@ -0,0 +1,36 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+class View {
+    fun enterNames(): String {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        return Console.readLine()
+    }
+
+    fun enterNumberTimes(): String {
+        println("시도할 횟수는 몇 회인가요?")
+        return Console.readLine()
+    }
+
+    fun printPosition(cars: List<Car>) {
+        println("실행 결과")
+        for (i in cars) {
+            println("${i.getName()} : ${i.getPosition()}")
+        }
+    }
+
+    fun printWinner(winnerList: List<String>) {
+        print("최종 우승자 : ")
+        if (winnerList.size == 1) {
+            print(winnerList[0])
+            return
+        }
+        print(winnerList[0])
+        for (i in winnerList) {
+            print(", $i")
+        }
+    }
+
+
+}
