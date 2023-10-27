55c32cb2048dd322137973d6065dc418ddf9f16d
okm1172 <okm1172@naver.com>
2023-10-27 10:25:18 +0900

    exception : 자동차 이름 5자 이하 예외 처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,6 +3,13 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
+fun checkAvailableCarName(carName: List<String>) {
+    for(name in carName) {
+        if(name.length > 5)
+            throw IllegalArgumentException("Car name must at most 5 letters")
+    }
+}
+
 fun splitCarName(initCarName: String): List<String> {
     return initCarName.split(",")
 }
@@ -11,8 +18,11 @@ fun enterCarName(): List<String> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 
     val initCarName = Console.readLine()
+    val carName = splitCarName(initCarName)
 
-    return splitCarName(initCarName)
+    checkAvailableCarName(carName)
+
+    return carName
 }
 
 fun main() {
