cccc3060879da611f2a5a9dea951961df07ff9b9
Minjun Kim <damon911@naver.com>
2023-10-27 23:30:00 +0900

    feat: 게임 시작 기능 추가

---
+++ src/main/kotlin/racingcar/game/Game.kt
@@ -0,0 +1,10 @@
+package racingcar.game
+
+import racingcar.util.Constants.TEXT_START_GAME
+
+class Game {
+
+    fun startGame() {
+        println(TEXT_START_GAME)
+    }
+}
---
+++ src/main/kotlin/racingcar/util/Constants.kt
@@ -0,0 +1,6 @@
+package racingcar.util
+
+object Constants {
+    // TEXT
+    const val TEXT_START_GAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+}
