e212eeca452295b015cce3a088d0c1253ab71779
MinhoiKoo <minlove2013@gmail.com>
2023-10-27 23:17:23 +0900

    feat: InputView Class 생성
    -자동차 이름, 반복 횟수 입력

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,18 @@
+package racingcar.view
+
+import camp.nextstep.edu.missionutils.Console
+import racingcar.model.CarName
+
+class InputView {
+    fun inputCarName(): List<CarName> {
+        val cars = Console.readLine().split(",").map { name -> CarName(name) }
+        return cars
+    }
+
+    fun inputRound(): Int {
+        val round = Console.readLine().toInt()
+        return round
+    }
+
+
+}
