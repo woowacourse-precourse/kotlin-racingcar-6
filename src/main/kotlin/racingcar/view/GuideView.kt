78902d1d74100e34c5287c424e851d6c9b38998e
윤동현 <donghyunyoon81@gmail.com>
2023-10-27 22:57:11 +0900

    feat: 시도할 횟수 입력을 안내할 문자열 출력하는 기능 추가

--- src/main/kotlin/racingcar/view/GuideView.kt
+++ src/main/kotlin/racingcar/view/GuideView.kt
@@ -2,8 +2,14 @@ package racingcar.view
 
 object GuideView {
     private const val INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    private const val INPUT_ROUND_COUNT = "시도할 횟수는 몇 회인가요?"
+
     fun inputCarsName() {
         println(INPUT_CARS_NAME)
+    }
+
+    fun inputRoundCnt(){
+        println(INPUT_ROUND_COUNT)
     }
 
 }
