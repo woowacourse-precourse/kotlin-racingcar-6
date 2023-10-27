f0dcb435515c4bcc0ec4e3f23c675be099f6aaaa
Hogu59 <dbstjdguswkd@naver.com>
2023-10-27 13:38:43 +0900

    feat: 출력 - 각 차수별 실행결과 출력 구현

---
+++ src/main/kotlin/racingcar/OutputView.kt
@@ -0,0 +1,24 @@
+package racingcar
+
+class OutputView {
+    companion object {
+        fun printExecutionResultMention() = println("실행 결과")
+
+        fun printExecutionResult(list : List<Car>) {
+            val sb = StringBuilder()
+            list.forEach {
+                 sb.append("${it.name} : ${printCurrentPosition(it.getPosition())}\n")
+            }
+            sb.append("\n")
+            println(sb)
+        }
+
+        fun printCurrentPosition(position : Int) : String {
+            var curPosition = ""
+            for(i in 0 until position) curPosition += "-"
+            return curPosition
+        }
+
+
+    }
+}
