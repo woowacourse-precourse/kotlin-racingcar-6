82f79be690a3174308ad165d3ac07c79f1dd497e
윤동현 <donghyunyoon81@gmail.com>
2023-10-27 22:58:40 +0900

    rename: 출력 역할에 어울리는 함수명으로 변경

--- src/main/kotlin/racingcar/view/GuideView.kt
+++ src/main/kotlin/racingcar/view/GuideView.kt
@@ -4,11 +4,11 @@ object GuideView {
     private const val INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     private const val INPUT_ROUND_COUNT = "시도할 횟수는 몇 회인가요?"
 
-    fun inputCarsName() {
+    fun printInputCarsName() {
         println(INPUT_CARS_NAME)
     }
 
-    fun inputRoundCnt(){
+    fun printInputRoundCnt(){
         println(INPUT_ROUND_COUNT)
     }
 
