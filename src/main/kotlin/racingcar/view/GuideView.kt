c67c99745c722b78ba7d882b647aae6d0d8233fc
윤동현 <donghyunyoon81@gmail.com>
2023-10-27 23:02:55 +0900

    feat: 다음 출력 내용이 실행 결과라는것을 알려줄 문자열 출력 기능 추가

--- src/main/kotlin/racingcar/view/GuideView.kt
+++ src/main/kotlin/racingcar/view/GuideView.kt
@@ -3,6 +3,7 @@ package racingcar.view
 object GuideView {
     private const val INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     private const val INPUT_ROUND_COUNT = "시도할 횟수는 몇 회인가요?"
+    private const val EXECUTION_RESULT = "실행 결과"
 
     fun printInputCarsName() {
         println(INPUT_CARS_NAME)
@@ -10,6 +11,10 @@ object GuideView {
 
     fun printInputRoundCnt(){
         println(INPUT_ROUND_COUNT)
+    }
+
+    fun printExecutionResult(){
+        println(EXECUTION_RESULT)
     }
 
 }
