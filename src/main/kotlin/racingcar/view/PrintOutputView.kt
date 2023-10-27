bae7ef4d2b47314cb4174fa332f76fc38aa493a5
posite <su84053@gmail.com>
2023-10-27 17:32:46 +0900

    feat: 각 차의 전진상황 출력기능 구현

--- src/main/kotlin/racingcar/view/PrintOutputView.kt
+++ src/main/kotlin/racingcar/view/PrintOutputView.kt
@@ -1,6 +1,7 @@
 package racingcar.view
 
 import racingcar.constant.PrintText
+import racingcar.domain.RacingCars
 
 class PrintOutputView {
 
@@ -11,4 +12,16 @@ class PrintOutputView {
     fun printRequireAttemptCount() {
         println(PrintText.REQUIRE_ATTEMPT_COUNT.text)
     }
+
+    fun printAttemptResult(racingCars: RacingCars) {
+        for (racingCar in racingCars.loadRacingCars()) {
+            print(racingCar.readRacingCarName() + PrintText.SEPARATE_CAR_NAME_AND_FORWARD_COUNT.text)
+            for (count in 0 until racingCar.receiveForwardCount()) {
+                print(PrintText.PRINT_FORWARD_MARK.text)
+            }
+            println()
+        }
+        println()
+    }
+
 }
