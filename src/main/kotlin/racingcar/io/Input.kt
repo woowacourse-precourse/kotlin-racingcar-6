c450948949b107f7cee2d24437ab6dccbce68e39
kimhm0728 <kimhm0728@naver.com>
2023-10-27 23:54:23 +0900

    style: [Input.kt] 출력문 추가

--- src/main/kotlin/racingcar/io/Input.kt
+++ src/main/kotlin/racingcar/io/Input.kt
@@ -4,10 +4,12 @@ import camp.nextstep.edu.missionutils.Console
 import racingcar.util.toCarNameList
 
 fun inputRacingCars(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val racingCars = Console.readLine()
     return racingCars.toCarNameList()
 }
 
 fun inputTryCount(): String {
+    println("시도할 횟수는 몇 회인가요?")
     return Console.readLine()
 }
