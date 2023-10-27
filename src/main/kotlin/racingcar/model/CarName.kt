2138f42aa820f2438a360640971377325c9fb416
MinhoiKoo <minlove2013@gmail.com>
2023-10-27 23:33:39 +0900

    feat: Name 유효성 검증 추가
    -공백으로 들어온 경우 예외 발생
    -5자리 초과일 경우 예외 발생

--- src/main/kotlin/racingcar/model/CarName.kt
+++ src/main/kotlin/racingcar/model/CarName.kt
@@ -1,5 +1,12 @@
 package racingcar.model
 
-class CarName(val name: String) {
+private const val MAX_NAME_LENGTH = 5
 
+class CarName(
+    val name: String
+) {
+    init {
+        require(name.isNotBlank()) { "이름은 공백일 수 없습니다!" }
+        require(name.length <= MAX_NAME_LENGTH) { "자동차의 이름은 5자리를 넘을 수 없습니다!" }
+    }
 }
