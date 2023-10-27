d4fe272e516bc1aedb2ddbe7644ba938fb26925a
jiwon2724 <stopone3119@naver.com>
2023-10-27 11:57:08 +0900

    [chore] : 함수 블록 제거

--- src/main/kotlin/racingcar/iomanager/OutputManager.kt
+++ src/main/kotlin/racingcar/iomanager/OutputManager.kt
@@ -1,17 +1,10 @@
 package racingcar.iomanager
 
 class OutputManager {
-    fun racingCarNames() {
-        println(INPUT_RACING_CAR_NAMES)
-    }
+    fun racingCarNames() = println(INPUT_RACING_CAR_NAMES)
+    fun attemptNumber()  = println(INPUT_ATTEMPT_NUMBER)
+    fun racingResult() = println(RACING_RESULT)
 
-    fun attemptNumber() {
-        println(INPUT_ATTEMPT_NUMBER)
-    }
-
-    fun racingResult() {
-        println(RACING_RESULT)
-    }
 
     companion object {
         private const val INPUT_RACING_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
