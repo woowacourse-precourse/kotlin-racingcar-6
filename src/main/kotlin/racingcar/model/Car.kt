4e5835294828c002af0fcd59990add41753492ca
kimhm0728 <kimhm0728@naver.com>
2023-10-27 13:28:26 +0900

    feat: [Car.kt] 자동차 이름이 유효한 값이면 객체를 생성하는 기능 반환

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -3,14 +3,19 @@ package racingcar.model
 import racingcar.constants.EXCEPTION_LENGTH
 import racingcar.constants.EXCEPTION_LETTER
 
-class Car {
+class Car private constructor(val name: String) {
+
+    fun of(name: String): Car {
+        validateName(name)
+        return Car(name)
+    }
+
+    private fun validateName(name: String) {
+        validateNameLength(name)
+        validateNameLetter(name)
+    }
 
     companion object {
-        fun validateName(name: String) {
-            validateNameLength(name)
-            validateNameLetter(name)
-        }
-
         fun validateNameLength(name: String) {
             require(name.length in 1..5) { EXCEPTION_LENGTH }
         }
