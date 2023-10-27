09f5c3bc9792209d38f761aa3c7723742dc4930e
gretea5 <gretea4489@gmail.com>
2023-10-27 23:06:19 +0900

    feat: 자동차 전진 상태 출력 함수 구현

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -3,8 +3,18 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
 class Car(val name: String, var distance: Int) {
+    companion object {
+        const val distanceString = "-"
+    }
     fun move() {
         val randomNumber = Randoms.pickNumberInRange(0, 9)
         if(randomNumber >= 4) distance += 1
+    }
+    fun printState() {
+        val sb = StringBuilder()
+        for(i in 0 until distance) {
+            sb.append(distanceString)
+        }
+        println("$name : $sb")
     }
 }
