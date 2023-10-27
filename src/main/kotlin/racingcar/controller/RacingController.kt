476981bc786293a34afe78fa7157ccb1234c520d
MinhoiKoo <minlove2013@gmail.com>
2023-10-27 23:34:11 +0900

    feat: RacingController 초기 구현

---
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -0,0 +1,11 @@
+package racingcar.controller
+
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
+class RacingController(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView()
+) {
+
+}
