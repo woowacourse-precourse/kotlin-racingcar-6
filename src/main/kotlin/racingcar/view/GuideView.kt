e01dd83cbfbb72810bfbe3e9bde0482c7ce7bc9c
윤동현 <donghyunyoon81@gmail.com>
2023-10-27 22:55:41 +0900

    feat: 자동차 이름 입력을 안내할 문자열 출력하는 기능 추가

---
+++ src/main/kotlin/racingcar/view/GuideView.kt
@@ -0,0 +1,9 @@
+package racingcar.view
+
+object GuideView {
+    private const val INPUT_CARS_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    fun inputCarsName() {
+        println(INPUT_CARS_NAME)
+    }
+
+}
