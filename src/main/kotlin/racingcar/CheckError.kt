e6ae1b80c268a41a1b2bbea601495202ea49ce39
sunkk8482 <sunkk8482@naver.com>
2023-10-27 21:53:40 +0900

    refactor : 예외 처리 분할

--- src/main/kotlin/racingcar/CheckError.kt
+++ src/main/kotlin/racingcar/CheckError.kt
@@ -1,10 +1,15 @@
 package racingcar
 
 fun nameErrorCheck(name: String) {
+    nameLengthCheck(name)
+    nameEmptyCheck(name)
+}
+
+fun nameLengthCheck(name :String){
     if (name.length >= 6) throw IllegalArgumentException("입력의 길이가 잘못되었습니다.")
-    for (i in name.indices) {
-        if (name[i] in '0'..'9') throw IllegalArgumentException("이름에 숫자가 섞여있습니다")
-    }
+}
+
+fun nameEmptyCheck(name:String){
     if (name.isEmpty()) {
         throw IllegalArgumentException("공백은 안됩니다!")
     }
@@ -17,9 +22,17 @@ fun redundancyErrorCheck(names: List<String>) {
 }
 
 fun numberTimesErrorCheck(number: String) {
+    numberTimesEmptyCheck(number)
+    numberTimesCharCheck(number)
+}
+
+fun numberTimesEmptyCheck(number : String){
     if (number.isEmpty()) {
         throw IllegalArgumentException("공백은 안됩니다!")
     }
+}
+
+fun numberTimesCharCheck(number:String){
     for (i in number.indices) {
         if (number[i] > '9' || number[i] < '0') {
             throw IllegalArgumentException("입력값에 숫자가 아닌 값이 있습니다.")
