a441ea34efc9583c753ae49bae96f177e9498149
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-27 17:58:22 +0900

    test: 입력 받은 라운드 값 유효성 검사 추가

--- src/test/kotlin/study/ValidatorTest.kt
+++ src/test/kotlin/study/ValidatorTest.kt
@@ -8,15 +8,24 @@ import java.math.BigInteger
 
 class ValidatorTest {
     @Test
-    fun `roundCountCheck 입력 받은 라운드 값 체크`() {
+    fun `roundCountCheck 입력 받은 라운드 값 유효성 검사`() {
         val firstInput = "1234"
         val firstResult = BigInteger("1234")
         assertThat(firstResult).isEqualTo(inputRacingRoundContentCheck(firstInput))
 
         val secondInput = "0000"
-        assertThatIllegalArgumentException().isThrownBy { inputRacingRoundContentCheck(secondInput) }
+        assertThatIllegalArgumentException().isThrownBy {
+            inputRacingRoundContentCheck(secondInput)
+        }
 
         val thirdInput = "125as"
-        assertThatIllegalArgumentException().isThrownBy { inputRacingRoundContentCheck(thirdInput) }
+        assertThatIllegalArgumentException().isThrownBy {
+            inputRacingRoundContentCheck(thirdInput)
+        }
+
+        val fourthInput = "12345"
+        assertThatIllegalArgumentException().isThrownBy {
+            inputRacingRoundContentCheck(fourthInput)
+        }
     }
 }
