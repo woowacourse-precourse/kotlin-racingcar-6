59a45312795a5795aea1d5c6c742ad6d33c20862
kimhm0728 <kimhm0728@naver.com>
2023-10-28 00:26:11 +0900

    feat: [RacingGame.kt] 우승자를 선택하는 기능 구현

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -11,6 +11,11 @@ class RacingGame {
         return number >= MOVING_MORE_NUMBER
     }
 
+    fun pickWinner(carList: List<Car>): List<Car> {
+        val winnerForwardCount = carList.sorted()[0].forwardCount
+        return carList.filter { car -> car.forwardCount == winnerForwardCount }
+    }
+
     fun validateCarNameDuplication(racingCars: List<String>) {
         require(racingCars.size == racingCars.distinct().size) { EXCEPTION_DUPLICATION }
     }
