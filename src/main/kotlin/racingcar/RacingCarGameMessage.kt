152f7536e1e5c87c1ada02de94b1bf2270295cd0
gretea5 <gretea4489@gmail.com>
2023-10-27 23:15:58 +0900

    feat: 메시지 출력 함수 전체 구현

--- src/main/kotlin/racingcar/RacingCarGameMessage.kt
+++ src/main/kotlin/racingcar/RacingCarGameMessage.kt
@@ -1,4 +1,25 @@
 package racingcar
 
 class RacingCarGameMessage {
+    companion object {
+        const val CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        const val TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+        const val RESULT_MESSAGE = "실행 결과"
+    }
+
+    fun printCarNameInputMessage() {
+        println(CAR_NAME_INPUT_MESSAGE)
+    }
+
+    fun printTryCountMessage() {
+        println(TRY_COUNT_MESSAGE)
+    }
+
+    fun printResultMessage() {
+        println(RESULT_MESSAGE)
+    }
+
+    fun printWinnerMessage(winners: MutableList<String>) {
+        println("최종 우승자 : ${winners.joinToString(", ")}")
+    }
 }
