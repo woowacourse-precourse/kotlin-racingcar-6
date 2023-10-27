7ddef5df18ae1ba22e3ed898d3e4fb76c88b139c
dpcks0509 <dpcks0509@gmail.com>
2023-10-28 00:24:11 +0900

    refactor : carNameList 변수 삭제, makeCarList 매개변수로 getCarName 함수 사용

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,7 +3,7 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    val carNameList = getCarName()
+    val carList = makeCarList(getCarName())
 }
 
 // 기능 1. 자동차 이름 입력받기
@@ -12,4 +12,14 @@ fun getCarName(): List<String> {
     val carNameInputList = Console.readLine()
 
     return carNameInputList.split(",")
+}
+
+// 기능 2. 자동차 리스트 만들기
+fun makeCarList(carNameList: List<String>): List<Car> {
+    val carList = mutableListOf<Car>()
+    for(name in carNameList) {
+        carList.add(Car(name))
+    }
+
+    return carList
 }
