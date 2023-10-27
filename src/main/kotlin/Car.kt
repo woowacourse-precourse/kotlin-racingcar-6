500774873cf0c9acd0eb03b17999ccfac4aff1e3
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-27 20:45:48 +0900

    refactor: Car 인터페이스 -> 추상클래스 상속 변경

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -2,7 +2,7 @@ import util.CarValue.CAR_INIT_DISTANCE_VALUE
 import util.CarValue.DISTANCE_SYMBOL
 import java.math.BigInteger
 
-class Car(name: String) : Acceleration {
+class Car(name: String) : Acceleration() {
     private var carName = name
     private var distanceTraveled = CAR_INIT_DISTANCE_VALUE.toBigInteger()
 
