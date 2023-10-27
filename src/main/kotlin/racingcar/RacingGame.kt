7fcd1a0c534303c759be7c44375715bd986f7ddd
kimhm0728 <kimhm0728@naver.com>
2023-10-27 15:02:55 +0900

    feat: 중복되는 이름을 입력한 경우 예외 처리 구현

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -1,13 +1,17 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Randoms
+import racingcar.constants.EXCEPTION_DUPLICATION
 import racingcar.constants.MOVING_MORE_NUMBER
-import racingcar.constants.RANDOM_END_NUMBER
-import racingcar.constants.RANDOM_START_NUMBER
+import racingcar.model.Car
 
 class RacingGame {
+    private val carList = mutableListOf<Car>()
 
     fun isMovingForward(number: Int): Boolean {
         return number >= MOVING_MORE_NUMBER
+    }
+
+    fun validateCarNameDuplication(racingCars: List<String>) {
+        require(racingCars.size == racingCars.distinct().size) { EXCEPTION_DUPLICATION }
     }
 }
--- src/main/kotlin/racingcar/constants/Constants.kt
+++ src/main/kotlin/racingcar/constants/Constants.kt
@@ -1,7 +1,9 @@
 package racingcar.constants
 
 const val EXCEPTION_LENGTH = "1글자와 5글자 사의의 자동차 이름을 입력해주세요."
-const val EXCEPTION_LETTER = "대소문자의 영어로만 이루어진 자동차 이름을 입력해주세요."
+const val EXCEPTION_LETTER = "자동차 이름은 대소문자의 영어로만 이루어질 수 있습니다."
+const val EXCEPTION_DUPLICATION = "자동차 이름은 중복되지 않아야 합니다."
+
 const val EXCEPTION_DIGIT = "시도할 횟수는 숫자만 입력할 수 있습니다."
 const val EXCEPTION_INT_MAX = "시도할 횟수가 너무 큽니다."
 const val EXCEPTION_POSITIVE = "시도할 횟수가 너무 작습니다. 0 이상의 수를 입력해주세요."
