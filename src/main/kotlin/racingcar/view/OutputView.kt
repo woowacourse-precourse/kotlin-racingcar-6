d5548ead988110854a2ac1e35d9ef3ecf864f70c
JunJangE <wnswkd486@gmail.com>
2023-10-27 21:12:28 +0900

    feat: 빈 줄 출력 기능

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -6,4 +6,5 @@ import racingcar.utils.Constant.CAR_NAME_PROMPT_MESSAGE
 class OutputView {
     fun printCarNamePrompt() = println(CAR_NAME_PROMPT_MESSAGE)
     fun printAttemptsPrompt() = println(ATTEMPTS_NUM_PROMPT_MESSAGE)
+    fun printEmptyLine() = println()
 }
