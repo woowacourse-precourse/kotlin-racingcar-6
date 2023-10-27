db3e562fc248130bab862d65ae46b447eb43bc06
Sihwan Kim <vkn123@naver.com>
2023-10-27 14:40:54 +0900

    feat: 자동차 이름에 대한 조건 추가

--- src/main/kotlin/racingcar/UserInput.kt
+++ src/main/kotlin/racingcar/UserInput.kt
@@ -2,21 +2,32 @@ package racingcar
 
 class UserInput {
     private val restPointRegex = Regex("[^,]+")
-    val underFiveCharRegex = Regex("^.{1,5}\$")
+    private val underFiveCharRegex = Regex("^.{1,5}\$")
     fun inputCarNames(): List<Car> {
         val input = readln().trim()
         checkRestPoint(input)
-
+        checkUnderFiveCharRegex(input)
+        return convertInputToCarList(input)
     }
 
+    private fun convertInputToCarList(input: String): List<Car> {
+        return restPointRegex.findAll(input).map { Car(it.value) }.toList()
+    }
 
-
-    fun checkRestPoint(input: String){
-        if(!restPointRegex.matches(input)){
-            throw IllegalArgumentException("잘못된 입력")
+    private fun checkUnderFiveCharRegex(input: String) {
+        val cars = restPointRegex.findAll(input)
+        for (car in cars) {
+            if (!underFiveCharRegex.matches(car.value)) {
+                throw IllegalArgumentException("잘못된 입력")
+            }
         }
     }
 
+    fun checkRestPoint(input: String) {
+        if (!restPointRegex.matches(input)) {
+            throw IllegalArgumentException("잘못된 입력")
+        }
+    }
 
 
 }
