3d7f9755c3a9142a418365e69771ddb6e068310d
JunJangE <wnswkd486@gmail.com>
2023-10-27 20:59:24 +0900

    feat: RacingCar move 기능

--- src/main/kotlin/racingcar/model/RacingCar.kt
+++ src/main/kotlin/racingcar/model/RacingCar.kt
@@ -4,4 +4,7 @@ data class RacingCar(
     val name: String,
     val position: Int = 0,
     val raceRecord: String = "",
-)
+) {
+
+    fun move(): RacingCar = copy(position = position + 1, raceRecord = "$raceRecord-")
+}
