c3df0838b3893de87d091ee09461b923f0b9b4cf
JunJangE <wnswkd486@gmail.com>
2023-10-27 20:45:56 +0900

    feat: 상수 값 정의

---
+++ src/main/kotlin/racingcar/utils/Constant.kt
@@ -0,0 +1,10 @@
+package racingcar.utils
+
+object Constant {
+    const val CAR_NAME_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val ATTEMPTS_NUM_PROMPT_MESSAGE = "시도할 회수는 몇회인가요?"
+    const val RACING_CAR_RESULT_MESSAGE = "%s : %s"
+    const val GAME_WINNER_MESSAGE = "최종 우승자 : %s"
+    const val GAME_OVER_MESSAGE = "게임 종료"
+    const val GAME_RESULT_MESSAGE = "실행 결과"
+}
