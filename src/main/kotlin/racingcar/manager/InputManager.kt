9c3b249410c1901682d6b02f3e850cb1688c8b00
jiwon2724 <stopone3119@naver.com>
2023-10-27 22:26:33 +0900

    [refactor] : 클래스 코드 구조 개선

--- src/main/kotlin/racingcar/manager/InputManager.kt
+++ src/main/kotlin/racingcar/manager/InputManager.kt
@@ -3,37 +3,7 @@ package racingcar.manager
 import camp.nextstep.edu.missionutils.Console.readLine
 
 
-class InputManager() {
-    fun carNames(): List<String> = getValidCarNames(readLine()).toCarNamesList()
-
-    private fun getValidCarNames(userInput: String): String {
-        return userInput.trim().let { input ->
-            isEmptyString(input)
-            isStartAndEndWithComma(input)
-            isLengthFive(input)
-            userInput
-        }
-    }
-
-    private fun isEmptyString(userInput: String): Boolean {
-        return if (userInput != EMPTY_STRING) true else throw IllegalArgumentException()
-    }
-
-    private fun isStartAndEndWithComma(carName: String): Boolean {
-        val isCheckStartEndWithComma = carName.startsWith(",") || carName.endsWith(",")
-        return if (!isCheckStartEndWithComma) true else throw IllegalArgumentException()
-    }
-
-    private fun isLengthFive(carName: String): Boolean {
-        val carNameList = carName.toCarNamesList()
-        carNameList.forEach { if (it.length >= CAR_NAME_SIZE) throw IllegalArgumentException() }
-        return true
-    }
-
-    private fun String.toCarNamesList(): List<String> = split(",").map { it.trim() }
-
-    companion object {
-        private const val CAR_NAME_SIZE = 5
-        private const val EMPTY_STRING = ""
-    }
+class InputManager(private val validationManager: ValidationManager) {
+    fun carNames(): List<String> =
+        validationManager.getValidCarNames(readLine()).split(",").map { it.trim() }
 }
