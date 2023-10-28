6ec382050835a72964da8efc26d2295cd5eb478d
Joontae <chlwnsxo0@gmail.com>
2023-10-28 16:33:39 +0900

    feat(racing): 자동차 이름을 쉼표 기준으로 입력 및 구분하는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,15 +1,21 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    askCarName()
 
 }
 
 fun askCarName(){
-
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNames :MutableList<String> = Console.readLine().split(",") as MutableList<String>
+    for (carName in carNames){
+        if(carName.length>5)
+            throw IllegalArgumentException()
+    }
 }
 fun askNumber(){
-
 }
 
 fun printWinner(){
