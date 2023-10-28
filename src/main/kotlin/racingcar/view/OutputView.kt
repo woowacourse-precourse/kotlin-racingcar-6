aac013f736261c79d062dc15b4730b2f7ac71e57
MinhoiKoo <minlove2013@gmail.com>
2023-10-28 16:43:09 +0900

    feat: OutputView 초기 구현
    -이름, 라운드 수 입력받는 문구 출력

---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,12 @@
+package racingcar.view
+
+class OutputView {
+
+    fun printInputName() {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    }
+
+    fun printInputRound() {
+        println("시도할 횟수는 몇 회인가요?")
+    }
+}
