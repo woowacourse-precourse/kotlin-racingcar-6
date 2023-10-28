73bd59fef855930994e56decd36b079daa29b09c
eunji <nagosoo@kakao.com>
2023-10-28 15:35:17 +0900

    refac: 시도 입력 횟수 검증 로직 변경

--- src/main/kotlin/racingcar/common/ExceptionConst.kt
+++ src/main/kotlin/racingcar/common/ExceptionConst.kt
@@ -4,4 +4,5 @@ object ExceptionConst {
     const val EXCEPTION_CAR_NAME_LENGTH_INVALIDATE = "자동차 이름은 1글자 이상, 5글자 이하만 가능합니다."
     const val EXCEPTION_CAR_NAME_DUPLICATED = "자동차 이름은 중복이 불가합니다."
     const val EXCEPTION_TRY_CNT_NOT_INT = "시도 횟수는 숫자만 입력해주세요."
+    const val EXCEPTION_TRY_CNT_LESS_THAN_ONE = "시도 횟수는 1 이상만 가능합니다."
 }
