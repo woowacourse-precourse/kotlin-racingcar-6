0928e1b5f2e04c5d71531c354349564c34cf2ea4
BugyeongNa <nbg9712@naver.com>
2023-10-28 23:02:31 +0900

    feat: 사용자로부터 자동차 이름 입력 받는 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,16 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
-    // TODO: 프로그램 구현
+
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val names = Console.readLine().split(",").map {
+        if (it.length > 5) {
+            throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.")
+        }
+        it
+    }
+
 }
