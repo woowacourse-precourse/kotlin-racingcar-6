f94a9253f7603100dc5b186d23086b8747f5fe46
sungsik-git <kss950708@gamil.com>
2023-10-28 22:37:52 +0900

    자동차 이름 글자수 제한 기능 구현

--- src/main/kotlin/User/User.kt
+++ src/main/kotlin/User/User.kt
@@ -3,8 +3,27 @@ package User
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 class User {
+    companion object {
+        const val MAX_NAME = 5
+    }
     fun inputCarName(){
         var userNames = Console.readLine()
         var userNameList = userNames.split(',')
+
+        try {
+            checkUserName(userNameList)
+        }catch (e: IllegalArgumentException){
+            e.printStackTrace()
+        }
     }
+
+    fun checkUserName(userNameList: List<String>){
+        for( i in 0..userNameList.size-1){
+            if(userNameList[i].length > MAX_NAME){
+                throw IllegalArgumentException()
+            }
+        }
+    }
+
+
 }
