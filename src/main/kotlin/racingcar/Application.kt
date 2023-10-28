35b7e6b446254d13c1e29df672b4c5aeba9762d9
eunji <nagosoo@kakao.com>
2023-10-28 11:01:19 +0900

    feat: 사용자에게 자동차이름 입력받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,24 @@
 package racingcar
 
-fun main() {
+import camp.nextstep.edu.missionutils.Console
+import racingcar.model.Car
 
+val carList = mutableListOf<Car>()
+
+fun main() {
+    enterCarName()
+}
+
+fun enterCarName() {
+    println(Const.ENTER_CAR_NAME_MSG)
+    val inputCarNames = Console.readLine()
+    validateCarName(inputCarNames)
+    //makeCar(Console.readLine())
+}
+
+fun validateCarName(inputCarNames: String) {
+    val nameList = inputCarNames.split(",")
+    nameList.map { name ->
+        if (name.length > 5) throw IllegalArgumentException(Const.EXCEPTION_INPUT_RANGE_OVERFLOW)
+    }
 }
--- src/main/kotlin/racingcar/Const.kt
+++ src/main/kotlin/racingcar/Const.kt
@@ -5,4 +5,5 @@ object Const {
     val ENTER_TRY_CNT_MSG = "시도할 횟수는 몇 회인가요?"
     val GAME_RESULT_MSG = "실행 결과"
     val GAME_WINNER_MSG = "최종 우승자 : "
+    val EXCEPTION_INPUT_RANGE_OVERFLOW = "자동차 이름은 5글자 이하만 가능합니다."
 }
