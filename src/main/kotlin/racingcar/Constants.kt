488ce24e19207a47957e7ed681784c95f5904c83
Sheep1sik <bohg6799@naver.com>
2023-10-28 18:08:48 +0900

    refactor: 클래스 명 변경

--- src/main/kotlin/racingcar/Constants.kt
+++ src/main/kotlin/racingcar/Constants.kt
@@ -1,6 +1,6 @@
 package racingcar
 
-class Message {
+class Constants {
     companion object {
         const val MIN_NUMBER = 0
         const val MAX_NUMBER = 9
@@ -8,6 +8,6 @@ class Message {
         const val INPUT_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         const val TRY_COUNT_PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?"
         const val EXECUTION_RESULT_MESSAGE = "실행 결과"
-        const val FINAL_WINNER_MESSAGE = " 최종 승자 : "
+        const val FINAL_WINNER_MESSAGE = "최종 우승자 : "
     }
 }
