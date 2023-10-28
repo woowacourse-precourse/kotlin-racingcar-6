a053e930e5fe8ecc05a99a250f6be202761330ac
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-28 23:47:11 +0900

    feat: 주행거리 만큼 SYMBOL 출력 기능 작성

--- src/main/kotlin/caroption/DashBoard.kt
+++ src/main/kotlin/caroption/DashBoard.kt
@@ -1,10 +1,17 @@
 package caroption
 
+import CarConfiguration.DIST_SYMBOL
 import CarConfiguration.INIT_DIST
 
 class DashBoard {
     private var dist = INIT_DIST
     fun distPlus() {
         dist++
+    }
+
+    fun printDist() {
+        repeat(dist) {
+            print(DIST_SYMBOL)
+        }
     }
 }
