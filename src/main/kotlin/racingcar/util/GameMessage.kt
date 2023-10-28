0e9ac17fbe2192369aa529727043ab2eb6cd18d3
jaehan4707 <jahan4707@naver.com>
2023-10-28 16:03:33 +0900

    style : GameInputErrorMessage 수정

--- src/main/kotlin/racingcar/util/GameMessage.kt
+++ src/main/kotlin/racingcar/util/GameMessage.kt
@@ -5,6 +5,6 @@ object GameMessage {
     const val GAME_INPUT_ATTEMPT_MESSAGE = "시도할 횟수는 몇 회인가요?"
     const val GAME_RESULT_MESSAGE = "실행 결과"
     const val GAME_WINNER_MESSAGE = "최종 우승자 :"
-    const val GAME_INPUT_ERROR_MESSAGE = "이름은 알파벳 소 ﹒ 대문자 5자 이하만 가능합니다."
+    const val GAME_INPUT_ERROR_MESSAGE = "이름은 알파벳 소﹒대문자 5자 이하만 가능합니다."
 
 }
