698d3e31354048ad377efa9e3be5b882528b6027
sumin <gosumin0710@naver.com>
2023-10-28 23:18:46 +0900

    feat : 경주할 자동차 입력받기 예외처리 테스트코드 작성

--- src/test/kotlin/racingcar/InputManagerTest.kt
+++ src/test/kotlin/racingcar/InputManagerTest.kt
@@ -1,8 +1,17 @@
 package racingcar
 
-import org.assertj.core.api.Assertions
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class InputManagerTest {
 
+    @Test
+    fun `입력 받은 자동차 이름의 예외 처리`() {
+        val inputData = "ejlfklf, dkd".split(",")
+        assertSimpleTest{
+            assertThrows<IllegalArgumentException> { ExceptionManager.carException(inputData) }
+        }
+    }
 }
