350fd83f68aad2a1d3a47e96f943fb947dd81bbe
PParkcode <qazwsxedc1226@gmail.com>
2023-10-28 22:59:53 +0900

    Test: 자동차 입력 예외 테스트
    자동차 이름의 사이즈가 적절하지 않을 경우, 입력에 공백만 있는 경우에 IllegalArgumentException을 발생시키는지 테스트하였다.
    테스트를 모두 성공적으로 마쳤으므로, 자동차 이름을 입력 받는 기능을 완성하였다.

--- src/test/kotlin/racingcar/IoTest.kt
+++ src/test/kotlin/racingcar/IoTest.kt
@@ -3,13 +3,17 @@ package racingcar
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import racingcar.exception.ExceptionChecker
+import racingcar.io.Input
 import racingcar.io.UserInterface
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
+import java.lang.IllegalArgumentException
 import java.util.*
 
 class IoTest {
@@ -25,23 +29,68 @@ class IoTest {
 
     @BeforeEach
     fun setUp() {
-        //val carNamesInput = "aaa,bbb,ccc,ddd"
-        //System.setIn(ByteArrayInputStream(carNamesInput.toByteArray()))
+
         System.setOut(PrintStream(outputStream))
     }
+
+    @AfterEach
+    fun tearDown() {
+        System.setIn(originalSystemIn)
+        System.setOut(originalSystemOut)
+    }
+
+
     @Test
     fun `자동차 이름 입력 받기 함수`() {
         System.setIn(carNameInput1)
         val ui = UserInterface()
 
         val carNames = ui.enterCarNames()
-        val expected = listOf("aaa","bbb","ccc","ddd")
+        val expected = listOf("aaa", "bbb", "ccc", "ddd")
         val expectedOutput = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 
         val consoleOutput = outputStream.toString().trim()
 
         assertThat(consoleOutput).isEqualTo(expectedOutput)
         assertThat(carNames).isEqualTo(expected)
+    }
+
+
+    @Test
+    fun `자동차 이름 입력 예외 테스트1`() {
+
+        val checker = ExceptionChecker()
+        val cars = listOf("abc", "123456")
+        assertThrows<IllegalArgumentException>() {
+            checker.checkCarNames(cars)
+        }
+
+    }
+
+
+    @Test
+    fun `자동차 이름 입력 예외 테스트2`() {
+
+
+        val checker = ExceptionChecker()
+        val cars = listOf("abc", "", "def", "aaa")
+        assertThrows<IllegalArgumentException>() {
+            checker.checkCarNames(cars)
+        }
+
+    }
+
+
+    @Test
+    fun `자동차 이름 입력 예외 테스트3`() {
+
+
+        val checker = ExceptionChecker()
+        val cars = listOf("   ", "abc", "aaa")
+        assertThrows<IllegalArgumentException>() {
+            checker.checkCarNames(cars)
+        }
+
     }
 
 
