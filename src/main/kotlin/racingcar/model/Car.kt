3f30608446678f4bcb380378548c84a4b1815cef
MinhoiKoo <minlove2013@gmail.com>
2023-10-28 17:03:25 +0900

    feat: position getter 생성

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -2,7 +2,10 @@ package racingcar.model
 
 class Car(
     name: String,
-    private var position: Int = 0
+    private var _position: Int = 0
 ) {
     val name: CarName = CarName(name)
+    val position: Int
+        get() = _position
+
 }
