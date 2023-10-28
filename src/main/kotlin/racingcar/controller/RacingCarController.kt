21b32d6707019b4ee1f11dbf7ec535ee7232dd1f
stopstone <wltjr6432@naver.com>
2023-10-28 16:51:11 +0900

    feat: 랜덤 수를 뽑아내는 기능 추가

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -1,13 +1,18 @@
 package racingcar.controller
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+import racingcar.domain.Cars
 import racingcar.view.InputView
 
 class RacingCarController(private val inputView: InputView) {
     fun racingRun() {
         val carNames = inputCarName()
         val tryCount = inputTryRaceCount()
+        val cars: List<Cars> = carNames.map { Cars(it) }
+
     }
+
     private fun inputCarName(): List<String> {
         inputView.inputCarNamesMessage()
         val inputName = Console.readLine()
@@ -22,17 +27,17 @@ class RacingCarController(private val inputView: InputView) {
     }
 
     private fun validateInputName(splitInputName: List<String>) {
-        splitInputName.map{ names ->
-            if(names.length > 5) throw IllegalArgumentException("5글자가 넘는 자동차 이름이 있습니다.")
+        splitInputName.map { names ->
+            if (names.length > 5) throw IllegalArgumentException("5글자가 넘는 자동차 이름이 있습니다.")
         }
     }
 
     private fun inputTryRaceCount(): Int {
-        var tryCount:Int = 0
+        var tryCount: Int = 0
         inputView.inputTryRaceCountMessage()
         try {
             tryCount = Console.readLine().toInt()
-        }catch (e:NumberFormatException){
+        } catch (e: NumberFormatException) {
             throw IllegalArgumentException("숫자가 아닌 다른 값이 입력되었습니다.")
         }
         validateInputTryRace(tryCount)
@@ -41,7 +46,15 @@ class RacingCarController(private val inputView: InputView) {
     }
 
     private fun validateInputTryRace(tryCount: Int) {
-        if(tryCount == 0)
+        if (tryCount == 0)
             throw IllegalArgumentException("0은 입력될 수 없습니다.")
     }
+
+
+
+
+    private fun generateRandomNumber(): Int {
+        return Randoms.pickNumberInRange(0, 9)
+    }
+
 }
