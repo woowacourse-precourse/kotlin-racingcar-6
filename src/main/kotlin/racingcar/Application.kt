c1bf9c033aa4cec5cd8bb4ca1b6a4d4f398bb875
Insu Kang <kin4496@naver.com>
2023-10-29 02:32:03 +0900

    feat: 진행할 라운드를 입력받는 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,4 +6,16 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val listOfCar = Console.readLine()
         .split(',')
+
+    println("시도할 횟수는 몇 회인가요?")
+    var round = getUserIntInput()
+}
+fun getUserIntInput(): Int {
+    var input: Int
+    try {
+        input = Console.readLine().toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException()
+    }
+    return input
 }
