dd05fc34a75e3a362f594873df60a0dd59c2ea77
posite <su84053@gmail.com>
2023-10-28 19:38:47 +0900

    refactor: 자동차 이름 검증 domain 변경

--- src/main/kotlin/racingcar/domain/RacingCarMover.kt
+++ src/main/kotlin/racingcar/domain/RacingCarMover.kt
@@ -1,10 +1,13 @@
 package racingcar.domain
 
+import racingcar.constant.ErrorMessage
 import racingcar.util.RandomNumberGenerator
 
 class RacingCarMover {
 
     fun moveRacingCars(racingCars: RacingCars): RacingCars {
+        validateNameListSize(racingCars)
+        validateNameLength(racingCars)
         racingCars.loadRacingCars().forEach {
             if (RandomNumberGenerator.generateRandomNumber() >= BOUNDARY_NUMBER) {
                 it.addForwardCount()
@@ -13,8 +16,24 @@ class RacingCarMover {
         return racingCars
     }
 
+    private fun validateNameListSize(racingCars: RacingCars) {
+        require(racingCars.loadRacingCars().size >= MIN_RACING_CAR_COUNT) { ErrorMessage.RACING_CAR_LESS_THAN_TWO.message }
+    }
+
+    private fun validateNameLength(racingCars: RacingCars) {
+        validateCarNamesLength(racingCars)
+    }
+
+    private fun validateCarNamesLength(racingCars: RacingCars) {
+        require(
+            racingCars.loadRacingCars()
+                .none { it.readRacingCarName().length > MAX_NAME_LENGTH }) { ErrorMessage.NAME_LENGTH_GREATER_THAN_FIVE }
+    }
+
 
     companion object {
         private const val BOUNDARY_NUMBER = 4
+        private const val MAX_NAME_LENGTH = 5
+        private const val MIN_RACING_CAR_COUNT = 2
     }
 }
--- src/main/kotlin/racingcar/domain/RacingCars.kt
+++ src/main/kotlin/racingcar/domain/RacingCars.kt
@@ -1,7 +1,5 @@
 package racingcar.domain
 
-import racingcar.constant.ErrorMessage
-
 class RacingCars(nameList: List<String>) {
     private val racingCars = mutableListOf<RacingCar>()
 
@@ -14,18 +12,6 @@ class RacingCars(nameList: List<String>) {
     }
 
     fun loadRacingCars() = racingCars
-
-    private fun validateNameListSize(nameList: List<String>) {
-        require(nameList.size >= MIN_RACING_CAR_COUNT) { ErrorMessage.RACING_CAR_LESS_THAN_TWO.message }
-    }
-
-    private fun validateNameLength(carNames: List<String>) {
-        validateCarNamesLength(carNames)
-    }
-
-    private fun validateCarNamesLength(carNames: List<String>) {
-        require(carNames.none { it.length > MAX_NAME_LENGTH }) { ErrorMessage.NAME_LENGTH_GREATER_THAN_FIVE }
-    }
 
     companion object {
         private const val MAX_NAME_LENGTH = 5
