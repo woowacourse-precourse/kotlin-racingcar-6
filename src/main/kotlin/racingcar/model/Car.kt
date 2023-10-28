c5b7621012e6f6fa267787894791fe36c0ed0351
GreenRain <47217640+greenrain78@users.noreply.github.com>
2023-10-28 23:44:07 +0900

    feat(model/car): 자동차 객체 구성
    자동차 객체 구성
    - name 생성 및 제한 조건 추가
    - isMove 함수로 전진 유무 판별
    - pos 생성 및 move 함수로
    pos 증가를 관리
    - toString으로 이름과 현재 위치를 문자열로 반환

---
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -0,0 +1,40 @@
+package racingcar.model
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class Car(name: String) {
+    companion object {
+        private const val NAME_ERROR_MESSAGE = "이름이 %d 이하가 아닙니다."
+        private const val NAME_MAX_SIZE = 5
+        private const val MOVABLE_LEVEL = 4
+        private val RANDOM_RANGE = Pair(0, 9)
+    }
+
+    private val name: String
+    private var pos: Int = 0
+
+    init {
+        validateName(name = name)
+        this.name = name
+    }
+
+    private fun validateName(name: String) {
+        require(name.length <= NAME_MAX_SIZE) { NAME_ERROR_MESSAGE.format(NAME_MAX_SIZE) }
+    }
+
+    fun move() {
+        if (isMove()) {
+            this.pos += 1
+        }
+    }
+
+    private fun isMove(): Boolean {
+        val num = Randoms.pickNumberInRange(RANDOM_RANGE.first, RANDOM_RANGE.second)
+        return num >= MOVABLE_LEVEL
+    }
+
+    override fun toString(): String {
+        val stringPos = "-".repeat(this.pos)
+        return "${this.name} : $stringPos"
+    }
+}
