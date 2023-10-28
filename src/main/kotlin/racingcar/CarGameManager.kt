8945992854894db1c0b6413de55c54244d7ab57a
Sheep1sik <bohg6799@naver.com>
2023-10-28 18:11:22 +0900

    refactor: 출력문 간결하게 수정
    각 차수별 실행결과, 우승자 안내문구 출력 간결하게 수정

--- src/main/kotlin/racingcar/CarGameManager.kt
+++ src/main/kotlin/racingcar/CarGameManager.kt
@@ -2,51 +2,47 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
-import racingcar.Message.Companion.EXECUTION_RESULT_MESSAGE
-import racingcar.Message.Companion.FORWARD_CONDITION
-import racingcar.Message.Companion.INPUT_PROMPT_MESSAGE
-import racingcar.Message.Companion.MAX_NUMBER
-import racingcar.Message.Companion.MIN_NUMBER
-import racingcar.Message.Companion.TRY_COUNT_PROMPT_MESSAGE
+import racingcar.Constants.Companion.EXECUTION_RESULT_MESSAGE
+import racingcar.Constants.Companion.FINAL_WINNER_MESSAGE
+import racingcar.Constants.Companion.FORWARD_CONDITION
+import racingcar.Constants.Companion.INPUT_PROMPT_MESSAGE
+import racingcar.Constants.Companion.MAX_NUMBER
+import racingcar.Constants.Companion.MIN_NUMBER
+import racingcar.Constants.Companion.TRY_COUNT_PROMPT_MESSAGE
 
 class CarGameManager {
 
     // 전반적인 게임의 구동을 담당.
-    fun play(){
+    fun play() {
         println(INPUT_PROMPT_MESSAGE)
-        val carNames =  getInputUser()
+        val carNames = getInputUser()
         val carNamesList = carNames.split(",")
         Validator().validateUserInput(carNamesList.map { it.trim() })
         println(TRY_COUNT_PROMPT_MESSAGE)
         val tryCount = getInputUser().toInt()
         println(EXECUTION_RESULT_MESSAGE)
         val carListCount = carNamesList.size
-        val carResultList = MutableList(carListCount){""}
-        for (countTry in 0..<tryCount){
-            for (countList in 0..<carListCount){
-                print(carNamesList[countList]+" : ")
+        val carResultList = MutableList(carListCount) { "" }
+
+        for (countTry in 0 until tryCount) {
+
+            for (countList in 0 until carListCount) {
                 val randomNumber = generateRandomNumber()
-                if (randomNumber>= FORWARD_CONDITION) carResultList[countList] += "-"
-                println(carResultList[countList])
+                if (randomNumber >= FORWARD_CONDITION) carResultList[countList] += "-"
+                println("${carNamesList[countList]} : ${carResultList[countList]}")
             }
             println()
         }
 
         val winnerListLength = carResultList.max().length
         val winnersList = mutableListOf<String>()
-        print(Message.FINAL_WINNER_MESSAGE)
-        for (countList in 0 ..<carListCount){
-            if (winnerListLength == carResultList[countList].length){
+
+        for (countList in 0 until  carListCount) {
+            if (winnerListLength == carResultList[countList].length) {
                 winnersList.add(carNamesList[countList])
             }
         }
-        val winner = winnersList.joinToString( ", " )
-        println(winner)
-
-
-
-
-
+        println("$FINAL_WINNER_MESSAGE${winnersList.joinToString(", ")}")
     }
 
     // 사용자의 자동차 이름, 시도횟수를 입력 받는 기능
