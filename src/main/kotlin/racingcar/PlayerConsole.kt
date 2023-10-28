ad42986f1bf8d58687b08a3b2c64781431cf2be2
somin <salth0601@gmail.com>
2023-10-28 23:58:28 +0900

    feat: validateCarNames 함수 추가 - 글자수가 1자 이상, 5자 이하인지 - 문자와 숫자인지 - 각 문자열이 고유한지 - 위 조건 중 하나라도 해당하지 않으면 예외

--- src/main/kotlin/racingcar/PlayerConsole.kt
+++ src/main/kotlin/racingcar/PlayerConsole.kt
@@ -6,4 +6,12 @@ object PlayerConsole {
 
     private fun parseInputByComma() = readLine().split(",")
 
+    //확장 함수로 짠 이유 : getValidCarNames에서 메서드 체이닝으로 코드의 흐름을 한눈에 파악
+    private fun List<String>.validateCarNames(): List<String> {
+        require(this.all { it.length <= 5 && it.isNotEmpty() }) { "자동차 이름은 한 글자 이상, 5글자 이하로 입력해 주세요." }
+        require(this.all { playerName -> playerName.all { it.isLetter() || it.isDigit() } }) { "문자와 숫자만 입력해 주세요." }
+        require(this.toSet().size == this.size) { "자동차 이름은 서로 중복될 수 없습니다." }
+        return this
+    }
+
 }
