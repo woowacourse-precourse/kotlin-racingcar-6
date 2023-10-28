369e89b2d7a42cfbfb3c698953a1e78ae2908553
Sheep1sik <bohg6799@naver.com>
2023-10-28 14:47:18 +0900

    feat(racingcar-CargameManager): 자동차의 랜덤값이 4이상이면 전진후 출력

--- src/main/kotlin/racingcar/CarGameManager.kt
+++ src/main/kotlin/racingcar/CarGameManager.kt
@@ -2,23 +2,51 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+import racingcar.Message.Companion.EXECUTION_RESULT_MESSAGE
+import racingcar.Message.Companion.FORWARD_CONDITION
 import racingcar.Message.Companion.INPUT_PROMPT_MESSAGE
+import racingcar.Message.Companion.MAX_NUMBER
+import racingcar.Message.Companion.MIN_NUMBER
+import racingcar.Message.Companion.TRY_COUNT_PROMPT_MESSAGE
 
 class CarGameManager {
 
     // 전반적인 게임의 구동을 담당.
+    fun play(){
+        println(INPUT_PROMPT_MESSAGE)
+        val carNames =  getInputUser()
+        val carNamesList = carNames.split(",")
+        Validator().validateUserInput(carNamesList.map { it.trim() })
+        println(TRY_COUNT_PROMPT_MESSAGE)
+        val tryCount = getInputUser().toInt()
+        println(EXECUTION_RESULT_MESSAGE)
+        val carListCount = carNamesList.size
+        val carResultList = MutableList(carListCount){""}
+        for (countTry in 0..<tryCount){
+            for (countList in 0..<carListCount){
+                print(carNamesList[countList]+" : ")
+                val randomNumber = generateRandomNumber()
+                if (randomNumber>= FORWARD_CONDITION) carResultList[countList] += "-"
+                println(carResultList[countList])
+            }
+            println()
+        }
 
+
+
+
+
+
+    }
 
     // 사용자의 자동차 이름, 시도횟수를 입력 받는 기능
     private fun getInputUser(): String {
         return Console.readLine()
     }
 
-
-
     // 0 - 9 사이의 무작위 값을 부여하는 기능
     private fun generateRandomNumber(): Int {
-        return Randoms.pickNumberInRange(0, 9)
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)
     }
 
     // 전진하는 자동차 이름과 함께 출력하는 기능
