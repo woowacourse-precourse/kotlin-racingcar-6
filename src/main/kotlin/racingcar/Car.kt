797107919fba52f5de951434f3e4a297f4729b67
sumin <gosumin0710@naver.com>
2023-10-28 18:18:52 +0900

    feat: 자동차 전진 기능 추가
    자동차 클래스를 생성한 후, 각 자동차에 무작위 값을 구하여 값에 따라 전진하도록 하는 기능을 추가함

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,18 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+
+const val FORWARDABLE_NUMBER = 4
+
+class Car(name: String) {
+
+    private var forwardCount = 0
+
+    private fun increaseForwardCount() = forwardCount++
+
+    private fun forwardCar() {
+        val randomNumber = pickNumberInRange(0, 9)
+
+        if (randomNumber >= FORWARDABLE_NUMBER) increaseForwardCount()
+    }
+}
