6404c5b353e3715db4764dc0f64e8bce3ea9c2a7
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-10-28 23:26:12 +0900

    2. 자동차 입력 쉼표 기준 분할

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,13 @@
 package racingcar
 
+val carNames = mutableListOf<String>()
+
 fun main() {
-    print("dd")
+    print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n")
+    val input = readLine()
+    if(input!=null){
+        carNames.addAll(input.split(",").map { it.trim() }
+        )
+    }
+    print(carNames)
 }
