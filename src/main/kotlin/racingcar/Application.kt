18ae35372a657df44da8c0cf42d9dcb749aa3f78
getupminaaa <getupmina@gmail.com>
2023-10-28 17:24:17 +0900

    feat: 경주할 자동차 이름을 입력받는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,10 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val cars = getCarList()
+    
 }
+
+fun getCarList() = Console.readLine().split(",")
