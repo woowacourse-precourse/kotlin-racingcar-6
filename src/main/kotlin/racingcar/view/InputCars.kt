96954af6565b1976e700db5517b0d9aaf982baa2
sh1mj1 <7wlgns@gmail.com>
2023-10-28 17:08:07 +0900

    feat(InputCars): implement feature that input to CarList
    * 쉼표 앞,뒤로 공백이 있다면 이를 제거하고 리스트로 반환한다.

--- src/main/kotlin/racingcar/view/InputCars.kt
+++ src/main/kotlin/racingcar/view/InputCars.kt
@@ -1,6 +1,9 @@
 package racingcar.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputCars {
-    operator fun invoke(vararg cars: String) {
+    operator fun invoke(str: String = Console.readLine()): List<String> {
+        return str.split(',').map { it.trim() }
     }
 }
