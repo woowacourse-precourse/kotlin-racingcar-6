85954defb50fc9a67bfe582ec2f0a3e331ba032d
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-29 01:14:31 +0900

    feat: Car start 기능 작성

---
+++ src/main/kotlin/Car.kt
@@ -0,0 +1,20 @@
+import CarConfiguration.MAX_FLAG_VALUE
+import CarConfiguration.MIN_FLAG_VALUE
+import camp.nextstep.edu.missionutils.Randoms
+import caroption.DashBoard
+import caroption.Engine
+
+class Car(name: String) {
+
+    private val carName = name
+    private val engine = Engine()
+    private val dashBoard = DashBoard()
+
+    fun start() {
+        val startFlag = Randoms.pickNumberInRange(MIN_FLAG_VALUE, MAX_FLAG_VALUE)
+        if (engine.start(startFlag)) {
+            dashBoard.distPlus()
+        }
+    }
+
+}
