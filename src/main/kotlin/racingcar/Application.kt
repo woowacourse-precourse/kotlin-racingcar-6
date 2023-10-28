b9d6016d0aad9103e1194da206e70255be75b7db
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-10-29 00:06:42 +0900

    fix - 플레이 횟수 null값 입력시 예외처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,19 +2,33 @@ package racingcar
 
 val carNames = mutableListOf<String>()
 
+fun checkInputIsNull(input: String?){
+    if (input.isNullOrEmpty()){
+        throw IllegalArgumentException("입력값이 없거나 공백입니다.")
+    }
+}
+
 fun main() {
-    print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n")
+    print("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)\n")
     val input = readLine()
 
-    if (input.isNullOrEmpty()){
-        throw IllegalArgumentException("자동차의 입력값이 없습니다.")
+    checkInputIsNull(input)
+
+    input?.let { nonNullInput ->
+        carNames.addAll(nonNullInput.split(",").map { it.trim() })
     }
 
-    carNames.addAll(input.split(",").map { it.trim() })
     val invalidCarNames = carNames.filter { it.length > 5 }
     if (invalidCarNames.isNotEmpty()){
-        throw IllegalArgumentException("자동차의 입력값의 길이는 5이하로 입력해주세요.")
+        throw IllegalArgumentException("자동차의 입력값의 길이는 5 이하로 입력해주세요.")
     }
 
     print("시도할 횟수는 몇 회인가요?\n")
+    val playTime = readLine()
+
+    checkInputIsNull(playTime)
+
+
+
+
 }
