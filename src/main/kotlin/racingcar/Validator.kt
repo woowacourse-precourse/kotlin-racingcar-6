63ba8fd75ca51f53b8785455f3c046566be50e94
Sheep1sik <bohg6799@naver.com>
2023-10-28 18:09:20 +0900

    refactor: 5자 이하 검증기능 수정

--- src/main/kotlin/racingcar/Validator.kt
+++ src/main/kotlin/racingcar/Validator.kt
@@ -2,13 +2,12 @@ package racingcar
 
 class Validator {
     // 요구사항 검증, 전진 조건 검증을 담당
-    fun validateUserInput(carNames: List<String>) {
+    fun validateUserInput(carNamesList: List<String>) {
         // 자동차의 이름이 5자 이하인지 검증 기능
-        for (i in 0..carNames.size) {
-            if (carNames[i].length > 5) {
-                throw IllegalArgumentException("이름은 5자 이하여야 합니다.")
-            }
+        if (carNamesList.any { it.length > 5 }) {
+            throw IllegalArgumentException("이름은 5자 이하여야 합니다.")
         }
+
 
         // 시도 횟수가 숫자가 맞는지 검증 기능
 
