0b6bccc0e7a89a3ccffa4fa52d9af2aa2d880259
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-10-28 23:58:11 +0900

    fix - 자동차 입력값 null값 입력시 예외처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,12 +5,16 @@ val carNames = mutableListOf<String>()
 fun main() {
     print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n")
     val input = readLine()
-    if(input!=null){
-        carNames.addAll(input.split(",").map { it.trim() })
-        val invalidCarNames = carNames.filter { it.length > 5 }
-        if (invalidCarNames.isNotEmpty()){
-            throw IllegalArgumentException("자동차의 입력값의 길이는 5이하로 입력해주세요.")
+
+    if (input.isNullOrEmpty()){
+        throw IllegalArgumentException("자동차의 입력값이 없습니다.")
     }
-    print(carNames)
-}
+
+    carNames.addAll(input.split(",").map { it.trim() })
+    val invalidCarNames = carNames.filter { it.length > 5 }
+    if (invalidCarNames.isNotEmpty()){
+        throw IllegalArgumentException("자동차의 입력값의 길이는 5이하로 입력해주세요.")
+    }
+
+    print("시도할 횟수는 몇 회인가요?\n")
 }
