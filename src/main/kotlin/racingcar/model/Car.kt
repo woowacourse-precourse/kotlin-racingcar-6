6c4f232ecb69a065100b781f6d857f55dc1233a4
eunji <nagosoo@kakao.com>
2023-10-28 12:18:43 +0900

    fix: Car 클래스 _forwardCnt 변수를 실행결과가 아니라 전진횟수를 나타내도록 수정

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -5,16 +5,16 @@ import racingcar.Const
 
 data class Car(
     private val _name: String,
-    private var _forwardResult: String = "",
+    private var _forwardCnt: Int = 0,
 ) {
     val name
         get() = _name
     val forwardCnt: Int
-        get() = _forwardResult.length
+        get() = _forwardCnt
 
     fun moveForward() {
         val enableMove = Randoms.pickNumberInRange(0, 9) >= 4
-        if (enableMove) _forwardResult += Const.FORWARD_STRING
-        println("$name : $_forwardResult")
+        if (enableMove) _forwardCnt += 1
+        println("$name : ${Const.FORWARD_STRING.repeat(_forwardCnt)}")
     }
 }
