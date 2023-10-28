ba5db2f8397230e3c40de353625a51263d9c57e3
mk43313@gmail.com <mk43313@gmail.com>
2023-10-28 23:49:40 +0900

    FEAT : Define ErrorString

--- src/main/kotlin/constant/ErrorString.kt
+++ src/main/kotlin/constant/ErrorString.kt
@@ -1,2 +1,10 @@
-package Constant
+package constant
 
+class ErrorString {
+    companion object {
+        const val NO_INPUT_ERROR = "자동차 이름을 입력해주세요."
+        const val INPUT_VALUE_ERROR = "올바른 자동차 이름을 입력해 주세요."
+        const val INPUT_LENGTH_ERROR = "자동차 이름의 길이는 1에서 5사이여야 합니다."
+        const val INTEGER_INPUT_ERROR = "정수를 입력하세요."
+    }
+}
