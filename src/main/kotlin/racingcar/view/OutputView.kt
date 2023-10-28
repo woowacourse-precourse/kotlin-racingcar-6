e482906fcf11fcf89fc01de0f7fd344cd86c1756
jaehan4707 <jahan4707@naver.com>
2023-10-28 16:14:32 +0900

    feat : OutputView 추가

---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,10 @@
+package racingcar.view
+
+import racingcar.util.GameMessage
+
+class OutputView {
+    fun printGameStartMessage() = println(GameMessage.GAME_START_MESSAGE)
+    fun printGameInputAttemptMessage() = println(GameMessage.GAME_INPUT_ATTEMPT_MESSAGE)
+    fun printGameResultMessage() = println(GameMessage.GAME_RESULT_MESSAGE)
+    fun printGameWinnerMessage() = println(GameMessage.GAME_WINNER_MESSAGE)//TODO 우승자 리스트 받아서 ,로 구분 출력
+}
