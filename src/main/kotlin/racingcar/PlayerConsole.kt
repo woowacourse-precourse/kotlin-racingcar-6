a11297d153f6633f10f1fe1d22c8ea675bccf0b8
somin <salth0601@gmail.com>
2023-10-28 23:59:56 +0900

    feat: getValidCarNames 함수 추가 - 유효한 자동차 이름을 입력받는다

--- src/main/kotlin/racingcar/PlayerConsole.kt
+++ src/main/kotlin/racingcar/PlayerConsole.kt
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 object PlayerConsole {
 
+    fun getValidCarNames() = parseInputByComma().validateCarNames()
+
     private fun parseInputByComma() = readLine().split(",")
 
-    //확장 함수로 짠 이유 : getValidCarNames에서 메서드 체이닝으로 코드의 흐름을 한눈에 파악
+    //확장함수로 짠 이유 : getValidCarNames에서 메서드 체이닝으로 코드의 흐름을 한눈에 파악
     private fun List<String>.validateCarNames(): List<String> {
         require(this.all { it.length <= 5 && it.isNotEmpty() }) { "자동차 이름은 한 글자 이상, 5글자 이하로 입력해 주세요." }
         require(this.all { playerName -> playerName.all { it.isLetter() || it.isDigit() } }) { "문자와 숫자만 입력해 주세요." }
