3fce653963141a6a593ea4a6f0b46f1d25da0aca
somin <salth0601@gmail.com>
2023-10-29 01:51:42 +0900

    feat: carList 프로퍼티, createRacingCars 메소드 추가
    - 입력값을 바탕으로 List<RacingCar> 반환
    - location의 default 값은 0

--- src/main/kotlin/racingcar/RacingCarController.kt
+++ src/main/kotlin/racingcar/RacingCarController.kt
@@ -1,4 +1,11 @@
 package racingcar
 
+import racingcar.PlayerConsole.getValidCarNames
+
 class RacingCarController {
+
+    val carList = createRacingCars()
+
+    private fun createRacingCars() = getValidCarNames().map { RacingCar(it, 0) }
+
 }
