021bc3ad3c116842fc253feeaf64a17618635238
posite <su84053@gmail.com>
2023-10-28 19:40:10 +0900

    chore: 필요없는 코드 제거

--- src/main/kotlin/racingcar/domain/RacingCars.kt
+++ src/main/kotlin/racingcar/domain/RacingCars.kt
@@ -4,17 +4,11 @@ class RacingCars(nameList: List<String>) {
     private val racingCars = mutableListOf<RacingCar>()
 
     init {
-        validateNameListSize(nameList)
-        validateNameLength(nameList)
         nameList.forEach {
             racingCars.add(RacingCar(it))
         }
     }
 
     fun loadRacingCars() = racingCars
-
-    companion object {
-        private const val MAX_NAME_LENGTH = 5
-        private const val MIN_RACING_CAR_COUNT = 2
-    }
+    
 }
