e89762faf414ac9e9af7fccace425b4274a2eb1f
jiwon2724 <stopone3119@naver.com>
2023-10-28 13:14:55 +0900

    [feat] : 각 차수별 실행 결과 함수 구현

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,17 +1,41 @@
 package racingcar.domain
 
-import racingcar.manager.InputManager
-import racingcar.manager.OutputManager
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 
 class Car(
-    private val carRace: CarRace = CarRace(),
     private val carNames: List<String>,
     private val attemptCount: Int
 ) {
     fun race() {
-        carRace.racing (
-            carNames = carNames,
-            attemptCount = attemptCount
-        )
+        
+    }
+    private fun executeRace(): ArrayList<MoveResult> {
+        val moveResultList = arrayListOf<MoveResult>()
+        carNames.forEach { carName ->
+            val moveResult = MoveResult(
+                carName = carName,
+                moveCount = isMovable()
+            )
+            moveResultList.add(moveResult)
+        }
+        return moveResultList
+    }
+
+    private fun isMovable(): Int =
+         if(pickNumberInRange(MIN_VALUE, MAX_VALUE) >= POSSIBLE_FORWARD) FORWARD else STOP
+
+    companion object {
+        private const val MIN_VALUE = 0
+        private const val MAX_VALUE = 0
+
+        private const val POSSIBLE_FORWARD = 4
+
+        private const val FORWARD = 1
+        private const val STOP = 1
     }
 }
+
+data class MoveResult(
+    val carName: String = "",
+    val moveCount: Int = 0
+)
