a77270bcaff7e9de8432f5482dbc2ff5383d00cf
Sheep1sik <bohg6799@naver.com>
2023-10-28 20:08:53 +0900

    feat: validateUserInput 함수 기능 추가
    숫자가 아닌 문자로만 구성되어 있는지 검증하는 기능 추가

--- src/main/kotlin/racingcar/Validator.kt
+++ src/main/kotlin/racingcar/Validator.kt
@@ -1,18 +1,17 @@
 package racingcar
 
 class Validator {
-    // 요구사항 검증, 전진 조건 검증을 담당
     fun validateUserInput(carNamesList: List<String>) {
-        // 자동차의 이름이 5자 이하인지 검증 기능
         if (carNamesList.any { it.length > 5 }) {
-            throw IllegalArgumentException("이름은 5자 이하여야 합니다.")
+            throw IllegalArgumentException("이름은 5자 이하로 입력해주세요.")
         }
-
-
-        // 시도 횟수가 숫자가 맞는지 검증 기능
-
+        if (carNamesList.any{it.toIntOrNull() != null}){
+            throw IllegalArgumentException("이름은 문자로 입력하세요.")
+        }
     }
-
-    // 랜덤값이 전진하는 조건에 일지하는지 검증 기능
-
+    fun validateUserTryCount(tryCount: String){
+        if (tryCount.toIntOrNull() == null){
+            throw IllegalArgumentException("유효한 숫자를 입력하세요.")
+        }
+    }
 }
