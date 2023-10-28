c1b25aa2b8fc66828dd8e349965ad8d4768a30ed
bak <bak4017@gmail.com>
2023-10-28 13:17:27 +0900

    feat(racingcar): 뼈대 코드 생성

---
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -0,0 +1,22 @@
+package racingcar.controller
+
+class GameController {
+
+    fun run() {
+        getUserInput()
+        play()
+        printResult()
+    }
+
+    private fun getUserInput() {
+
+    }
+
+    private fun play() {
+
+    }
+
+    private fun printResult() {
+
+    }
+}
