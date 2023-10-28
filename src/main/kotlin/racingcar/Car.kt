005e30c99a2fd41aaded0bcedf4d1c5e7e1c55df
jklom0326 <jklom0326@gmail.com>
2023-10-29 02:50:06 +0900

    feat: 자동차 생성 기능 추가

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,6 @@
+package racingcar
+
+class Car(val name: String, position: Int = 0) {
+    var position = position
+        private set
+}
---
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -0,0 +1,30 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+class RacingGame {
+
+    fun gameStart() {
+        val cars = initCar()
+        val computer = Computer()
+    }
+
+    private fun initCar(): List<Car> {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        val name = readCarNames()
+        name.forEach {
+            if (it.length > 5) {
+                throw IllegalArgumentException("5글자를 초과할 수 없습니다.")
+            }
+            if (it.isEmpty()) {
+                throw IllegalArgumentException("이름이 비어있습니다.")
+            }
+        }
+        return name.map { Car(it) }
+    }
+
+    private fun readCarNames(): List<String> {
+        val input = Console.readLine()
+        return input.split(",")
+    }
+}
