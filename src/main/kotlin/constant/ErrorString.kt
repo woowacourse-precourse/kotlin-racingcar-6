619459729f6889d0ddf6ad6a9520de742a07da4f
mk43313@gmail.com <mk43313@gmail.com>
2023-10-29 01:24:02 +0900

    Complete ErrorString

--- src/main/kotlin/constant/ErrorString.kt
+++ src/main/kotlin/constant/ErrorString.kt
@@ -5,6 +5,7 @@ class ErrorString {
         const val NO_INPUT_ERROR = "자동차 이름을 입력해주세요."
         const val INPUT_VALUE_ERROR = "올바른 자동차 이름을 입력해 주세요."
         const val INPUT_LENGTH_ERROR = "자동차 이름의 길이는 1에서 5사이여야 합니다."
+        const val INPUT_REPEAT_ERROR = "중복된 자동차 이름이 있습니다."
         const val INTEGER_INPUT_ERROR = "정수를 입력하세요."
     }
 }
