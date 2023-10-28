aea01e79869507ccdae96786083b384f8aae59e6
eunji <nagosoo@kakao.com>
2023-10-28 12:43:32 +0900

    refac: 코드 리팩토링

--- src/main/kotlin/racingcar/Const.kt
+++ src/main/kotlin/racingcar/Const.kt
@@ -1,12 +1,12 @@
 package racingcar
 
 object Const {
-    val FORWARD_STRING = "-"
-    val ENTER_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    val ENTER_TRY_CNT_MSG = "시도할 횟수는 몇 회인가요?"
-    val GAME_RESULT_MSG = "실행 결과"
-    val GAME_WINNER_MSG = "최종 우승자 : "
-    val EXCEPTION_CAR_NAME_LENGTH_INVALIDATE = "자동차 이름은 1글자 이상, 5글자 이하만 가능합니다."
-    val EXCEPTION_CAR_NAME_DUPLICATED = "자동차 이름은 중복이 불가합니다."
-    val EXCEPTION_TRY_CNT_NOT_INT = "시도 횟수는 숫자만 입력해주세요."
+    const val FORWARD_STRING = "-"
+    const val ENTER_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val ENTER_TRY_CNT_MSG = "시도할 횟수는 몇 회인가요?"
+    const val GAME_RESULT_MSG = "실행 결과"
+    const val GAME_WINNER_MSG = "최종 우승자 : "
+    const val EXCEPTION_CAR_NAME_LENGTH_INVALIDATE = "자동차 이름은 1글자 이상, 5글자 이하만 가능합니다."
+    const val EXCEPTION_CAR_NAME_DUPLICATED = "자동차 이름은 중복이 불가합니다."
+    const val EXCEPTION_TRY_CNT_NOT_INT = "시도 횟수는 숫자만 입력해주세요."
 }
