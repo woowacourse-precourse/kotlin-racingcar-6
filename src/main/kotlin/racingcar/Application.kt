7b747f1db8fa637122341b7f22dad4003277800d
Insu Kang <kin4496@naver.com>
2023-10-29 02:33:45 +0900

    feat: 클래스 Car를 정의

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,11 +1,13 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val listOfCar = Console.readLine()
         .split(',')
+        .map{Car(it)}
 
     println("시도할 횟수는 몇 회인가요?")
     var round = getUserIntInput()
@@ -18,4 +20,19 @@ fun getUserIntInput(): Int {
         throw IllegalArgumentException()
     }
     return input
+}
+class Car(
+    val name: String,
+    var position: Int = 0
+) {
+    init {
+        if (name.length > 5) throw IllegalArgumentException()
+    }
+    fun move() {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        if (randomNumber >= 4) position += 1
+    }
+    override fun toString(): String {
+        return "$name : " + "-".repeat(position)
+    }
 }
