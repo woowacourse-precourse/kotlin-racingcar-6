6af6ecbd845b769122924932a071aeca7d3b1727
kyunghwa <snoopn303@gmail.com>
2023-10-28 16:14:53 +0900

    자동자의 이름 입력 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,27 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
+class Car(val name: String) {
+
+}
+
+class InputHandler {
+    private val cars = mutableListOf<Car>()
+
+    fun start() {
+        inputCarNames()
+    }
+    private fun inputCarNames() {
+        println("자동차 이름을 5자 이하로 입력해주세요.")
+        val input = Console.readLine()
+        val names = input.split(",").map { it.trim() }
+        cars.addAll(names.map { Car(it) })
+    }
+}
+
 fun main() {
     // TODO: 프로그램 구현
+    val inputHandler = InputHandler()
+    inputHandler.start()
 }
