d520e6b1d575bf4800f449def8ca78901cd4dac2
somin <salth0601@gmail.com>
2023-10-29 00:19:07 +0900

    feat: isAbleToMove 함수 추가 - 0에서 9까지의 정수 중 무작위 선택 - 4 이상의 값인지 여부 반환

--- src/main/kotlin/racingcar/RacingCar.kt
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -1,6 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 data class RacingCar(
     val name: String,
     var location: Int = 0
-)
+) {
+
+    private fun isAbleToMove(): Boolean {
+        val random = Randoms.pickNumberInRange(0, 9)
+        return (4 <= random)
+    }
+}
