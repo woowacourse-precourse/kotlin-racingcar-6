87db85ae7f9d169d1c4223521f54ad8b29a9217e
somin <salth0601@gmail.com>
2023-10-29 02:21:02 +0900

    feat: moveCount 프로퍼티, play 메소드 추가 - moveCount : 이동 횟수 저장 - play : carList의 각 원소에 moveCount만큼 이동 수행

--- src/main/kotlin/racingcar/RacingCarController.kt
+++ src/main/kotlin/racingcar/RacingCarController.kt
@@ -5,6 +5,14 @@ import racingcar.PlayerConsole.getValidCarNames
 class RacingCarController {
 
     val carList = createRacingCars()
+    val moveCount = PlayerConsole.getValidMoveCount()
+
+    fun play() {
+        createRacingCars()
+        repeat(moveCount) {
+            // TODO:전진
+        }
+    }
 
     private fun createRacingCars() = getValidCarNames().map { RacingCar(it, 0) }
 
