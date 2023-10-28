1d70007322b9b0bce4471e0ccbef90d3cd789269
leedohyung28 <dhleee0123@gmail.com>
2023-10-28 20:13:35 +0900

    3-3. checkAdvance 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,14 +2,17 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    // 자동차 이름 입력 메시지
     println("경주할 자동차 이름을 입력하세오.(이름은 쉼표(,) 기준으로 구분)")
     // 자동차의 이름을 String으로 받는 carsNameInput
     val carsNameInput = Console.readLine()
+    // ,로 구분된 자동차 리스트를 carsList에 저장
+    val carsList: MutableList<String> = presetCars(carsNameInput)
 
+    // 시도 횟수 입력 메시지
     println("시도할 횟수는 몇 회인가요?")
     // 시도 입력을 받을 String형 trialInput
     val trialInput = Console.readLine()
     // 제대로 된 입력인지 체크하여 맞다면 trial에 저장
     val trial = checkValidInput(trialInput)
-
 }
--- src/main/kotlin/racingcar/CarGame.kt
+++ src/main/kotlin/racingcar/CarGame.kt
@@ -1,11 +1,22 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun repeatMove(trial: Int) {
     TODO("trial만큼 게임을 반복")
 }
 
 fun checkAdvance(): Boolean {
-    TODO("Random을 통해 1~9까지 ")
+    // 0에서 9까지의 정수 중 무작위 값 추출
+    val randomNumber = Randoms.pickNumberInRange(0,9)
+
+    // 무작위 값이 4이상 일 경우 true 반환
+    if (randomNumber >= 4)
+        return true
+
+    // 무작위 값이 4보다 작을 경우 false 반환
+    else
+        return false
 }
 
 fun moveCar() {
