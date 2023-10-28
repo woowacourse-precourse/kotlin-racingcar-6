b77b1c404cce92c7cfe9db07d49f069546bd62cd
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-28 23:58:03 +0900

    feat: 현재 주행거리 반환 기능 작성

--- src/main/kotlin/caroption/DashBoard.kt
+++ src/main/kotlin/caroption/DashBoard.kt
@@ -5,9 +5,8 @@ import CarConfiguration.INIT_DIST
 
 class DashBoard {
     private var dist = INIT_DIST
-    fun distPlus() {
-        dist++
-    }
+    fun distPlus() = dist++
+    fun nowDist() = dist
 
     fun printDist() {
         repeat(dist) {
