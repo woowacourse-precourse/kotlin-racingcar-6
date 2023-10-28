581863447a0825f66bc8ca6aac5c7e57f8e2ab99
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-10-29 00:34:25 +0900

    feature - 자동차 객체 생성 및 name(자동차 이름), count(전진 횟수) 저장

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,10 @@
 package racingcar
 
-val carNames = mutableListOf<String>()
+import camp.nextstep.edu.missionutils.Randoms
+
+data class Car(val name: String, val count: Int) // Car 데이터 클래스 정의
+
+val carNames = mutableListOf<Car>() // Car 객체를 저장하는 리스트
 
 fun checkInputIsNull(input: String?){
     if (input.isNullOrEmpty()){
@@ -15,10 +19,10 @@ fun main() {
     checkInputIsNull(input)
 
     input?.let { nonNullInput ->
-        carNames.addAll(nonNullInput.split(",").map { it.trim() })
+        carNames.addAll(nonNullInput.split(",").map { Car(it.trim(), 0) }) // Car 객체로 저장
     }
 
-    val invalidCarNames = carNames.filter { it.length > 5 }
+    val invalidCarNames = carNames.filter { it.name.length > 5 }
     if (invalidCarNames.isNotEmpty()){
         throw IllegalArgumentException("자동차의 입력값의 길이는 5 이하로 입력해주세요.")
     }
@@ -27,11 +31,22 @@ fun main() {
     val playTime = readLine()
 
     checkInputIsNull(playTime)
+    val playTimeAsInt: Int
 
     try {
-        val playTimeAsInt = playTime!!.toInt()
+        playTimeAsInt = playTime!!.toInt()
     } catch (e: NumberFormatException) {
         throw IllegalArgumentException("유효한 정수를 입력해주세요.")
+    }
+
+    print(carNames)
+
+    // 경주 게임 실행
+    for (i in 1..playTimeAsInt){
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        if(randomNumber >= 4){
+            // 이곳에서 경주 로직을 처리
+        }
     }
 
 
