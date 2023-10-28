50864d59a9f3f7f18132ca4a129e6830c9ee50dc
bak <bak4017@gmail.com>
2023-10-28 23:18:44 +0900

    feat(racingcar): 사용자 입력값에 따른 에러발생 기능 완성

--- src/main/kotlin/racingcar/util/Validator.kt
+++ src/main/kotlin/racingcar/util/Validator.kt
@@ -9,14 +9,29 @@ class Validator(carNames: ArrayList<String>, attemptCount: String) {
     }
 
     private fun isValidLengthAndAlphabetic(carNames: ArrayList<String>) {
-
+        carNames.forEach { carName ->
+            if (carName.length !in 1..5 || !carName.all { it.isLetter() }) {
+                throw IllegalArgumentException("Car names must be alphabetic and have a length of 1 to 5 characters.")
+            }
+        }
     }
 
     private fun checkIfDuplicateNameExists(carNames: ArrayList<String>) {
-
+        val uniqueNames = carNames.toSet()
+        if (uniqueNames.size != carNames.size) {
+            throw IllegalArgumentException("Car names must not be duplicated.")
+        }
     }
 
     private fun isPositiveInteger(attemptCount: String) {
+        val attemptCountInt = try {
+            attemptCount.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("Attempt count must be a positive integer.")
+        }
 
+        if (attemptCountInt <= 0) {
+            throw IllegalArgumentException("Attempt count must be a positive integer.")
+        }
     }
 }
