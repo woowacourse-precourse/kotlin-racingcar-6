1f2f74c3017d02643ec9a87c6503cad3e342edc6
GreenRain <47217640+greenrain78@users.noreply.github.com>
2023-10-29 01:13:53 +0900

    feat(view/OutputView): OutputView 구성
    OutputView 사용자에게 메세지 출력 기능 구성
    - printMessage 사용자에게 메세지를 출력하는 공통
    기능
    -
    printInputGuideCarNameList 자동차 이름 목록을 입력 유도하는 메세지
    출력
    -
    printInputGuidePlayCount 경기 횟수를 입력 유도하는 메세지 출력
    - printResultWinner
    우승자를 출력
    - printPlayResult 경기 내용을 출력

---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,33 @@
+package racingcar.view
+
+class OutputView {
+    companion object {
+        private const val RESULT_WINNER_MESSAGE = "최종 우승자 : %s"
+        private const val INPUT_GUIDE_CAR_NAME_LIST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        private const val INPUT_GUIDE_PLAY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+        private const val PLAY_RESULT_MESSAGE = "실행 결과"
+    }
+
+    fun printInputGuideCarNameList() {
+        printMessage(INPUT_GUIDE_CAR_NAME_LIST_MESSAGE)
+    }
+
+    fun printInputGuidePlayCount() {
+        printMessage(INPUT_GUIDE_PLAY_COUNT_MESSAGE)
+    }
+
+    fun printResultWinner(winnerList: List<String>) {
+        val winnerString = winnerList.joinToString(", ")
+        printMessage(RESULT_WINNER_MESSAGE.format(winnerString))
+    }
+
+    fun printPlayResult(playResult: String) {
+        printMessage(PLAY_RESULT_MESSAGE)
+        printMessage(playResult)
+    }
+
+    private fun printMessage(message: String) {
+        println(message)
+    }
+
+}
