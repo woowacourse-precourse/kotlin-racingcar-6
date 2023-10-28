2d621bebf121f195d3b1d6e6bd5089cb2553b431
Minjun Kim <damon911@naver.com>
2023-10-28 20:12:02 +0900

    feat: 사용자로부터 자동차 이름 입력받기

--- src/main/kotlin/racingcar/game/Game.kt
+++ src/main/kotlin/racingcar/game/Game.kt
@@ -1,10 +1,27 @@
 package racingcar.game
 
+import camp.nextstep.edu.missionutils.Console
+import racingcar.util.Constants.CAR_NAME_DELIMITER
 import racingcar.util.Constants.TEXT_START_GAME
 
 class Game {
 
+    private var carList = mutableListOf<String>()
+
     fun startGame() {
         println(TEXT_START_GAME)
+
+        val input = Console.readLine()
+        carList = inputCarName(input)
+    }
+
+    fun inputCarName(input: String) : MutableList<String> {
+        val splitResult = mutableListOf<String>()
+
+        input.split(CAR_NAME_DELIMITER).forEach {
+            splitResult.add(it)
+        }
+
+        return splitResult
     }
 }
--- src/main/kotlin/racingcar/util/Constants.kt
+++ src/main/kotlin/racingcar/util/Constants.kt
@@ -3,4 +3,7 @@ package racingcar.util
 object Constants {
     // TEXT
     const val TEXT_START_GAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+
+    // VALUE
+    const val CAR_NAME_DELIMITER = ","
 }
