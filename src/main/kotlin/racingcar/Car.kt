11365661bddfa156d8e6c579d1e7eab297dd45ac
dpcks0509 <dpcks0509@gmail.com>
2023-10-28 15:48:02 +0900

    feat : 상태출력함수 구현

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,4 +4,8 @@ data class Car(val name: String, var numberOfMove: Int = 0) {
     fun moveForward() {
         this.numberOfMove += 1
     }
+
+    fun printStatus() {
+        println("${this.name} : " + "-".repeat(this.numberOfMove))
+    }
 }
