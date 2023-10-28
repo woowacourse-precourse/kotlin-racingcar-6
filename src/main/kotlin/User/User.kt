a92e2a338c0ab82b80a1c9e788465ed608f81ebb
sungsik-git <kss950708@gamil.com>
2023-10-28 12:10:37 +0900

    시작 안내문구 출력, 자동차 이름 입력받는 기능 구현

---
+++ src/main/kotlin/User/User.kt
@@ -0,0 +1,10 @@
+package User
+
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+class User {
+    fun inputCarName(){
+        var userNames = Console.readLine()
+        var userNameList = userNames.split(',')
+    }
+}
--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,10 @@
 package racingcar
 
+import User.User
+
 fun main() {
     // TODO: 프로그램 구현
+    val user = User()
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    user.inputCarName()
 }
