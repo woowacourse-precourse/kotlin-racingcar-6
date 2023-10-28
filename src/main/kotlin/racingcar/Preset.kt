0d5259c6b50833e44d88ae86c7cda55c7157e415
leedohyung28 <dhleee0123@gmail.com>
2023-10-28 21:39:25 +0900

    4-2. 글자수가 5글자 이상인 자동차 존재 시 오류 반환

--- src/main/kotlin/racingcar/Preset.kt
+++ src/main/kotlin/racingcar/Preset.kt
@@ -4,6 +4,12 @@ fun presetCars(carsNameInput: String): List<String> {
     // 입력받은 자동차들의 이름을 ,에 따라 구분하여 List에 저장
     val splitedCars = carsNameInput.split(",".toRegex()).toList()
 
+    // 만약 이 중 글자 수가 5 이상인 자동차가 있다면 오류 반환
+    repeat(splitedCars.size) {
+        if (splitedCars[it].length >= 5)
+            throw IllegalArgumentException()
+    }
+
     // 이름에 따라 구분된 자동차가 들어가나 List 반환
     return splitedCars
 }
