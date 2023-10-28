45e11728e0e3aa32a5adf02284a281503b1e1b0c
getupminaaa <getupmina@gmail.com>
2023-10-28 18:24:37 +0900

    feat: 경주할 자동차 이름이 유효한 지 검사하는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,9 +2,18 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 
+private const val CAR_NAME_MAX_LENGTH = 5
 fun main() {
     val cars = getCarList()
-    
+
+    if (!isCarNameUnique(cars)) throw IllegalArgumentException("Duplicate car names are not allowed.")
+    cars.forEach { car ->
+        if (!isCarNameNotEmpty(car)) throw IllegalArgumentException("Car name cannot be empty.")
+        if (!isCarNameLengthValid(car)) throw IllegalArgumentException("Car name '$car' exceeds the maximum allowed length of $CAR_NAME_MAX_LENGTH.")
+    }
 }
 
 fun getCarList() = Console.readLine().split(",")
+fun isCarNameUnique(carList: List<String>): Boolean = carList.size == carList.toSet().size
+fun isCarNameLengthValid(carName: String): Boolean = carName.length <= CAR_NAME_MAX_LENGTH
+fun isCarNameNotEmpty(carName: String): Boolean = carName.trim().isNotEmpty()
