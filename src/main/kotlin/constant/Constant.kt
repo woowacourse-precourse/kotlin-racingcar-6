1c4b83d235df31007af6899218434268a11abdc7
mk43313@gmail.com <mk43313@gmail.com>
2023-10-29 01:20:51 +0900

    FEAT : Define Constant

--- src/main/kotlin/constant/Constant.kt
+++ src/main/kotlin/constant/Constant.kt
@@ -1,2 +1,16 @@
 package constant
 
+class Constant {
+    companion object {
+        const val MOVE_BASE_NUMBER = 4
+        const val RANDOM_START_NUMBER = 0
+        const val RANDOM_END_NUMBER = 9
+        const val CAR_NAME_MAX_LENGTH = 5
+
+        // 출력문
+        const val CAR_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        const val TRY_NUMBER_INPUT_STRING = "시도할 횟수는 몇 회인가요?"
+        const val RESULT_STRING = "실행 결과"
+        const val WINNER_STRING = "최종 우승자 : "
+    }
+}
