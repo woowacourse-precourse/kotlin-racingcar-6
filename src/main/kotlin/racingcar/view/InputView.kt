b05c5c3fa90cf52622723499f2c67268518989fd
Namyunsuk <yunsuk0328@naver.com>
2023-10-28 17:24:42 +0900

    feat(InputView): 사용자 입력 값 받기 기능 구현

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,17 @@
+package racingcar.view
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+    companion object {
+        fun inputCarName(): String {
+            println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+            return Console.readLine()
+        }
+
+        fun inputNumberAttempts(): String {
+            println("시도할 횟수는 몇 회인가요?")
+            return Console.readLine()
+        }
+    }
+}
