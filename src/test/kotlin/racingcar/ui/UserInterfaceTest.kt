63264a20a1d5a0cb2c7745d89808d644e7c39b59
book3039 <book3039@knou.ac.kr>
2023-10-30 03:43:32 +0900

    test: 자동차 이름 입력시 5자 이하인지 검사 테스트 작성

--- src/test/kotlin/racingcar/ui/UserInterfaceTest.kt
+++ src/test/kotlin/racingcar/ui/UserInterfaceTest.kt
@@ -2,15 +2,20 @@ package racingcar.ui
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import racingcar.ui.UserInput.createNameList
 
 class UserInterfaceTest {
+    val input = "  choi  , gerrard  ,  mint  "
+    val names = listOf("choi", "gerrard", "mint")
+    val inputNames = input.createNameList()
     @Test
     fun `입력을 쉼표로 구분하고, 앞 뒤 공백을 제거하여 리스트를 생성한다`() {
-        val input = "  choi  , gerrard  ,  mint  "
-        val names = listOf("choi", "gerrard", "mint")
-        val inputNames = input.createNameList()
-
         assertEquals(names, inputNames)
+    }
+
+    @Test
+    fun `자동차 이름이 5자 이하인지 검사`() {
+        assertThrows<IllegalArgumentException> { Validator.checkNamesLength(inputNames)}
     }
 }
