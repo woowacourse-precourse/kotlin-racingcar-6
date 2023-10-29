8c93ac53a2cf36cfabad1876850c60729eaa042f
jujeong <jujeong0108@naver.com>
2023-10-30 04:09:44 +0900

    Added getCarList on RacingGameController.kt

--- src/main/kotlin/racingcar/controller/RacingGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingGameController.kt
@@ -1,7 +1,15 @@
 package racingcar.controller
 
+import camp.nextstep.edu.missionutils.Console
 import racingcar.view.RacingGameView
 import racingcar.model.RacingGameModel
 
 class RacingGameController(private val view: RacingGameView, private val model: RacingGameModel) {
+    fun run() {
+        view.requestCarNameInput()
+        getCarList()
+    }
+    private fun getCarList() {
+        val carList: List<String> = listOf(*Console.readLine().split(",").toTypedArray())
+    }
 }
