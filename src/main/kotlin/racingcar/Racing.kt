5ddaae70381094abebc2ae49f3498a5fbd13a2d0
getupminaaa <getupmina@gmail.com>
2023-10-29 13:58:31 +0900

    feat: 각 차수별 실행 결과를 출력하는 기능 구현

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -5,16 +5,21 @@ import camp.nextstep.edu.missionutils.Randoms
 class Racing(private val cars: List<Car>) {
 
     //true 면 move, false면 stop
-    private fun determineMoveOrStop(randomNumber:Int) = randomNumber>= BASE_NUMBER
+    private fun determineMoveOrStop(randomNumber: Int) = randomNumber >= BASE_NUMBER
     private fun makeRandomNumber() = Randoms.pickNumberInRange(0, 9)
     fun getRacingDistances(numberAttempts: Int) {
         repeat(numberAttempts) {
             for (j in cars.indices) {
                 val randomNumber = makeRandomNumber()
                 if (determineMoveOrStop(randomNumber)) cars[j].distance++
-                println("$randomNumber => ${cars[j]}")
             }
-            println("-------------------------$it--------------------------")
+            printMatchProgress()
         }
     }
+
+    private fun printMatchProgress() {
+        cars.forEach { car -> println("${car.name} : ${"-".repeat(car.distance)}") }
+        println()
+    }
+
 }
