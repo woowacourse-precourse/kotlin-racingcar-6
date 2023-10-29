6252b351570adb6d91c624c297c1384aba9fb6cb
이홍진\iit80 <hong80714@gmail.com>
2023-10-29 22:51:01 +0900

    [feat] 자동차 객체 생성 기능 사용자에게 자동차의 이름을 입력받아, 쉽표단위로 구분을 하여 Cars라는 객체를 만듦  각 객체는 위치와 이름값을 갖음

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,23 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)")
+    val carNames = Console.readLine()
+
+    println(carNames.split(','))
+
+    val cars = carNames.split(',').map { Cars(it) }
+
+    cars.forEach { car ->
+        println("${car.name} : ${car.position}")
+    }
+
+}
+
+class Cars(carName: String) {
+    val position = 0
+    val name = carName
 }
