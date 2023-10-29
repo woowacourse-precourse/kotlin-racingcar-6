d9583fba442cc06eae8e6eacd7aa64d741771b5e
Nohsein <si2238@naver.com>
2023-10-30 01:23:11 +0900

    feat: 자동차 이름 입력 받기
    자동차 이름 입력 받기
    빈칸, 5자 초과 시 에러 처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,19 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    askCarNames()
 }
+
+fun askCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carList = Console.readLine().split(",")
+    if (carList.any { it == "" || it.length > 5 }) {
+        throw IllegalArgumentException("자동차 이름은 1~5자로 작성해주세요")
+    }
+    return carList
+}
+
+
+
