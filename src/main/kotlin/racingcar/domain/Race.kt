4f7531cc17e7dc5e540773fd389d25bad1b36b5d
hoogje <ajyong316@gmail.com>
2023-10-29 20:05:53 +0900

    feat(Race):이동 횟수에 따라 모든 자동차를 전진 혹은 정지시키는 기능 구현

--- src/main/kotlin/racingcar/domain/Race.kt
+++ src/main/kotlin/racingcar/domain/Race.kt
@@ -2,17 +2,28 @@ package racingcar.domain
 
 import racingcar.utils.Converter
 
-class Race(carNames: String) {
+class Race(carNames: String, private var moveCount: Int) {
     private var carList = mutableListOf<Car>()
 
     init {
         createCarList(carNames)
     }
 
+    fun startRace() {
+        while (moveCount > 0) {
+            moveOrStayAllCars()
+            moveCount--
+        }
+    }
+
+    private fun moveOrStayAllCars() {
+        carList.forEach { it.moveOrStay() }
+    }
+
     private fun createCarList(carNames: String) {
         val carNameList = Converter.splitByCommaToStringList(carNames)
 
-        carNameList.forEach{
+        carNameList.forEach {
             val car = Car(it)
             carList.add(car)
         }
