4ca27625c07604f81bf908f93377f98b175ea867
ErroredPasta <ForeignServices3132@pm.me>
2023-10-29 16:12:44 +0900

    feat: 자동차 이름 생성시 5글자 이하인지 확인
    만약 5글자 초과시 IllegalArgumentException 발생

--- src/main/kotlin/racingcar/car/CarName.kt
+++ src/main/kotlin/racingcar/car/CarName.kt
@@ -1,4 +1,14 @@
 package racingcar.car
 
 @JvmInline
-value class CarName(val name: String)
+value class CarName(val name: String) {
+
+    init {
+        require(name.length <= 5) { CAR_NAME_LENGTH_EXCEPTION }
+    }
+
+    companion object {
+        private const val CAR_NAME_LENGTH_EXCEPTION = "CarName must be less than or equal to 5 characters"
+    }
+
+}
