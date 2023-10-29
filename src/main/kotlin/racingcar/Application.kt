63f65d2543d22ca6df411c41aa948afe178082df
Chanwoo <overelectric@gmail.com>
2023-10-29 19:09:46 +0900

    차 움직이는지 여부 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,22 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
+fun isCarMove(car: String): Boolean {
+    val dice = Randoms.pickNumberInRange(0, 9)
+    return dice >= 4
+}
+
+fun runRacingGame(carList: List<String>, racingTime: Int) {
+    val racingGame = LinkedHashMap<String, Int>()
+    for (car in carList) {
+        racingGame.put(car, 0)
+    }
+
+}
+
 fun main() {
     // TODO: 프로그램 구현
+    val carList = readLine()!!.split(", ")
+    val racingTime = readLine()!!.toInt()
 }
