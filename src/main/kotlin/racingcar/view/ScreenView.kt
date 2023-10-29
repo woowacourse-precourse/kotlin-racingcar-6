d1a6ef6350c49600e422ceea04df9a33a44aa0af
taewoong98 <kcong1@ajou.ac.kr>
2023-10-30 02:17:14 +0900

    feat: view 메세지 함수화

---
+++ src/main/kotlin/racingcar/view/ScreenView.kt
@@ -0,0 +1,14 @@
+package racingcar.view
+
+class ScreenView {
+    private val inputCarNameMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    private val inputGameCountMessage = "시도할 횟수는 몇 회인가요?"
+
+    fun inputCarName(){
+        println(inputCarNameMessage)
+    }
+
+    fun inputGameCount() {
+        println(inputGameCountMessage)
+    }
+}
