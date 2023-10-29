f9f1b8340ff8fbcd922cf9363e65a8c1401ed602
nighttwo1 <ys12j1@gmail.com>
2023-10-29 20:08:00 +0900

    feat: 자동차의 이름과 시도할 횟수를 불러와 저장한다

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,22 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val cars: List<Car> = readLine()
+        .split(",")
+        .toCarListOrNull()
+
+    println("시도할 횟수는 몇 회인가요?")
+    val rounds: Int = readLine().toIntOrNull() ?: throw IllegalArgumentException()
+}
+
+fun List<String>.toCarListOrNull(): List<Car>{
+    return this.map {name ->
+        if(name.length > 5) throw IllegalArgumentException()
+        if(name == "") throw IllegalArgumentException()
+
+        Car(name, 0)
+    }
 }
