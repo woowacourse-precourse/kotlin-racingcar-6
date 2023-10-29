6fc437b3fe7946a6d2e2e366eb761e98b936ce0d
jaejunlee11 <sktks11@naver.com>
2023-10-29 21:29:08 +0900

    경주할 자동차를 입력 받아 Car 객체 생성 및 collection에 저장

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -8,7 +8,15 @@ class Racing {
 
     }
     private fun registerCar(){
-
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        val carCollection=CarCollection()
+        val registerCarNames = Console.readLine()
+        val registerCarNamesList=registerCarNames.split(",")
+        for (name in registerCarNamesList){
+            val carName=CarName(name)
+            val car = Car(carName)
+            carCollection.putCar(car)
+        }
     }
     fun setRacingNum(){
 
