25e00197b82b77c09fd3fb7b7fad0d22857094dd
hugesilver <xor3160@naver.com>
2023-10-29 19:39:25 +0900

    update car name list

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,7 +2,9 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 
+val CAR_NAME_LIST = mutableListOf<String>()
+
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    val cars = Console.readLine()
+    val carsName = Console.readLine()
 }
