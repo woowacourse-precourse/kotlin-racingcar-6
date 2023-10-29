2a14841025ee6474f259afd2bfd589430481cd98
seonghyun <skyladder8895@gmail.com>
2023-10-29 17:12:42 +0900

    refactor: update Car class method

--- src/main/kotlin/racingcar/racingcar.kt
+++ src/main/kotlin/racingcar/racingcar.kt
@@ -1,5 +1,19 @@
 package racingcar
 
-class Car(val name: String) {
-    var position: Int = 0
+import camp.nextstep.edu.missionutils.Randoms
+
+class Car(private val name: String) {
+    private var position: Int = 0
+
+    fun printCarData() {
+        print("$name : ")
+        repeat(position) {
+            print("-")
+        }
+    }
+    fun isAdvance (): Int {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++
+        }
+    }
 }
