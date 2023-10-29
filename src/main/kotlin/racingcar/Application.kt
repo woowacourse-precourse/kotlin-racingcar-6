f85711203921d01da99f5060423aad13f9f4f7aa
leedaeul <ouowinnie@gmail.com>
2023-10-29 12:47:15 +0900

    [FEAT] : 시도할 횟수를 입력받고, 양의 정수가 아닐 경우 예외처리한다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -8,24 +8,40 @@ fun main() {
 
 const val CAR_NAME_LENGTH = 5
 fun runGame() {
-    val setCars: MutableList<String> = mutableListOf()
+
+    val carName = getCarNames()
+    val tryNumber = getTryNumbers()
+
+}
+fun getCarNames(): List<String> {
+    val carNameList: MutableList<String> = mutableListOf()
 
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    try {
-        val carName = getCarNames()
-        val carNameList = carName.split(",").map { it.trim() }
-        for (car in carNameList) {
+    return try {
+        val carName = Console.readLine() ?: ""
+        val carNames = carName.split(",").map { it.trim() }
+        for (car in carNames) {
             if (car.length <= CAR_NAME_LENGTH) {
-                setCars.add(car)
+                carNameList.add(car)
             } else {
                 throw IllegalArgumentException("자동차 이름은 5자 이하로 설정해주세요.")
             }
         }
-        println(setCars)
+        carNameList
     } catch (e: IllegalArgumentException) {
-        println(e.message)
+        throw IllegalArgumentException("잘못된 입력입니다 : ${e.message}")
     }
 }
-fun getCarNames(): String {
-    return Console.readLine() ?: ""
+
+fun getTryNumbers(): Int {
+    println("시도할 횟수는 몇 회인가요?")
+    return try {
+        val tryNumber = Console.readLine()?.toInt() ?: 0
+        if (tryNumber <= 0) {
+            throw IllegalArgumentException("양의 정수를 입력해주세요.")
+        }
+        tryNumber
+    } catch (e: IllegalArgumentException) {
+        throw IllegalArgumentException("잘못된 입력입니다 : ${e.message}")
+    }
 }
