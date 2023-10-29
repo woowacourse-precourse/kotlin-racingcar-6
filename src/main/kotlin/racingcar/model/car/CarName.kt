f03a3c512a20074a9541c4e66c697f6cfec5b311
ErroredPasta <ForeignServices3132@pm.me>
2023-10-29 23:27:52 +0900

    refactor: CarName에서 이름 최대 길이를 상수로 빼냄

--- src/main/kotlin/racingcar/model/car/CarName.kt
+++ src/main/kotlin/racingcar/model/car/CarName.kt
@@ -2,9 +2,8 @@ package racingcar.model.car
 
 @JvmInline
 value class CarName(val name: String) {
-
     init {
-        require(name.length <= 5) { CAR_NAME_LENGTH_EXCEPTION }
+        require(name.length <= MAX_NAME_LENGTH) { CAR_NAME_LENGTH_EXCEPTION }
     }
 
     // Car에서 이름을 출력하려면 car.name.name 해야하는 것을 막기 위함
@@ -13,7 +12,8 @@ value class CarName(val name: String) {
     }
 
     companion object {
-        private const val CAR_NAME_LENGTH_EXCEPTION = "CarName must be less than or equal to 5 characters"
+        private const val MAX_NAME_LENGTH = 5
+        private const val CAR_NAME_LENGTH_EXCEPTION = "CarName must be shorter than or equal to 5 characters"
     }
 
 }
