cf5b75594aa073df0378ca104705ea95b02b21e0
ErroredPasta <ForeignServices3132@pm.me>
2023-10-29 21:25:40 +0900

    feat: TurnNumber 생성 테스트 작성

--- src/main/kotlin/racingcar/model/TurnNumber.kt
+++ src/main/kotlin/racingcar/model/TurnNumber.kt
@@ -7,7 +7,7 @@ value class TurnNumber(val number: Int) {
     }
 
     companion object {
-        private const val MIN_TURN_NUMBER = 0
+        const val MIN_TURN_NUMBER = 0
         private const val LESS_THAN_MIN_TURN_NUMBER = "Turn number must be greater than $MIN_TURN_NUMBER"
     }
 }
