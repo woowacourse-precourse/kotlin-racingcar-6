cf92053e2a384a024ec2027d116c7d2fc47aacef
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-29 14:32:15 +0900

    feat: 자동차 생성기능 작성

--- src/main/kotlin/CarFactory.kt
+++ src/main/kotlin/CarFactory.kt
@@ -1,12 +1,5 @@
-import util.Validator.inputCarNameCheck
-
 object CarFactory {
-    fun makeCar(input: String): List<Car> {
-        val makeCarList = inputCarNameCheck(input)
-        val racingCarList = mutableListOf<Car>()
-        makeCarList.forEach {
-            racingCarList.add(Car(it))
-        }
-        return racingCarList
+    fun makeCar(name: String): Car {
+        return Car(name)
     }
 }
