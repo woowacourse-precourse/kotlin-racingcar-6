87bbdd619ef7cfc6830fe5eac006b062e18099ae
getupminaaa <getupmina@gmail.com>
2023-10-29 11:44:06 +0900

    feat: “시도할 횟수는 몇 회인가요?”를 출력하는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,10 +4,11 @@ import camp.nextstep.edu.missionutils.Console
 
 private const val CAR_NAME_MAX_LENGTH = 5
 private const val RACE_START_MSG = "경주할 자동차 이름을 입력하세요."
+private const val NUMBER_ATTEMPTS_MSG = "시도할 횟수는 몇 회인가요?"
+
 private const val  NUMBER_ATTEMPTS_REGEX = "[1-9]"
 fun main() {
     println(RACE_START_MSG)
-    /*
     val cars = getCarList()
 
     if (!isCarNameUnique(cars)) throw IllegalArgumentException("Duplicate car names are not allowed.")
@@ -15,7 +16,9 @@ fun main() {
         if (!isCarNameNotEmpty(car)) throw IllegalArgumentException("Car name cannot be empty.")
         if (!isCarNameLengthValid(car)) throw IllegalArgumentException("Car name '$car' exceeds the maximum allowed length of $CAR_NAME_MAX_LENGTH.")
     }
-     */
+
+    println(NUMBER_ATTEMPTS_MSG)
+
     val numberAttempts = getNumberAttempts()
     if(!isNumberAttemptsValid(numberAttempts)) throw IllegalArgumentException("Invalid format for number of attempts. Please use the specified format.")
 }
