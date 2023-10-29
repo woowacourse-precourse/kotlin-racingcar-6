a783ea8088df0a5666988833a122293406ef0869
pipeline <sorkwhwo@naver.com>
2023-10-29 19:08:38 +0900

    test: 통합 테스트 추가

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -20,6 +20,63 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
+    fun `정지와 전진이 적절히 반영되어 우승자가 결정되는지 - 여러명일때`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni,hawai", "3")
+                assertThat(output()).contains("pobi : -", "woni : ---", "hawai : ---", "최종 우승자 : woni, hawai")
+            },
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+            STOP, MOVING_FORWARD, MOVING_FORWARD,
+            STOP, MOVING_FORWARD, MOVING_FORWARD
+        )
+    }
+
+    @Test
+    fun `정지와 전진이 적절히 반영되어 우승자가 결정되는지 - 다수의차량`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni,hawai,jae,young", "5")
+                assertThat(output()).contains(
+                    "pobi : ----",
+                    "woni : ---",
+                    "hawai : ---",
+                    "jae : -----",
+                    "young : -",
+                    "최종 우승자 : jae"
+                )
+            },
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, STOP,
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+            STOP, STOP, STOP, MOVING_FORWARD, STOP,
+            MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP,
+            MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP,
+        )
+    }
+
+    @Test
+    fun `입력 순서대로 최종 우승자가 결정되는지`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni,hawai,jae,young", "5")
+                assertThat(output()).contains(
+                    "pobi : -----",
+                    "woni : -----",
+                    "hawai : -----",
+                    "jae : -----",
+                    "young : -----",
+                    "최종 우승자 : pobi, woni, hawai, jae, young"
+                )
+            },
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+        )
+    }
+
+    @Test
     fun `이름에 대한 예외 처리`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
