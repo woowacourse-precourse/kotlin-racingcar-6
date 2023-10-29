09148e4a461563c294db189b9ac017c6e0353487
추민수 <devsoftychoo@gmail.com>
2023-10-30 02:23:03 +0900

    [Feat] : CommonStrings
    전역에서 불러올 String값을 지정해둘 CommonStrings Object 구현

---
+++ src/main/kotlin/racingcar/util/CommonStrings.kt
@@ -0,0 +1,8 @@
+package racingcar.util
+
+object CommonStrings {
+    const val MESSAGE_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val MESSAGE_INPUT_NUMBER_OF_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
+    const val MESSAGE_EXECUTION_RESULT = "실행 결과"
+    const val MESSAGE_FINAL_WINNER = "최종 우승자"
+}
