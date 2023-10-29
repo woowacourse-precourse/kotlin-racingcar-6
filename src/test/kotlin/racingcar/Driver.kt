f1274f2f75391d6ca3eeb49f49570c6f7fa95dc9
yoon <cbj0010@naver.com>
2023-10-30 01:13:21 +0900

    [Feat] 플래이 할 게임 수 예외처리

--- src/test/kotlin/racingcar/Driver.kt
+++ src/test/kotlin/racingcar/Driver.kt
@@ -9,20 +9,22 @@ class Driver {
         carList = carName
         return carName
     }
+
     private fun checkCarNameExeption() {
         //자동차의 이름은 5자 이하만 가능, 자동차는 string만
         for (carName in carList) {
             if (carName.length !in 0..5) {
-                IllegalArgumentException(ERROR_CAR_NAME_LENGTH)
+                throw IllegalArgumentException(ERROR_CAR_NAME_LENGTH)
             }
         }
     }
 
     fun inputTryGame(): Int {
-        return Console.readLine().toInt()
+        return Console.readLine().toIntOrNull() ?: throw IllegalArgumentException(ERROR_CAR_PLAY_GAME_NULL)
     }
 
     companion object {
         const val ERROR_CAR_NAME_LENGTH = "자동차의 이름이 5자 이하가 아닙니다."
+        const val ERROR_CAR_PLAY_GAME_NULL = "null 값이 들어왔습니다."
     }
 }
