38dcb73f92bc96b628c533ac7a0dfdda59704aa7
jaehan4707 <jahan4707@naver.com>
2023-10-29 17:10:43 +0900

    feat : 승자 출력하기 추가

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -2,12 +2,18 @@ package racingcar.view
 
 import racingcar.model.Car
 import racingcar.util.GameMessage
+import java.lang.StringBuilder
 
 class OutputView {
     fun printGameStartMessage() = println(GameMessage.GAME_START_MESSAGE)
     fun printGameInputAttemptMessage() = println(GameMessage.GAME_INPUT_ATTEMPT_MESSAGE)
     fun printGameResultMessage() = println(GameMessage.GAME_RESULT_MESSAGE)
-    fun printGameWinnerMessage() = println(GameMessage.GAME_WINNER_MESSAGE)//TODO 우승자 리스트 받아서 ,로 구분 출력
+    fun printGameWinnerMessage(winners : List<Car>){//TODO 우승자 리스트 받아서 ,로 구분 출력
+        val message = StringBuilder()
+        message.append("${GameMessage.GAME_WINNER_MESSAGE} ")
+        message.append(winners.joinToString { it.name })
+        println(message.toString())
+    }
     fun printGameStatusMessage(cars : List<Car>){
         for(car in cars){
             println(car)
