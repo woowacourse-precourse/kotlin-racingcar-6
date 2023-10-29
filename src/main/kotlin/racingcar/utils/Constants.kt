493682863f7700dd4f87cecc13886925329ca4f0
seoyeon <seoyeonn051@gmail.com>
2023-10-30 03:11:07 +0900

    fix: 상수 스네이크 케이스 적용

--- src/main/kotlin/racingcar/utils/Constants.kt
+++ src/main/kotlin/racingcar/utils/Constants.kt
@@ -1,8 +1,9 @@
 package racingcar.utils
 
 object Constants {
-    const val gameStartMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    const val attemptCountQuestion = "시도할 횟수는 몇 회인가요?"
-    const val resultMessage = "실행 결과"
-    const val finalWinnerMessage = "최종 우승자 : "
+    const val GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val ATTEMPT_COUNT_QUESTION = "시도할 횟수는 몇 회인가요?"
+    const val RESULT_MESSAGE = "실행 결과"
+    const val FINAL_WINNER_MESSAGE = "최종 우승자 : "
+    const val STEP_SYMBOL = "-"
 }
