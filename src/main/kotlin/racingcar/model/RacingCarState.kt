273c325342846a90113afd396a7b7d88ebdfbb80
jun <hky08067@naver.com>
2023-10-29 10:15:58 +0900

    style: 클래스 명을 좀 더 직관적으로 변경

--- src/main/kotlin/racingcar/model/RacingCarState.kt
+++ src/main/kotlin/racingcar/model/RacingCarState.kt
@@ -1,15 +1,13 @@
 package racingcar.model
 
-import racingcar.resources.Comments.COMMA
 import racingcar.resources.Comments.FINAL_WINNER_COMMENT
 import racingcar.resources.Comments.MOVEMENT
 import racingcar.resources.Comments.SEPARATOR
-import racingcar.resources.Comments.SPACE
 import racingcar.resources.GameValue.MOVEMENT_THRESHOLD_VALUE
 import racingcar.resources.GameValue.MOVEMENT_VALUE
 import racingcar.utils.generateRandomNumbers
 
-class RacingCars(
+class RacingCarState(
     private var carList: List<Car>
 ) {
 
@@ -44,7 +42,7 @@ class RacingCars(
 
         val winners = carList.filter { car ->
             car.movement == maxMovement
-        }.map { it.name }.joinToString(COMMA + SPACE)
+        }.map { it.name }.joinToString(", ")
 
         return FINAL_WINNER_COMMENT + winners
     }
