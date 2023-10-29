e3558e7fcadd83a6b3f3c2ccd7f510f1cb2f8d60
yeonghyeon <agilecatch@gmail.com>
2023-10-30 02:02:53 +0900

    [Feat] : 자동차 이름 입력 받기
    - 자동차 이름을 입력받는 기능을 구현하였습니다.
    - 빈 값을 입력하거나, 값을 하나만 입력하거나, 5글자 초과로 입력하면 종료 되도록 하였습니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,30 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    readCarNames()
 }
+
+fun readCarNames(): List<String> {
+    while (true) {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ")
+        val input = Console.readLine()
+        val carNames = input.split(",").map { it.trim() }
+
+
+        //빈값 입력이거나, 2개 이상의 이름 받기,5글자 이하까지
+        if (input == null) {
+            throw IllegalArgumentException("입력이 없습니다.")
+        } else if (carNames.size < 2) {
+            throw IllegalArgumentException("이름은 최소 2개 이상 입력 해야 합니다.")
+        } else if (carNames.all { it.length <= 5 }) {
+            return carNames
+        } else {
+            throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.")
+        }
+
+    }
+}
+
+
