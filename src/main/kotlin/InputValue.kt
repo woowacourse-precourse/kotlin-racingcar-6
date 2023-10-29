44ce38e87a0ff838ed05775ea707a41d69ae08b8
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-29 20:41:25 +0900

    feat: 입력 받은 라운드 수 유효성 검사 기능 추가

--- src/main/kotlin/InputValue.kt
+++ src/main/kotlin/InputValue.kt
@@ -1,3 +1,6 @@
 object InputValue {
     const val CAR_DELIMITER = ","
+    const val INPUT_NOT_FIRST_VALUE = '0'
+    const val INPUT_MIN_VALUE = '0'
+    const val INPUT_MAX_VALUE = '9'
 }
--- src/main/kotlin/Validator.kt
+++ src/main/kotlin/Validator.kt
@@ -1,5 +1,9 @@
 import InputValue.CAR_DELIMITER
+import InputValue.INPUT_MAX_VALUE
+import InputValue.INPUT_MIN_VALUE
+import InputValue.INPUT_NOT_FIRST_VALUE
 import car.CarConfiguration.MAX_NAME_LENGTH
+import java.math.BigInteger
 
 object Validator {
 
@@ -9,8 +13,24 @@ object Validator {
             nameLengthCheck(it)
         }
     }
-    private fun nameLengthCheck(name: String){
+
+    fun roundCheck(input: String) {
+        if (input.first() == INPUT_NOT_FIRST_VALUE) throw IllegalArgumentException()
+        input.forEach {
+            if (it !in INPUT_MIN_VALUE..INPUT_MAX_VALUE) throw IllegalArgumentException()
+        }
+        castingCheck(input)
+    }
+
+    private fun nameLengthCheck(name: String) {
         if (name.length > MAX_NAME_LENGTH) throw IllegalArgumentException()
     }
 
+    private fun castingCheck(input: String) {
+        try {
+            BigInteger(input)
+        } catch (e: Exception) {
+            throw IllegalArgumentException()
+        }
+    }
 }
