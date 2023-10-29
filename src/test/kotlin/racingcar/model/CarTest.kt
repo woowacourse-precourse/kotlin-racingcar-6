b1909b8cb17591441bb9150a752afdf921caa153
GreenRain <47217640+greenrain78@users.noreply.github.com>
2023-10-30 05:22:22 +0900

    test: 테스트 코드도 경기 상태를 RacingBoard에서 담당하도록 구성
    - 경기에 관한 모든 메세지 생성을 RacingBoard에서 담당하도록 구성

--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -23,12 +23,4 @@ class CarTest {
     fun `Car 의 처음 위치는 0 입니다`() {
         assertThat(Car("test").pos).isEqualTo(0)
     }
-
-    @Test
-    fun `Car는 요구사항에 맞게 문자열로 변환해야 합니다`() {
-        assertThat(Car("test").toString()).isEqualTo("test : ")
-        val car3 = Car("test")
-        car3.pos = 3
-        assertThat(car3.toString()).isEqualTo("test : ---")
-    }
 }
--- src/test/kotlin/racingcar/model/RacingBoardTest.kt
+++ src/test/kotlin/racingcar/model/RacingBoardTest.kt
@@ -1,6 +1,6 @@
 package racingcar.model
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -13,26 +13,30 @@ class RacingBoardTest {
     }
 
     @Test
-    fun `getResult로 우승자만 가져옵니다`() {
+    fun `getResult 우승자만 가져옵니다`() {
         val winner = Car("win")
         winner.pos = 3
         val racerList = listOf(Car("loser"), winner)
         RacingBoard(racerList).getResult()
-        Assertions.assertThat(RacingBoard(racerList).getResult()).isEqualTo(listOf(winner))
+        assertThat(RacingBoard(racerList).getResult()).isEqualTo(listOf(winner))
     }
 
     @Test
-    fun `getResult는 우승자가 여러명이어도 가능합니다`() {
+    fun `getResult 우승자가 여러명이어도 가능합니다`() {
         val winner = Car("win")
         winner.pos = 3
         val racerList = listOf(Car("loser"), winner, winner)
         RacingBoard(racerList).getResult()
-        Assertions.assertThat(RacingBoard(racerList).getResult()).isEqualTo(listOf(winner, winner))
+        assertThat(RacingBoard(racerList).getResult()).isEqualTo(listOf(winner, winner))
     }
 
     @Test
-    fun `문자열로 출력 확인합니다`() {
-        val racerList = listOf(Car("1"), Car("2"), Car("3"))
-        Assertions.assertThat(RacingBoard(racerList).toString()).isEqualTo(racerList.joinToString("\n"))
+    fun `getPlayStatus 현재 경기 상태를 출력`() {
+        val winner = Car("win")
+        winner.pos = 3
+        val racerList = listOf(Car("loser"), winner)
+
+        val status = RacingBoard(racerList).getPlayStatus()
+        assertThat(status).isEqualTo("loser : \n" + "win : ---")
     }
 }
