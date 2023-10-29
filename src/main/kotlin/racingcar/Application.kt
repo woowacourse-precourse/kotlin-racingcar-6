36c057d5caa0dcf3c7c7288b46c24bf913e9cd7f
dpcks0509 <dpcks0509@gmail.com>
2023-10-29 23:43:16 +0900

    style: 미사용 import 제거

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,12 +1,8 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
 import racingcar.controller.RacingCarController
-import racingcar.model.RacingCar
 import racingcar.view.InputView
 import racingcar.view.OutputView
-import java.lang.NumberFormatException
 
 fun main() {
     val racingCarController = RacingCarController(InputView(), OutputView())
