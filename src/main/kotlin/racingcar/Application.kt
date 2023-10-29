aa2f5ceb88af7d8ff07134d882a503652d70a8b3
leedaeul <ouowinnie@gmail.com>
2023-10-29 11:12:20 +0900

    [FEAT] : 경주할 자동차를 쉼표(,) 구분으로 배열에 저장하고, 조건에 불일치 시 예외처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,31 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    runGame()
+}
+
+const val CAR_NAME_LENGTH = 5
+fun runGame() {
+    val setCars: MutableList<String> = mutableListOf()
+
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    try {
+        val carName = getCarNames()
+        val carNameList = carName.split(",").map { it.trim() }
+        for (car in carNameList) {
+            if (car.length <= CAR_NAME_LENGTH) {
+                setCars.add(car)
+            } else {
+                throw IllegalArgumentException("자동차 이름은 5자 이하로 설정해주세요.")
+            }
+        }
+        println(setCars)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+}
+fun getCarNames(): String {
+    return Console.readLine() ?: ""
 }
