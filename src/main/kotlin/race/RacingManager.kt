dfddfe0b391cdb1c77ed10a8457e3b2d51765dc8
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-29 21:12:43 +0900

    feat: 레이싱 진행 라운드 설정 기능 추가

--- src/main/kotlin/race/RacingManager.kt
+++ src/main/kotlin/race/RacingManager.kt
@@ -1,10 +1,15 @@
 package race
 
 import Controller.inputCarInformation
+import Controller.inputRacingRoundInformation
+
 class RacingManager {
     private val racing = Racing()
     fun settingRacingCar() {
         racing.settingRacingCar(inputCarInformation())
     }
 
+    fun settingRacingRound() {
+        racing.settingRacingRound(inputRacingRoundInformation())
+    }
 }
