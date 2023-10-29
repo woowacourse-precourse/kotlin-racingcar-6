1789f9d9158d704dcd3c8bf44f6f7b365aabb8a4
yujin37 <yj061628@gmail.com>
2023-10-29 12:57:34 +0900

    feat(자동차 경주 게임): 경주 과정 구조 설계
    - 랜덤 숫자 뽑는 함수 구현
    - 해당 숫자가 4 미만인지 이상인지 체크 함수 구현
    - 체크한 결과에 따라 해당 회차 이동 함수
    처리
    - 전체 결과에 해당 회차 이동 업데이트 함수 구현

--- src/main/kotlin/racingcar/RacingGames.kt
+++ src/main/kotlin/racingcar/RacingGames.kt
@@ -1,8 +1,47 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class RacingGames {
+    //움직이는지 체크
+    fun racing(): Boolean {
+        val num = Randoms.pickNumberInRange(0, 9)
+        if(num>= 4){
+            return true
+        }
+        else{
+            return false
+        }
+
+    }
+    //차 움직임 체크
+    fun checkCars(cars:List<String>): MutableList<String> {
+        val check = MutableList(cars.size) {""}
+
+        for(i in 0 until cars.size){
+            val move = racing()
+            if(move) check[i] += "-"
+        }
+
+        return check
+    }
+    fun moveCars(race:MutableList<String>,updates:List<String>): MutableList<String> {
+
+        for(i in 0 until race.size){
+            race[i] = race[i] + updates[i]
+        }
+
+
+        return race
+    }
+    //횟수 체크
     fun checkTime(racingCars: List<String>, times: Int){
-        println(racingCars)
-        println(times)
+        var race = MutableList(racingCars.size) { "" }
+        for(i in 0 until times){
+            val updates = checkCars(racingCars)
+            race = moveCars(race, updates)
+            println(race)
+        }
+
     }
 }
