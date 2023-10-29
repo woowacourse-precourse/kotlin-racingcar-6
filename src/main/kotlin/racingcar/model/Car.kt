472dfaacb39a752b19cdc90ae455dc3569a6e360
sh1mj1 <7wlgns@gmail.com>
2023-10-29 17:40:45 +0900

    refactor(Car): modify `validateCar` method using extension method
    * 확장 함수를 사용하면 패러미터를 감춰서 더 높은 캡슐화가 가능하다.
    * 이전: 패러미터로 String 을 받고 패러미터에 대해 유효성을 검증한다.
    * 이후: 확장 함수로 변경하여 패러미터를 제거하고 수신 객체의
    프로퍼티를 사용하여 유효성을 검증한다.

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -9,15 +9,21 @@ data class Car(
             position++
         }
     }
+
     private fun isMove(): Boolean {
         // TODO: NumberGenerator 에게 메시지 보내기
         return true
     }
-    private fun validateCar(name: String): Boolean {
-        return when{
-            name.isBlank() -> {throw IllegalArgumentException("올바른 이름을 입력해주세요")}
-            name.length > 5 -> {throw IllegalArgumentException("이름은 5자 이하까지 입력 가능합니다.")}
-            else -> true
+}
+
+internal fun Car.validateCar() {
+    when {
+        name.isBlank() -> {
+            throw IllegalArgumentException("올바른 이름을 입력해주세요")
+        }
+
+        name.length > 5 -> {
+            throw IllegalArgumentException("이름은 5자 이하까지 입력 가능합니다.")
         }
     }
 }
