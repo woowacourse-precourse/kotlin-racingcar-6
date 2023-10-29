88f969bdfdf622f6e4899060e7821b4ee5823cdc
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-29 20:49:24 +0900

    refactor: 주행거리 자료형 변경 Int -> BigInteger

--- src/main/kotlin/caroption/DashBoard.kt
+++ src/main/kotlin/caroption/DashBoard.kt
@@ -2,17 +2,19 @@ package caroption
 
 import car.CarConfiguration.DIST_SYMBOL
 import car.CarConfiguration.INIT_DIST
+import java.math.BigInteger
 
 class DashBoard {
-    private var dist = INIT_DIST
+    private var dist = BigInteger.valueOf(INIT_DIST)
     fun distPlus() {
         dist++
     }
 
     fun nowDist() = dist
 
-    fun printDist(dist: Int) {
-        repeat(dist) {
+    fun printDist(dist: BigInteger) {
+        var count = BigInteger.ZERO
+        while(count < dist) {
             print(DIST_SYMBOL)
         }
     }
