a80fe3f42292a827256fc0803253c984597c9296
jaehan4707 <jahan4707@naver.com>
2023-10-29 16:43:14 +0900

    feat : Car 추가

---
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -0,0 +1,22 @@
+package racingcar.model
+
+import java.lang.StringBuilder
+
+data class Car(private val name : String){
+
+    private var _move = Move(position =0)
+    val move get() = _move
+
+    override fun toString(): String {
+        val str = StringBuilder()
+        str.append("$name : ")
+        repeat(move.position) {
+            str.append("-")
+        }
+        return str.toString()
+    }
+
+    fun attemptMove(condition : Int){
+        _move.checkMove(condition)
+    }
+}
