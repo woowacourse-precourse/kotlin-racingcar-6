59edb7d48ba0869965d6c24af0c9082c71b77f10
jaejunlee11 <sktks11@naver.com>
2023-10-29 21:37:30 +0900

    포장을 위해 carName클래스 기능 구현

--- src/main/kotlin/racingcar/CarName.kt
+++ src/main/kotlin/racingcar/CarName.kt
@@ -1,12 +1,17 @@
 package racingcar
 
 class CarName (name:String){
-    var name:String
+    private var name:String
     init {
         this.name=name
+        checkName()
     }
-    fun checkName(){
-
+    private fun checkName(){
+        if(name.length>5){
+            throw IllegalArgumentException("자동차 이름을 5글자 이하로 설정해 주세요.")
+        }else if(name.length<=0){
+            throw IllegalArgumentException("자동차 이름을 작성해 주세요.")
+        }
     }
     fun amount():String{
         return this.name
