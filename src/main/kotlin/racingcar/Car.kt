0d62bfca46b1d59df113d6be257a8215be763bf8
이용인 <yongin0517@gmail.com>
2023-10-29 20:48:25 +0900

    feat: 자동차 이동거리 set,show 메소드 추가

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,7 +1,9 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car {
-    private lateinit var name:String
+    private var name:String
     private var distance = 0
 
     constructor(name:String){
@@ -10,5 +12,20 @@ class Car {
 
     fun getName():String{
         return name
+    }
+
+    fun moveForward() {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        if (randomNumber >= 4) {
+            distance += 1
+        }
+    }
+
+    fun showDistanceWithName() {
+        print("${name} : ")
+        for (i in 1..distance){
+            print("-")
+        }
+        println()
     }
 }
