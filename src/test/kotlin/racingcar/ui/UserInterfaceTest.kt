12af2488bc980b3bbf80ed3ada2f8e282143f7f1
book3039 <book3039@knou.ac.kr>
2023-10-29 22:15:46 +0900

    test: 사용자 입력을 쉼표로 구분하고, 앞 뒤 공백을 제거한 리스트 생성 테스트 코드 작성

---
+++ src/test/kotlin/racingcar/ui/UserInterfaceTest.kt
@@ -0,0 +1,17 @@
+package racingcar.ui
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import racingcar.ui.UserInput.createNameList
+
+class UserInterfaceTest {
+    @Test
+    fun `입력을 쉼표로 구분하고, 앞 뒤 공백을 제거하여 리스트를 생성한다`() {
+        val input = "  choi  , gerrard  ,  mint  "
+        val names = listOf("choi", "gerrard", "mint")
+        val inputNames = input.createNameList()
+
+        assertEquals(names, inputNames)
+
+    }
+}
