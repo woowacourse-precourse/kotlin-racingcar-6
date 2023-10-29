c2d0b1518932cd1a49e5f0afa9f3746c50cf957f
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-10-29 15:01:32 +0900

    feat(move): implement move function in RacingPresenter

--- src/main/kotlin/racingcar/RacingPresenter.kt
+++ src/main/kotlin/racingcar/RacingPresenter.kt
@@ -1,9 +1,11 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class RacingPresenter {
 
     private val _racingCars = mutableListOf<RacingCar>()
-    private val racingCars: List<RacingCar> get() = _racingCars
+    val racingCars: List<RacingCar> get() = _racingCars
 
     fun createRacingCarsOrThrow(rawInput: String) {
         val names = rawInput.split(",")
@@ -11,5 +13,23 @@ class RacingPresenter {
             val car = RacingCar.createOrThrow(it)
             _racingCars.add(car)
         }
+    }
+
+    fun moveRacingCars() {
+        racingCars.forEach { car ->
+            val random = Randoms.pickNumberInRange(
+                MOVING_RANDOM_RANGE_START,
+                MOVING_RANDOM_RANGE_END
+            )
+            if (random >= MOVING_THRESHOLD) {
+                car.move()
+            }
+        }
+    }
+
+    companion object {
+        private const val MOVING_RANDOM_RANGE_START = 0
+        private const val MOVING_RANDOM_RANGE_END = 9
+        private const val MOVING_THRESHOLD = 4
     }
 }
--- src/test/kotlin/racingcar/RacingPresenterTest.kt
+++ src/test/kotlin/racingcar/RacingPresenterTest.kt
@@ -1,5 +1,7 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -31,5 +33,27 @@ class RacingPresenterTest {
         assertThrows<IllegalArgumentException> {
             presenter.createRacingCarsOrThrow(input)
         }
+    }
+
+    @Test
+    fun `레이싱 카들이 랜덤 값에 따라 움직이거나 움직이지 않음`() {
+        val presenter = RacingPresenter()
+        val input = "pobi,hello"
+        presenter.createRacingCarsOrThrow(input)
+
+        assertRandomNumberInRangeTest(
+            {
+                presenter.moveRacingCars()
+                val cars = presenter.racingCars
+                val movingCounts = cars.map { it.movingCount }
+                Assertions.assertThat(movingCounts).contains(1, 0)
+            },
+            MOVING_FORWARD, STOP
+        )
+    }
+
+    companion object {
+        private const val MOVING_FORWARD = 4
+        private const val STOP = 3
     }
 }
