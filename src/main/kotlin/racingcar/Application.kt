a22dba75a230d51f93da6ed753d4f617f7e483e5
leedaeul <ouowinnie@gmail.com>
2023-10-29 14:49:05 +0900

    [REFACTOR] : 기존 runGame 함수 이름 변경하고, 클래스 분리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,47 +1,6 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
-
 fun main() {
-    runGame()
-}
-
-const val CAR_NAME_LENGTH = 5
-fun runGame() {
-
-    val carName = getCarNames()
-    val tryNumber = getTryNumbers()
-
-}
-fun getCarNames(): List<String> {
-    val carNameList: MutableList<String> = mutableListOf()
-
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    return try {
-        val carName = Console.readLine() ?: ""
-        val carNames = carName.split(",").map { it.trim() }
-        for (car in carNames) {
-            if (car.length <= CAR_NAME_LENGTH) {
-                carNameList.add(car)
-            } else {
-                throw IllegalArgumentException("자동차 이름은 5자 이하로 설정해주세요.")
-            }
-        }
-        carNameList
-    } catch (e: IllegalArgumentException) {
-        throw IllegalArgumentException("잘못된 입력입니다 : ${e.message}")
-    }
-}
-
-fun getTryNumbers(): Int {
-    println("시도할 횟수는 몇 회인가요?")
-    return try {
-        val tryNumber = Console.readLine()?.toInt() ?: 0
-        if (tryNumber <= 0) {
-            throw IllegalArgumentException("양의 정수를 입력해주세요.")
-        }
-        tryNumber
-    } catch (e: IllegalArgumentException) {
-        throw IllegalArgumentException("잘못된 입력입니다 : ${e.message}")
-    }
+    val carGame = CarGame()
+    carGame.run()
 }
