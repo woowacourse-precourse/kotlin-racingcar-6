9e8432e22b6d6efe89d20512d3286563ef33a4c8
jaehan4707 <jahan4707@naver.com>
2023-10-29 16:43:43 +0900

    feat : printGameStatusMessage 추가

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,5 +1,6 @@
 package racingcar.view
 
+import racingcar.model.Car
 import racingcar.util.GameMessage
 
 class OutputView {
@@ -7,4 +8,10 @@ class OutputView {
     fun printGameInputAttemptMessage() = println(GameMessage.GAME_INPUT_ATTEMPT_MESSAGE)
     fun printGameResultMessage() = println(GameMessage.GAME_RESULT_MESSAGE)
     fun printGameWinnerMessage() = println(GameMessage.GAME_WINNER_MESSAGE)//TODO 우승자 리스트 받아서 ,로 구분 출력
+    fun printGameStatusMessage(cars : List<Car>){
+        for(car in cars){
+            println(car)
+        }
+        println()
+    }
 }
