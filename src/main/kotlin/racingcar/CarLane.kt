272082d9e8ff8a5190fccd3838f379405484037e
이용인 <yongin0517@gmail.com>
2023-10-30 00:01:40 +0900

    feat: 자동차 레인 클래스 생성
    -자동차 get
    - 자동차들의 이동거리를 비교하여 우승자를 판단한다.
    - 1개의 차수마다 모든 자동차를 랜덤하게 앞으로 이동시킨다.
    - 1개의 차수마다 모든 자동차에게 랜덤하게 이동한 거리를 요청한다.
    - 가장 멀리 간 이동거리를 도출한다.

---
+++ src/main/kotlin/racingcar/CarLane.kt
@@ -0,0 +1,39 @@
+package racingcar
+
+class CarLane {
+    private var cars:MutableList<Car> = arrayListOf()
+    constructor(carNames:List<String>){
+        carNames.forEach{
+            var car = Car(it)
+            cars.add(car)
+        }
+    }
+
+    fun getCars():List<Car>{
+        return cars
+    }
+
+    fun runCars(){
+        cars.forEach{
+            it.moveForward()
+        }
+        println()
+    }
+
+    fun showLap() {
+        cars.forEach {
+            it.showDistanceWithName()
+        }
+    }
+
+    fun judgeWinner(): List<String>{
+        var maxDistance = getMaxDistance()
+        return cars.filter { it.getDistance() == maxDistance }.map { it.getName() }
+    }
+
+    fun getMaxDistance():Int{
+        var winnerCar = cars.maxBy{ it.getDistance() }
+        return winnerCar!!.getDistance()
+    }
+
+}
