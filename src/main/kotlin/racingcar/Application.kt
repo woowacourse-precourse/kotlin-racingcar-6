a9b23a78fb17e44fa578a45eeade723f1c425fee
yujin37 <yj061628@gmail.com>
2023-10-29 11:39:05 +0900

    feat(자동차 경주 게임): 경주 할 자동차 이름 입력
    - 경주 할 자동차 이름 배열에 담음
    - 자동차 이름 조건 만족 여부 확인

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
+class RacingCarValidator {
+    fun validateCarName(checkName: String) {
+        if(checkName.isEmpty() || checkName.length > 5) {
+            throw IllegalArgumentException()
+        }
+    }
+}
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+    val cars = Console.readLine()
+    val racingCars = cars.split(',')
+    for(racingCar in racingCars){
+        RacingCarValidator().validateCarName(racingCar)
+    }
+    println(racingCars)
+
 }
