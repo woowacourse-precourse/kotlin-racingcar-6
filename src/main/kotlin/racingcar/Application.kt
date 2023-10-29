c49f4cc3c3315b40a436ce9d180296dc13d62fae
Choi Seho <kindseho@naver.com>
2023-10-29 16:42:15 +0900

    feat: 게임 시작을 위한 자동차 이름 입력 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNames = Console.readLine().split(',')
+    println("$carNames")
+
+    for(car in carNames){
+        if(car.length > 5) throw IllegalArgumentException("이름은 5자 이하만 가능합니다")
+    }
 }
