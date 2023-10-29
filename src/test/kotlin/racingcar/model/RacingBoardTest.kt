1ce1c913b40656b9b81a0e1c739f4d453668b967
GreenRain <47217640+greenrain78@users.noreply.github.com>
2023-10-30 05:57:06 +0900

    test: 랜덤하게 전진하는 Car 객체에 대한 테스트 코드 추가
    - Car move 함수로 이동시 pos가 정상적으로 이동 되는지 확인
    - RacingBoard play함수로 경기를 진행시 각 Car객체가 정상적으로 이동이
    되었는지 확인

--- src/test/kotlin/racingcar/model/RacingBoardTest.kt
+++ src/test/kotlin/racingcar/model/RacingBoardTest.kt
@@ -1,10 +1,16 @@
 package racingcar.model
 
+import camp.nextstep.edu.missionutils.test.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class RacingBoardTest {
+    companion object {
+        private const val MOVING_FORWARD = 4
+        private const val STOP = 3
+    }
+
     @Test
     fun `racerList 가 비어있으면 안됩니다`() {
         assertThrows<IllegalArgumentException> {
@@ -38,5 +44,19 @@ class RacingBoardTest {
 
         val status = RacingBoard(racerList).getPlayStatus()
         assertThat(status).isEqualTo("loser : \n" + "win : ---")
+    }
+
+    @Test
+    fun `play 경기를 진행합니다`() {
+        val racerList = listOf(Car("111"), Car("222"), Car("333"))
+        val board = RacingBoard(racerList)
+
+        Assertions.assertRandomNumberInRangeTest(
+            {
+                board.play()
+                assertThat(racerList.map { car: Car -> car.pos }).isEqualTo(listOf(1, 0, 1))
+            },
+            MOVING_FORWARD, STOP, MOVING_FORWARD
+        )
     }
 }
