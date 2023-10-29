387d06ef8ba03e44ef9ba551a763550279df7b62
stopstone <wltjr6432@naver.com>
2023-10-30 00:41:55 +0900

    refactor: 불필요한 매개변수 제거

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,11 +1,10 @@
 package racingcar
 
 import racingcar.controller.RacingCarController
-import racingcar.domain.Cars
 import racingcar.view.InputView
 import racingcar.view.OutputView
 
 fun main() {
-    val racingCarController = RacingCarController(InputView(), OutputView(Cars("")))
+    val racingCarController = RacingCarController(InputView(), OutputView())
     racingCarController.racingRun()
 }
