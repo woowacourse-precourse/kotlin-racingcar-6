f28403fb307326b22d14fa055749a0b36901115f
junyong <junyong008@gmail.com>
2023-10-29 15:46:50 +0900

    feat: 경주할 자동차 목록 입력 기능 추가

---
+++ src/main/kotlin/racingcar/InputView.kt
@@ -0,0 +1,54 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+object InputView {
+
+    private const val INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    private const val MIN_CAR_NAME_LENGTH = 1
+    private const val MAX_CAR_NAME_LENGTH = 5
+
+    private const val ERROR_CAR_NAME_EMPTY = "자동차의 이름 중 비어있는 이름이 있습니다."
+    private const val ERROR_CAR_NAME_LENGTH = "자동차의 이름은 1자 이상, 5자 이하의 길이로 입력할 수 있습니다. %s"
+    private const val ERROR_CAR_NAME_DUPLICATE = "중복된 이름이 있습니다. %s"
+    private const val ERROR_CAR_NAME_BLANK = "이름에 공백이 포함돼서는 안됩니다. %s"
+
+    fun inputCarNames(): List<String> {
+        println(INPUT_CAR_NAMES)
+        return validateInputCarNames(Console.readLine())
+    }
+
+    fun validateInputCarNames(input: String): List<String> {
+        val inputCarNames = input.split(",").map { it.trim() }
+        checkCarNamesNull(inputCarNames)
+        checkCarNamesLength(inputCarNames)
+        checkCarNamesDuplicate(inputCarNames)
+        checkCarNamesContainBlank(inputCarNames)
+        return inputCarNames
+    }
+
+    private fun checkCarNamesNull(carNames: List<String>) {
+        carNames.forEach {
+            if (it.isBlank()) throw IllegalArgumentException(ERROR_CAR_NAME_EMPTY)
+        }
+    }
+
+    private fun checkCarNamesLength(carNames: List<String>) {
+        carNames.forEach {
+            if (it.length < MIN_CAR_NAME_LENGTH || it.length > MAX_CAR_NAME_LENGTH)
+                throw IllegalArgumentException(ERROR_CAR_NAME_LENGTH.format(it))
+        }
+    }
+
+    private fun checkCarNamesDuplicate(carNames: List<String>) {
+        val duplicatedCarNames = carNames.groupingBy { it }.eachCount().filter { it.value > 1 }.keys.toList()
+        if (duplicatedCarNames.isNotEmpty())
+            throw IllegalArgumentException(ERROR_CAR_NAME_DUPLICATE.format(duplicatedCarNames))
+    }
+
+    private fun checkCarNamesContainBlank(carNames: List<String>) {
+        val blankContainedCarNames = carNames.filter { it.contains(" ") }
+        if (blankContainedCarNames.isNotEmpty())
+            throw IllegalArgumentException(ERROR_CAR_NAME_BLANK.format(blankContainedCarNames))
+    }
+}
--- src/test/kotlin/racingcar/function/InputTest.kt
+++ src/test/kotlin/racingcar/function/InputTest.kt
@@ -4,35 +4,36 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.Test
+import racingcar.InputView.validateInputCarNames
 
 class InputTest {
     @Test
     fun `자동차 목록 입력 - 정상 입력`() {
         val input = "jun,pobi,wow"
-        val result = inputCarNames(input)
+        val result = validateInputCarNames(input)
         assertEquals(result, listOf("jun", "pobi", "wow"))
     }
 
     @Test
     fun `자동차 목록 입력 - 빈 값 예외 처리`() {
-        assertThrows<IllegalArgumentException> { inputCarNames("") }
+        assertThrows<IllegalArgumentException> { validateInputCarNames("") }
     }
 
     @Test
     fun `자동차 목록 입력 - 이름 길이 예외 처리`() {
-        val exception = assertThrows<IllegalArgumentException> { inputCarNames("hi,wow,junyong,name") }
+        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames("hi,wow,junyong,name") }
         assertThat(exception.message).contains("junyong")
     }
 
     @Test
     fun `자동차 목록 입력 - 중복 이름 예외 처리`() {
-        val exception = assertThrows<IllegalArgumentException> { inputCarNames("hi,wow,nice,wow") }
+        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames("hi,wow,nice,wow") }
         assertThat(exception.message).contains("wow")
     }
 
     @Test
     fun `자동차 목록 입력 - 공백 포함 이름 예외 처리`() {
-        val exception = assertThrows<IllegalArgumentException> { inputCarNames("hi,wow,j y") }
+        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames("hi,wow,j y") }
         assertThat(exception.message).contains("j y")
     }
 }
