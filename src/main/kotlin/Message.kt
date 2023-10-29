41085e67ba7cc377936cc4dea2bd6a423b0910f1
oh in seong <superohinsung@naver.com>
2023-10-30 06:26:50 +0900

    [feat]: Printer class애 게임에서 요구하는 자동차 이름 입력 문구 프린트하는 기능 추가

--- src/main/kotlin/Message.kt
+++ src/main/kotlin/Message.kt
@@ -1,3 +1,3 @@
 object Message {
-
+    const val ENTERING_CAR_NAME : String = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 }
--- src/main/kotlin/Printer.kt
+++ src/main/kotlin/Printer.kt
@@ -1,3 +1,5 @@
 class Printer {
 
+    fun printOutEnteringCarName() = println(Message.ENTERING_CAR_NAME)
+
 }
