d3792bf9f1a35000f2d9004760dc5e97951e9872
sh1mj1 <7wlgns@gmail.com>
2023-10-29 15:35:38 +0900

    feat(InputTry): implement feature that input tryCount
    * 숫자가 아닌 문자열, 0 이하의 숫자,
    Int 형의 최대값보다 큰 숫자를 입력하면 예외를 던진다.
    * Int 형의 자연수를 입력하면 정수형으로 반환한다

--- src/main/kotlin/racingcar/view/InputTry.kt
+++ src/main/kotlin/racingcar/view/InputTry.kt
@@ -1,7 +1,16 @@
 package racingcar.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputTry {
-    operator fun invoke(tryCount: String): Int {
-        return 0
+    operator fun invoke(tryCount: String = Console.readLine()): Int {
+        val count = tryCount.toIntOrNull()
+        return when {
+            count == null || count <= 0 -> {
+                throw IllegalArgumentException("${Integer.MAX_VALUE} 이하의 자연수를 입력해주세요.")
+            }
+
+            else -> count
+        }
     }
 }
