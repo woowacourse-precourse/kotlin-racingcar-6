147d7e53645a3fb97d4eab5ab5e4dc8dff1eba4c
book3039 <book3039@knou.ac.kr>
2023-10-29 20:57:22 +0900

    style: 오타 수정

--- src/main/kotlin/racingcar/ui/UserInterface.kt
+++ src/main/kotlin/racingcar/ui/UserInterface.kt
@@ -3,15 +3,15 @@ package racingcar.ui
 import camp.nextstep.edu.missionutils.Console
 
 const val MSG_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-const val MSG_INPUT_ATTEMPT_COUNT = "시도할 횟수는 몇 회인가요?"
+const val MSG_INPUT_ATTEMPT_COUNTS = "시도할 횟수는 몇 회인가요?"
 
 object UserInput {
     fun readCarName(): String {
         println(MSG_INPUT_CAR_NAME)
         return Console.readLine()
     }
-    fun readAttemptCount(): String {
-        println(MSG_INPUT_ATTEMPT_COUNT)
+    fun readAttemptCounts(): String {
+        println(MSG_INPUT_ATTEMPT_COUNTS)
         return Console.readLine()
     }
 
