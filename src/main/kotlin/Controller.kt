365c8da980e874b48c42707c4f1fdbada71e99a5
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-29 17:59:27 +0900

    feat: 레이싱을 진행할 라운드 입력 기능 추가

--- src/main/kotlin/Controller.kt
+++ src/main/kotlin/Controller.kt
@@ -1,14 +1,23 @@
 import InputValue.CAR_DELIMITER
 import Validator.nameCheck
+import Validator.roundCheck
 import camp.nextstep.edu.missionutils.Console
 import race.RacingMessage.CAR_INPUT_MESSAGE
+import race.RacingMessage.TRIES_REQUEST_MESSAGE
 
 object Controller {
     fun inputCarInformation(): List<String> {
         println(CAR_INPUT_MESSAGE)
-        val inputValue = Console.readLine()
-        nameCheck(inputValue)
-        return inputValue.split(CAR_DELIMITER)
+        val input = Console.readLine()
+        nameCheck(input)
+        return input.split(CAR_DELIMITER)
+    }
+
+    fun inputRacingRoundInformation(): Int {
+        println(TRIES_REQUEST_MESSAGE)
+        val input = Console.readLine()
+        roundCheck(input)
+        return input.toInt()
     }
 
 }
