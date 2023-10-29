da407341517bcd399b7da80d04f29285928646b5
hugesilver <xor3160@naver.com>
2023-10-29 19:53:15 +0900

    update check name feature

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,4 +7,16 @@ val CAR_NAME_LIST = mutableListOf<String>()
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carsName = Console.readLine()
+    val names = carsName.split(',')
+    for (name in names) {
+        checkName(name)
+    }
+}
+
+fun checkName(name: String) {
+    if (name.length > 4) {
+        throw IllegalArgumentException()
+    } else {
+        CAR_NAME_LIST.add(name)
+    }
 }
