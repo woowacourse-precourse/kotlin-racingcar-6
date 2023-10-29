cec63bed6c2954ec59c2fda0c53d0c40a69eaeba
sungsik-git <kss950708@gamil.com>
2023-10-29 11:13:43 +0900

    feat: 랜덤숫자 생성 기능 구현

--- src/main/kotlin/Logic/Logic.kt
+++ src/main/kotlin/Logic/Logic.kt
@@ -1,4 +1,10 @@
 package Logic
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Logic {
+    fun makeRandomNumber(){
+        var moveNumber = Randoms.pickNumberInRange(0,9)
+        println(moveNumber)
+    }
 }
--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,13 +1,18 @@
 package racingcar
 
+import Logic.Logic
 import User.User
 
 fun main() {
     // TODO: 프로그램 구현
     val user = User()
+    val logic = Logic()
+
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     user.inputCarName()
     println("시도할 횟수는 몇 회인가요?")
     var cycleNumber = user.inputCycleNumber()
-
+    for(i in 0..cycleNumber-1){
+        logic.makeRandomNumber()
+    }
 }
