f7d197c1d6ccf1abf7c7a743fb5381462cac2fc4
getupminaaa <getupmina@gmail.com>
2023-10-29 11:39:50 +0900

    feat: 몇 번의 이동(시도)를 할 지 입력받는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,8 +4,10 @@ import camp.nextstep.edu.missionutils.Console
 
 private const val CAR_NAME_MAX_LENGTH = 5
 private const val RACE_START_MSG = "경주할 자동차 이름을 입력하세요."
+private const val  NUMBER_ATTEMPTS_REGEX = "[1-9]"
 fun main() {
     println(RACE_START_MSG)
+    /*
     val cars = getCarList()
 
     if (!isCarNameUnique(cars)) throw IllegalArgumentException("Duplicate car names are not allowed.")
@@ -13,10 +15,15 @@ fun main() {
         if (!isCarNameNotEmpty(car)) throw IllegalArgumentException("Car name cannot be empty.")
         if (!isCarNameLengthValid(car)) throw IllegalArgumentException("Car name '$car' exceeds the maximum allowed length of $CAR_NAME_MAX_LENGTH.")
     }
+     */
+    val numberAttempts = getNumberAttempts()
 
 }
 
 fun getCarList() = Console.readLine().split(",")
-fun isCarNameUnique(carList: List<String>): Boolean = carList.size == carList.toSet().size
-fun isCarNameLengthValid(carName: String): Boolean = carName.length <= CAR_NAME_MAX_LENGTH
-fun isCarNameNotEmpty(carName: String): Boolean = carName.trim().isNotEmpty()
+fun isCarNameUnique(carList: List<String>) = carList.size == carList.toSet().size
+fun isCarNameLengthValid(carName: String) = carName.length <= CAR_NAME_MAX_LENGTH
+fun isCarNameNotEmpty(carName: String) = carName.trim().isNotEmpty()
+
+fun getNumberAttempts(): String = Console.readLine().trim()
+
