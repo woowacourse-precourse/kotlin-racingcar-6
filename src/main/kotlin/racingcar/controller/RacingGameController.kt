c533cd933e5c3b6047afa358da2435ae9ddc8091
jujeong <jujeong0108@naver.com>
2023-10-30 05:53:59 +0900

    Added checkPlayCountNatural on RacingGameController.kt Fixed minor issues

--- src/main/kotlin/racingcar/controller/RacingGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingGameController.kt
@@ -5,21 +5,31 @@ import racingcar.view.RacingGameView
 import racingcar.model.RacingGameModel
 import racingcar.utils.ErrorMessage
 import racingcar.utils.Values
+import java.lang.NumberFormatException
+import kotlin.properties.Delegates
 
 class RacingGameController(private val view: RacingGameView, private val model: RacingGameModel) {
-    lateinit var carList: List<String>
+    private lateinit var carList: List<String>
+    private var playCount by Delegates.notNull<Int>()
     fun run() {
         view.requestCarNameInput()
-        if (isAnyCarNameLengthExceeded(carList = getCarList())) {
+        if (isAnyCarNameLengthExceeded(carList = setCarList())) {
             throw IllegalArgumentException("${ErrorMessage.ERRORMESSAGE_CAR_NAME_LENGTH_EXCEEDED} ${Values.MAXIMUM_CAR_NAME_LENGTH}")
         }
         view.requestPlayCountInput()
+        try {
+            checkPlayCountNatural(playCount = setPlayCount().toInt())
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(ErrorMessage.ERRORMESSAGE_PLAY_COUNT_NOT_NATURAL)
+        }
     }
     @JvmName("callFromString")
-    private fun getCarList(): List<String> {
+    private fun setCarList(): List<String> {
         return listOf(*Console.readLine().split(",").toTypedArray<String>())
     }
-
+    private fun setPlayCount(): String {
+        return Console.readLine()
+    }
     private fun isAnyCarNameLengthExceeded(carList: List<String>): Boolean {
         for (i in carList) {
             if (i.length > Values.MAXIMUM_CAR_NAME_LENGTH) {
@@ -27,5 +37,10 @@ class RacingGameController(private val view: RacingGameView, private val model: 
             }
         }
         return false
+    }
+    private fun checkPlayCountNatural(playCount: Int) {
+        if(playCount > 0) {
+            throw IllegalArgumentException(ErrorMessage.ERRORMESSAGE_PLAY_COUNT_NOT_NATURAL)
+        }
     }
 }
