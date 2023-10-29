4fb8da18357e3970744e6d87e8cca217f0c85a92
seonghyun <skyladder8895@gmail.com>
2023-10-29 13:10:47 +0900

    feat: add isAdvance function

--- src/main/kotlin/racingcar/game.kt
+++ src/main/kotlin/racingcar/game.kt
@@ -1,7 +1,18 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Game (private val cars: List<Car>, private val rounds: Int) {
     fun start() {
         println("실행 결과")
+    }
+
+    fun isAdvance (): Int {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+
+        if (randomNumber >= 4) {
+            return 1
+        }
+        return 0
     }
 }
