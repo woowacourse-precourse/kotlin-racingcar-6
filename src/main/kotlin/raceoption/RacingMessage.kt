83320bb9af33d1b6d9215e34a6cd006efd2d6a1e
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-29 14:50:04 +0900

    feat: 레이싱 진행 간 사용자에게 보여줄 메시지 저장 객체 생성

---
+++ src/main/kotlin/raceoption/RacingMessage.kt
@@ -0,0 +1,9 @@
+package raceoption
+
+object RacingMessage {
+    const val CAR_DIST_SEPARATOR = " : "
+    const val CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val TRIES_REQUEST_MESSAGE = "시도할 횟수는 몇 회인가요?"
+    const val FINAL_WINNER = "최종 우승자"
+    const val COMPUTATION_OUTCOME = "실행 결과"
+}
