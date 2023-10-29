8838fe92fd7d8e36dcb581415578be0c20fa6954
getupminaaa <getupmina@gmail.com>
2023-10-29 12:23:45 +0900

    feat: 자동차가 전진하는지 정지하는지 판단하는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 private const val CAR_NAME_MAX_LENGTH = 5
 private const val RACE_START_MSG = "경주할 자동차 이름을 입력하세요."
 private const val NUMBER_ATTEMPTS_MSG = "시도할 횟수는 몇 회인가요?"
+const val BASE_NUMBER = 4
 
 private const val  NUMBER_ATTEMPTS_REGEX = "[1-9]"
 fun main() {
--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -3,5 +3,9 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
 class Racing {
-    private fun makeRandomNumber() = Randoms.pickNumberInRange(0, 9).toString()
+
+
+    //true 면 move, false면 stop
+    private fun determineMoveOrStop(randomNumber:Int) = randomNumber>= BASE_NUMBER
+    private fun makeRandomNumber() = Randoms.pickNumberInRange(0, 9)
 }
