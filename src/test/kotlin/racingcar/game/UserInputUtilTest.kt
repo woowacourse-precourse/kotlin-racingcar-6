c9da05c8b20f205b1cbb51d27009e3e01c162c12
민대인 <bamin0422@gmail.com>
2023-10-29 15:57:59 +0900

    test: UserInputUtilTest 클래스 생성 및 구성

---
+++ src/test/kotlin/racingcar/game/UserInputUtilTest.kt
@@ -0,0 +1,14 @@
+package racingcar.game
+
+import org.junit.jupiter.api.Test
+
+internal class UserInputUtilTest {
+
+    @Test
+    fun getRacingCars() {
+    }
+
+    @Test
+    fun getAttempts() {
+    }
+}
