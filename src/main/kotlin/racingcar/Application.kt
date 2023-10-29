50b7dce4ac24a2ce984d4d01a9fb17880678dba9
yeonghyeon <agilecatch@gmail.com>
2023-10-30 02:25:28 +0900

    [Feat] : 시도할 횟수 입력 받기
    - 사용자로부터 시도할 횟수를 입력받을 수 있도록 하였습니다.
    - 정수값만 입력받도록 예외처리 하였습니다.
    - 이름을 공백으로 입력할 수 없도록 예외를 추가하였습니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,7 +3,10 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    readCarNames()
+    val carNames = readCarNames()//차이름
+    val tryCount = readTryCount()//입력한 수
+
+    printView(carNames, tryCount)
 }
 
 fun readCarNames(): List<String> {
@@ -16,6 +19,8 @@ fun readCarNames(): List<String> {
         //빈값 입력이거나, 2개 이상의 이름 받기,5글자 이하까지
         if (input == null) {
             throw IllegalArgumentException("입력이 없습니다.")
+        } else if (carNames.any { it.isBlank() }) {
+            throw IllegalArgumentException("이름을 공백으로 입력할 수 없습니다.")
         } else if (carNames.size < 2) {
             throw IllegalArgumentException("이름은 최소 2개 이상 입력 해야 합니다.")
         } else if (carNames.all { it.length <= 5 }) {
@@ -25,6 +30,25 @@ fun readCarNames(): List<String> {
         }
 
     }
+}
+
+fun readTryCount(): Int {
+    while (true) {
+        println("시도할 회수는 몇 회 인가요?")
+        val tryCount = Console.readLine()
+
+        if (tryCount != null && tryCount.all { it.isDigit() }) {
+            val tryCountInt = tryCount.toInt()
+            if (tryCountInt > 0) {
+                return tryCountInt
+            }
+        }
+        println("올바른 숫자를 입력해주세요.")
+    }
+}
+
+fun printView(carNames: List<String>, tryCount: Int) {
+    println("자동차 이름: ${carNames.joinToString(", ")}, 시도 횟수: $tryCount")
 }
 
 
