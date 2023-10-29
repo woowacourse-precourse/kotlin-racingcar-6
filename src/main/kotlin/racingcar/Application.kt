ee3d359015e7c9f3214fecfb6b427ea131780801
이홍진\iit80 <hong80714@gmail.com>
2023-10-29 23:30:09 +0900

    [feat] 이동 횟수 입력 기능 구현 게임 우승 목표 입력을 하면 그 조건에 맞을때 까지 게임이 루프가 돌아감
    ex) 5라고 입력을 하면 5번 움직인 자동차가 나오면 게임이 종료

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,18 +6,32 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)")
     val carNames = Console.readLine()
-
-    println(carNames.split(','))
-
     val cars = carNames.split(',').map { Cars(it) }
 
-    cars.forEach { car ->
-        println("${car.name} : ${car.position}")
+    println("시도할 횟수는 몇 회인가요?")
+    val winPoint = Console.readLine().toInt()
+    while (cars.none { it.position >= winPoint }){
+        cars.forEach { car->
+            car.move()
+            println("${car.name} : ${"-".repeat(car.position)}")
+        }
+        println()
     }
+}
 
+private fun inputCarNames() {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)")
+    val carNames = Console.readLine()
+    val cars = carNames.split(',').map { Cars(it) }
 }
 
 class Cars(carName: String) {
-    val position = 0
+    var position = 0
     val name = carName
+
+    fun move(){
+        if (Randoms.pickNumberInRange(0, 9)>=4){
+            position++
+        }
+    }
 }
