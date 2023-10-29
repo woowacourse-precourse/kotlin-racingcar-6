095e6e825a9c8ce3c426adc6f66c71b402ddf15a
hongjeZZ <ajyong316@gmail.com>
2023-10-30 02:11:53 +0900

    refactor(Converter,MoveCountValidator):사용자 입력 값 예외 처리 위치를 MoveCountValidator 클래스로 수정

--- src/main/kotlin/racingcar/utils/Converter.kt
+++ src/main/kotlin/racingcar/utils/Converter.kt
@@ -1,18 +1,11 @@
 package racingcar.utils
 
-import java.lang.NumberFormatException
-
 object Converter {
     fun splitByCommaToStringList(string: String): List<String> {
         return string.split(Constants.COMMA).toList()
     }
 
     fun convertStringToInt(string: String): Int {
-        try {
-            return string.toInt()
-        }
-        catch (e:NumberFormatException) {
-            throw IllegalArgumentException(Constants.NON_NUMERIC_INPUT_ERROR_MESSAGE)
-        }
+        return string.toInt()
     }
 }
--- src/main/kotlin/racingcar/validator/MoveCountValidator.kt
+++ src/main/kotlin/racingcar/validator/MoveCountValidator.kt
@@ -5,8 +5,13 @@ import racingcar.utils.Converter
 
 class MoveCountValidator {
     fun validate(stringMoveCount: String) {
+        requireNumericString(stringMoveCount)
         val moveCount = Converter.convertStringToInt(stringMoveCount)
         requirePositiveNumber(moveCount)
+    }
+
+    private fun requireNumericString(stringMoveCount: String) {
+        require(stringMoveCount.all { it.isDigit() }) {Constants.NON_NUMERIC_INPUT_ERROR_MESSAGE}
     }
 
     private fun requirePositiveNumber(moveCount: Int) {
