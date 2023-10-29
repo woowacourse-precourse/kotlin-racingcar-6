d2c17f87ef67361c233c2eabb4a5bb7c376c6909
book3039 <book3039@knou.ac.kr>
2023-10-30 01:09:04 +0900

    style: RacingCar 관련 상수 선언

--- src/main/kotlin/racingcar/play/RacingCar.kt
+++ src/main/kotlin/racingcar/play/RacingCar.kt
@@ -2,11 +2,15 @@ package racingcar.play
 
 import camp.nextstep.edu.missionutils.Randoms
 
+const val RANDOM_LOWER_BOUNDARY = 0
+const val RANDOM_UPPER_BOUNDARY = 9
+const val CAR_MOVE_BOUNDARY = 4
+
 class RacingCar(val name: String, var distance: Int = 0) {
 
-    private fun createRandomNumber(): Int = Randoms.pickNumberInRange(0, 9)
+    private fun createRandomNumber(): Int = Randoms.pickNumberInRange(RANDOM_LOWER_BOUNDARY, RANDOM_UPPER_BOUNDARY)
 
     fun move(random: Int = createRandomNumber()) {
-        if (random >= 4) distance++
+        if (random >= CAR_MOVE_BOUNDARY) distance++
     }
 }
