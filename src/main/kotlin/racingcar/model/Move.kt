708bf5cdcb6b6180df068d4c147a31691bada476
jaehan4707 <jahan4707@naver.com>
2023-10-29 16:36:14 +0900

    feat : 이동을 담당하는 Move 추가

---
+++ src/main/kotlin/racingcar/model/Move.kt
@@ -0,0 +1,13 @@
+package racingcar.model
+
+data class Move(var position : Int){
+    private val minCondition = 4
+    private val maxCondition = 9
+
+    fun checkMove(condition : Int){
+        if(condition in minCondition..maxCondition){
+            moveForward()
+        }
+    }
+    private fun moveForward() = this.position++
+}
