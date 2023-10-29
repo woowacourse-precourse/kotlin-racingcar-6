5094097d0bc0b1193200b6eebdcafd907cb4bf61
ErroredPasta <ForeignServices3132@pm.me>
2023-10-29 16:24:24 +0900

    test: CarName 생성에 관한 테스트 작성

---
+++ src/test/kotlin/racingcar/car/CarNameTest.kt
@@ -0,0 +1,32 @@
+package racingcar.car
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class CarNameTest {
+    @Test
+    fun createCarName_lengthIsLongerThan5_thenThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException::class.java) {
+            CarName(CAR_NAME_LENGTH_LONGER_THAN_5)
+        }
+    }
+
+    @Test
+    fun createCarName_emptyName_successfullyCreated() {
+        val carname = CarName(CAR_NAME_EMPTY)
+        assertThat(carname.name).isEqualTo(CAR_NAME_EMPTY)
+    }
+
+    @Test
+    fun createCarName_lengthIs5_successfullyCreated() {
+        val carname = CarName(CAR_NAME_LENGTH_5)
+        assertThat(carname.name).isEqualTo(CAR_NAME_LENGTH_5)
+    }
+
+    companion object {
+        private const val CAR_NAME_LENGTH_LONGER_THAN_5 = "ABCDEF"
+        private const val CAR_NAME_EMPTY = ""
+        private const val CAR_NAME_LENGTH_5 = "12345"
+    }
+}
