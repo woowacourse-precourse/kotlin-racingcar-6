eb22fbfb777a0f2d9478f071a634dc7e10a7756e
jiwon2724 <stopone3119@naver.com>
2023-10-30 06:30:04 +0900

    [refactor] : 주행 결과 코드 개선

--- src/main/kotlin/racingcar/domain/RaceResult.kt
+++ src/main/kotlin/racingcar/domain/RaceResult.kt
@@ -1,9 +1,17 @@
 package racingcar.domain
 
-class RaceResult(private val winner: Winners = Winners()) {
-    fun raceResult(moveResultList: List<List<MoveResult>>) {
-        moveResultList.forEach { round ->
+class RaceResult(private val winner: Winner = Winner()) {
+    fun raceResult(racingRoundResult: HashMap<String, ArrayList<Int>>): Map<String, ArrayList<Int>> {
+        return racingRoundResult.mapValues { (_, moveCount) -> moveCount.sumMoveCount() }
+    }
 
+    private fun ArrayList<Int>.sumMoveCount(): ArrayList<Int> {
+        val moveCountList: ArrayList<Int> = arrayListOf()
+        var sum = 0
+        for (moveCount in this) {
+            sum += moveCount
+            moveCountList.add(sum)
         }
+        return moveCountList
     }
 }
