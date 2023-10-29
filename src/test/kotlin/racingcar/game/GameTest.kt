c397ddd884b191b71a0841e0f3a53c8dceb851a3
Minjun Kim <damon911@naver.com>
2023-10-29 20:48:40 +0900

    refactor: ValidationTest로 Validation에 해당하는 테스트 코드 이전

--- src/test/kotlin/racingcar/game/GameTest.kt
+++ src/test/kotlin/racingcar/game/GameTest.kt
@@ -2,8 +2,6 @@ package racingcar.game
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -21,37 +19,5 @@ class GameTest {
     fun `입력값을 올바르게 split`(carNames: String) {
         val result = game.splitCarName(carNames)
         assertEquals(listOf("Car1", "Car2", "Car3"), result)
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["Car1,Car23", "pobi,woni,jun", " pob i , j u n "])
-    fun `자동차 이름이 1자 이상 5자인 경우`(carNames: String) {
-        assertDoesNotThrow {
-            game.splitCarName(carNames)
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["Car12,Car345", " p o b i , jun", ",jun", " ,jun"])
-    fun `자동차 이름이 1자 미만 또는 5자 초과인 경우`(carNames: String) {
-        assertThrows<IllegalArgumentException> {
-            game.splitCarName(carNames)
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["MyCar"])
-    fun `자동차가 1대인 경우`(carNames: String) {
-        assertThrows<IllegalArgumentException> {
-            game.splitCarName(carNames)
-        }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["Car1,Car 1, Car1 "])
-    fun `자동차 이름이 중복되는 경우`(carNames: String) {
-        assertThrows<IllegalArgumentException> {
-            game.splitCarName(carNames)
-        }
     }
 }
