8d875495042226306bfd797cb104c0265532fe15
sh1mj1 <7wlgns@gmail.com>
2023-10-29 16:16:47 +0900

    feat(Car): implement feature that validate car name
    * 이름이 유효하면 true 를 반환한다.
    * 이름이 blank 일 때,
    이름의 길이가 5 초과일 때 예외를 던진다.
    * docs/README 에 자동차 이름이 공백일 때 관련 기능 리스트를 추가한다.

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -5,12 +5,19 @@ data class Car(
     var position: Int,
 ) {
     fun moveForward() {
-
+        if (isMove()) {
+            position++
+        }
     }
-    private fun isMove(){
-
+    private fun isMove(): Boolean {
+        // TODO: NumberGenerator 에게 메시지 보내기
+        return true
     }
-    private fun validateCar(name: String) {
-
+    private fun validateCar(name: String): Boolean {
+        return when{
+            name.isBlank() -> {throw IllegalArgumentException("올바른 이름을 입력해주세요")}
+            name.length > 5 -> {throw IllegalArgumentException("이름은 5자 이하까지 입력 가능합니다.")}
+            else -> true
+        }
     }
 }
