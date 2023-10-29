42cd50606783077a6db4506bb8cd05656457bc67
hoogje <ajyong316@gmail.com>
2023-10-29 17:59:19 +0900

    refactor(CarNameValidator):코틀린 API를 적용하여 CarNameValidator 클래스 리팩토링

--- src/main/kotlin/racingcar/validator/CarNameValidator.kt
+++ src/main/kotlin/racingcar/validator/CarNameValidator.kt
@@ -1,29 +1,28 @@
 package racingcar.validator
 
 class CarNameValidator {
+    fun validate(carNames: String) {
+        val carNameList = splitByCommaAndTrimToStringList(carNames)
 
-    fun validateCarNames(carNames: String): List<String> {
-        val carNameList = splitAndTrimToStringList(carNames)
-
-        if (isEmptyName(carNameList)) throw IllegalArgumentException("이름에 공백을 입력할 수 없습니다.")
-        if (isCorrectNameLengthInRange(carNameList)) throw IllegalArgumentException("이름은 다섯 자 이하로 입력해야 합니다.")
-        if (isDuplicatedName(carNameList)) throw IllegalArgumentException("중복된 이름을 입력할 수 없습니다.")
-        return carNameList
+        requireNoEmptyName(carNameList)
+        requireNameLengthInRange(carNameList)
+        requireNoDuplicatedName(carNameList)
     }
 
-    private fun splitAndTrimToStringList(carNames: String): List<String> {
+    private fun splitByCommaAndTrimToStringList(carNames: String): List<String> {
         return carNames.split(",").map { it.trim() }
     }
 
-    private fun isEmptyName(carNameList: List<String>): Boolean {
-        return carNameList.any { it.isEmpty() }
+    private fun requireNoEmptyName(carNameList: List<String>) {
+        require(carNameList.all { it.isNotEmpty() }) { "이름에 공백을 입력할 수 없습니다." }
     }
 
-    private fun isCorrectNameLengthInRange(carNameList: List<String>): Boolean {
-        return carNameList.any { it.length > 5 }
+    private fun requireNameLengthInRange(carNameList: List<String>) {
+        require(carNameList.all { it.length <= 5 }) { "이름은 다섯 자 이하로 입력해야 합니다." }
     }
 
-    private fun isDuplicatedName(carNameList: List<String>): Boolean {
-        return carNameList.size != carNameList.toHashSet().size
+    private fun requireNoDuplicatedName(carNameList: List<String>) {
+        val carNameSet = carNameList.toHashSet()
+        require(carNameList.size == carNameSet.size) { "중복된 이름을 입력할 수 없습니다." }
     }
 }
