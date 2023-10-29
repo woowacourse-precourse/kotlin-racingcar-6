75d0e27a762d5479557ffc8a31266b8519f072f0
jujeong <jujeong0108@naver.com>
2023-10-30 03:53:29 +0900

    Added requestCarNameInput in RacingGameView.kt

--- src/main/kotlin/racingcar/view/RacingGameView.kt
+++ src/main/kotlin/racingcar/view/RacingGameView.kt
@@ -1,4 +1,9 @@
 package racingcar.view
 
+import racingcar.utils.ConsoleMessage
+
 class RacingGameView {
+    fun requestCarNameInput() {
+        println(ConsoleMessage.REQUEST_CAR_NAME_INPUT)
+    }
 }
