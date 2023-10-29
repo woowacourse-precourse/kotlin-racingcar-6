c4223b9411b0d1945fa14dd52923e83e6a7118b1
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-29 14:15:36 +0900

    refactor: 주행거리 출력 기능 수정

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import util.CarValue.CAR_INIT_DISTANCE_VALUE
-import util.CarValue.DISTANCE_SYMBOL
-import java.math.BigInteger
-
-class Car(name: String) : Acceleration() {
-    private var carName = name
-    private var distanceTraveled = CAR_INIT_DISTANCE_VALUE.toBigInteger()
-
-    fun printCarInfo() {
-        print("$carName : ")
-        var length = BigInteger.ZERO
-        while (length < distanceTraveled) {
-            print(DISTANCE_SYMBOL)
-            length++
-        }
-        println()
-    }
-
-    fun getCarName() = carName
-
-    fun lengthPlus() = ++distanceTraveled
-=======
 import CarConfiguration.MAX_FLAG_VALUE
 import CarConfiguration.MIN_FLAG_VALUE
 import camp.nextstep.edu.missionutils.Randoms
@@ -40,5 +17,9 @@ class Car(name: String) {
         }
     }
 
->>>>>>> 85954de (feat: Car start 기능 작성)
+    fun printDist() {
+        repeat(dashBoard.nowDist()) {
+            dashBoard.printDist()
+        }
+    }
 }
