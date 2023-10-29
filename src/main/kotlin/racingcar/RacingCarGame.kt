671fde3c8e4f65ae301f7c93198db96fe1bd74f2
songpink <jejeje04@naver.com>
2023-10-29 23:54:03 +0900

    RacingCarGame 클래스 완성

--- src/main/kotlin/racingcar/RacingCarGame.kt
+++ src/main/kotlin/racingcar/RacingCarGame.kt
@@ -5,15 +5,20 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class RacingCarGame {
     private val carInput: String = Console.readLine()
+    private val numberOfTry: Int = Console.readLine().toInt()
+    private val carList: List<String> = carInput.split(',')
+    private val gameProgress: MutableMap<String, String> = mutableMapOf()
+    private val moveCount: MutableMap<String, Int> = mutableMapOf()
 
-    val numberOfTry: Int = Console.readLine().toInt()
-    val carList: List<String> = carInput.split(',')
-    private val gameProgressEmpty = emptyList<String>()
-    private val gameProgress = gameProgressEmpty.toMutableList()
+    private fun setProgress() {
+        for (carName in carList) {
+            gameProgress[carName] = "$carName: "
+        }
+    }
 
-    private fun setProgress(){
-        for(carName in carList){
-            gameProgress.add("$carName: ")
+    private fun setMoveCount() {
+        for (carName in carList){
+            moveCount[carName] = 0
         }
     }
 
@@ -24,27 +29,48 @@ class RacingCarGame {
             }
         }
     }
-    private fun go(index: Int){
-        gameProgress[index] = gameProgress[index].plus('-')
+
+    private fun go(carName: String) {
+        gameProgress[carName] = gameProgress[carName] + '-'
     }
-    private fun goOrNot(){
-        for (i in carList.indices) {
+
+    private fun goOrNot() {
+        for (carName in carList) {
             val randomNumber = Randoms.pickNumberInRange(0, 9)
             if (randomNumber >= 4) {
-                go(i)
+                go(carName)
+                moveCount[carName] = moveCount[carName]!! + 1
             }
         }
     }
-    fun gameStart(){
+
+    fun gameStart() {
         isNameNotExceed5Letters()
         setProgress()
+        setMoveCount()
 
-        for (i in 0..< numberOfTry){
+        for (i in 0..<numberOfTry) {
             goOrNot()
+
+            for (carName in carList) {
+                println(gameProgress[carName])
+            }
+            println()
         }
 
-        for (i in carList.indices) {
-            println(gameProgress[i])
+        val moveCountList = moveCount.toList()
+        val sortedMoveCountList = moveCountList.sortedWith(compareByDescending { it.second })
+        val sortedMoveCount = sortedMoveCountList.toMap()
+        println("최종 우승자 : ")
+        for (i in carList.indices){
+            print(sortedMoveCountList[i].first)
+            if (i == carList.size-1){
+                break
+            }
+            if(sortedMoveCountList[i].second != sortedMoveCountList[i+1].second){
+                break
+            }
+            print(", ")
         }
     }
 }
