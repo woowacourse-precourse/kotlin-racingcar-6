7d92394c37a14a6c6aaa0d29af9a7766e2655168
추민수 <devsoftychoo@gmail.com>
2023-10-30 02:36:05 +0900

    [Feat] : checkRoundEnded
    round가 끝난는지 확인 후 Boolean값을 Return해줄 함수 구현

--- src/main/kotlin/racingcar/util/GameUtils.kt
+++ src/main/kotlin/racingcar/util/GameUtils.kt
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 object GameUtils {
 
+    // 라운드마다 전진 상태 업데이트
     fun updateCarsState(carList: List<RacingCarModel>): List<RacingCarModel> {
         val updatedCars = mutableListOf<RacingCarModel>()
 
@@ -22,10 +23,19 @@ object GameUtils {
         return updatedCars
     }
 
+    // 전진 여부
     private fun checkMoveForward() : Boolean{
         val randomValue = Randoms.pickNumberInRange(0, 9)
         val isMoving = randomValue >= 4
         return isMoving
     }
+
+    private fun checkRoundEnded(isRoundEnded : Int) : Boolean{
+        return isRoundEnded == 0
+    }
+
+
+
+
 }
 
