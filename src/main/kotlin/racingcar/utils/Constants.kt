f7d4e79cda4d1cd6accf3090b91346f6bee610fa
seoyeon <seoyeonn051@gmail.com>
2023-10-30 01:58:34 +0900

    feat: Constants.kt 생성

---
+++ src/main/kotlin/racingcar/utils/Constants.kt
@@ -0,0 +1,8 @@
+package racingcar.utils
+
+object Constants {
+    const val gameStartMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val attemptCountQuestion = "시도할 횟수는 몇 회인가요?"
+    const val resultMessage = "실행 결과"
+    const val finalWinnerMessage = "최종 우승자 : "
+}
