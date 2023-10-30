ba214e629217cdfa0cee7c8ac66c025cc710f82c
yeji0214 <1225yj50@naver.com>
2023-10-30 15:37:17 +0900

    시도횟수 테스트 코드 작성

---
+++ src/test/kotlin/racingcar/RaceTimeTest.kt
@@ -0,0 +1,40 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class RaceTimeTest : NsTest() {
+    @Test
+    fun `시도 횟수가 0인 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "0") }
+        }
+    }
+
+    @Test
+    fun `시도 횟수가 음수인 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "-1") }
+        }
+    }
+
+    @Test
+    fun `시도 횟수가 숫자가 아닌 경우`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "a") }
+        }
+    }
+
+    public override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val MOVING_FORWARD = 4
+        private const val STOP = 3
+    }
+}
