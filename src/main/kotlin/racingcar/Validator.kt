f3469c272c636cdc97748f4136028a2009f0e134
Sheep1sik <bohg6799@naver.com>
2023-10-31 00:48:29 +0900

    fix: 문자안에 숫자 못 찾는 버그 수정

--- src/main/kotlin/racingcar/Validator.kt
+++ src/main/kotlin/racingcar/Validator.kt
@@ -3,10 +3,13 @@ package racingcar
 class Validator {
     fun validateUserInput(carNamesList: List<String>) {
         if (carNamesList.any { it.length > 5 }) {
-            throw IllegalArgumentException("이름은 5자 이하로 입력해주세요.")
+            throw IllegalArgumentException("이름은 5글자 이하로 입력해주세요.")
         }
-        if (carNamesList.any { it.toIntOrNull() != null }) {
-            throw IllegalArgumentException("이름은 문자로 입력하세요.")
+        val regex = "\\d+".toRegex()
+        for (numberCheck in carNamesList){
+            if (regex.containsMatchIn(numberCheck)) {
+                throw IllegalArgumentException("이름은 문자로만만 입력하세요.")
+            }
         }
     }
 
