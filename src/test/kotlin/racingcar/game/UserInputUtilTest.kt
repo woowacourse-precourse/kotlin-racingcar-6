647f39a5590e03fc02c693b11e117bdc73d69087
민대인 <bamin0422@gmail.com>
2023-10-30 22:20:42 +0900

    test: test 메서드에 input 값을 받기 위한 command 메서드 정의 및 getRacingCars 리턴 값 테스팅 메서드 구현

--- src/test/kotlin/racingcar/game/UserInputUtilTest.kt
+++ src/test/kotlin/racingcar/game/UserInputUtilTest.kt
@@ -1,12 +1,25 @@
 package racingcar.game
 
+import camp.nextstep.edu.missionutils.Console
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import java.lang.String.*
 
 internal class UserInputUtilTest {
-
     @Test
     fun `getRacingCars 리턴 값 테스팅`() {
-
+        // given
+        try {
+            command("bamin,pobi,woni")
+            val racingCarList = UserInputUtil.getRacingCars()
+            // when
+            val expectedRacingCarList: List<Car> = listOf(Car("bamin"), Car("pobi"), Car("woni"))
+            // then
+            assertEquals(racingCarList.toString(), expectedRacingCarList.toString())
+        } finally {
+            Console.close()
+        }
     }
 
     @Test
@@ -32,5 +45,10 @@ internal class UserInputUtilTest {
     @Test
     fun `getAttempts 메서드 - 시도 횟수에 자연수가 아닌 경우 예외처리`() {
 
+    }
+
+    private fun command(vararg args: String) {
+        val buf = join("\n", *args).toByteArray()
+        System.setIn(ByteArrayInputStream(buf))
     }
 }
