ddae3d59b4e5b0ea8409bcd5584e54d9ca990d3f
이빵주 <yungju9982@naver.com>
2023-10-31 01:58:52 +0900

    [chore] 게임메세지 추가

---
+++ src/main/kotlin/racingcar/GameMessage.kt
@@ -0,0 +1,8 @@
+package racingcar
+
+object GameMessage {
+    const val INPUT_RACING_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val INPUT_NUMBER_ATTEMPTS_MESSAGE = "시도할 횟수는 몇 회인가요?"
+    const val GAME_RESULT_MESSAGE = "실행 결과"
+    const val WINNER_MESSAGE = "최종 우승자 : "
+}
