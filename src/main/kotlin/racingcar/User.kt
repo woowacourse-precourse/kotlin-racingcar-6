a154db7198c836009c5cb3ea07fb5335cb0cd4da
Junyoung-WON <wjyo60@gmail.com>
2023-10-30 22:02:33 +0900

    feat: validator 추가와 문자열 검사 기능 추가
    입력된 문자열에 쉼표와 언더바 이외에 다른 특수문자가 있는지 검사

--- src/main/kotlin/racingcar/User.kt
+++ src/main/kotlin/racingcar/User.kt
@@ -2,9 +2,12 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 class User {
+    val validator = Validator()
+
     fun getCarNames(): List<String>{
         var carNames : String = Console.readLine()
         // validator로 올바른 문자열 입력인지 확인
+        validator.validateString(carNames)
         // List로 쪼개기
         val nameList : List<String> = carNames.split(',')
         // validator로 올바른 길이의 이름인지 확인
