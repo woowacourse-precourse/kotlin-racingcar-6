9dfd94c487172605b07a2837cee1e04888ed6dfa
jklom0326 <jklom0326@gmail.com>
2023-10-30 10:57:50 +0900

    feat: 자동차 이동 기능 구현

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,6 +1,13 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car(val name: String, position: Int = 0) {
     var position = position
         private set
+
+    fun moving() {
+        val num = Randoms.pickNumberInRange(1, 9)
+        if (num >= 4) position++
+    }
 }
