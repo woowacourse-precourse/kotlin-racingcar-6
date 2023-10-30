93c3806dbdc437cdb96590f4db1e94d4662a6ea9
Sheep1sik <bohg6799@naver.com>
2023-10-31 02:29:16 +0900

    feat: 진행 결과를 출력하는 테스트 코드 구현

--- src/test/kotlin/racingcar/CarGameManagerTest.kt
+++ src/test/kotlin/racingcar/CarGameManagerTest.kt
@@ -1,6 +1,7 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import racingcar.Constants.Companion.MAX_NUMBER
@@ -12,6 +13,21 @@ class CarGameManagerTest {
     fun `랜덤값이 0 - 9 사이가 맞는지 확인`() {
         val randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)
         assertTrue(randomNumber in MIN_NUMBER..MAX_NUMBER)
+    }
+    @Test
+    fun `진행결과 출력`() {
+        val carNameListTest: List<String> = listOf("yang", "won", "sik")
+        val carListCountTest = carNameListTest.size
+        val carResultListTest = MutableList(carListCountTest) { "" }
+        val randomValueList: List<Int> = listOf(4, 2, 8)
+        val outputTestList: List<String> = listOf("yang : -","won : ","sik : -")
+        for (countList in 0 until carListCountTest) { // MIN_NUMBER 제거
+            if (randomValueList[countList] >= Constants.FORWARD_CONDITION) {
+                carResultListTest[countList] += "-"
+            }
+            val output = "${carNameListTest[countList]} : ${carResultListTest[countList]}"
+            Assertions.assertEquals(output, outputTestList[countList])
+        }
     }
 
 }
