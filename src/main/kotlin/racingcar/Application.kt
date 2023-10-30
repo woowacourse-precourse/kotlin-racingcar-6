90f19e730102112c81ef123c26523b747c5f0557
상은 <71699054+vmkmym@users.noreply.github.com>
2023-10-30 21:08:18 +0900

    <feature> main 함수, 상수 선언

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,7 +3,12 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-data class CarRace(val name: String) {
+
+const val PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+const val PROMPT_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
+const val MAX_CAR_NAME_LENGTH = 5
+
+data class Car(val name: String) {
     var position: Int = 0
 
     fun move() {
@@ -17,24 +22,44 @@ data class CarRace(val name: String) {
     }
 }
 
-private fun carNames(): String {
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+fun main() {
+    val inputNames = carNames()
+    val inputAttempts = attempts()
+    var maxPosition = 0
+    val cars = inputNames.split(",").map { Car(it) }
 
-    val inputCarNames = Console.readLine()
+    println("\n실행 결과")
 
-    if (inputCarNames.split(",").any { it.length > 5 }) {
-        throw IllegalArgumentException()
+    repeat(inputAttempts) {
+        cars.forEach { it.move() }
+        val max = cars.map { it.position }.maxOrNull()
+        if (max != null && max > maxPosition) {
+            maxPosition = max
+        }
+        cars.forEach { println(it.display()) }
+        println()
     }
-    return inputCarNames
+
+    val winners = cars.filter { it.position == maxPosition }.joinToString(", ") { it.name }
+
+    println("최종 우승자 : $winners")
 }
 
-private fun checkWin(): Int {
-    println("시도할 횟수는 몇 회인가요?")
+private fun carNames(): String {
+    println(PROMPT_CAR_NAMES)
+    val inputNames = Console.readLine()
+    if (inputNames.split(",").any { it.length > MAX_CAR_NAME_LENGTH }) {
+        throw IllegalArgumentException()
+    }
+    return inputNames
+}
 
+private fun attempts(): Int {
+    println(PROMPT_ATTEMPTS)
     return try {
-        val attempts = Console.readLine().toInt()
-        if (attempts <= 0) throw IllegalArgumentException()
-        attempts
+        val input = Console.readLine().toInt()
+        if (input <= 0) throw IllegalArgumentException()
+        input
     } catch (e: NumberFormatException) {
         throw IllegalArgumentException()
     }
