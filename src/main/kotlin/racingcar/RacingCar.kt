47c481d0087864f2cca0a8be7aaba3b174f2aca3
vonvolous <hanyoojin39@gmail.com>
2023-10-30 15:39:14 +0900

    feat : add new class RacingCar

---
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -0,0 +1,23 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class RacingCar(_name: String) {
+    var name: String = _name
+    private var randomNum = 10
+    private var moveCnt = 0
+
+    fun getRandomNum() {
+        this.randomNum = Randoms.pickNumberInRange(1, 9)
+    }
+
+    private fun isMove(): Boolean{
+        return this.randomNum>=4
+    }
+
+    fun updateMoveCnt() {
+        if (isMove()){
+            moveCnt += 1
+        }
+    }
+}
