96ad07e84f21cad0ff3071764e7d6f7949fb92a3
BBIYACK-0834 <erty0834@naver.com>
2023-10-31 00:37:07 +0900

    객체가 아닌 절차로 구현된 1차 코드 / 객체로 쪼개기 시작해야함

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,56 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    // TODO: 프로그램 구현
+    val carNames = inputCarNames()
+    val trynum = inputTryCount()
+    val goCount = raceCar(carNames, trynum)
+    printRaceResults(carNames, goCount)
+    printWinners(carNames, goCount)
 }
+
+fun inputCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)")
+    val carNamesInput = Console.readLine()
+    val carNames = carNamesInput.split(",").map { it.trim() }
+
+    if (carNames.any { it.length > 5 }) {
+        throw IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.")
+    }
+
+    return carNames
+}
+
+fun inputTryCount(): Int {
+    println("시도할 횟수는 몇 회 인가요?")
+    return Console.readLine().toInt()
+}
+
+fun raceCar(carNames: List<String>, trynum: Int): Array<Int> {
+    val goCount = Array(carNames.size) { 0 }
+    for (i in 1..trynum) {
+        for (j in carNames.indices) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                goCount[j]++
+            }
+        }
+    }
+    return goCount
+}
+
+fun printRaceResults(carNames: List<String>, goCount: Array<Int>) {
+    println("\n실행 결과")
+    for (i in carNames.indices) {
+        val distance = "-".repeat(goCount[i])
+        println("${carNames[i]} : $distance")
+    }
+}
+
+
+fun printWinners(carNames: List<String>, goCount: Array<Int>) {
+    val maxCount = goCount.max()
+    val winners = carNames.filterIndexed { index, _ -> goCount[index] == maxCount }
+    println("최종 우승자 : ${winners.joinToString(",")}")
+}
+
