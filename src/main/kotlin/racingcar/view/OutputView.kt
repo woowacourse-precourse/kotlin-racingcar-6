246a6b4246c71e659b0c2331b7af8adf560a770d
junyong <junyong008@gmail.com>
2023-10-30 16:23:11 +0900

    refactor: OutputView 상수명 변경

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -4,9 +4,9 @@ import racingcar.Car
 
 object OutputView {
 
-    private const val RESULT_TITLE = "실행 결과"
+    private const val OUTPUT_RESULT_TITLE = "실행 결과"
 
-    fun outputResultTitle() = println(System.lineSeparator() + RESULT_TITLE)
+    fun outputResultTitle() = println(System.lineSeparator() + OUTPUT_RESULT_TITLE)
 
     fun outputCurrentCarPositions(cars: List<Car>) {
         cars.forEach {
