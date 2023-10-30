2581cc461fe0f36582ad35b6d35e444b5aed4658
Seannnn55 <sally99h@gmail.com>
2023-10-30 20:22:51 +0900

    feat : [PrepareGame] 경주 실행 횟수 입력기 생성

--- src/main/kotlin/racingcar/PrepareGame.kt
+++ src/main/kotlin/racingcar/PrepareGame.kt
@@ -2,11 +2,17 @@ package racingcar
 
 class PrepareGame {
 
-    fun racingCarName() :List<String>{
-        var userName:String = readLine()
+    fun racingCarName(): List<String> {
+        var userName: String = readLine()
         var userList: List<String> = userName.split(",")
 
         return userList
+    }
+
+    fun racingNumber(): Int{
+        var raceNum: Int = readLine().toInt()
+
+        return raceNum
     }
 
 }
