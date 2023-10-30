fbba3416d781687657a31409bac2cf17f4c32853
K_Gs:D <rootachieve6053@gmail.com>
2023-10-31 02:47:01 +0900

    feat(TestMoveStrategy): 클래스 생성
    TestMoveStrategy 클래스를 생성했습니다.
    - 인터페이스를 오버라이딩한 calculateLocation 함수가 있습니다.
    - 생성자로 미리 만들어진 리스트를 받고 해당 리스트의 값을 보고 위치를 반환합니다.
    - 한번 호출시마다 pointer가 증가해 다음 값을 봅니다.

---
+++ src/main/kotlin/racingcar/TestMoveStrategy.kt
@@ -0,0 +1,12 @@
+package racingcar
+
+class TestMoveStrategy(private val numberList: List<Int>) : MoveStrategyInterface {
+    private var pointer = 0
+    override fun calculateLocation(speed: Int, location: Int): Int {
+        return if (numberList[pointer++] > 4) {
+            location + speed
+        } else {
+            location
+        }
+    }
+}
