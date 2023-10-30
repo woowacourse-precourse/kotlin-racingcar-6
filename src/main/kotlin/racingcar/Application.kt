f86cf2455beb58667af649981d9209f1871a4c51
gwPhoenix <na90815@naver.com>
2023-10-30 21:56:33 +0900

    feat : Application.kt modify
    gameStart message add

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,11 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
+    gameStart()
+}
+
+fun gameStart(){
+    println("레이싱 카 게임")
+    println("=====================================")
+    print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 }
