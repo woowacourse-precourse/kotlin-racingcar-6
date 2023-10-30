9fc942d27d5189cd929b4721ffdf243a6d262a21
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-10-30 11:40:45 +0900

    feat(input carnames)
    docs/README.md 에서 정리한 1번 기능 자동차들 이름 입력 기능을 추가하였다

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    val carNames = inputCarNames()
+
+}
+
+fun inputCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    return Console.readLine().split(",")
 }
