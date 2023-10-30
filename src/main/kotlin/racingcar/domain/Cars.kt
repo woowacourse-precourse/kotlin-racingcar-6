b50ead0e2013231dd23794b763cdc2dd7f934ed0
stopstone <wltjr6432@naver.com>
2023-10-30 23:22:32 +0900

    move: 랜덤 수 생성 및 전진/정지 기능 Car 클래스로 이동

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -1,6 +1,20 @@
 package racingcar.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Cars(name:String) {
     val carName= name
     var carPosition = 0
+
+    private fun generateRandomNumber(): Int {
+        return Randoms.pickNumberInRange(0, 9)
+    }
+
+    fun chooseActionFromRandomNumber() {
+        val randomNumber = generateRandomNumber()
+        if (randomNumber >= 4){
+            carPosition++
+        }
+    }
+
 }
