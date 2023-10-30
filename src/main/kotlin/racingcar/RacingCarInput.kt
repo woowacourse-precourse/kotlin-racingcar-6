853184394616c74ba3804150d3b04e4a7c020c94
이빵주 <yungju9982@naver.com>
2023-10-31 02:23:49 +0900

    [feat] 사용자에게 자동차이름 입력받는 함수 구현

--- src/main/kotlin/racingcar/RacingCarInput.kt
+++ src/main/kotlin/racingcar/RacingCarInput.kt
@@ -1,12 +1,24 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import racingcar.Converter.convertStringToMap
 import racingcar.ErrorMessage.CAR_MINIMUM_COUNT_ERROR
 import racingcar.ErrorMessage.CAR_NAME_LENGTH_OVER_ERROR
 import racingcar.ErrorMessage.DUPLICATE_CAR_NAME_ERROR
+import racingcar.GameMessage.INPUT_RACING_CAR_NAME_MESSAGE
 
 object RacingCarInput {
     private const val MIN_CAR_COUNT = 1
     private const val MAX_CAR_NAME_LENGTH = 5
+
+    fun getCarNameMap(): Map<String, Int> {
+        try {
+            print(INPUT_RACING_CAR_NAME_MESSAGE)
+            return convertStringToMap(Console.readLine()).apply { validate(this) }
+        } catch (e: IllegalArgumentException) {
+            throw e
+        }
+    }
 
     private fun validate(carNameMap: Map<String, Int>) {
         validateCarNameDuplicate(carNameMap)
