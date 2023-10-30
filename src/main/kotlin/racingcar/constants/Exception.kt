5a412f4526e4a213bad0a367c34c3eda87a126ef
kimhm0728 <kimhm0728@naver.com>
2023-10-31 01:41:03 +0900

    style: 예외 메시지 변경

--- src/main/kotlin/racingcar/constants/Exception.kt
+++ src/main/kotlin/racingcar/constants/Exception.kt
@@ -1,10 +1,10 @@
 package racingcar.constants
 
 enum class CarException(private val phrases: String) {
-    LENGTH("자동차 이름은 최대 5글자입니다."),
-    LETTER("자동차 이름은 대소문자의 영어로만 이루어질 수 있습니다."),
-    COMMA("자동차 이름을 구분하기 위한 쉼표는 이름 사이에 하나만 입력할 수 있습니다."),
-    DUPLICATION("자동차 이름은 중복되지 않아야 합니다.");
+    LENGTH("1글자에서 5글자 사이의 이름을 입력해 주세요."),
+    LETTER("대소문자의 영어로만 이루어진 이름을 입력해 주세요."),
+    COMMA("자동차 이름을 구분하기 위한 쉼표는 이름 사이에 하나만 입력해 주세요."),
+    DUPLICATION("중복되지 않는 이름을 입력해 주세요.");
 
     override fun toString() = phrases
 }
@@ -12,7 +12,7 @@ enum class CarException(private val phrases: String) {
 enum class TryException(private val phrases: String) {
     DIGIT("시도할 횟수는 숫자만 입력할 수 있습니다."),
     MAX("시도할 횟수가 너무 큽니다."),
-    MIN("시도할 횟수가 너무 작습니다. 1 이상의 수를 입력해주세요.");
+    MIN("시도할 횟수가 너무 작습니다. 1 이상의 수를 입력해 주세요.");
 
     override fun toString() = phrases
 }
