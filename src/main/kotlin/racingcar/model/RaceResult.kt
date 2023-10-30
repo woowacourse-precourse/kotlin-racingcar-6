ccc69f871a0dffc89d178e422817e4f9e128b14d
murjune <sis9221302@gmail.com>
2023-10-31 01:11:32 +0900

    fix(RaceResult) : toString()에 줄바꿈(\n) 추가

--- src/main/kotlin/racingcar/model/RaceResult.kt
+++ src/main/kotlin/racingcar/model/RaceResult.kt
@@ -1,9 +1,8 @@
 package racingcar.model
 
 data class RaceResult(val carStateList: List<CarState>) {
-
     override fun toString(): String {
-        return carStateList.joinToString(NEW_LINE)
+        return carStateList.joinToString(NEW_LINE) + NEW_LINE
     }
 
     companion object {
