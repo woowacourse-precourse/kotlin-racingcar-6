55d1db546ef50e2706fb7d98d12f1f8115d9941b
sooohi <68193613+sooohi@users.noreply.github.com>
2023-10-30 13:39:53 +0900

    feat : 자동차 이름 입력

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carName = readLine()?.split(",") ?: throw IllegalArgumentException("올바르게 다시 입력해주세요")
+
+    for (car in carName) {
+        if (car.length > 5) {
+            throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.")
+        }
+    }
 }
