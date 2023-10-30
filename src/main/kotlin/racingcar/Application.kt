a410d59a11ed0735d2691b353273c930a77ab108
nhkim <nhkim1529@gmail.com>
2023-10-30 23:29:17 +0900

    Feat: 자동차 이름 입력하는 함수 inputCarList 작성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,4 +2,13 @@ package racingcar
 
 fun main() {
 
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val CarList = inputCarList()
+
+}
+
+fun inputCarList(): List<String>{
+    val inputCarName = readLine()
+    val CarList = inputCarName!!.split(",")
+    return CarList
 }
