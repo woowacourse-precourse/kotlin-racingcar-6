35abd9cc9ca48db4ab8ee1bc351a38b873d011c8
MinhoiKoo <minlove2013@gmail.com>
2023-10-31 01:48:16 +0900

    feat: Round class 생성
    -객체 생성시 value 값이 1보다 작거나 1000보다 크면(조건 추가) 예외 발생

---
+++ src/main/kotlin/racingcar/model/Round.kt
@@ -0,0 +1,11 @@
+package racingcar.model
+
+private const val MIN_ROUND = 1
+private const val MAX_ROUND = 1000
+private const val ILLEGAL_ROUND_EXCEPTION = "진행할 라운드는 0보다 크고 1000보다 작아야 합니다!"
+class Round(val value: Int) {
+
+    init {
+        require(value in MIN_ROUND..MAX_ROUND) { ILLEGAL_ROUND_EXCEPTION}
+    }
+}
