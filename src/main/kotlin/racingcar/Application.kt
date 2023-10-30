10e282adc2d8cd146c3decfe9c1ac310b2941110
DJ_Lumiere <djminstar573@gmail.com>
2023-10-30 21:46:22 +0900

    Update Application.kt
    경주 우승자 결정 함수 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,7 +3,7 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-class raceCar(private val name: String, var position: Int = 0) {
+class RaceCar(private val name: String, var position: Int = 0) {
     init {
         require((name.length <= 5)) { "Invalid Car Name." }
     }
@@ -14,17 +14,17 @@ class raceCar(private val name: String, var position: Int = 0) {
 
 }
 
-fun isMovable(): Int {
-    return (Randoms.pickNumberInRange(0, 9) >= 4)
+fun moveDistance(): Int {
+    return (Randoms.pickNumberInRange(0, 9).compareTo(4)).compareTo(-1)
 }
 
-private fun raceCar.move() {
-    position += isMovable()
+private fun RaceCar.move() {
+    position += moveDistance()
 }
 
-fun parseCarName(): List<raceCar> {
+fun parseCarName(): List<RaceCar> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    return Console.readLine().split(",").map { raceCar(it.trim()) }
+    return Console.readLine().split(",").map { RaceCar(it.trim()) }
 }
 
 fun isNonnegativeInt(target: String): Boolean {
@@ -38,10 +38,15 @@ fun parseTurnCount(): Int {
     return turnCountString.toInt()
 }
 
-fun turnProcess(raceCars: List<raceCar>) {
+fun turnProcess(raceCars: List<RaceCar>) {
     for (raceCar in raceCars) {
         raceCar.move()
     }
+}
+
+fun decideWinners(raceCars: List<RaceCar>): List<String> {
+    val longestDistance = raceCars.maxOfOrNull { it.position } ?: 0
+    return raceCars.filter { it.position == longestDistance }.map { it.name }
 }
 
 fun main() {
@@ -52,4 +57,5 @@ fun main() {
         turnProcess(raceCars)
         println(raceCars.joinToString("\n", postfix = "\n"))
     }
+    val winners = decideWinners(raceCars)
 }
