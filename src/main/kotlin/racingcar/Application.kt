2f00c1056bb171242cfe06eeaf6958e15c51f5b9
eunma <aa39043533@gmail.com>
2023-10-30 14:13:14 +0900

    제시된 라이브러리 import, 자동차 이름 선언

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,11 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 fun main() {
-    // TODO: 프로그램 구현
+    class Car(val name: String) {
+        init {
+            require(name.length <= 5) { "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" }
+        }
+    }
 }
