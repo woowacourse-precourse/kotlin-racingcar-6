bbf661c11f7abf23e454ddb37da83f3148655913
tgyuu-An <atg0614@gmail.com>
2023-10-31 04:58:34 +0900

    refactor : Race 클래스 생성 및 공개되는 함수들 private로 집어 넣음

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,33 +1,5 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
-
 fun main() {
-    setUpAndStartRace()
-}
-
-fun setUpAndStartRace(){
-    val raceManager = RaceManager()
-
-    val carNames = getUserInputCarNames()
-    carNames.forEach { carName ->
-        raceManager.addCarToRace(Car(carName))
-    }
-
-    val movementAttemptInput = getUserInputMovementAttempt()
-    raceManager.setMovementAttemptCount(movementAttemptInput)
-
-    raceManager.startRace()
-}
-
-fun getUserInputCarNames(): List<String> {
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    val carsInput = Console.readLine()
-    return carsInput.split(",")
-}
-
-fun getUserInputMovementAttempt(): String {
-    println("시도할 횟수는 몇 회인가요?")
-    val movementAttemptInput = Console.readLine()
-    return movementAttemptInput
+    Race().start()
 }
