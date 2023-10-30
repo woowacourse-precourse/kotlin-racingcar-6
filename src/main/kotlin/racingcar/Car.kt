893a515962ce7cd15893593eda025c2199ec9f92
oh in seong <superohinsung@naver.com>
2023-10-30 14:17:42 +0900

    [feat] 횟수마다 랜덤 값을 배정하는 기능

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,10 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car(private val name: String, private var howGoForward: Int = 0) {
     init {
         require(name.length <= NAME_MAX_LENGTH)
         require(name.matches(Regex("^[A-Za-z가-힣]*\$")))
     }
+
+    private fun createRandomNumber() = Randoms.pickNumberInRange(0, 9)
 
     fun getCarName() = this.name
 
