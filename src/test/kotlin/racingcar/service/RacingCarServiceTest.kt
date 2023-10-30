b5553c9fd97f26981c1dc2dbdcda943dea6ea8fc
posite <su84053@gmail.com>
2023-10-30 22:09:41 +0900

    test: PrintOutputView의 우승자 출력 refactoring으로인한 test code 수정

--- src/test/kotlin/racingcar/service/RacingCarServiceTest.kt
+++ src/test/kotlin/racingcar/service/RacingCarServiceTest.kt
@@ -4,35 +4,40 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import racingcar.domain.RacingCars
+import racingcar.repository.RacingCarsRepository
+import racingcar.util.RandomNumberGenerator
 
 @DisplayName("서비스를 하는 RacingCarService에서")
 class RacingCarServiceTest {
-    private val racingCarService = RacingCarService()
+    private val racingCarService = RacingCarService(RandomNumberGenerator(), RacingCarsRepository())
 
     @Test
-    @DisplayName("단일 우승자의 경우 크기가 1인 list를 받을 수 있다")
+    @DisplayName("단일 우승자의 경우 우승자의 이름만 출력한다")
     fun soleWinner() {
         //given
-        val racingCars = RacingCars(listOf("911", "718"))
-        racingCars.loadRacingCars()[0].addForwardCount()
-        racingCarService.initGame(racingCars)
+        val winner = "911"
+        val loser = "718"
+        val racingCars = RacingCars(listOf(winner, loser))
+        racingCars.moveRacingCars(listOf(5, 2))
+        racingCarService.saveRacingCars(racingCars)
         //when
         val result = racingCarService.calculateRacingResult()
         //then
-        Assertions.assertThat(result.getWinners().size).isEqualTo(1)
+        Assertions.assertThat(result.toWinnerNamesGameResult()).contains(winner).doesNotContain(loser)
     }
 
     @Test
     @DisplayName("공동 우승자가 2명일 경우 크기가 2인 list를 받을 수 있다")
     fun jointWinner() {
         //given
-        val racingCars = RacingCars(listOf("911", "718"))
-        racingCars.loadRacingCars()[0].addForwardCount()
-        racingCars.loadRacingCars()[1].addForwardCount()
-        racingCarService.initGame(racingCars)
+        val winner1 = "911"
+        val winner2 = "718"
+        val racingCars = RacingCars(listOf(winner1, winner2))
+        racingCars.moveRacingCars(listOf(5, 6))
+        racingCarService.saveRacingCars(racingCars)
         //when
         val result = racingCarService.calculateRacingResult()
         //then
-        Assertions.assertThat(result.getWinners().size).isEqualTo(2)
+        Assertions.assertThat(result.toWinnerNamesGameResult()).contains(winner1, winner2)
     }
 }
