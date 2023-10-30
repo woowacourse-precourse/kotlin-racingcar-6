94e196af599f6655df6546b3ee885a9a3648ebe5
이빵주 <yungju9982@naver.com>
2023-10-31 06:33:38 +0900

    [feat] 입력된 숫자만큼 경주를 진행하고 프린트하는 함수 구현

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -1,9 +1,19 @@
 package racingcar
 
+import racingcar.GameMessage.GAME_RESULT_MESSAGE
+
 class RacingGame(
     private val racingCarInput: RacingCarInput,
     private val moveCountInput: MoveCountInput,
     private val raceManager: RaceManager,
     private val racePrinter: RacePrinter
 ) {
+
+    private fun runRace(count: Int) {
+        println(GAME_RESULT_MESSAGE)
+        repeat(count) {
+            val currentRace: List<Car> = raceManager.race()
+            racePrinter.showCurrentRace(currentRace)
+        }
+    }
 }
