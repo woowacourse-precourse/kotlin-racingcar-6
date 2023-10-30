555a3fcc40b21a253a284c7dbfd23b242664e593
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-10-31 01:21:47 +0900

    [feat] 경주할 자동자 이름 입력

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,20 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val cars = enterCarName()
+}
+
+fun enterCarName(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val cars = Console.readLine().split(",")
+    validationCarName(cars)
+    return cars
+}
+
+fun validationCarName(cars: List<String>) {
+    for (car in cars) {
+        if (car.length > 5) throw IllegalArgumentException("5자 이하만 가능합니다.")
+    }
 }
