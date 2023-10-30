fd4c97f54e78259d6261e93569a4f2d1abb975a5
SeongWonYoon <dbswjdqhd12@gmail.com>
2023-10-30 22:19:41 +0900

    Feat: "추가 랜덤 난수 추출 후 조건에 맞을 경우 자동차 전진"
    전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -1,5 +1,7 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class RacingGame(carList : List<String>, amount : Int) {
     private var gameRound : Int = amount
     private val cars = HashMap<Int, Car>(carList.size)
@@ -10,9 +12,17 @@ class RacingGame(carList : List<String>, amount : Int) {
         }
     }
 
-    fun startNextRound(){
-
+    fun initRound(){
+        cars.forEach { index, car ->
+            if (canCarMove()){
+                car.move()
+            }
+        }
     }
+
+    fun canCarMove() : Boolean = Randoms.pickNumberInRange(1, 9) >= 4
+
+
 
 
 }
