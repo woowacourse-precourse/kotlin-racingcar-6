8df92d006e242701afac6e1141e3ff78850325fe
seongsan <tjdtks030@naver.com>
2023-10-30 12:01:02 +0900

    test: Validator 테스트 코드 작성
    ValidatorTest - validateCarNames 메서드 테스트 코드 작성

---
+++ src/test/kotlin/racingcar/ValidatorTest.kt
@@ -0,0 +1,21 @@
+package racingcar
+
+import org.junit.jupiter.api.Test
+
+class ValidatorTest {
+
+    @Test
+    fun `validateCarNames 메서드 사용 시 입력값에 쉼표가 존재하지 않을 때 예외 발생`() {
+
+    }
+
+    @Test
+    fun `validateCarNames 메서드 사용 시 carName 이 1자 이상 5자 이하로 이루어지지 않았을 때 예외 발생`() {
+
+    }
+
+    @Test
+    fun `validateCarNames 메서드 사용 시 carName 의 개수가 1개 이하일 때 예외 발생`() {
+
+    }
+}
