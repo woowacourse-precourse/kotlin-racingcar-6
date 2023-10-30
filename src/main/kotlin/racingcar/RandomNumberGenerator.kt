660e0fc940b0fdc769790dabe5c6bc67c4f5dde1
tgyuu-An <atg0614@gmail.com>
2023-10-30 22:21:04 +0900

    feat : RandomNumberGenerator class 생성

--- src/main/kotlin/racingcar/RandomNumberGenerator.kt
+++ src/main/kotlin/racingcar/RandomNumberGenerator.kt
@@ -1,4 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class RandomNumberGenerator {
+    companion object{
+        private const val MIN = 0
+        private const val MAX = 9
+    }
+
+    fun generateRandomNumber(): Int {
+        return Randoms.pickNumberInRange(MIN, MAX)
+    }
 }
