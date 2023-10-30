b04184b45a123a00ff2888b83780cbf5f5cb0a06
crAy <craycoding@gmail.com>
2023-10-31 02:41:59 +0900

    feat : Validation.kt

---
+++ src/main/kotlin/racingcar/Validation.kt
@@ -0,0 +1,19 @@
+package racingcar
+
+class Validation {
+
+    fun validCar(carList : List<String>) {
+        for (car in carList) {
+            require(car.isNotEmpty()) { "차 이름은 1글자 이상이어야 합니다." }
+            require(car.length <= 5) { " 차 이름은 5글자 이하이어야 합니다." }
+        }
+    }
+
+    fun validAttempt(inputInt : String) {
+        inputInt.forEach { char ->
+            val charCode = char.code
+            require(charCode >= 48 && charCode <= 57) { " 숫자만 입력 받을 수 있습니다. "}
+        }
+        require(inputInt.toInt() > 0) { " 시도할 횟수는 1 이상의 수이어야 합니다. "}
+    }
+}
