cb90f4315cd5a8889249f9e8b18835d4d84e238c
이홍진\iit80 <hong80714@gmail.com>
2023-10-30 11:43:29 +0900

    [feat] 예외 처리 기능 구현 자동차 이름 입력이 5글자 이상일시, 이동 횟수가 자연수가 아닐시 예외처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,10 +6,16 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)")
     val carNames = Console.readLine()
+    if (carNames.length > 5){
+        throw IllegalArgumentException("5글자 이하로 이름을 지어주세요")
+    }
     val cars = carNames.split(',').map { Cars(it) }
 
     println("시도할 횟수는 몇 회인가요?")
     val winPoint = Console.readLine().toInt()
+    if (winPoint <= 0){
+        throw IllegalArgumentException("자연수로 입력해주세요")
+    }
     while (cars.none { it.position >= winPoint }){
         cars.forEach { car->
             car.move()
@@ -26,7 +32,7 @@ fun main() {
             winner.add(car.name)
         }
     }
-    print("최종 우승자: ${winner.joinToString(separator = ", ")}")
+    print("최종 우승자 : ${winner.joinToString(separator = ", ")}")
 }
 
 private fun inputCarNames() {
