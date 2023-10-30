50506008e4e2c761232a4dde95229ecc6e067a46
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-10-30 22:13:58 +0900

    Add: 이름에 대한 예외 처리 테스트 코드 추가

--- src/test/kotlin/racing/RacingTest.kt
+++ src/test/kotlin/racing/RacingTest.kt
@@ -1,8 +1,11 @@
 package racing
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import racingcar.Racing
+
 
 class RacingTest {
     private var racing: Racing = Racing()
@@ -13,6 +16,11 @@ class RacingTest {
         racing.addCar("Car2")
         assertEquals(2, racing.getCars().size)
     }
-
+    @Test
+    fun `이름에 대한 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { racing.addCar("pobi,veryLongName") }
+        }
+    }
 
 }
