a96b344f17f090bc0c6d7dc73e08b8c88e8f2e7e
tgyuu-An <atg0614@gmail.com>
2023-10-31 00:34:57 +0900

    feat : 자동차 isPossibleMoveForward 로직 구현 및 매직넘버 제거

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,11 +4,17 @@ class Car(
     private val numberGenerator: NumberGenerator = RandomNumberGenerator(),
     val name: String,
 ) {
+
+    companion object{
+        private const val THRESHHOLD_FOR_MOVE_FORWARD = 4
+        private const val MAX_NAME_LENGTH = 5
+    }
+
     init {
-        require(name.isNotEmpty() && name.length <= 5)
+        require(name.isNotEmpty() && name.length <= MAX_NAME_LENGTH)
     }
 
     fun isPossibleMoveForward(): Boolean {
-        return true
+        return numberGenerator.generateNumber() >= THRESHHOLD_FOR_MOVE_FORWARD
     }
 }
