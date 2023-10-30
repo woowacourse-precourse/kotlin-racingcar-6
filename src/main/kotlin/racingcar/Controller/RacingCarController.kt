8dbb98aea3ad0b106c1826bb2e14df5627ad963e
Jejun Min <alswpwns11@naver.com>
2023-10-31 01:47:52 +0900

    Controller 클래스 구현

--- src/main/kotlin/racingcar/Controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/Controller/RacingCarController.kt
@@ -1,7 +1,23 @@
 package racingcar.Controller
 
-import java.util.concurrent.CompletionService
+import racingcar.Model.Race
+import racingcar.View.InputView
+import racingcar.View.OutputView
 
-class RacingCarController(
 
-)
+class RacingCarController { // 사용자의 입력이나 요청을 받아 처리하고 결과를 출력하는 로직을 담당
+    companion object {
+        private val inputView = InputView()
+        private val outputView = OutputView()
+        fun start() {
+            val carNames = inputView.getCarNames()
+            val tries = inputView.getRaceTries()
+
+            val race = Race(carNames, tries)
+            race.race()
+
+            val winners = race.getWinners()
+            outputView.printWinners(winners)
+        }
+    }
+}
