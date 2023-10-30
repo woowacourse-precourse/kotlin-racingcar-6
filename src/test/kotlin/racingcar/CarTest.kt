498466ba9a471a549fb3418eb7a05e89933d1c78
tgyuu-An <atg0614@gmail.com>
2023-10-30 23:19:53 +0900

    test : CarTest 테스트 코드 작성

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,5 +1,31 @@
 package racingcar
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class CarTest{
 
+    @Test
+    fun `랜덤으로 생성된 숫자가 4미만일 경우 자동차는 전진하지 않는다`(){
+        //given
+        val car = Car()
+
+        //when
+
+        //then
+        val actual = car.tryMoveForward()
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `랜덤으로 생성된 숫자가 4이상일 경우 자동차는 전진한다`(){
+        //given
+        val car = Car()
+
+        //when
+
+        //then
+        val actual = car.tryMoveForward()
+        assertThat(actual).isTrue()
+    }
 }
