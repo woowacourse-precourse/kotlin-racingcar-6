0a6a0a0cf0812a892ffa4757bd1427547b56545a
이빵주 <yungju9982@naver.com>
2023-10-31 02:06:23 +0900

    [feat] 최소 자동차 개수 검사하는 함수 구현

--- src/main/kotlin/racingcar/RacingCarInput.kt
+++ src/main/kotlin/racingcar/RacingCarInput.kt
@@ -1,13 +1,21 @@
 package racingcar
 
+import racingcar.ErrorMessage.CAR_MINIMUM_COUNT_ERROR
 import racingcar.ErrorMessage.DUPLICATE_CAR_NAME_ERROR
 
 object RacingCarInput {
+    private const val MIN_CAR_COUNT = 1
 
     private fun validateCarNameDuplicate(carNameMap: Map<String, Int>) {
         val carNames = carNameMap.keys
         if (carNames.size != carNames.distinct().size) {
             throw IllegalArgumentException(DUPLICATE_CAR_NAME_ERROR)
+        }
+    }
+
+    private fun validateMinimumCarCount(carNameMap: Map<String, Int>) {
+        if (carNameMap.size <= MIN_CAR_COUNT) {
+            throw IllegalArgumentException(CAR_MINIMUM_COUNT_ERROR)
         }
     }
 }
