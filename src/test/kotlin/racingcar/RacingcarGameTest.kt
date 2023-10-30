5f834668d5fd004536981093c6004bf5e4298736
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-31 00:17:52 +0900

    Test (RacingcarGameTest) : 0 ~ 9 사이 무작위 값만 생성 검증

--- src/test/kotlin/racingcar/RacingcarGameTest.kt
+++ src/test/kotlin/racingcar/RacingcarGameTest.kt
@@ -3,14 +3,24 @@ package racingcar
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import racingcar.controller.RacingcarController
+import racingcar.model.GenerateRandomNumber
+import org.junit.jupiter.api.Assertions.assertTrue
+
 
 class RacingcarGameTest {
     private val racingcarController = RacingcarController()
+    private val generateRandomNumber = GenerateRandomNumber()
     @Test
     fun `입력한 자동차 이름이 쉼표 기준으로 구분 검증`() {
         val input = "test1,test2,test3"
         val result = racingcarController.splitRacingcarName(input)
         Assertions.assertThat(result).contains("test3", "test2", "test1")
         Assertions.assertThat(result).containsExactly("test1", "test2", "test3")
+    }
+
+    @Test
+    fun `0 ~ 9 사이 무작위 값만 생성 검증`() {
+        val result = generateRandomNumber.generate()
+        assertTrue(result in 0..9)
     }
 }
