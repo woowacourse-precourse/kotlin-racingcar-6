4717aa9d412363548d01057b04aab1c0d87079e5
이홍진\iit80 <hong80714@gmail.com>
2023-10-30 21:24:32 +0900

    [refactor] 기능별로 함수구조 변경 main에 있는 기능들을
    게임을 시작하는 함수 startRacingGame()
    
    자동차 이름을 입력해 각각의 이름으로 자동차 객체를 생성해주는 carNamesInput()
    
    게임의 라운드수를 정하는 roundInput()
    
    라운드마다 결과를 보여주는 roundResult()
    
    라운드가 전부 끝나면 나오는 우승자를 출력하는 printWinners()
    
    로 나눔

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,6 +4,19 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
+    startRacingGame()
+}
+
+fun startRacingGame() {
+    val cars = carNamesInput()
+
+    var raceRound = roundInput()
+
+    raceRound = roundResult(raceRound, cars)
+
+    printWinners(raceRound, cars)
+}
+fun carNamesInput(): List<Cars> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)")
     val carNames = Console.readLine()
     val cars = carNames.split(',').map { Cars(it) }
@@ -12,13 +25,20 @@ fun main() {
             throw IllegalArgumentException("5글자 이하로 이름을 지어주세요")
         }
     }
+    return cars
+}
 
+fun roundInput(): Int {
     println("시도할 횟수는 몇 회인가요?")
-    var raceRound = Console.readLine().toInt()
+    val raceRound = Console.readLine().toInt()
     if (raceRound <= 0) {
         throw IllegalArgumentException("자연수로 입력해주세요")
     }
+    return raceRound
+}
 
+fun roundResult(inputRound:Int, cars: List<Cars>): Int{
+    var raceRound = inputRound
     println("\n실행 결과")
     while (raceRound > 0) {
         cars.forEach { car ->
@@ -28,20 +48,17 @@ fun main() {
         println()
         raceRound--
     }
+    return raceRound
+}
 
+fun printWinners(raceRound: Int, cars: List<Cars>) {
     val winners = if (raceRound == 0) {
         val headPosition = cars.maxOf { it.position }
         cars.filter { it.position == headPosition }.map { it.name }
     } else {
         mutableListOf()
     }
-    println("최종 우승자 : ${winners.joinToString(separator = ", ")}")
-}
-
-private fun inputCarNames() {
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)")
-    val carNames = Console.readLine()
-    val cars = carNames.split(',').map { Cars(it) }
+    println("최종 우승자 : ${winners.joinToString(", ")}")
 }
 
 class Cars(carName: String) {
