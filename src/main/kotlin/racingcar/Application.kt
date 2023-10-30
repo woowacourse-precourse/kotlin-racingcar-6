324aef9d6dc98778bc218f09854255b541ed7b31
DJ_Lumiere <djminstar573@gmail.com>
2023-10-30 14:24:38 +0900

    Update Application.kt
    자동차의 이름과 위치를 저장하는 raceCar class 추

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,11 @@
 package racingcar
 
+class raceCar(private val name: String, private val position: Int){
+    override fun toString(): String {
+        return "Car name : ${name}, Car position : {$position}"
+    }
+}
+
 fun main() {
     // TODO: 프로그램 구현
 }
