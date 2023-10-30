8125bd30df795beba5baa3b1ab718bfcfc6928a4
gaeun <geun5744@gmail.com>
2023-10-30 15:20:40 +0900

    [FEAT(Input)] 자동차 이름 입력 및 입력 validation 구현

--- src/main/kotlin/racingcar/domain/Input.kt
+++ src/main/kotlin/racingcar/domain/Input.kt
@@ -1,12 +1,32 @@
 package racingcar.domain
 
+import camp.nextstep.edu.missionutils.Console
+
 class Input {
 
-    fun carName() {
+    fun carName(): List<String> {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        val userInput = Console.readLine()
+        checkContainComma(userInput)
 
+        val carNameList = userInput.split(",")
+        checkNameLength(carNameList)
+
+        return carNameList
+    }
+
+    private fun checkContainComma(userInput: String) {
+        if (!userInput.contains(","))
+            throw IllegalArgumentException("쉼표(,)를 기준으로 구분해주세요")
+    }
+
+    private fun checkNameLength(carNameList: List<String>) {
+        carNameList.forEach { name ->
+            if (name.length > 5)
+                throw IllegalArgumentException("각 이름은 5자 이하만 가능합니다.")
+        }
     }
 
     fun attemptNum() {
-
     }
 }
