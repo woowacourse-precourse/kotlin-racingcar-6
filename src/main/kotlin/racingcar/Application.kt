bb651c0602bee6ede5c31593d66f41efd5b0b0e1
상은 <71699054+vmkmym@users.noreply.github.com>
2023-10-30 14:41:22 +0900

    <feature> CarNames 함수 만들기 - 자동차 이름을 입력 받을 때 쉼표로 구분하고 이름 길이를 5자 이하로 제한 - 5자 이하면 예외처리, 이상이면 입력값 반환

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -16,3 +16,12 @@ data class CarRace(val name: String) {
         return "$name : ${"-".repeat(position)}"
     }
 }
+
+private fun carNames(): String {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val inputNames = Console.readLine()
+    if (inputNames.split(",").any { it.length > 5 }) {
+        throw IllegalArgumentException()
+    }
+    return inputNames
+}
