45b79119eb1110332fad9c81348de98573068ffd
stopstone <wltjr6432@naver.com>
2023-10-30 23:43:41 +0900

    move: input 관련 기능 View 단으로 이동
    - 출력 문구를 const val로 상수 지정
    - Controller에 있던 inputCarName(), inputTryRaceCount() 등 입력에 관련된 기능 이동

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -8,63 +8,14 @@ import racingcar.view.OutputView
 
 class RacingCarController(private val inputView: InputView, private val outputView: OutputView) {
     fun racingRun() {
-        val carNames = inputCarName()
-        val tryCount = inputTryRaceCount()
+        val carNames = inputView.inputCarName()
+        val tryCount = inputView.inputTryRaceCount()
         val cars: List<Cars> = carNames.map { Cars(it) }
 
         tryRacing(cars, tryCount)
         outputView.raceWinners(cars)
     }
 
-    private fun inputCarName(): List<String> {
-        inputView.inputCarNamesMessage()
-        val inputName = Console.readLine()
-        validateCommaNotFind(inputName)
-        return splitInputName(inputName.trim())
-    }
-
-    private fun splitInputName(inputName: String): List<String> {
-        val splitString = inputName.split(",")
-        validateInputName(splitString)
-        validateDuplicateCheck(splitString)
-        return splitString
-    }
-
-    private fun validateInputName(splitInputName: List<String>) {
-        splitInputName.map { names ->
-            if (names.length > 5) throw IllegalArgumentException("5글자가 넘는 자동차 이름이 있습니다.")
-        }
-    }
-
-    private fun validateDuplicateCheck(splitInputName: List<String>) {
-        for (x in splitInputName) {
-            require(splitInputName.count { it == x } <= 1) { "중복된 자동차 이름이 있습니다." } // 같은 수가 1개 이상인 경우
-        }
-    }
-
-    private fun validateCommaNotFind(inputName: String) {
-        if (!inputName.contains(",")){
-            throw IllegalArgumentException("입력값에 쉼표가 포함되지 않았습니다.")
-        }
-    }
-
-    private fun inputTryRaceCount(): Int {
-        val tryCount:Int
-        inputView.inputTryRaceCountMessage()
-        try {
-            tryCount = Console.readLine().toInt()
-        } catch (e: NumberFormatException) {
-            throw IllegalArgumentException("숫자가 아닌 다른 값이 입력 되었습니다.")
-        }
-        validateInputTryRace(tryCount)
-
-        return tryCount
-    }
-
-    private fun validateInputTryRace(tryCount: Int) {
-        if (tryCount == 0)
-            throw IllegalArgumentException("0은 입력될 수 없습니다.")
-    }
 
     private fun tryRacing(cars: List<Cars>, tryCount: Int) {
         outputView.printHeadResult()
--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,12 +1,72 @@
 package racingcar.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
-    fun inputCarNamesMessage() {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    companion object {
+        private val INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        private val INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
     }
 
-    fun inputTryRaceCountMessage() {
-        println("시도할 횟수는 몇 회인가요?")
+    private fun inputCarNamesMessage() {
+        println(INPUT_CAR_NAMES_MESSAGE)
     }
+
+    fun inputCarName(): List<String> {
+        inputCarNamesMessage()
+        val inputName = Console.readLine()
+        validateCommaNotFind(inputName)
+        return splitInputName(inputName.trim())
+    }
+
+    private fun splitInputName(inputName: String): List<String> {
+        val splitString = inputName.split(",")
+        validateInputName(splitString)
+        validateDuplicateCheck(splitString)
+        return splitString
+    }
+
+    private fun validateInputName(splitInputName: List<String>) {
+        splitInputName.map { names ->
+            if (names.length > 5) throw IllegalArgumentException("5글자가 넘는 자동차 이름이 있습니다.")
+        }
+    }
+
+    private fun validateDuplicateCheck(splitInputName: List<String>) {
+        for (x in splitInputName) {
+            require(splitInputName.count { it == x } <= 1) { "중복된 자동차 이름이 있습니다." } // 같은 수가 1개 이상인 경우
+        }
+    }
+
+    private fun validateCommaNotFind(inputName: String) {
+        if (!inputName.contains(",")){
+            throw IllegalArgumentException("입력값에 쉼표가 포함되지 않았습니다.")
+        }
+    }
+
+    private fun inputTryRaceCountMessage() {
+        println(INPUT_TRY_COUNT_MESSAGE)
+    }
+
+    fun inputTryRaceCount(): Int {
+        val tryCount:Int
+        inputTryRaceCountMessage()
+        try {
+            tryCount = Console.readLine().toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("숫자가 아닌 다른 값이 입력 되었습니다.")
+        }
+        validateInputTryRace(tryCount)
+
+        return tryCount
+    }
+
+    private fun validateInputTryRace(tryCount: Int) {
+        if (tryCount == 0)
+            throw IllegalArgumentException("0은 입력될 수 없습니다.")
+    }
+
+
+
 
 }
