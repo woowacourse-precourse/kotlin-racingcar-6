9572783846a15c1a30c178cac92cdda155cfb709
이빵주 <yungju9982@naver.com>
2023-10-31 03:37:48 +0900

    [feat] 입력된 횟수의 유효성을 검사하는 함수 구현

--- src/main/kotlin/racingcar/MoveCountInput.kt
+++ src/main/kotlin/racingcar/MoveCountInput.kt
@@ -1,4 +1,13 @@
 package racingcar
 
+import racingcar.ErrorMessage.INVALID_ATTEMPT_COUNT_ERROR
+
 object MoveCountInput {
+    private const val MIN_ATTEMPT_COUNT = 0
+
+    private fun validateAttempts(attempts: Int) {
+        if (attempts <= MIN_ATTEMPT_COUNT) {
+            throw IllegalArgumentException(INVALID_ATTEMPT_COUNT_ERROR)
+        }
+    }
 }
