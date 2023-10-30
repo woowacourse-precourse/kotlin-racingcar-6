2f5e69ab45e4f27d865ea466bb4b916423b64a80
Jejun Min <alswpwns11@naver.com>
2023-10-31 01:46:29 +0900

    Model 패키지 구현

--- src/main/kotlin/racingcar/Model/Race.kt
+++ src/main/kotlin/racingcar/Model/Race.kt
@@ -1,23 +1,25 @@
-package racingcar.Domain
+package racingcar.Model
 
-import camp.nextstep.edu.missionutils.Randoms
-import camp.nextstep.edu.missionutils.Console
-import racingcar.Domain.Car
 import racingcar.View.OutputView
 
-class Race(private val carNames: List<String>, private val tries: Int) {
+class Race(private val carNames: List<String>, val tries: Int) {
+    // 게임이 시작해서 어떻게 동작하는지 작성해야 함.
+    // 게임의 승자를 어떻게 고를 것인지 작성해야 함.
 
-   private val cars = carNames.map {Car(it)}
-    fun start() {
+    private val outputView: OutputView = OutputView()
+    private val cars = carNames.map { Car(it) }
+
+    fun race() {
         repeat(tries) {
-            cars.forEach { it.carMove() }
-            OutputView.printCarsPosition(cars)
+            cars.forEach { car ->
+                car.moveCar()
+            }
+            outputView.printCarsPosition(cars)
         }
-        OutputView.printWinners(cars.filter { it.distance == cars.maxOf { car -> car.distance } })
     }
 
-    private fun getWinners(): List<Car> {
-        val maxDistance = cars.maxOf { it.getDistance() }
-        return cars.filter {it.getDistance() == maxDistance }
+    fun getWinners(): List<Car> { // 가장 멀리 이동한 자동차를 우승자로 반환하는 함수
+        val maxDistance = cars.maxOf { it.fetchDistance() }
+        return cars.filter {it.fetchDistance() == maxDistance }
     }
 }
