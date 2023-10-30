11fd8a9a61059e9163311f177fa2b8567a4e3dd5
j2j2 <fineimagination@gmail.com>
2023-10-31 03:08:45 +0900

    feat(racingcar): 차의 전진 또는 정지를 결정하는 기능 구현

--- src/main/kotlin/racingcar/domain/Race.kt
+++ src/main/kotlin/racingcar/domain/Race.kt
@@ -8,7 +8,9 @@ class Race {
         return null
     }
 
-    fun racing(racingCar: String): Boolean? {
-        return null
+    fun racing(racingCar: String): Boolean {
+        val go: Boolean = true
+        val stop: Boolean = false
+        return if (Randoms.pickNumberInRange(0, 9) >= 4) go else stop
     }
 }
