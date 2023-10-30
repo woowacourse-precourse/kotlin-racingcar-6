00de92aef6fc351d2990ec6aafb943bc848e4703
이홍진\iit80 <hong80714@gmail.com>
2023-10-30 19:25:18 +0900

    [fix] 우승조건 논리오류 fix 입력한 숫자만큼 가야지만 우승하는 전진횟수 우승조건을 입력한 라운드안에 가장 멀리간 자동차가 우승하는 매커니즘으로 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,33 +6,36 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)")
     val carNames = Console.readLine()
-    if (carNames.length > 5){
-        throw IllegalArgumentException("5글자 이하로 이름을 지어주세요")
-    }
     val cars = carNames.split(',').map { Cars(it) }
+    for (car in cars) {
+        if (car.name.length > 5) {
+            throw IllegalArgumentException("5글자 이하로 이름을 지어주세요")
+        }
+    }
 
     println("시도할 횟수는 몇 회인가요?")
-    val winPoint = Console.readLine().toInt()
-    if (winPoint <= 0){
+    var raceRound = Console.readLine().toInt()
+    if (raceRound <= 0) {
         throw IllegalArgumentException("자연수로 입력해주세요")
     }
-    while (cars.none { it.position >= winPoint }){
-        cars.forEach { car->
+
+    println("\n실행 결과")
+    while (raceRound > 0) {
+        cars.forEach { car ->
             car.move()
             println("${car.name} : ${"-".repeat(car.position)}")
         }
         println()
+        raceRound--
     }
 
-    //만약 car.position == winPoint 일때
-    //그 car에 대해서 car.name이 출력
-    val winner = mutableListOf<String>()
-    for (car in cars) {
-        if (car.position == winPoint){
-            winner.add(car.name)
-        }
+    val winners = if (raceRound == 0) {
+        val headPosition = cars.maxOf { it.position }
+        cars.filter { it.position == headPosition }.map { it.name }
+    } else {
+        mutableListOf()
     }
-    print("최종 우승자 : ${winner.joinToString(separator = ", ")}")
+    println("최종 우승자 : ${winners.joinToString(separator = ", ")}")
 }
 
 private fun inputCarNames() {
@@ -45,10 +48,9 @@ class Cars(carName: String) {
     var position = 0
     val name = carName
 
-    fun move(){
-        if (Randoms.pickNumberInRange(0, 9)>=4){
+    fun move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
             position++
         }
     }
 }
-
