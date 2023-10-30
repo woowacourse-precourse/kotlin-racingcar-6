eab3165dfe9f5a27a06897a3e4ff201c0a1ebe02
oh in seong <superohinsung@naver.com>
2023-10-30 13:16:36 +0900

    [feat] 정확한 함수 이름 표현 수정과 차의 이름 입력에서 예외처리 기능 추가
    이름이 여러 개 입력될 경우 중복성 검증

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -14,20 +14,28 @@ class RacingGame {
 
         val carsList = carsInputString.split(",").filter { it.isNotBlank() }.map { Car(it) }
         requireValidCarsInput(carsInputString = carsInputString, carsList = carsList)
+        requireCheckingForDuplicateNames(carsList = carsList)
 
         printer.printOutEnteringAttemptsNumber()
         val attemptsNumberInputString = userInput()
-        requirePositiveNumber(attemptsNumberInputString = attemptsNumberInputString)
+        requireValidAttemptsNumberInput(attemptsNumberInputString = attemptsNumberInputString)
         val attemptsNumber = attemptsNumberInputString.toInt()
 
     }
 
     private fun requireValidCarsInput(carsInputString: String, carsList: List<Car>) {
         require(carsInputString.count { it == ',' } == carsList.size - 1)
-
     }
 
-    private fun requirePositiveNumber(attemptsNumberInputString: String) {
+    private fun requireCheckingForDuplicateNames(carsList: List<Car>) {
+        val nameSet = mutableSetOf<String>()
+        for (car in carsList) {
+            require(car.getCarName() !in nameSet)
+            nameSet.add(car.getCarName())
+        }
+    }
+
+    private fun requireValidAttemptsNumberInput(attemptsNumberInputString: String) {
         require(attemptsNumberInputString.matches(Regex("^\\d+\$")))
         require(attemptsNumberInputString.toInt() >= 0)
     }
