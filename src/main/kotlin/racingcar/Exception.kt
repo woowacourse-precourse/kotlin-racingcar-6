bf5803a6596e7152abe5fc3cd58fa468d0d3b53a
jklom0326 <jklom0326@gmail.com>
2023-10-31 01:41:16 +0900

    feat: round 횟수 예외 처리 추가

--- src/main/kotlin/racingcar/Exception.kt
+++ src/main/kotlin/racingcar/Exception.kt
@@ -7,4 +7,10 @@ class Exception {
             if (it.isEmpty()) throw IllegalArgumentException("이름이 비어있습니다.")
         }
     }
+
+    fun roundsValidation(input: String): Int {
+        val count = input.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닙니다.")
+        if (count < 0) throw IllegalArgumentException("0보다 작은 수는 입력할 수 없습니다.")
+        return count
+    }
 }
