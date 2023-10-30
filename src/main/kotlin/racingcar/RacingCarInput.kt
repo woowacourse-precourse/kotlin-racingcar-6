eb84aec858b22f3610aee9642043e05c77c0d58b
이빵주 <yungju9982@naver.com>
2023-10-31 02:22:27 +0900

    [feat] 자동차 이름 길이 검사하는 함수 구현

--- src/main/kotlin/racingcar/RacingCarInput.kt
+++ src/main/kotlin/racingcar/RacingCarInput.kt
@@ -1,10 +1,12 @@
 package racingcar
 
 import racingcar.ErrorMessage.CAR_MINIMUM_COUNT_ERROR
+import racingcar.ErrorMessage.CAR_NAME_LENGTH_OVER_ERROR
 import racingcar.ErrorMessage.DUPLICATE_CAR_NAME_ERROR
 
 object RacingCarInput {
     private const val MIN_CAR_COUNT = 1
+    private const val MAX_CAR_NAME_LENGTH = 5
 
     private fun validateCarNameDuplicate(carNameMap: Map<String, Int>) {
         val carNames = carNameMap.keys
@@ -16,6 +18,18 @@ object RacingCarInput {
     private fun validateMinimumCarCount(carNameMap: Map<String, Int>) {
         if (carNameMap.size <= MIN_CAR_COUNT) {
             throw IllegalArgumentException(CAR_MINIMUM_COUNT_ERROR)
+        }
+    }
+
+    private fun validateCarNameLength(carNameMap: Map<String, Int>) {
+        for (carName in carNameMap.keys) {
+            validateLength(carName)
+        }
+    }
+
+    private fun validateLength(carName: String) {
+        if (carName.length > MAX_CAR_NAME_LENGTH) {
+            throw IllegalArgumentException(CAR_NAME_LENGTH_OVER_ERROR)
         }
     }
 }
