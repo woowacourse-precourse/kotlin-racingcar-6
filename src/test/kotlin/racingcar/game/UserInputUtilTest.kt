e0578866e7c71b45844df194eec5270f583f43a4
민대인 <bamin0422@gmail.com>
2023-10-30 18:19:25 +0900

    test: UserInputUtilTest 테스팅 메서드 명명 및 구성

--- src/test/kotlin/racingcar/game/UserInputUtilTest.kt
+++ src/test/kotlin/racingcar/game/UserInputUtilTest.kt
@@ -5,10 +5,32 @@ import org.junit.jupiter.api.Test
 internal class UserInputUtilTest {
 
     @Test
-    fun getRacingCars() {
+    fun `getRacingCars 리턴 값 테스팅`() {
+
     }
 
     @Test
-    fun getAttempts() {
+    fun `getRacingCars - 자동차가 없는 경우 예외처리`() {
+
+    }
+
+    @Test
+    fun `getRacingCars - 중복된 이름인 경우 예외처리`() {
+
+    }
+
+    @Test
+    fun `getRacingCars - 이름의 길이가 유효하지 않은 경우 예외처리`() {
+
+    }
+
+    @Test
+    fun `getAttempts 리턴 값 테스팅`() {
+
+    }
+
+    @Test
+    fun `getAttempts 메서드 - 시도 횟수에 자연수가 아닌 경우 예외처리`() {
+
     }
 }
