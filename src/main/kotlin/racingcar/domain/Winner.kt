ae42464f1fc29da55d15c8b95ba2efb71ea034fc
jiwon2724 <stopone3119@naver.com>
2023-10-30 23:31:20 +0900

    [chore] : 변수 이름 변경

--- src/main/kotlin/racingcar/domain/Winner.kt
+++ src/main/kotlin/racingcar/domain/Winner.kt
@@ -1,18 +1,18 @@
 package racingcar.domain
 
 class Winner {
-    fun raceGameWinner(racingRoundResult: Map<String, ArrayList<Int>>): String {
+    fun raceGameWinner(racingResult: Map<String, ArrayList<Int>>): String {
         val winner = StringBuilder(WINNER)
-        val moveMaxCount = racingRoundResult.maxBy { racingRound -> racingRound.value.max() }.value.max()
+        val moveMaxCount = racingResult.maxBy { racingRound -> racingRound.value.max() }.value.max()
 
-        racingRoundResult.forEach { racingRound ->
-            val isMax = racingRound.value.findMax(moveMaxCount)
-            if(isMax) winner.append(racingRound.key).append(SPACE_COMMA)
+        racingResult.forEach { racingRound ->
+            val isMaxMoveCountMultiple = racingRound.value.findMaxMoveCount(moveMaxCount)
+            if (isMaxMoveCountMultiple) winner.append(racingRound.key).append(SPACE_COMMA)
         }
         return winner.toString().removeSuffix(SPACE_COMMA)
     }
 
-    private fun ArrayList<Int>.findMax(maxMoveCount: Int): Boolean = contains(maxMoveCount)
+    private fun ArrayList<Int>.findMaxMoveCount(maxMoveCount: Int): Boolean = contains(maxMoveCount)
 
     companion object {
         private const val WINNER = "최종 우승자 : "
