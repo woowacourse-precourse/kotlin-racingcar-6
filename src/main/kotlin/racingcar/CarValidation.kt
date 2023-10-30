8bf7cef3601fd8220bd2a6ec8e7a83a9549164a0
stopkite <stopkite98@gmail.com>
2023-10-31 03:17:10 +0900

    feat: (예외) 이름 가장자리 공백여부 검사

--- src/main/kotlin/racingcar/CarValidation.kt
+++ src/main/kotlin/racingcar/CarValidation.kt
@@ -3,13 +3,18 @@ package racingcar
 class CarValidation {
     fun checkNameInputValidation(input: List<String>) {
         isInputWithinFiveChar(input)
+        hasWhiteSpaceAtEdge(input)
     }
 
     private fun isInputWithinFiveChar(list: List<String>) {
-        require(list.any { it.length > 5 }) { "자동차 이름은 5자 이하만 입력 가능합니다." }
+        if (list.any { it.length > 5 }) {
+            throw IllegalArgumentException("자동차 이름은 5자 이하만 입력 가능합니다.")
+        }
     }
 
-
-
-
+    private fun hasWhiteSpaceAtEdge(list: List<String>) {
+        if (list.any { it.startsWith(' ') || it.endsWith(' ') }) {
+            throw IllegalArgumentException("이름의 시작과 끝에는 공백 입력이 불가능합니다.")
+        }
+    }
 }
