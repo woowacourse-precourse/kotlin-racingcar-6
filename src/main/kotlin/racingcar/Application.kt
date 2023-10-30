22f551f3f53b9df44dc1e09459ab10235f91f16c
LeeChoongHwan <choo901@naver.com>
2023-10-30 23:10:53 +0900

    [feat] read number of moves

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,6 +5,12 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carInput = Console.readLine()
     val splitCarList = splitCarName(carInput)
+
+    println("시도할 횟수는 몇 회인가요?")
+    val numberOfMove = Console.readLine()
+    checkNumberOfMove(numberOfMove)
+
+
 }
 
 fun splitCarName(carInput: String): List<String> {
@@ -12,5 +18,14 @@ fun splitCarName(carInput: String): List<String> {
         carInput.isBlank() -> throw IllegalArgumentException("빈칸을 입력할 수 없습니다.")
         carInput.split(",").any { it.length > 5 } -> throw IllegalArgumentException("자동차 이름은 5자 이하이며, 쉼표로 구분되어야 합니다.")
         else -> carInput.split(",")
+    }
+}
+
+fun checkNumberOfMove(numberOfMove : String) {
+    return when {
+        numberOfMove.isBlank() -> throw IllegalArgumentException("빈칸을 입력할 수 없습니다.")
+        numberOfMove.toIntOrNull() == null -> throw IllegalArgumentException("숫자를 입력해야 합니다.")
+        numberOfMove.toInt() <= 0 -> throw IllegalArgumentException("양의 정수를 입력하세요.")
+        else -> return
     }
 }
