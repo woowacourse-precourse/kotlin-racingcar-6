000e4c2159267ec8c8bf1a48ed31ff701f09eb1a
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2023-10-31 01:16:35 +0900

    feat: 클래스 추가
    - 차의 상태 저장하는 Car 클래스 추가
    - 게임의 상태 저장하는 GameStatus 클래스 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,20 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     gameStart()
+}
+
+class Car {
+    var name: String = ""
+    var movementCount: Int = 0
+}
+
+class GameStatus {
+    var listOfCar = mutableListOf<Car>()
+    var countOfTrial: Int = 0
 }
 
 fun gameStart() {
@@ -13,7 +24,28 @@ fun gameStart() {
 
     println("시도할 횟수는 몇 회인가요?")
     val inputCount = Console.readLine()
-    val counOfTrial = checkValidNumber(inputCount)
+    val countOfTrial = checkValidNumber(inputCount)
+
+    startRace(listOfCarName, countOfTrial)
+}
+
+fun startRace(listOfCarName: List<String>, count: Int) {
+    var carMovementCount = mutableListOf<Int>()
+    println("실행 결과")
+    for (i in 0..count) {
+        carMovementCount += selectRandomNumber(listOfCarName)
+    }
+}
+
+fun selectRandomNumber(listOfCarName: List<String>): List<Int> {
+    var isCarMoved = mutableListOf<Int>(listOfCarName.size)
+    for (i in 0..listOfCarName.size) {
+        val randomNumber = Randoms.pickNumberInRange(0,9)
+        if (randomNumber >= 4) {
+            isCarMoved[i]++
+        }
+    }
+    return isCarMoved
 }
 
 fun checkValidName(stringOfNames: String): List<String> {
