49699fe59a69211ac007c17162f37b9bfc549784
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-10-30 14:28:48 +0900

    refactor(Remove unused imports)
    사용하지않는 import들을 지워주었습니다

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,6 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
 import racingcar.controllers.CarRaceController
-import java.lang.IllegalArgumentException
 
 fun main() {
     val controller = CarRaceController()
