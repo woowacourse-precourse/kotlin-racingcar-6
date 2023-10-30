d6461e4fbed46df206d2598fb6d5f27a3ddccd5f
murjune <sis9221302@gmail.com>
2023-10-31 02:40:49 +0900

    feat(Race) : 경주차들과 심판이 있는 경주로 객체
    - 경기 결과 getter
    - 경기 우승자 getter
    - race가 진행중인지 판단하는 getter
    - 1step Race 진행

---
+++ src/main/kotlin/racingcar/Race.kt
@@ -0,0 +1,17 @@
+package racingcar
+
+class Race(
+    private val attemptCount: Int,
+    private val cars: List<Car>,
+    private val referee: Referee = Referee(attemptCount)
+) {
+    val result get() = referee.determineRaceResult(cars)
+    val winner get() = referee.determineWinner(cars)
+    val isRunning get() = winner.cars.isEmpty()
+
+    fun runOnStep() {
+        cars.forEach { car ->
+            if (referee.canCarMove(car)) car.move()
+        }
+    }
+}
