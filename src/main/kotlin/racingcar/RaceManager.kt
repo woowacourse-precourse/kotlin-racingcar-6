e3e267aaf3e37be3f15372abbf9637db2b05b49f
이빵주 <yungju9982@naver.com>
2023-10-31 06:15:36 +0900

    [feat] 자동차 경주 함수 구현

--- src/main/kotlin/racingcar/RaceManager.kt
+++ src/main/kotlin/racingcar/RaceManager.kt
@@ -7,6 +7,13 @@ object RaceManager {
     private const val RANDOM_END_NUMBER = 9
     private const val MOVE_MIN_NUMBER = 4
 
+    fun race(cars: List<Car>): List<Car> {
+        for (car in cars) {
+            moveIfPossible(car)
+        }
+        return cars
+    }
+
     private fun moveIfPossible(car: Car) {
         if (Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER) >= MOVE_MIN_NUMBER) {
             car.move()
