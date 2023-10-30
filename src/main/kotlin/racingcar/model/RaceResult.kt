d45c974911e6d24e7dad4f3050470891cc69307a
murjune <sis9221302@gmail.com>
2023-10-31 00:40:19 +0900

    feat(RaceResult) : Race의 결과를 나타내는 data class
    - toString overriding : 각 CarState과 줄 바꿈을 합쳐서 반환한다

---
+++ src/main/kotlin/racingcar/model/RaceResult.kt
@@ -0,0 +1,12 @@
+package racingcar.model
+
+data class RaceResult(val carStateList: List<CarState>) {
+
+    override fun toString(): String {
+        return carStateList.joinToString(NEW_LINE)
+    }
+
+    companion object {
+        private const val NEW_LINE = "\n"
+    }
+}
