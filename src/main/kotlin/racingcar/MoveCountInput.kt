517189c9df019a420a6cc132ff70ad5767ca6119
이빵주 <yungju9982@naver.com>
2023-10-31 03:38:26 +0900

    [feat] 이동횟수를 입력받는 함수 구현

--- src/main/kotlin/racingcar/MoveCountInput.kt
+++ src/main/kotlin/racingcar/MoveCountInput.kt
@@ -1,9 +1,26 @@
 package racingcar
 
+import racingcar.Converter.convertStringToInt
 import racingcar.ErrorMessage.INVALID_ATTEMPT_COUNT_ERROR
+import racingcar.ErrorMessage.NUMBER_FORMAT_ERROR
+import racingcar.GameMessage.INPUT_NUMBER_ATTEMPTS_MESSAGE
+import racingcar.UserInput.getUserInput
 
 object MoveCountInput {
     private const val MIN_ATTEMPT_COUNT = 0
+
+    fun getMoveCount(): Int {
+        println(INPUT_NUMBER_ATTEMPTS_MESSAGE)
+        val userInput = getUserInput()
+        return runCatching {
+            convertStringToInt(userInput).apply { validateAttempts(this) }
+        }.getOrElse { throwable ->
+            when (throwable) {
+                is NumberFormatException -> throw IllegalArgumentException(NUMBER_FORMAT_ERROR)
+                else -> throw IllegalArgumentException(throwable)
+            }
+        }
+    }
 
     private fun validateAttempts(attempts: Int) {
         if (attempts <= MIN_ATTEMPT_COUNT) {
