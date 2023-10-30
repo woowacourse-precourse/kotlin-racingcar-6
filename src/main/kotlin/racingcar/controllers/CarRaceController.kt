9b09f388c78a797d2472ff3618f5b6ad1edd0ea0
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-10-30 17:30:33 +0900

    feat(Add exception handling, Add splitMethod)
    입력에따른 예외처리기능 추가 및 테스트케이스 작성을 위해 console.readline().split 해서 바로 변수에 넣어둔걸 메서드로 따로 분리하였다

--- src/main/kotlin/racingcar/controllers/CarRaceController.kt
+++ src/main/kotlin/racingcar/controllers/CarRaceController.kt
@@ -24,18 +24,31 @@ class CarRaceController {
 
     private fun inputCarNames(): List<String> {
         view.printSentenceWriteCarName()
-        val carNames = Console.readLine().split(",")
+        val carNames = splitCarNames(Console.readLine())
         val validNames = carNames.filter { it.length <= 5 }
+        println(validNames)
 
-        if (validNames.size != carNames.size) {
-            throw IllegalArgumentException("5자 이하로 입력해주세요")
+        when {
+            validNames.size != carNames.size -> throw IllegalArgumentException("5자 이하로 입력해주세요.")
+            carNames.any { it.isBlank() } -> throw IllegalArgumentException("공백입력 불가합니다")
+            carNames.size < 2 -> throw IllegalArgumentException("1명이상 입력해주세요")
+            carNames.toSet().size != carNames.size -> throw IllegalArgumentException("중복된 이름은 불가합니다")
+            else -> return validNames
         }
+    }
 
-        return validNames
+    fun splitCarNames(readLine: String): List<String> {
+        return readLine.split(",")
     }
 
     private fun inputNumberOfAttempts(): Int {
         view.printSentenceWordWriteAttempts()
-        return Console.readLine().toInt()
+        val attempt = Console.readLine()
+
+        if (attempt.isEmpty() || attempt.isBlank() || attempt.toIntOrNull() == null || attempt.toInt() <= 0) {
+            throw IllegalArgumentException("정수만 입력해주세요")
+        }
+
+        return attempt.toInt()
     }
 }
