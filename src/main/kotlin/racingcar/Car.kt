c0823d11837aff2f1527e1ac72b46f824d006cc9
oh in seong <superohinsung@naver.com>
2023-10-30 14:16:46 +0900

    [feat] 입력받은 이름에 대해서 에외를 처리하는 기능

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,9 +1,14 @@
 package racingcar
 
-class Car(
-    private val name: String,
-    private val howAdvanced: Int = 0
-) {
+class Car(private val name: String, private var howGoForward: Int = 0) {
+    init {
+        require(name.length <= NAME_MAX_LENGTH)
+        require(name.matches(Regex("^[A-Za-z가-힣]*\$")))
+    }
+
     fun getCarName() = this.name
 
+    companion object {
+        const val NAME_MAX_LENGTH = 5
+    }
 }
