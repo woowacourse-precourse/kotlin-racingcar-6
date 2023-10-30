458eb3ecc0b49c21864ac230b651c1ba71ed264c
hxeyexn <kck85075@kumoh.ac.kr>
2023-10-31 01:16:29 +0900

    Refactor: Validation 클래스내 메서드 파라미터 추가

--- src/main/kotlin/racingcar/Validation.kt
+++ src/main/kotlin/racingcar/Validation.kt
@@ -2,34 +2,32 @@ package racingcar
 
 import java.lang.IllegalArgumentException
 
-class Validation(player: Player) {
+class Validation {
 
-    private val inputCarsName = player.inputCarName()
-    private val carList = inputCarsName.split(",")
-
-    fun isValidNumberOfCars() {
-        if (!inputCarsName.contains(",")) {
+    fun isValidNumberOfCars(carsName: String) {
+        if (carsName.contains(",")) {
             throw IllegalArgumentException("자동차 이름은 2대 이상 입력해 주세요.")
         }
     }
 
-    fun checkContainsSpace() {
-        if (inputCarsName.contains(" ")) {
+    fun checkContainsSpace(carsName: String) {
+        if (carsName.contains(" ")) {
             throw IllegalArgumentException("공백은 들어갈 수 없습니다.")
         }
     }
 
-    fun checkCarNameDuplication() {
-        if (carList.distinct().size != carList.size) {
+    fun checkCarNameDuplication(carsNameList: List<String>) {
+        if (carsNameList.distinct().size != carsNameList.size) {
             throw IllegalArgumentException("중복된 이름은 입력할 수 없습니다.")
         }
     }
 
-    fun checkCarNameLength() {
-        carList.forEach {
+    fun checkCarNameLength(carsNameList: List<String>) {
+        carsNameList.forEach {
             if (it.length > 5) {
                 throw IllegalArgumentException("자동차 이름은 5자 이하만 입력 가능합니다.")
             }
         }
     }
+
 }
