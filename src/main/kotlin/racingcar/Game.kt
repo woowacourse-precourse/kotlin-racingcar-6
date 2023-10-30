ead19c94512bee4c1a289a7e84c18e68f915c730
yoon <cbj0010@naver.com>
2023-10-31 00:20:58 +0900

    [Feat] 랜덤 수에 맞게 자동차 GO, STOOP 라인 출력

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -1,13 +1,51 @@
 package racingcar
 
-class Game {
+import racingcar.Car.Companion.GO
+import racingcar.Car.Companion.STOP
 
-    private fun playGame() {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-        val carName = Driver().inputCarName()
-        println("시도할 횟수는 몇 회인가요?")
-        Driver().inputTryGame()
+class Game {
+    lateinit var carNameList: List<String>
+    var tryGameNum: Int = 0
+    fun playGame() {
+        playUser()
+        playCar()
     }
 
+    private fun playUser() {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        carNameList = Driver().inputCarName()
+        Driver().checkCarNameExeption()
+        println("시도할 횟수는 몇 회인가요?")
+        tryGameNum = Driver().inputTryGame()
+    }
+
+    private fun playCar() {
+        println("실행 결과")
+        playResult()
+
+    }
+
+    private fun playResult() {
+        for (carName in carNameList) {
+            print("$carName : ")
+            print("${printPlayLine()}")
+            println()
+        }
+    }
+
+    private fun printPlayLine(): String {
+        val randomNum = Car().generatorRandomNum(tryGameNum)
+        val goList = Car().checkIsGoToStop(randomNum)
+        val result = StringBuilder()
+
+        for (line in goList) {
+            when (line) {
+                GO -> result.append("-")
+                STOP -> result.append("")
+            }
+        }
+
+        return result.toString()
+    }
 
 }
