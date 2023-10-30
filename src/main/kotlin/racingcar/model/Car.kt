0020fde18e98aca99a7022cd99c2e10c9b466942
m6z1 <sonmyungj1zz@gmail.com>
2023-10-31 00:39:02 +0900

    refactor(Car) : 자동차 이름 입력 및 단독 출전인 경우인지 확인하는 클래스

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,4 +1,23 @@
 package racingcar.model
 
+import racingcar.inputView
+import racingcar.validateCarName
+
 class Car {
+    private val soloRacingGame = SoloRacingGame()
+    fun getCarName() {
+        val inputCarName = inputView.inputView()
+        validateCarName.validateInputBlank(inputCarName)
+        checkSoloCar(inputCarName)
+    }
+
+    private fun checkSoloCar(inputCarName: String): Boolean {
+        if (!inputCarName.contains(",")) {
+            soloRacingGame.soloCarGame(inputCarName)
+        } else {
+            val multiCarName = inputCarName.split(",")
+            validateCarName.validateInputMultiCarName(multiCarName)
+        }
+        return true
+    }
 }
