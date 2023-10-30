76a47120ed79ec0cd913f36fa274195ecad6ee92
이빵주 <yungju9982@naver.com>
2023-10-31 03:38:07 +0900

    [feat] String을 Int로 바꾸는 컨버터 함수 추가

--- src/main/kotlin/racingcar/Converter.kt
+++ src/main/kotlin/racingcar/Converter.kt
@@ -2,4 +2,6 @@ package racingcar
 
 object Converter {
     fun convertStringToMap(input: String) = input.split(",").map { it.trim() }.associateWith { 0 }
+
+    fun convertStringToInt(input: String) = input.toInt()
 }
