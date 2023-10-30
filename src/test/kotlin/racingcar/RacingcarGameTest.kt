0f8d7b25e0ea4adfd82c7228f1e39918ff6246e2
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-31 01:09:34 +0900

    Test (RacingcarGameTest) : 최종 우승자가 올바른지 검증

--- src/test/kotlin/racingcar/RacingcarGameTest.kt
+++ src/test/kotlin/racingcar/RacingcarGameTest.kt
@@ -7,12 +7,14 @@ import racingcar.controller.RacingcarController
 import racingcar.model.GenerateRandomNumber
 import org.junit.jupiter.api.Assertions.assertTrue
 import racingcar.model.MoveCar
+import racingcar.model.Winner
 
 
 class RacingcarGameTest {
     private val racingcarController = RacingcarController()
     private val generateRandomNumber = GenerateRandomNumber()
     private val moveCar = MoveCar()
+    private val winner = Winner()
     @Test
     fun `입력한 자동차 이름이 쉼표 기준으로 구분 검증`() {
         val input = "test1,test2,test3"
@@ -25,5 +27,16 @@ class RacingcarGameTest {
     fun `0 ~ 9 사이 무작위 값만 생성 검증`() {
         val result = generateRandomNumber.generate()
         assertTrue(result in 0..9)
+    }
+
+    @Test
+    fun `최종 우승자가 올바른지 검증`() {
+        val input = mutableMapOf(
+            "test1" to 3,
+            "test2" to 2,
+            "test3" to 4
+        )
+        val result = winner.calculateWinner(input)
+        Assertions.assertThat(result).containsExactly("test3")
     }
 }
