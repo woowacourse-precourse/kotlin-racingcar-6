1f6e847d53f33bb2dc30b530c357daace5411b0b
JeongSG <jsk41755@naver.com>
2023-10-30 17:17:58 +0900

    feat: 자동차 이름 목록 입력 getCarNames()

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,9 +5,18 @@ class RacingCar(val name: String) {
 
 fun main() {
     try {
-
+        val carNames = getCarNames()
 
     } catch (e: IllegalArgumentException) {
         println("에러: ${e.message}")
     }
+}
+
+fun getCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = readLine()?.split(",")?.map { it.trim() } ?: throw IllegalArgumentException("잘못된 입력입니다.")
+    if (input.any { !it.matches(Regex("[a-zA-Z]+")) || it.length !in 1..5 }) {
+        throw IllegalArgumentException("자동차 이름은 알파벳 문자로 이루어져 있고, 1자 이상 5자 이하만 가능합니다.")
+    }
+    return input
 }
