7ca3b0c0835fc79b4be851004ef765886d7133e4
dpcks0509 <dpcks0509@gmail.com>
2023-10-31 02:30:32 +0900

    test: 자동차 이름 첫글자 공백 예외 처리 테스트
    자동차 이름 첫글자 공백에 대한 테스트 코드를 추가하였다.

--- src/test/kotlin/racingcar/RacingCarTest.kt
+++ src/test/kotlin/racingcar/RacingCarTest.kt
@@ -27,10 +27,16 @@ class RacingCarTest {
         assertThrows<IllegalArgumentException> { inputView.validateRacingCarName(racingCarNameList) }
     }
 
-
     @Test
     fun `자동차 이름 쉼표(,) 뒤 빈값 예외 처리`() {
         val racingCarNameList = "pobi,woni,".split(",")
+
+        assertThrows<IllegalArgumentException> { inputView.validateRacingCarName(racingCarNameList) }
+    }
+
+    @Test
+    fun `자동차 이름 첫글자 공백 예외 처리`() {
+        val racingCarNameList = "pobi, woni".split(",")
 
         assertThrows<IllegalArgumentException> { inputView.validateRacingCarName(racingCarNameList) }
     }
