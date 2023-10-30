9bb838f0b96de41d29f7d07013bb621a06593c63
DJ_Lumiere <djminstar573@gmail.com>
2023-10-30 19:46:21 +0900

    Update Application.kt
    raceCar를 불러오는 함수 추가
    raceCar의 name에 대한 validation 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,11 +1,22 @@
 package racingcar
 
-class raceCar(private val name: String, private val position: Int){
+import camp.nextstep.edu.missionutils.Console
+
+class raceCar(private val name: String, private val position: Int = 0) {
+    init {
+        require((name.length <= 5)) { "Invalid Car Name." }
+    }
+
     override fun toString(): String {
-        return "Car name : ${name}, Car position : {$position}"
+        return "$name : ${"-".repeat(position)}"
     }
 }
 
+fun parseCarName(): List<raceCar> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    return Console.readLine().split(",").map { raceCar(it.trim()) }
+}
+
 fun main() {
-    // TODO: 프로그램 구현
+    val raceCars = parseCarName()
 }
