7a2180a482f6f290f8697db76d14c9c48ddf169b
hxeyexn <kck85075@kumoh.ac.kr>
2023-10-31 02:13:38 +0900

    Feat: 자동차 이름 유효성 검사 기능

--- src/main/kotlin/racingcar/Validation.kt
+++ src/main/kotlin/racingcar/Validation.kt
@@ -4,30 +4,42 @@ import java.lang.IllegalArgumentException
 
 class Validation {
 
-    fun isValidNumberOfCars(carsNameList: List<String>) {
+    private val player = Player()
+
+    private fun isValidNumberOfCars(carsNameList: List<String>) {
         if (carsNameList.size < 2) {
             throw IllegalArgumentException("자동차 이름은 2대 이상 입력해 주세요.")
         }
     }
 
-    fun checkContainsSpace(carsName: String) {
+    private fun checkContainsSpace(carsName: String) {
         if (carsName.contains(" ")) {
             throw IllegalArgumentException("공백은 들어갈 수 없습니다.")
         }
     }
 
-    fun checkCarNameDuplication(carsNameList: List<String>) {
+    private fun checkCarNameDuplication(carsNameList: List<String>) {
         if (carsNameList.distinct().size != carsNameList.size) {
             throw IllegalArgumentException("자동차 이름은 쉼표(,)로 구분해 주세요.")
         }
     }
 
-    fun checkCarNameLength(carsNameList: List<String>) {
+    private fun checkCarNameLength(carsNameList: List<String>) {
         carsNameList.forEach {
             if (it.length > 5) {
                 throw IllegalArgumentException("자동차 이름은 5자 이하만 입력 가능합니다.")
             }
         }
+    }
+
+    fun isValidCarName() {
+        val inputCarsName = player.inputCarName()
+        val carList = inputCarsName.split(",")
+
+        checkContainsSpace(inputCarsName)
+        isValidNumberOfCars(carList)
+        checkCarNameDuplication(carList)
+        checkCarNameLength(carList)
     }
 
     fun checkNumberOfAttemptOneAndMore(numberOfAttempt: Int) {
