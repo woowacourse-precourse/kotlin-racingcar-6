16661fbe721e810bd7115d8f9ef4ce6b117c6fe9
ii2001 <iicks2001@gmail.com>
2023-10-30 14:38:30 +0900

    fead: 자동차 이름 입력 함수 제작

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,16 @@
-package racingcar
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+data class Car(val name: String, var position: Int = 0)
 
 fun main() {
-    // TODO: 프로그램 구현
+    vla cars=inputCarNames()
+}
+
+fun inputCarNames(): List<Car> { //차 이름 입력받기
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = Console.readLine()
+    val carNames = input.split(",").map { it.trim() }
+
+    return carNames.map { Car(it) }
 }
