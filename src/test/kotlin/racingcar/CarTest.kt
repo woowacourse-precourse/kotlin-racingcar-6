bc211cf05c13edfd5b0a77ec872297a1866eb6eb
sunkk8482 <sunkk8482@naver.com>
2023-10-30 16:49:27 +0900

    test : Car 클래스 테스트 추가

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,36 @@
+package racingcar
+
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CarTest {
+    @Test
+    fun testJudgeNumber() {
+        val car = Car("test")
+        assertThat(car.judgeNumber(3)).isFalse()
+        assertThat(car.judgeNumber(5)).isTrue()
+        assertThat(car.judgeNumber(7)).isTrue()
+    }
+
+    @Test
+    fun testUpdatePosition(){
+        val car = Car("test")
+        assertThat(car.getPositionNumber()).isEqualTo(0)
+        car.updatePosition(true)
+        assertThat(car.getPositionNumber()).isEqualTo(1)
+        car.updatePosition(false)
+        assertThat(car.getPositionNumber()).isEqualTo(1)
+    }
+
+    @Test
+    fun testGetPosition(){
+        val car = Car("test")
+        car.updatePosition(true)
+        assertThat(car.getPosition()).isEqualTo("-")
+        car.updatePosition(true)
+        assertThat(car.getPosition()).isEqualTo("--")
+        car.updatePosition(false)
+        assertThat(car.getPosition()).isEqualTo("--")
+    }
+}
