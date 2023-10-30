9a5bcd0ab7990ed38b26c3c153cae2f459a01d1f
posite <su84053@gmail.com>
2023-10-30 17:18:53 +0900

    fix: 잘못 선언된 object class로 수정

--- src/main/kotlin/racingcar/util/RandomNumberGenerator.kt
+++ src/main/kotlin/racingcar/util/RandomNumberGenerator.kt
@@ -2,9 +2,11 @@ package racingcar.util
 
 import camp.nextstep.edu.missionutils.Randoms
 
-object RandomNumberGenerator {
+class RandomNumberGenerator {
     fun generateRandomNumber(): Int = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)
 
-    private const val MIN_NUMBER = 0
-    private const val MAX_NUMBER = 9
+    companion object {
+        private const val MIN_NUMBER = 0
+        private const val MAX_NUMBER = 9
+    }
 }
