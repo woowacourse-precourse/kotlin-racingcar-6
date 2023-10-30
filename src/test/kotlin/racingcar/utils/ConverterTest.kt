453e578518d588360f84e67f919a76e58de62fc5
hongjeZZ <ajyong316@gmail.com>
2023-10-31 01:33:24 +0900

    test(ConverterTest):Converter object 테스트 코드 작성

---
+++ src/test/kotlin/racingcar/utils/ConverterTest.kt
@@ -0,0 +1,28 @@
+package racingcar.utils
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ConverterTest {
+
+    @Test
+    fun `주어진 값을 콤마로 분리하여 리스트로 반환 1`() {
+        val string = "pobi,woni,jun"
+        val result = Converter.splitByCommaToStringList(string)
+        assertThat(result).containsExactly("pobi", "woni", "jun")
+    }
+
+    @Test
+    fun `주어진 값을 콤마로 분리하여 리스트로 반환 2`() {
+        val string = "pobi"
+        val result = Converter.splitByCommaToStringList(string)
+        assertThat(result).containsExactly("pobi")
+    }
+
+    @Test
+    fun `주어진 값을 문자열로 반환`() {
+        val string = "25"
+        val expected = 25
+        assertThat(expected).isEqualTo(Converter.convertStringToInt(string))
+    }
+}
