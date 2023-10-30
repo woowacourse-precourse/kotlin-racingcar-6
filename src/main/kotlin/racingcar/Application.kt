423c870522365eabd06bc03dbe9d56b01bb2478f
ii2001 <iicks2001@gmail.com>
2023-10-30 21:02:35 +0900

    docs: 구현할 기능 목록 교체

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,10 +7,12 @@ fun main() {
     vla cars=inputCarNames()
 }
 
-fun inputCarNames(): List<Car> { //차 이름 입력받기
+fun inputCarNames(): List<Car> { //자동차 이름 입력받기
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val input = Console.readLine()
     val carNames = input.split(",").map { it.trim() }
 
     return carNames.map { Car(it) }
 }
+
+fun printCarNames():
