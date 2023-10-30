4f0e56304b920df163bcdf4657d81a3a61e3d1a1
sooohi <68193613+sooohi@users.noreply.github.com>
2023-10-30 14:14:04 +0900

    feat :랜덤 값으로 전진

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -16,6 +16,23 @@ fun main() {
     if (tryCountInput <= 0) {
         throw IllegalArgumentException("시도 횟수는 0보다 커야 합니다.")
     }
-    print(tryCountInput)
+    val carMove = MutableList(carName.size) { 0 }
+
+    for (i in 1..tryCountInput) {
+        println()
+        for (j in carName.indices) {
+            val randomNumber = (0..9).random()
+            if (randomNumber >= 4) {
+                carMove[j] = carMove[j] + 1
+            }
+        }
+        for (j in carName.indices) {
+            print("${carName[j]} : ")
+            for (k in 1..carMove[j]) {
+                print("-")
+            }
+            println()
+        }
+    }
 
 }
