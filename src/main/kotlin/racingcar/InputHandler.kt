2d0e41f719a6051e9ead86117b56b13a26069abb
SeongWonYoon <dbswjdqhd12@gmail.com>
2023-10-30 13:43:00 +0900

    Implement: 추가 "콘솔 입력을 검사하는 InputHandler"
    기존 RacingGameManager에서 관리하던 콘솔 입력을 InputHandler Object로 분할하고 입력의 무결성을 검사하는 기능 추가하였음

---
+++ src/main/kotlin/racingcar/InputHandler.kt
@@ -0,0 +1,36 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+object InputHandler {
+
+    fun getCarName(): List<String> {
+        val inputReadLine = Console.readLine()
+        if(inputReadLine.contains(" ")){
+            throw IllegalArgumentException("")
+        }
+        val inputData = inputReadLine.split(",")
+        return if (isValidCarName(inputData)) inputData else throw IllegalArgumentException("")
+    }
+
+    fun getAmount(): Int {
+        val inputData = Console.readLine().toIntOrNull()
+        return if (isValidAmount(inputData)) inputData!! else throw IllegalArgumentException("")
+    }
+
+    fun isValidCarName(carNames: List<String>) : Boolean = carNames.all { isAllLowerCase(it) }
+
+    fun isValidAmount(amount : Int?) : Boolean{
+        amount?.let {
+            if (isPositiveNumber(it)){
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun isAllLowerCase(carName: String): Boolean = carName.all { it.isLowerCase() }
+
+    private fun isPositiveNumber(int: Int): Boolean = if (int > 0) true else false
+
+}
--- src/main/kotlin/racingcar/RacingGameManager.kt
+++ src/main/kotlin/racingcar/RacingGameManager.kt
@@ -1,22 +1,26 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
 
 object RacingGameManager {
     private lateinit var racingGame : RacingGame
 
     fun startGame() {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        val carList = InputHandler.getCarName()
+
+        println("시도할 횟수는 몇 회인가요?")
+        val amount = InputHandler.getAmount()
+
 
         racingGame = RacingGame(
-            carList = receiveCarNamesFromUser(),
-            amount = recieveTryAmountFromUser()
+            carList = carList,
+            amount = amount
         )
     }
-    private fun receiveCarNamesFromUser() = Console.readLine().split(",")
 
 
-    private fun recieveTryAmountFromUser() = Console.readLine().toInt()
 
 
 
