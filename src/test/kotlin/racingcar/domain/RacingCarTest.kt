e63b8a2bf511298ccc0930baad00af150b3d1348
posite <su84053@gmail.com>
2023-10-30 22:06:45 +0900

    test: RacingCar 전진기능 추가로 인한 test code 추가

--- src/test/kotlin/racingcar/domain/RacingCarTest.kt
+++ src/test/kotlin/racingcar/domain/RacingCarTest.kt
@@ -7,26 +7,40 @@ import org.junit.jupiter.api.Test
 @DisplayName("RacingCar에서")
 class RacingCarTest {
 
-    private val racingCar = RacingCar("i30")
 
     @Test
-    @DisplayName("forwardCount를 증가시키면 1이 더해진다")
-    fun addForwardCount() {
+    @DisplayName("random number가 4 이상이면 forwarCount가 1이 더해진다")
+    fun moveWhenNumberGreaterThan3() {
         //given
-        val beforeForwardCount = racingCar.receiveForwardCount()
+        val racingCar = RacingCar("i30")
+        val beforeForwardCount = racingCar.readForwardCount()
         //when
-        racingCar.addForwardCount()
+        racingCar.moveRacingCar(5)
         //then
-        val afterForwardCount = racingCar.receiveForwardCount()
+        val afterForwardCount = racingCar.readForwardCount()
         Assertions.assertThat(afterForwardCount).isEqualTo(beforeForwardCount + 1)
+    }
+
+    @Test
+    @DisplayName("random number가 4 미만이면 forwarCount가 더해지지 않는다")
+    fun moveWhenNumberLessThan4() {
+        //given
+        val racingCar = RacingCar("i30")
+        val beforeForwardCount = racingCar.readForwardCount()
+        //when
+        racingCar.moveRacingCar(2)
+        //then
+        val afterForwardCount = racingCar.readForwardCount()
+        Assertions.assertThat(afterForwardCount).isEqualTo(beforeForwardCount)
     }
 
     @Test
     @DisplayName("생성한 초기의 forwardCount를 받을 수 있다")
     fun receiveForwardCount() {
         //given
+        val racingCar = RacingCar("i30")
         //when
-        val forwardCount = racingCar.receiveForwardCount()
+        val forwardCount = racingCar.readForwardCount()
         //then
         Assertions.assertThat(forwardCount).isEqualTo(0)
     }
@@ -35,9 +49,11 @@ class RacingCarTest {
     @DisplayName("자동차 이름을 받을 수 있다")
     fun readRacingCarName() {
         //given
+        val originCarName = "i30"
+        val racingCar = RacingCar(originCarName)
         //when
-        val carName = racingCar.readRacingCarName()
+        val receivedCarName = racingCar.readRacingCarName()
         //then
-        Assertions.assertThat(carName).isEqualTo("i30")
+        Assertions.assertThat(receivedCarName).isEqualTo(originCarName)
     }
 }
