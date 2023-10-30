d2f358c64dfc37a1816fc8a4d9f4f34c678c1e0e
이빵주 <yungju9982@naver.com>
2023-10-31 06:32:30 +0900

    [feat] RacingGame 클래스 생성 및 생성자 인자 추가

---
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -0,0 +1,9 @@
+package racingcar
+
+class RacingGame(
+    private val racingCarInput: RacingCarInput,
+    private val moveCountInput: MoveCountInput,
+    private val raceManager: RaceManager,
+    private val racePrinter: RacePrinter
+) {
+}
