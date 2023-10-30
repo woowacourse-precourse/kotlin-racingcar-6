76bb88bdca79151982b239dc3a59f4c2e3b9cc0c
hxeyexn <kck85075@kumoh.ac.kr>
2023-10-30 20:44:44 +0900

    Feat: 시도 횟수 질문 출력 기능

--- src/main/kotlin/racingcar/Host.kt
+++ src/main/kotlin/racingcar/Host.kt
@@ -4,4 +4,9 @@ class Host {
     fun printRequestEnterCarName() {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     }
+
+    fun printQuestionNumberOfAttempt() {
+        println("시도할 횟수는 몇 회인가요?")
+    }
+
 }
