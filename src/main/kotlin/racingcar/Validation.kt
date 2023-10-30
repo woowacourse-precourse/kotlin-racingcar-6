8e4cc448087ca2cf32dddf8f9ced3a620fea8809
hxeyexn <kck85075@kumoh.ac.kr>
2023-10-30 18:59:42 +0900

    Feat: 차량 이름 유효성 검사(이름 5자 이하 아닐 때)

--- src/main/kotlin/racingcar/Validation.kt
+++ src/main/kotlin/racingcar/Validation.kt
@@ -24,5 +24,13 @@ class Validation(private val player: Player) {
             throw IllegalArgumentException("중복된 이름은 입력할 수 없습니다.")
         }
     }
-    
+
+    fun checkCarNameLength() {
+        val carList = inputCarsName.split(",")
+        carList.forEach {
+            if (it.length > 5) {
+                throw IllegalArgumentException("자동차 이름은 5자 이하만 입력 가능합니다.")
+            }
+        }
+    }
 }
