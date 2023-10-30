c06fd0bb94c303511f56e497a38a7ce380074b97
eunyeong <feijoaplz@gmail.com>
2023-10-31 03:29:34 +0900

    feat: 입력값이 유효한지 확인하는 기능

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -11,16 +11,21 @@ fun main() {
 
 fun generateCars(): List<String> {
     println(INPUT_CAR_NAMES)
-    val carNames = Console.readLine().split(",").map { it.trim() }
-
+    val carNames = Console.readLine().split(",").map { it.trim() }.requireNoNulls()
+    carNames.forEach {
+        if(it.length <= 5) return carNames
+        else throw IllegalArgumentException("이름은 1자이상 5자 이하로 입력해주세요.")
+    }
     return carNames
 }
 
 fun getAttemptTimes(): Int {
-    val attemptTimes = Console.readLine().toInt()
-
+    val attemptTimes = Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요.")
     return  attemptTimes
 }
-
-
-
+fun isValidateInput(carNames:String,attemptTimes:Int): Boolean{
+    if(carNames.isBlank()) throw IllegalArgumentException ("이름을 입력해주세요")
+    if(carNames.isEmpty() && carNames.length > 5) throw IllegalArgumentException ("이름은 1자 이상 5자 이하로 입력해주세요.")
+    if(attemptTimes.equals(" ")) throw IllegalArgumentException ("공백 없이 입력해주세요")
+    return true
+}
