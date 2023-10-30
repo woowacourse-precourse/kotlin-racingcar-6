0c10d71fe3e79fbac29d82775e1ffe098bb11770
junyong <junyong008@gmail.com>
2023-10-30 14:48:23 +0900

    feat: 각 자동차마다 랜덤값을 뽑아 전진 여부 결정 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,12 +1,22 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
 import racingcar.view.InputView.inputCarNames
 import racingcar.view.InputView.inputRoundCount
 import racingcar.view.OutputView.outputResultTitle
 
+private const val RANDOM_MIN_NUM = 0
+private const val RANDOM_MAX_NUM = 9
+private const val MIN_NUM_TO_FORWARD = 4
+
 fun main() {
+
     val carNames = inputCarNames()
     val roundCount = inputRoundCount()
 
     outputResultTitle()
+
+    carNames.forEach {
+        if (Randoms.pickNumberInRange(RANDOM_MIN_NUM, RANDOM_MAX_NUM) >= MIN_NUM_TO_FORWARD) it.position++
+    }
 }
