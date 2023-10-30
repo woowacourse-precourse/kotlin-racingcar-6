ad93fd81ad9405dc6b06945d9347d8e111736fd4
j2j2 <fineimagination@gmail.com>
2023-10-31 05:25:08 +0900

    feat(racingcar): 사용자가 차들의 이름과 시도횟수를 입력하는 기능 구현

--- src/main/kotlin/racingcar/domain/Car.kt
+++
@@ -1,9 +0,0 @@
-package racingcar.domain
-
-class Car {
-
-    fun namingCar(): String? {
-        //map에 담기 -> 자동차이름 key , 진행정도 value
-        return null
-    }
-}
---
+++ src/main/kotlin/racingcar/domain/User.kt
@@ -0,0 +1,17 @@
+package racingcar.domain
+
+import camp.nextstep.edu.missionutils.Console
+
+class User {
+
+    fun namingCar(): Map<String, Int> {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+        val carsName: String = Console.readLine()
+        return carsName.split(",").map { it }.associateWith { 0 }
+    }
+
+    fun attemptNumber(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        return Console.readLine().toInt()
+    }
+}
