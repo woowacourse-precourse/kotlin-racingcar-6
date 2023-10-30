93a0bc47b9d24ab950c2d7c7794c9520352f3600
Junyoung-WON <wjyo60@gmail.com>
2023-10-30 21:29:32 +0900

    feat: 차의 이름을 입력받는 getCarNames 함수 생성
    제공된 라이브러리의 Console을 활용하여 사용자 입력 받음.
    validator를 활용한 입력 검사 필요.

--- src/main/kotlin/racingcar/User.kt
+++ src/main/kotlin/racingcar/User.kt
@@ -1,4 +1,13 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
 
 class User {
+    fun getCarNames(): List<String>{
+        var carNames : String = Console.readLine()
+        // validator로 올바른 문자열 입력인지 확인
+        // List로 쪼개기
+        val nameList : List<String> = carNames.split(',')
+        // validator로 올바른 길이의 이름인지 확인
+        return nameList
+    }
 }
