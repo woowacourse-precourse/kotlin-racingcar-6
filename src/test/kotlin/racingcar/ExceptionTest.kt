865dd23a0b09370bde74b6ff27dcef5089b4c738
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-31 00:13:23 +0900

    Test (ExceptionTest) : 입력한 시도할 횟수가 올바른지 검사

--- src/test/kotlin/racingcar/ExceptionTest.kt
+++ src/test/kotlin/racingcar/ExceptionTest.kt
@@ -6,11 +6,20 @@ import racingcar.controller.RacingcarController
 
 class ExceptionTest {
     private val racingcarController = RacingcarController()
+
     @Test
     fun `자동차 이름이 5자 이하 인지 검사`() {
         val input = "rangeTest"
         assertThrows<IllegalArgumentException> {
             racingcarController.checkCarNameLength(input)
+        }
+    }
+
+    @Test
+    fun `입력한 시도할 횟수가 올바른지 검사`() {
+        val input = "a"
+        assertThrows<IllegalArgumentException> {
+            racingcarController.checkTryNumber(input)
         }
     }
 }
