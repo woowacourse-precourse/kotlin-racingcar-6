fe532bb2fe588b8ee0d9a6e6e6b9419c868dc9a5
oh in seong <superohinsung@naver.com>
2023-10-30 13:45:16 +0900

    [feat] carsInputString의 예외처리 기능 추가
    자동차 멤버의 이름은 5글자를 넘지 않는다. 즉, 입력받은 문자열의 길이는 멤버의수 * 5 + (콤마의 갯수) 이하로 한정할 수 있다.

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -6,7 +6,7 @@ class RacingGame {
 
     private fun userInput() = Console.readLine()
 
-    fun playRacing() {
+    fun startRacing() {
         val printer = Printer()
 
         printer.printOutEnteringCarName()
@@ -20,11 +20,10 @@ class RacingGame {
         val attemptsNumberInputString = userInput()
         requireValidAttemptsNumberInput(attemptsNumberInputString = attemptsNumberInputString)
         val attemptsNumber = attemptsNumberInputString.toInt()
-
     }
-
     private fun requireValidCarsInput(carsInputString: String, carsList: List<Car>) {
         require(carsInputString.count { it == ',' } == carsList.size - 1)
+        require(carsInputString.length <= carsList.size * CARS_MAX_LENGTH + carsInputString.count { it == ',' })
     }
 
     private fun requireCheckingForDuplicateNames(carsList: List<Car>) {
@@ -38,6 +37,10 @@ class RacingGame {
     private fun requireValidAttemptsNumberInput(attemptsNumberInputString: String) {
         require(attemptsNumberInputString.matches(Regex("^\\d+\$")))
         require(attemptsNumberInputString.toInt() >= 0)
+    }
+
+    companion object {
+        const val CARS_MAX_LENGTH = 5
     }
 
 }
