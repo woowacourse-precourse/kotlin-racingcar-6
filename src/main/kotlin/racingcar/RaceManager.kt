a32bcfa18745546a6a8399ddcb974fe088fd2a0b
이빵주 <yungju9982@naver.com>
2023-10-31 06:14:02 +0900

    [feat] 랜덤수가 4 이상일 경우에 position을 증가시키는 함수 구현

---
+++ src/main/kotlin/racingcar/RaceManager.kt
@@ -0,0 +1,15 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+
+object RaceManager {
+    private const val RANDOM_START_NUMBER = 0
+    private const val RANDOM_END_NUMBER = 9
+    private const val MOVE_MIN_NUMBER = 4
+
+    private fun moveIfPossible(car: Car) {
+        if (Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER) >= MOVE_MIN_NUMBER) {
+            car.move()
+        }
+    }
+}
