bdac5cc2aa725708aa3ae89cd6cc7670c8e5392a
이빵주 <yungju9982@naver.com>
2023-10-31 02:02:04 +0900

    [feat] 중복된 자동차 이름을 검사하는 함수 구현

--- src/main/kotlin/racingcar/RacingCarInput.kt
+++ src/main/kotlin/racingcar/RacingCarInput.kt
@@ -1,4 +1,13 @@
 package racingcar
 
+import racingcar.ErrorMessage.DUPLICATE_CAR_NAME_ERROR
+
 object RacingCarInput {
+
+    private fun validateCarNameDuplicate(carNameMap: Map<String, Int>) {
+        val carNames = carNameMap.keys
+        if (carNames.size != carNames.distinct().size) {
+            throw IllegalArgumentException(DUPLICATE_CAR_NAME_ERROR)
+        }
+    }
 }
