91b23f6b11952397d463eca8ed2839c802599a8a
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-30 21:33:29 +0900

    Test (ExceptionTest) : 자동차 이름이 5자 이하 인지 검사

--- src/test/kotlin/racingcar/ExceptionTest.kt
+++ src/test/kotlin/racingcar/ExceptionTest.kt
@@ -1,4 +1,16 @@
 package racingcar
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import racingcar.controller.RacingcarController
+
 class ExceptionTest {
+    private val racingcarController = RacingcarController()
+    @Test
+    fun `자동차 이름이 5자 이하 인지 검사`() {
+        val input = "rangeTest"
+        assertThrows<IllegalArgumentException> {
+            racingcarController.checkCarNameLength(input)
+        }
+    }
 }
