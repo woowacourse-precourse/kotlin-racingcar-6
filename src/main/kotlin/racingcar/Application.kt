26476f3f31450de76e7d377447528aef90646d06
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-10-30 11:42:47 +0900

    feat(input NumberOfAttempts)
    docs/README.md 에서 정리한 2번 기능 경주횟수 입력 받는 기능 추가하였습니다

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,10 +5,16 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     val carNames = inputCarNames()
+    val numberOfAttempts = inputNumberOfAttempts()
 
 }
 
 fun inputCarNames(): List<String> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     return Console.readLine().split(",")
+}
+
+fun inputNumberOfAttempts(): Int {
+    println("시도할 횟수는 몇 회인가요?")
+    return Console.readLine().toInt()
 }
