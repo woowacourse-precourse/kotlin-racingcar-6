dbf0690bbcf4f81d2b9804b78260c6cd1ef36df9
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2023-10-31 00:27:57 +0900

    feat: 입력과 검증 구현
    - 자동차 이름, 횟수 입력 함수 구현
    - 자동차 이름과 횟수 검증 함수 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,33 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    gameStart()
+}
+
+fun gameStart() {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val inputNames = Console.readLine()
+    val listOfCarName = checkValidName(inputNames)
+
+    println("시도할 횟수는 몇 회인가요?")
+    val inputCount = Console.readLine()
+    val counOfTrial = checkValidNumber(inputCount)
+}
+
+fun checkValidName(stringOfNames: String): List<String> {
+    val listOfCarName = stringOfNames.split(",")
+    for (name in listOfCarName) {
+        if (name.length > 5)
+            throw IllegalArgumentException("5자 이상인 이름 입력 발생")
+    }
+    return listOfCarName
+}
+
+fun checkValidNumber(countString: String): Int {
+    if (countString.toIntOrNull() == null) {
+        throw IllegalArgumentException("숫자가 아닌 횟수 입력 발생")
+    }
+    return countString.toInt()
 }
