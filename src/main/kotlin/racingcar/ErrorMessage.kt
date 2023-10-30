4c983b2f7a4936392a348b96125347b839ddcf58
이빵주 <yungju9982@naver.com>
2023-10-31 02:24:19 +0900

    [chore] 자동차 최소 개수 상수명 수정

--- src/main/kotlin/racingcar/ErrorMessage.kt
+++ src/main/kotlin/racingcar/ErrorMessage.kt
@@ -2,7 +2,7 @@ package racingcar
 
 object ErrorMessage {
     const val CAR_NAME_LENGTH_OVER_ERROR = "입력된 자동차 이름이 5자 초과입니다."
-    const val CAR_COUNT_ERROR = "입력된 자동차의 개수가 1개 이하입니다."
+    const val CAR_MINIMUM_COUNT_ERROR = "입력된 자동차의 개수가 1개 이하입니다."
     const val DUPLICATE_CAR_NAME_ERROR = "중복되는 자동차 이름이 있습니다."
     const val NUMBER_FORMAT_ERROR = "입력된 수가 숫자가 아닙니다."
     const val INVALID_ATTEMPT_COUNT_ERROR = "입력된 횟수가 0 이하입니다."
