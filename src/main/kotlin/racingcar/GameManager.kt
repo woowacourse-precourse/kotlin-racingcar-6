5194bd634761b64738f0912efe871da448c41fbf
tgyuu-An <atg0614@gmail.com>
2023-10-31 02:54:58 +0900

    feature : 사용자의 입력 유효성 검증 로직 추가

--- src/main/kotlin/racingcar/GameManager.kt
+++ src/main/kotlin/racingcar/GameManager.kt
@@ -15,6 +15,13 @@ class GameManager {
     }
 
     fun setMovementAttemptCount(input : String){
+        requireNotNull(input.toIntOrNull()){
+            "전진을 시도할 횟수는 0 혹은 양수만 입력하실 수 있습니다."
+        }
+        require(input.toInt() >= 0){
+            "전진을 시도할 횟수는 음수일 수 없습니다."
+        }
+
         movementAttemptCount = input.toInt()
     }
 }
