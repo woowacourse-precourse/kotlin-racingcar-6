135988e24ce463888fe639a2807e6b36bc5975b3
murjune <sis9221302@gmail.com>
2023-10-31 03:17:38 +0900

    fix(GameInstruction) : 더 readable하게 네이밍 변경

--- src/main/kotlin/racingcar/constants/GameInstruction.kt
+++ src/main/kotlin/racingcar/constants/GameInstruction.kt
@@ -1,7 +1,7 @@
 package racingcar.constants
 
 enum class GameInstruction(val message: String) {
-    ENTER_NUMBER("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ENTER_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     RACING_RESULT("실행결과"),
     ATTEMPT_COUNT("시도할 횟수는 몇 회인가요?"),
 }
