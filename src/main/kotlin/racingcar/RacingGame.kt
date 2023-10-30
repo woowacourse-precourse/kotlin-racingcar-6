9c29fd9e865923aaf49701ce07cf434db991b46a
oh in seong <superohinsung@naver.com>
2023-10-30 12:18:23 +0900

    [feat] RacingGame class에서 사용자 입력을 받는 기능

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -3,5 +3,12 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 class RacingGame {
-    
+
+    private fun userInput() = Console.readLine()
+
+    fun playRacing() {
+        val printer = Printer()
+        printer.printOutEnteringCarName()
+        val cars = userInput()
+    }
 }
