9b5e93e73efc10872f4fda92fc095defd61ee068
상은 <71699054+vmkmym@users.noreply.github.com>
2023-10-30 14:22:00 +0900

    <feature> Car class 만들기 - move 메서드를 통해 자동차가 전진하거나 멈출 수 있음 - display 메서드를 통해 자동차 이름과 위치를 출력함 - 전진하는 조건으로Randoms.pickNumberInRange 사용함

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,18 @@
 package racingcar
 
-fun main() {
-    // TODO: 프로그램 구현
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+data class CarRace(val name: String) {
+    var position: Int = 0
+
+    fun move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++
+        }
+    }
+
+    fun display(): String {
+        return "$name : ${"-".repeat(position)}"
+    }
 }
