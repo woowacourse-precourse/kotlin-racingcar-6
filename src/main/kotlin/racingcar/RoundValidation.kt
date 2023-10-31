ada9e547a2224dd0e1a94762c6717d2b5a52298d
stopkite <stopkite98@gmail.com>
2023-10-31 13:43:47 +0900

    feat: (예외) 라운드 횟수 1이상 체크

--- src/main/kotlin/racingcar/RoundValidation.kt
+++ src/main/kotlin/racingcar/RoundValidation.kt
@@ -3,11 +3,18 @@ package racingcar
 class RoundValidation {
     fun checkRoundInputValidation(input: String) {
         checkIsNumber(input)
+        checkIsBiggerThanOne(input)
     }
 
     private fun checkIsNumber(input: String) {
         if (input.toIntOrNull() == null) {
             throw IllegalArgumentException("입력값은 숫자여야 합니다.")
+        }
+    }
+
+    private fun checkIsBiggerThanOne(input: String) {
+        if (input.toInt() < 1) {
+            throw IllegalArgumentException("입력값은 1보다 큰 값이어야 합니다.")
         }
     }
 }
