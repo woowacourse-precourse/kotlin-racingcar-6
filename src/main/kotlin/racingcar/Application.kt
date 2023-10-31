7f37668f3e67a9508a4d4a47960cb587b9080f8f
Bruce <bruce@Bruceui-MacBookPro.local>
2023-11-01 00:26:17 +0900

    [feat] input cars name(length limit 5)

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,22 @@
 package racingcar
 
+// import camp.nextstep.edu.missionutils.Randoms //pickNumberInRange
+// import camp.nextstep.edu.missionutils.Console //readLine
+
 fun main() {
-    // TODO: 프로그램 구현
+    
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+
+    val inputNames = readLine()
+    val nameList = inputNames?.split(",") 
+    val carMap = mutableMapOf<String, Int>()
+    for (name in nameList!!) {
+        if (name.trim().length <= 5) {
+            carMap.put(name.trim(), 0)
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
+    println(carMap)
+    
 }
