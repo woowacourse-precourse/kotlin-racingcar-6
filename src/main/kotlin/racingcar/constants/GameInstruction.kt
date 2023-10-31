c7a58174302c39b2f3b6f101865c57ecb1cf094d
murjune <sis9221302@gmail.com>
2023-10-31 21:55:15 +0900

    fix(GameInstruction) : RACING_RESULT message 띄어쓰기

--- src/main/kotlin/racingcar/constants/GameInstruction.kt
+++ src/main/kotlin/racingcar/constants/GameInstruction.kt
@@ -2,6 +2,6 @@ package racingcar.constants
 
 enum class GameInstruction(val message: String) {
     ENTER_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    RACING_RESULT("실행결과"),
+    RACING_RESULT("실행 결과"),
     ATTEMPT_COUNT("시도할 횟수는 몇 회인가요?"),
 }
