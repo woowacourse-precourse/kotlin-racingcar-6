b7579a754b4b4424abfb47553c51cc2e3b1c4555
SeongWooHong <pluto0303@naver.com>
2023-11-01 01:31:45 +0900

    feat: Car function construction

--- src/main/kotlin/racingcar/CarManager.kt
+++ src/main/kotlin/racingcar/CarManager.kt
@@ -3,5 +3,24 @@ import camp.nextstep.edu.missionutils.Randoms
 
 const val MOVE_CONDITION = 4
 class Car(name: String) {
+    private val name: String = name
+    private var randomNumber = Randoms.pickNumberInRange(0, 9)
+    private var location = 0
 
+    fun compareRandom() : Boolean = randomNumber > MOVE_CONDITION
+
+    fun moveForward() {
+        location++
+    }
+
+    fun setNewRandomNumber() {
+        randomNumber = Randoms.pickNumberInRange(0, 9)
+    }
+    fun printLocation() {
+        print("$name : ")
+        for (x in 1..location) {
+            print("-")
+        }
+        println()
+    }
 }
--- src/main/kotlin/racingcar/InputManager.kt
+++ src/main/kotlin/racingcar/InputManager.kt
@@ -4,16 +4,15 @@ import camp.nextstep.edu.missionutils.Console
 import kotlin.IllegalArgumentException
 
 class InputManager {
-    fun getCarName() :List<String> {
+    fun getCarName(): List<String> {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)) 기준으로 구분")
         val inputLine = Console.readLine()
 
-        if(inputLine.contains(" ")) {
+        if (inputLine.contains(" ")) {
             throw IllegalArgumentException("")
         }
-        val inputLineSplit = inputLine.split(",")
 
-        return inputLineSplit
+        return inputLine.split(",")
     }
 
     fun getTryTime(): Int {
