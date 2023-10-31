141c5e817d57c52e6db62f7aed7ef9212e483907
jklom0326 <jklom0326@gmail.com>
2023-10-31 23:24:27 +0900

    test: 자동차 대한 테스트 추가
    - moving 함수 테스트
    - 위치값 출력 테스트

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,16 +4,17 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Car(val name: String, var position: Int = 0) {
 
-    fun moving() {
-        val num = Randoms.pickNumberInRange(1, 9)
+    fun moving(num: Int) {
         if (num >= 4) position++
     }
 
-    fun printPosition() {
+    fun getStatus(): String {
         var status = "${this.name} : "
         repeat(this.position) {
             status += "-"
         }
-        println(status)
+        return status
     }
+
+    fun generate() = Randoms.pickNumberInRange(1, 9)
 }
