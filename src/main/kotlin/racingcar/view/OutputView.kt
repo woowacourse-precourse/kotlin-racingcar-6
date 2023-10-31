46d6d065f51e6fa38e55ccaa3d7bc76bb96bbcf8
lee989898 <wnsrud1352@gmail.com>
2023-10-31 17:05:41 +0900

    [feat] 최종 우승자 문구 출력 메서드 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -4,8 +4,11 @@ import racingcar.car.Car
 
 object OutputView {
     private const val COLON = ":"
+    private const val COMMA = ","
+    private const val GAP = " "
     private const val ENTER_NAMES_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     private const val HOW_MANY_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
+    private const val FINAL_WINNER = "최종 우승자"
 
     fun enterNamesOfCars() {
         println(ENTER_NAMES_OF_CARS)
@@ -19,5 +22,9 @@ object OutputView {
         for (car in cars) {
             println("${car.name} $COLON ${car.currentPosition}")
         }
+    }
+
+    fun finalWinner(winners: List<String>) {
+        println("$FINAL_WINNER $COLON ${winners.joinToString("$COMMA$GAP")}")
     }
 }
