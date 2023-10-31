f9210927af75af6611c45090cba1e2530f33963f
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 02:26:12 +0900

    feat: 분할된 이름이 null인지 확인
    이름이 Null 또는 빈 문자열인지 확인한다.

--- src/main/kotlin/racingcar/Validator.kt
+++ src/main/kotlin/racingcar/Validator.kt
@@ -5,4 +5,17 @@ class Validator {
         if (!inputString.contains(','))
             throw IllegalArgumentException("invalid input string, there are another special characters")
     }
+
+    fun validateNames(carsNames: List<String>) {
+        for (name in carsNames) {
+            isNull(name)
+            
+        }
+    }
+
+    private fun isNull(name: String) {
+        if (name.isNullOrEmpty()) {
+            throw IllegalArgumentException("invalid car's name, name is null or empty")
+        }
+    }
 }
