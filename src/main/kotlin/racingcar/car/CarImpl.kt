d223764eb9928020885be7d070f9f8bb62298954
lee989898 <wnsrud1352@gmail.com>
2023-10-31 16:06:20 +0900

    [feat] 자동차 상태 반환 및 진행 멈춤 메서드 구현

--- src/main/kotlin/racingcar/car/CarImpl.kt
+++ src/main/kotlin/racingcar/car/CarImpl.kt
@@ -1,10 +1,22 @@
 package racingcar.car
 
 class CarImpl : Car {
+    private var _currentPosition: String = ""
     override val currentPosition: String
-        get() = TODO("Not yet implemented")
+        get() = _currentPosition
 
     override fun forwardOrStop(randomNumber: Int) {
-        TODO("Not yet implemented")
+        _currentPosition += when (randomNumber) {
+            in MINIMUM_OF_STOP..MAXIMUM_OF_STOP -> CarState.STOP.state
+            in MINIMUM_OF_FORWARD..MAXIMUM_OF_FORWARD -> CarState.FORWARD.state
+            else -> throw IllegalArgumentException()
+        }
+    }
+
+    companion object {
+        private const val MINIMUM_OF_STOP = 0
+        private const val MAXIMUM_OF_STOP = 3
+        private const val MINIMUM_OF_FORWARD = 4
+        private const val MAXIMUM_OF_FORWARD = 9
     }
 }
