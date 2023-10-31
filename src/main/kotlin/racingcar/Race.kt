1bbbac780c512f2baf7156b713c7d2d44ad5b27d
Hogu59 <dbstjdguswkd@naver.com>
2023-11-01 04:07:24 +0900

    feat: 출력부 및 Race 클래스(Controller 기능) 구현

---
+++ src/main/kotlin/racingcar/Race.kt
@@ -0,0 +1,40 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+import racingcar.model.Car
+
+class Race {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    fun start() {
+        outputView.printCarNameInputMention()
+        val carList = getCarList(inputView.inputCarName())
+
+        outputView.printRoundNumberInputMention()
+        for (attemptNumber in 0 until inputView.inputRoundNumber()) {
+            for (car in carList) {
+                moveOrStop(car, getRandomNumber())
+            }
+
+            if (attemptNumber == 0) outputView.printResultMention()
+
+            outputView.printResult(carList)
+        }
+        outputView.printWinner(getWinnerList(carList))
+    }
+
+    fun getCarList(carNameList: List<String>): List<Car> = carNameList.map { name -> Car(name) }
+
+    private fun getRandomNumber(): Int = Randoms.pickNumberInRange(0, 9)
+
+    fun moveOrStop(car : Car, randomNumber: Int) {
+        if (randomNumber >= 4) {
+            car.move()
+        }
+    }
+
+    fun getWinnerList(carList: List<Car>): List<Car> {
+        val maxMove = carList.maxBy { car -> car.moves }.moves
+        return carList.filter { car -> car.moves == maxMove }
+    }
+}
