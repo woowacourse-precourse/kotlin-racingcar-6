4afb3b440bc208bd93ade336e18c8f4059534c12
이홍진\iit80 <hong80714@gmail.com>
2023-10-31 22:34:45 +0900

    [refactor] 변하지 않는 상수값 대체 Constants 객체를 만들어 변하지 않는 상수값을 전부 대체

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -16,36 +16,35 @@ fun startRacingGame() {
 }
 
 fun carNamesInput(): List<Cars> {
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)")
+    println(Constants.CAR_NAME_INPUT_MESSAGE)
     val carNames = Console.readLine()
-    val cars = carNames.split(',').map { Cars(it) }
+    val cars = carNames.split(Constants.CAR_NAMES_SPLITER).map { Cars(it) }
     validateCarNames(cars)
     return cars
 }
 
 fun validateCarNames(cars: List<Cars>) {
-    if (cars.any { it.name.length > 5 }) {
-        throw IllegalArgumentException("5글자 이하로 이름을 지어주세요")
+    if (cars.any { it.name.length > Constants.NAME_LENGTH_RULE }) {
+        throw IllegalArgumentException(Constants.OVER_INPUT_ALARM)
     }
 }
 
 fun roundInput(): Int {
-    println("시도할 횟수는 몇 회인가요?")
+    println(Constants.ROUND_INPUT)
     val raceRound = Console.readLine().toInt()
     validateRaceRound(raceRound)
     return raceRound
 }
 
 fun validateRaceRound(raceRound: Int) {
-    if (raceRound <= 0) {
-        throw IllegalArgumentException("자연수로 입력해주세요")
+    if (raceRound <= Constants.RACE_ROUND_RULE) {
+        throw IllegalArgumentException(Constants.NOT_POSITIVE_INTEGER_ALARM)
     }
 }
 
 fun gameResult(inputRound: Int, cars: List<Cars>) {
-    println("\n실행 결과")
+    println(Constants.EXECUTION_RESULT)
     val racedCars = raceCars(inputRound, cars)
-    raceCars(inputRound, cars)
     printWinners(getWinners(racedCars))
 }
 
@@ -60,27 +59,27 @@ fun raceCars(inputRound: Int, cars: List<Cars>): List<Cars> {
 
 fun printCarPositions(cars: List<Cars>) {
     cars.forEach { car ->
-        println("${car.name} : ${"-".repeat(car.position)}")
+        println("${car.name} : ${Constants.POSITION_MARK.repeat(car.position)}")
     }
 }
 
-fun getWinners(cars: List<Cars>): List<Cars>{
+fun getWinners(cars: List<Cars>): List<Cars> {
     val headPosition = cars.maxOf { it.position }
     return cars.filter { it.position == headPosition }
 }
 
 fun printWinners(winners: List<Cars>) {
     val winnersName = winners.map { it.name }
-    println("최종 우승자 : ${winnersName.joinToString(", ")}")
+    println(Constants.FINAL_WINNER + Constants.COLON_SPACES + winnersName.joinToString(Constants.WINNERS_NAME_SEPERATOR))
 }
 
 
 class Cars(carName: String) {
-    var position = 0
+    var position = Constants.START_POSITION
     val name = carName
 
     fun move() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+        if (Randoms.pickNumberInRange(Constants.RANDOM_NUMBER_START, Constants.RANDOM_NUMBER_END) >= Constants.MOVING_CONDITION) {
             position++
         }
     }
---
+++ src/main/kotlin/racingcar/Constants.kt
@@ -0,0 +1,20 @@
+package racingcar
+
+object Constants {
+    const val CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)"
+    const val OVER_INPUT_ALARM = "5글자 이하로 이름을 지어주세요"
+    const val ROUND_INPUT = "시도할 횟수는 몇 회인가요?"
+    const val NOT_POSITIVE_INTEGER_ALARM = "자연수로 입력해주세요"
+    const val EXECUTION_RESULT = "\n실행 결과"
+    const val POSITION_MARK = "-"
+    const val CAR_NAMES_SPLITER = ','
+    const val WINNERS_NAME_SEPERATOR = ", "
+    const val COLON_SPACES = " : "
+    const val FINAL_WINNER = "최종 우승자"
+    const val NAME_LENGTH_RULE = 5
+    const val RACE_ROUND_RULE = 0
+    const val START_POSITION = 0
+    const val RANDOM_NUMBER_START = 0
+    const val RANDOM_NUMBER_END = 9
+    const val MOVING_CONDITION = 4
+}
