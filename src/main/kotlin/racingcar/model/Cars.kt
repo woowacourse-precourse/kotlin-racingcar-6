8fbc6a6bde948f724a603e28796a255c957e4e83
sh1mj1 <7wlgns@gmail.com>
2023-10-31 20:53:35 +0900

    feat(Cars): return the winner of the game (decideWinner)
    * Car 의 position 이 가장 큰 Car 들을 StringList 로 리턴한다.

--- src/main/kotlin/racingcar/model/Cars.kt
+++ src/main/kotlin/racingcar/model/Cars.kt
@@ -1,10 +1,19 @@
 package racingcar.model
 
 class Cars(val cars: List<Car>) {
+
+    fun decideWinner(): List<String> {
+        val maxPosition = cars.maxOf(Car::position)
+        return cars.filter {
+            it.position == maxPosition
+        }.map { it.name }.toList()
+    }
+
     fun validate() {
         validateDuplicate()
         cars.forEach(Car::validateCar)
     }
+
     internal fun validateDuplicate() {
         val set = cars.map(Car::name).toSet()
         if (cars.size > set.size) {
