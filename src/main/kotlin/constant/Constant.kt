9d1d8deeba512666961e620dd70217017635534b
mk43313@gmail.com <mk43313@gmail.com>
2023-10-31 17:11:36 +0900

    STYLE : RESULT_STRING

--- src/main/kotlin/constant/Constant.kt
+++ src/main/kotlin/constant/Constant.kt
@@ -10,7 +10,7 @@ class Constant {
         // 출력문
         const val CAR_INPUT_STRING = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         const val TRY_NUMBER_INPUT_STRING = "시도할 횟수는 몇 회인가요?"
-        const val RESULT_STRING = "실행 결과"
+        const val RESULT_STRING = "\n실행 결과"
         const val WINNER_STRING = "최종 우승자 : "
     }
 }
