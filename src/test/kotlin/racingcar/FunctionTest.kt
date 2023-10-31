2eb43079ad13c9673bebe07ebcb4cfb4f02f98ea
조형석 <chs9902@naver.com>
2023-10-31 20:53:25 +0900

    test: 자동차 이름 분리 테스트

--- src/test/kotlin/racingcar/FunctionTest.kt
+++ src/test/kotlin/racingcar/FunctionTest.kt
@@ -1,58 +1,15 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.test.NsTest
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import racingcar.model.Constants
-import racingcar.view.OutputView
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
+import racingcar.model.CarNameSeparator
 
 class FunctionTest {
-    val outputStreamCaptor = ByteArrayOutputStream()
-    @BeforeEach
-    fun setUp() {
-        System.setOut(PrintStream(outputStreamCaptor))
-    }
-
-    @AfterEach
-    fun tearDown() {
-        System.setOut(System.out)
-    }
-
     @Test
-    fun `각 게임 결과 반환 테스트`() {
-        val cars = listOf("bmw", "lexus", "lambo")
-        val progress = listOf("--", "---", "-")
-
-        OutputView().eachRacingResult(cars, progress)
-
-        val expectedOutput = """
-            bmw : --
-            lexus : ---
-            lambo : -
-        """.trimIndent()
-
-
-        assertEquals(expectedOutput, outputStreamCaptor.toString().trim())
+    fun testSeparator() {
+        val names = "kim,su,han,mu"
+        val result = CarNameSeparator().separator(names)
+        val expectedList = listOf("kim","su","han","mu")
+        assertEquals(expectedList, result)
     }
-
-    @Test
-    fun `우승자 반환 테스트`() {
-        val cars = listOf("bmw", "lexus")
-
-        OutputView().winner(cars)
-
-        val expectedOutput = """
-            최종 우승자 : bmw, lexus
-        """.trimIndent()
-
-
-        assertEquals(expectedOutput, outputStreamCaptor.toString().trim())
-    }
-
-
 }
