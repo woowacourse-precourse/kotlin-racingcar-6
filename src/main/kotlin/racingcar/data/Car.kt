6e9836b20d3f8f53dd57b86876c410a5d9cd9d25
w36495 <w36495@naver.com>
2023-11-01 00:51:08 +0900

    refactor : data class 인 Car 의 distance 를 파라미터로 수정

--- src/main/kotlin/racingcar/data/Car.kt
+++ src/main/kotlin/racingcar/data/Car.kt
@@ -1,14 +1,3 @@
 package racingcar.data
 
-data class Car(val name: String) {
-    var distance: Int = 0
-        private set
-
-    fun addDistance() {
-        this.distance += 1
-    }
-
-    override fun toString(): String {
-        return name
-    }
-}
+data class Car(val name: String, val distance: Int = 0)
