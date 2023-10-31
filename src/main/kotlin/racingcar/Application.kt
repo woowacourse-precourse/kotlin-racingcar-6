7668978b4dd6758921d0c6e381ed78dfd3b41d18
junyeoleee <117695928+junyeoleee@users.noreply.github.com>
2023-10-31 17:48:21 +0900

    사용자로부터 경주할 자동차 이름과 시도 횟수 입력

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,3 +1,14 @@
 package racingcar
 
 import kotlin.random.Random
+
+fun main() {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNames = readLine()
+    println("시도할 횟수는 몇 회인가요?")
+    val tryCount = readLine()?.toIntOrNull() ?: throw IllegalArgumentException("올바른 숫자를 입력하세요.")
+
+    val cars = carNames?.split(",")?.map { Car(it.trim()) }
+        ?: throw IllegalArgumentException("올바른 자동차 이름을 입력하세요.")
+
+}
