027703fbbe858c3a454968cd6d90fdfce5af7b4e
김진아 <c2010333@naver.com>
2023-10-31 22:41:44 +0900

    [FEAT] 게임 시작 시 출력되는 문구, 시도 할 횟수 질문

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,24 @@
 package racingcar
 
-fun main() {
-    // TODO: 프로그램 구현
+import camp.nextstep.edu.missionutils.Console
+
+
+fun main(arge: Array<String>) {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val car = Console.readLine()!!.split(",")
+    val cars = car.map { Car() }
+
+    println("시도할 횟수는 몇 회인가요?")
+    val rounds = Console.readLine()!!.toInt()
+
+    val race = Race(cars, rounds)
+    race.run()
+}
+
+class Car {
+
+}
+
+class Race {
+
 }
