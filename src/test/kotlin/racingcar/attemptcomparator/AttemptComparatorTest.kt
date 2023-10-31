9c53a70f5e78a95587984205a6b97e180da63efa
lee989898 <wnsrud1352@gmail.com>
2023-10-31 17:42:08 +0900

    [test] 시도횟수와 자동차 위치 비교해서 적절한 값 반환하는지 테스트 구현

--- src/test/kotlin/racingcar/attemptcomparator/AttemptComparatorTest.kt
+++ src/test/kotlin/racingcar/attemptcomparator/AttemptComparatorTest.kt
@@ -1,6 +1,9 @@
 package racingcar.attemptcomparator
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class AttemptComparatorTest {
 
@@ -9,5 +12,13 @@ class AttemptComparatorTest {
     @BeforeEach
     fun setUp() {
         attemptComparator = AttemptComparatorImpl()
+    }
+
+    @ParameterizedTest
+    @CsvSource("5,-----,true", "4,---,false", "2,--,true", "4,--,false")
+    fun `시도횟수와 자동차 위치 비교해서 적절한 값 반환`(attempt: Int, position: String, output: Boolean) {
+        val result = attemptComparator.compareAttemptAndCarPosition(attempt, position)
+
+        assertThat(output).isEqualTo(result)
     }
 }
