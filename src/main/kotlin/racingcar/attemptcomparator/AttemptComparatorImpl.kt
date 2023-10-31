71e89aba0ab71e3ca19f83bb1884e9af18ec6c54
lee989898 <wnsrud1352@gmail.com>
2023-10-31 17:33:32 +0900

    [feat] 시도 횟수와 자동차 전진 횟수 비교하는 클래스 생성

---
+++ src/main/kotlin/racingcar/attemptcomparator/AttemptComparatorImpl.kt
@@ -0,0 +1,10 @@
+package racingcar.attemptcomparator
+
+import racingcar.attempts.Attempts
+import racingcar.car.Car
+
+class AttemptComparatorImpl : AttemptComparator {
+    override fun compareAttemptAndCarPosition(attempts: Attempts, car: Car): Boolean {
+        TODO("Not yet implemented")
+    }
+}
