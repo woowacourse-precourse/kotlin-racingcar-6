dcdff3077be97eed655c9deafe16bc448ca999b4
aprilgom <aprilgom@gmail.com>
2023-11-01 08:41:11 +0900

    feat($main): main logic

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -70,4 +70,14 @@ object OutputManager {
 }
 
 fun main() {
+    val participants = InputManager.getCarNames()
+    val roundNum = InputManager.getRoundNum()
+    val referee = Referee(
+        namesOfParticipants = participants,
+        roundNum = roundNum,
+        OutputManager::printGameStatus
+    )
+    println("실행 결과")
+    val winners = referee.getWinners()
+    OutputManager.printWinners(winners)
 }
