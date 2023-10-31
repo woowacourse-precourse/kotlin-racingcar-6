cc376f6953b9fcd25361ebeb14f7bf2347a6c7af
김진아 <c2010333@naver.com>
2023-10-31 23:07:04 +0900

    [FEAT] 자동차 이름에 대한 조건 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,19 +6,23 @@ import camp.nextstep.edu.missionutils.Console
 fun main(arge: Array<String>) {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val car = Console.readLine()!!.split(",")
-    val cars = car.map { Car() }
+    val cars = car.map { Car(it) }
 
     println("시도할 횟수는 몇 회인가요?")
     val rounds = Console.readLine()!!.toInt()
-
-    val race = Race(cars, rounds)
-    race.run()
 }
 
-class Car {
-
+class Car(val name: String, var position: Int = 0) {
+    init {
+        checkName(name)
+    }
+    private fun checkName(name: String) {
+        if(name.length > 5 || name.contains(",") || name.isBlank()) {
+            throw IllegalArgumentException()
+        }
+    }
 }
 
-class Race {
-
-}
+//class Race {
+//
+//}
