4d0fea2e1424dc34108d56109aa5fab739a21d67
lee989898 <wnsrud1352@gmail.com>
2023-10-31 16:48:27 +0900

    [feat] Car 인터페이스에 name 추상 프로퍼티 추가

--- src/main/kotlin/racingcar/car/Car.kt
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -3,6 +3,7 @@ package racingcar.car
 import racingcar.randomnumbergenerator.RandomNumberGenerator
 
 interface Car {
+    val name: String
     val currentPosition: String
 
     fun forwardOrStop(randomNumber: Int)
