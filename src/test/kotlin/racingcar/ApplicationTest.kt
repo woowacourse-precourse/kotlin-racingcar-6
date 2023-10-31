1b9f0942e88c20ea25857d05c086b74afcc39371
DoDoBest <send1684@gmail.com>
2023-10-31 22:56:12 +0900

    test: Analyzer 테스트 코드 AnalyzerTest로 분리

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.assertThrows
 import racingcar.data.Car
-import racingcar.domain.Analyzer
 import racingcar.domain.IOHandler
 import racingcar.domain.Validator
 import java.io.ByteArrayInputStream
@@ -22,14 +21,12 @@ class ApplicationTest : NsTest() {
 
     private lateinit var validator: Validator
     private lateinit var ioHandler: IOHandler
-    private lateinit var anaylazer: Analyzer
     private val standardOut = System.out
 
     @BeforeEach
     fun setUp() {
         validator = Validator()
         ioHandler = IOHandler(validator)
-        anaylazer = Analyzer()
     }
 
     @AfterEach
@@ -74,163 +71,6 @@ class ApplicationTest : NsTest() {
 
         // then
         val expected = false
-        assertThat(actual).isEqualTo(expected)
-    }
-
-
-    @Test
-    fun `숫자 0이 나오면 움직일 수 없다는 의미의 false를 반환한다`() {
-        // given
-        val pickedNum = 0
-
-        // when
-        val actual = anaylazer.isMoveAllowed(pickedNum)
-
-        // then
-        val expected = false
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `숫자 3이 나오면 움직일 수 없다는 의미의 false를 반환한다`() {
-        // given
-        val pickedNum = 3
-
-        // when
-        val actual = anaylazer.isMoveAllowed(pickedNum)
-
-        // then
-        val expected = false
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `숫자 4가 나오면 움직일 수 있다는 의미의 true를 반환한다`() {
-        // given
-        val pickedNum = 4
-
-        // when
-        val actual = anaylazer.isMoveAllowed(pickedNum)
-
-        // then
-        val expected = true
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `숫자 9가 나오면 움직일 수 있다는 의미의 true를 반환한다`() {
-        // given
-        val pickedNum = 9
-
-        // when
-        val actual = anaylazer.isMoveAllowed(pickedNum)
-
-        // then
-        val expected = true
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `3칸 전진한 A, 2칸 전진한 B, 0칸 전진한 C를 입력하면 A를 승자로 반환한다`() {
-        // given
-        val a = Car("A")
-        repeat(3) {
-            a.move()
-        }
-        val b = Car("B")
-        repeat(2) {
-            b.move()
-        }
-        val c = Car("C")
-        val candidates = listOf(a, b, c)
-
-        // when
-        val actual = anaylazer.judgeWinner(candidates)
-
-        // then
-        val expected = listOf(a)
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `0칸 전진한 A, 3칸 전진한 B, 2칸 전진한 C를 입력하면 B를 승자로 반환한다`() {
-        // given
-        val a = Car("A")
-        val b = Car("B")
-        repeat(3) {
-            b.move()
-        }
-        val c = Car("C")
-        repeat(2) {
-            c.move()
-        }
-        val candidates = listOf(a, b, c)
-
-        // when
-        val actual = anaylazer.judgeWinner(candidates)
-
-        // then
-        val expected = listOf(b)
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `2칸 전진한 A, 0칸 전진한 B, 3칸 전진한 C를 입력하면 C를 승자로 반환한다`() {
-        // given
-        val a = Car("A")
-        repeat(2) {
-            a.move()
-        }
-        val b = Car("B")
-        val c = Car("C")
-        repeat(3) {
-            c.move()
-        }
-        val candidates = listOf(a, b, c)
-
-        // when
-        val actual = anaylazer.judgeWinner(candidates)
-
-        // then
-        val expected = listOf(c)
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `2칸 전진한 A, 0칸 전진한 B, 2칸 전진한 C를 입력하면 A, C를 승자로 반환한다`() {
-        // given
-        val a = Car("A")
-        repeat(2) {
-            a.move()
-        }
-        val b = Car("B")
-        val c = Car("C")
-        repeat(2) {
-            c.move()
-        }
-        val candidates = listOf(a, b, c)
-
-        // when
-        val actual = anaylazer.judgeWinner(candidates)
-
-        // then
-        val expected = listOf(a, c)
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `0칸 전진한 A, 0칸 전진한 B, 0칸 전진한 C를 입력하면 A, B, C를 승자로 반환한다`() {
-        // given
-        val a = Car("A")
-        val b = Car("B")
-        val c = Car("C")
-        val candidates = listOf(a, b, c)
-
-        // when
-        val actual = anaylazer.judgeWinner(candidates)
-
-        // then
-        val expected = listOf(a, b, c)
         assertThat(actual).isEqualTo(expected)
     }
 
---
+++ src/test/kotlin/racingcar/domain/AnalyzerTest.kt
@@ -0,0 +1,174 @@
+package racingcar.domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+
+import org.junit.jupiter.api.Test
+import racingcar.data.Car
+
+class AnalyzerTest {
+
+    private lateinit var anaylazer: Analyzer
+
+    @BeforeEach
+    fun setUp() {
+        anaylazer = Analyzer()
+    }
+
+    @Test
+    fun `숫자 0이 나오면 움직일 수 없다는 의미의 false를 반환한다`() {
+        // given
+        val pickedNum = 0
+
+        // when
+        val actual = anaylazer.isMoveAllowed(pickedNum)
+
+        // then
+        val expected = false
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+
+    @Test
+    fun `숫자 3이 나오면 움직일 수 없다는 의미의 false를 반환한다`() {
+        // given
+        val pickedNum = 3
+
+        // when
+        val actual = anaylazer.isMoveAllowed(pickedNum)
+
+        // then
+        val expected = false
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `숫자 4가 나오면 움직일 수 있다는 의미의 true를 반환한다`() {
+        // given
+        val pickedNum = 4
+
+        // when
+        val actual = anaylazer.isMoveAllowed(pickedNum)
+
+        // then
+        val expected = true
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `숫자 9가 나오면 움직일 수 있다는 의미의 true를 반환한다`() {
+        // given
+        val pickedNum = 9
+
+        // when
+        val actual = anaylazer.isMoveAllowed(pickedNum)
+
+        // then
+        val expected = true
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `3칸 전진한 A, 2칸 전진한 B, 0칸 전진한 C를 입력하면 A를 승자로 반환한다`() {
+        // given
+        val a = Car("A")
+        repeat(3) {
+            a.move()
+        }
+        val b = Car("B")
+        repeat(2) {
+            b.move()
+        }
+        val c = Car("C")
+        val candidates = listOf(a, b, c)
+
+        // when
+        val actual = anaylazer.judgeWinner(candidates)
+
+        // then
+        val expected = listOf(a)
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `0칸 전진한 A, 3칸 전진한 B, 2칸 전진한 C를 입력하면 B를 승자로 반환한다`() {
+        // given
+        val a = Car("A")
+        val b = Car("B")
+        repeat(3) {
+            b.move()
+        }
+        val c = Car("C")
+        repeat(2) {
+            c.move()
+        }
+        val candidates = listOf(a, b, c)
+
+        // when
+        val actual = anaylazer.judgeWinner(candidates)
+
+        // then
+        val expected = listOf(b)
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `2칸 전진한 A, 0칸 전진한 B, 3칸 전진한 C를 입력하면 C를 승자로 반환한다`() {
+        // given
+        val a = Car("A")
+        repeat(2) {
+            a.move()
+        }
+        val b = Car("B")
+        val c = Car("C")
+        repeat(3) {
+            c.move()
+        }
+        val candidates = listOf(a, b, c)
+
+        // when
+        val actual = anaylazer.judgeWinner(candidates)
+
+        // then
+        val expected = listOf(c)
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `2칸 전진한 A, 0칸 전진한 B, 2칸 전진한 C를 입력하면 A, C를 승자로 반환한다`() {
+        // given
+        val a = Car("A")
+        repeat(2) {
+            a.move()
+        }
+        val b = Car("B")
+        val c = Car("C")
+        repeat(2) {
+            c.move()
+        }
+        val candidates = listOf(a, b, c)
+
+        // when
+        val actual = anaylazer.judgeWinner(candidates)
+
+        // then
+        val expected = listOf(a, c)
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `0칸 전진한 A, 0칸 전진한 B, 0칸 전진한 C를 입력하면 A, B, C를 승자로 반환한다`() {
+        // given
+        val a = Car("A")
+        val b = Car("B")
+        val c = Car("C")
+        val candidates = listOf(a, b, c)
+
+        // when
+        val actual = anaylazer.judgeWinner(candidates)
+
+        // then
+        val expected = listOf(a, b, c)
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+}
