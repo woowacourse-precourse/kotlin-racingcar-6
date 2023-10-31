a84daf90400f8bc66a1d042d847a1fe7f5e31a24
Seannnn55 <sally99h@gmail.com>
2023-10-31 17:43:59 +0900

    refactor [RunGame]] repeatRace 리턴 문 추가

--- src/main/kotlin/racingcar/RunGame.kt
+++ src/main/kotlin/racingcar/RunGame.kt
@@ -8,13 +8,8 @@ class RunGame {
         var racingCars : MutableList<Players> = pG.makingPlayerList(pG.racingCarName())
         var racingNum: Int = pG.racingNumber()
 
-        for( i in 1..racingNum){ // 전체 경주 실행 횟수 반복
+        for( i in 1..racingNum){
             repeatRace(racingCars.size,racingCars)
-
-            // 그 안에서 플레이어 횟수만큼 반복
-                //그리고 랜덤 넘버 생성
-                // 넘버가 4보다 큰지 확인
-                // 넘버가 4보다 크면 전진
         }
 
 
@@ -23,7 +18,7 @@ class RunGame {
 
     }
 
-    fun repeatRace(numOfCar:Int,carList:MutableList<Players>){
+    fun repeatRace(numOfCar:Int,carList:MutableList<Players>):MutableList<Players>{
 
         for(i in 1..numOfCar){
             var eachNum:Int = RacingGame().randomRace()
@@ -33,7 +28,7 @@ class RunGame {
         }
 
         RacingGame().broadcastRace(carList.size, carList)
-
+        return carList
     }
 
     fun ifGoForward(randomNum:Int):Boolean{
