ea5d3b938106a87e251018fb598e04a52aef9712
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-01 02:58:44 +0900

    fix) all

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,94 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+val cars = mutableListOf<Car>()
+val winners = mutableListOf<String>()
+val error = Error()
+var MAX = 0
+val INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+val INPUT_REPEAT = "시도할 횟수는 몇 회인가요?"
+val NOT_INT_OR_NULL = "숫자가 아니거나 null입니다."
+val INPUT_UNDER_ZERO = "입력 값에 음수가 있습니다."
+val INPUT_OVER_FIVE_OR_NULL = "이름이 5자가 넘어가거나 값이 없습니다."
+val INPUT_HAVE_SPACE = ",뒤에 공백이 있습니다."
+val INPUT_DUPLICATE = "중복된 수가 있습니다."
+val INPUT_SIZE = "2개 이상 입력해야 합니다."
+val SPACE = " "
+val MAX_LENGTH = 5
+val MOVE = "-"
+
 fun main() {
-    val game = Game()
-    game.gameStart()
+    cars.clear()
+    winners.clear()
+    println(INPUT_NAME)
+    input()
+    val repeat = inputRepeat()
+    processGame(repeat)
+    findMaxPos()
+    winner()
+}
+private fun winner() {
+    for (car in cars) {
+        if (car.position == MAX) {
+            winners.add(car.name)
+        }
+    }
+    printWinner(winners)
+}
+
+private fun findMaxPos() {
+    for (car in cars) {
+        if (car.position >= MAX) {
+            MAX = car.position
+        }
+    }
+}
+private fun printWinner(winners: MutableList<String>) {
+    print("최종 우승자 : ${winners.joinToString(", ")}")
+}
+private fun processGame(repeat: Int) {
+    println()
+    println("실행 결과")
+    for(i in 0 until repeat){
+        gamePlay()
+    }
+}
+fun gamePlay(){
+    for(car in cars){
+        var random = Randoms.pickNumberInRange(0, 9)
+        step(random,car)
+    }
+    println()
+}
+fun step(random: Int, car: Car) {
+    if(random >= 4){
+        car.step()
+    }
+    println("${car.name} : ${MOVE.repeat(car.position)}")
+}
+fun input(){
+    val carname = Console.readLine()
+    val names = carname.split(',')
+    error.checkName(names)
+    for (name in names) {
+        cars.add(Car(name))
+    }
+}
+
+fun inputRepeat(): Int {
+    println(INPUT_REPEAT)
+    val repeat = Console.readLine()
+    checkNum(repeat)
+    Console.close()
+    return repeat.toInt()
+}
+fun checkNum(repeat: String) {
+    if (repeat.toIntOrNull() == null) {
+        throw IllegalArgumentException(NOT_INT_OR_NULL)
+    }
+    if (repeat.toInt() < 1) {
+        throw IllegalArgumentException(INPUT_UNDER_ZERO)
+    }
 }
--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,34 +1,10 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Randoms
+class Car(carname: String) {
+    var position = 0
+    var name = carname
 
-class Car(var name: String) {
-    var position: Int = 0
-
-    companion object {
-        private const val MOVE = "-"
-    }
-
-    fun getName(): String {
-        return name
-    }
-
-    fun play() {
-        makeRandom()
-        printResult()
-    }
-
-    fun getPosition(): Int {
-        return position
-    }
-
-    private fun printResult() {
-        println("$name : ${MOVE.repeat(position)}")
-    }
-
-    private fun makeRandom() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
-            position++
-        }
+    fun step(){
+        position++
     }
 }
