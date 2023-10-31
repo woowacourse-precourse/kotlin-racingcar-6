f198451a4b17aa936dc723abb4664a7f6d6d14f2
jklom0326 <jklom0326@gmail.com>
2023-10-31 23:25:50 +0900

    refactor: RacingGame 클래스 문자열 상수화

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -4,54 +4,38 @@ import camp.nextstep.edu.missionutils.Console
 
 class RacingGame {
     private val exception = Exception()
+    private val computer = Computer()
 
     fun gameStart() {
+        printGameStartMessage()
         val cars = initCar()
         val roundCount = readRounds()
-        printResult(roundCount, cars)
-        printWinner(cars)
+        computer.printResult(roundCount, cars)
+        computer.printWinner(cars)
     }
 
-    private fun printWinner(cars: List<Car>) {
-        var status = "최종 우승자 : "
-        val position = arrayListOf<Int>()
-        cars.forEach { car ->
-            position.add(car.position)
-        }
-        val winnerPosition = position.max()
-        val winner = cars.filter { it.position == winnerPosition }
-        winner.forEach { car ->
-            status += "${car.name}, "
-        }
-        println(status.substring(0, status.length - 2))
-    }
-
-    private fun printResult(roundCount: Int, cars: List<Car>) {
-        println("실행 결과")
-        repeat(roundCount) {
-            cars.forEach { car ->
-                car.moving()
-                car.printPosition()
-            }
-            println()
-        }
-    }
-
-    fun initCar(): List<Car> {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    private fun initCar(): List<Car> {
         val name = readCarNames()
         exception.nameValidation(name)
         return name.map { Car(it) }
     }
 
-    fun readCarNames(): List<String> {
+    private fun readCarNames(): List<String> {
         val input = Console.readLine()
         return input.split(",")
     }
 
-    fun readRounds(): Int {
-        println("시도할 횟수는 몇 회인가요?")
+    private fun readRounds(): Int {
+        printRoundMessage()
         val input = Console.readLine()
         return exception.roundsValidation(input)
+    }
+
+    private fun printGameStartMessage() = println(GAME_START_MESSAGE)
+    private fun printRoundMessage() = println(ROUND_MESSAGE)
+
+    companion object {
+        const val GAME_START_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        const val ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?"
     }
 }
