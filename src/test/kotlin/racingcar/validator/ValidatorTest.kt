f05ed8295ac80f02b6b7af6c35cc746655e644f7
lee989898 <wnsrud1352@gmail.com>
2023-10-31 16:24:35 +0900

    [test] 자동차 이름이 5글자 이하가 아닐 때 예외를 던지는지 테스트 구현

--- src/test/kotlin/racingcar/validator/ValidatorTest.kt
+++ src/test/kotlin/racingcar/validator/ValidatorTest.kt
@@ -1,6 +1,10 @@
 package racingcar.validator
 
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 class ValidatorTest {
 
@@ -9,5 +13,11 @@ class ValidatorTest {
     @BeforeEach
     fun setUp() {
         validator = ValidatorImpl()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["람보르기니우르스", "제네시스쿠페", "SORENTO"])
+    fun `자동차 이름이 5글자 이하가 아닐 때 예외를 던지는지`(carName: String) {
+        assertThrows<IllegalArgumentException> { validator.nameLength(carName) }
     }
 }
