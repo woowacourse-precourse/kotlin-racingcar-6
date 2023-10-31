32522dc756aaebf29e9090b2e1187435207f3d40
aprilgom <aprilgom@users.noreply.github.com>
2023-10-31 20:20:03 +0900

    feat($Car): compareTo

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,18 +1,23 @@
 package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
-class Car(val name: String) {
+class Car(val name: String) : Comparable<Car> {
     var position = 0
         private set
+
     init {
         require(name.length <= 5){ "Car name length exceeds 5 letters" }
     }
+
     fun goForwardOrStop() {
         val random = Randoms.pickNumberInRange(0,9)
         if (random >= 4) {
             position++
         }
     }
+
+    override fun compareTo(other: Car): Int = compareValuesBy(this, other, Car::position)
+
 }
 
 fun main() {
