17e8f11ea1a12cf121001e828050eaf7173c208b
wnsgml517 <wnsgml517@naver.com>
2023-10-31 22:16:53 +0900

    Application.kt main문 내용 입력

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,22 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
+    try {
+        val carNames = CarRace.getCarNames()
+        val moveCount = CarRace.getMoveCount()
+
+        val carPositions = IntArray(carNames.size)
+
+        println("\n자동차 경주 게임을 시작합니다!\n")
+
+        repeat(moveCount) {
+            CarMovement.moveCars(carNames, carPositions)
+            CarResults.printCurrentPositions(carNames, carPositions)
+        }
+
+        val winners = CarResults.findWinners(carNames, carPositions)
+        CarResults.printWinners(winners)
+    } catch (e: IllegalArgumentException) {
+        println("오류: 유효하지 않은 입력입니다. 게임을 종료합니다.")
+    }
 }
