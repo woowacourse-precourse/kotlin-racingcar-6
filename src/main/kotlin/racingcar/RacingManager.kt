86aa4bd5a86a7ad038af358ab1a3187784b031d3
LEE <tpqms775@naver.com>
2023-10-31 21:31:38 +0900

    refactor(racingcar): 출력 문구 수정

--- src/main/kotlin/racingcar/RacingManager.kt
+++ src/main/kotlin/racingcar/RacingManager.kt
@@ -7,6 +7,7 @@ class RacingManager {
     fun startGame(){
         val ranNum = RandomNumCreator()
         val inputManager = InputManager()
+
         for (index in 1..inputManager.readRepeatNum()){
             for (car in cars) {
                 car.move(ranNum.creator()) // 0부터 9까지의 무작위 숫자 전달
