8ae50b09f80a56c1da72164adb8b585002ce3ffe
lee989898 <wnsrud1352@gmail.com>
2023-10-31 17:26:57 +0900

    [test] 시도 횟수가 저장되는지 확인 테스트 구현

--- src/test/kotlin/racingcar/attempts/AttemptsTest.kt
+++ src/test/kotlin/racingcar/attempts/AttemptsTest.kt
@@ -1,6 +1,9 @@
 package racingcar.attempts
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class AttemptsTest {
 
@@ -9,5 +12,14 @@ class AttemptsTest {
     @BeforeEach
     fun setUp() {
         attempts = AttemptsImpl()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [5, 10, 15])
+    fun `시도 횟수가 저장되는지 확인`(attempt: Int) {
+        attempts.setAttempt(attempt)
+        val result = attempts.attempt
+
+        assertThat(attempt).isEqualTo(result)
     }
 }
