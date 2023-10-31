05156b16060050e7a353b60077bcbfcc75239ff1
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-01 02:56:45 +0900

    refact) all

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,10 +1,38 @@
 package racingcar
 
-class Car(carname: String) {
-    var position = 0
-    var name = carname
+import camp.nextstep.edu.missionutils.Randoms
 
-    fun step(){
-        position++
+class Car(var name: String) {
+    var position: Int = 0
+
+    companion object {
+        private const val MOVE = "-"
+    }
+
+    fun getName(): String {
+        return name
+    }
+
+    fun play() {
+        makeRandom()
+        printResult()
+    }
+
+    fun getPosition(): Int {
+        return position
+    }
+
+    private fun printResult() {
+        var go = ""
+        for(i in 0..<position){
+            go+= MOVE
+        }
+        println("$name : ${MOVE.repeat(position)}")
+    }
+
+    private fun makeRandom() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++
+        }
     }
 }
