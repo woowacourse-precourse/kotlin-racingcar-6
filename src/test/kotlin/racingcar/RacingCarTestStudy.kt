637d1f7ebe5f18b171866da2669b5feaf8a381cc
okm1172 <110343070+okm1172@users.noreply.github.com>
2023-10-31 18:52:41 +0900

    build error 해결 후 test case 오류 부분 삭제

--- src/test/kotlin/racingcar/RacingCarTestStudy.kt
+++ src/test/kotlin/racingcar/RacingCarTestStudy.kt
@@ -6,16 +6,6 @@ import org.junit.jupiter.api.assertThrows
 
 class RacingCarTestStudy {
 
-    //standardOut는 원래의 system.out으로 바꿔주기 위해 백업
-    /*private val standardOut = System.`out`
-    private val outputStreamCaptor = ByteArrayOutputStream()
-
-    @BeforeEach
-    fun setUp() {
-        //출력 바꿔주기
-        System.setOut(PrintStream(outputStreamCaptor))
-    }*/
-
     @Test
     fun `자동차 이름 쉼표(,)구분 테스트`() {
         var carList = splitCarName("aa,bb,cc,ddd,eee")
@@ -33,19 +23,5 @@ class RacingCarTestStudy {
     fun `경기 횟수 입력 에외 테스트`() {
         assertThrows<IllegalArgumentException> { checkExceptionTryCount(-1) }
     }
-
-    //outputStreamCaptor에 print값이 들어감
-    /*@Test
-    fun `경기 우승 자동차 출력 테스트`() {
-        val carNameList = listOf("aa","bb","cc")
-        var carForwardCount = mutableListOf(5,3,5)
-        printVictoryCar(carForwardCount,carNameList)
-        val answer = "최종 우승자 : aa, cc"
-        assertThat(answer).isEqualTo(outputStreamCaptor.toString())
-    }
-
-    @AfterEach
-    fun tearDown() {
-        System.setOut(standardOut)
-    }*/
+    
 }
