cff59f167c71730a8ff8d234986ba073b66ad98f
junyeoleee <117695928+junyeoleee@users.noreply.github.com>
2023-10-31 17:49:44 +0900

    사용자 입력을 파싱하고 자동차 객체를 생성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,14 +1,22 @@
-package racingcar
-
-import kotlin.random.Random
-
 fun main() {
+    val carNames = getCarNames()
+    val tryCount = getTryCount()
+    val cars = createCars(carNames)
+
+    race(cars, tryCount)
+}
+
+fun getCarNames(): String? {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    val carNames = readLine()
+    return readLine()
+}
+
+fun getTryCount(): Int {
     println("시도할 횟수는 몇 회인가요?")
-    val tryCount = readLine()?.toIntOrNull() ?: throw IllegalArgumentException("올바른 숫자를 입력하세요.")
+    return readLine()?.toIntOrNull() ?: throw IllegalArgumentException("올바른 숫자를 입력하세요.")
+}
 
-    val cars = carNames?.split(",")?.map { Car(it.trim()) }
+fun createCars(carNames: String?): List<Car> {
+    return carNames?.split(",")?.map { Car(it.trim()) }
         ?: throw IllegalArgumentException("올바른 자동차 이름을 입력하세요.")
-
 }
