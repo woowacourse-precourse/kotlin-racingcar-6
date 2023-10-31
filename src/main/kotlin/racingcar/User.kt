b77ecd24eb93ab28616d5a3a07a6dea345388cdf
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 03:12:48 +0900

    feat: 차량 이름 입력받는 기능 구현
    validator를 사용하여 입력 문자열과 차량 이름에 대한 검증 구현

--- src/main/kotlin/racingcar/User.kt
+++ src/main/kotlin/racingcar/User.kt
@@ -1,16 +1,22 @@
 package racingcar
+
 import camp.nextstep.edu.missionutils.Console
 
 class User {
     val validator = Validator()
 
-    fun getCarNames(): List<String>{
-        var carNames : String = Console.readLine()
+    fun getCarNames(): List<String> {
+        var carNames: String = Console.readLine()
         // validator로 올바른 문자열 입력인지 확인
-        validator.validateString(carNames)
+        validator.validateInputString(carNames)
         // List로 쪼개기
-        val nameList : List<String> = carNames.split(',')
+        val nameList: List<String> = carNames.split(',')
         // validator로 올바른 길이의 이름인지 확인
+        validator.validateNames(nameList)
         return nameList
+    }
+
+    fun getTryCount(): Int {
+        
     }
 }
