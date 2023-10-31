7c12723d7f0d8cb7df6eeba1d78cdd3e96950638
Bruce <bruce@Bruceui-MacBookPro.local>
2023-11-01 01:16:35 +0900

    [feat] get random num && if num is over 4, car can go

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,8 @@
 package racingcar
 
-import kotlin.collections.mutableMapOf
+// import kotlin.collections.mutableMapOf
+import java.util.Random
+import kotlin.text.toInt
 
 // import camp.nextstep.edu.missionutils.Randoms //pickNumberInRange
 // import camp.nextstep.edu.missionutils.Console //readLine
@@ -8,14 +10,14 @@ import kotlin.collections.mutableMapOf
 fun main() {
     
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-
+    
     val inputNames = readLine()
     val nameList = inputNames?.split(",") 
-    val carMap = mutableMapOf<String, Int>()
+    var carMap = mutableMapOf<String, Int>()
     
     for (name in nameList!!) {
         if (name.trim().length <= 5) {
-            carMap.put(name.trim(), 3)
+            carMap.put(name.trim(), 0)
         } else {
             throw IllegalArgumentException()
         }
@@ -26,9 +28,23 @@ fun main() {
     val moveConut = readLine()
     
     println("실행결과\n")
-    printResult(carMap)
-    println("")
     
+    for (i in 0 until moveConut!!.toInt()) {
+        playGames(carMap)
+        printResult(carMap)
+        println("")
+    } 
+}
+
+fun playGames(carMap : MutableMap<String, Int>){
+    
+    val random = Random()
+
+    for (car in carMap) {
+        if (random.nextInt(10) >= 4) {
+            carMap.put(car.key, car.value + 1)
+        }    
+    }
 }
 
 fun printResult(carMap : MutableMap<String, Int>) {
