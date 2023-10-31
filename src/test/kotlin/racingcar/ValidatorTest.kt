e91219c1b34d3e15dd60df5c587a40560bc0e15a
seongsan <tjdtks030@naver.com>
2023-10-31 19:18:06 +0900

    test: Validator 테스트 코드 수정
    RacingCarGameManager, Validator 클래스 리팩토링을 위해 테스트 코드 재작성

--- src/test/kotlin/racingcar/ValidatorTest.kt
+++ src/test/kotlin/racingcar/ValidatorTest.kt
@@ -8,112 +8,62 @@ class ValidatorTest {
     private val validator = Validator()
 
     @Test
-    fun `validateCarNames 메서드 사용 시 carName 이 쉼표로 구분되어있지 않을 때 예외 발생`() {
-        val input = "A B"
+    fun `checkHasNoBlank 메서드 사용 시 입력값에 공백이 있을 때 예외 발생`() {
+        val input = "A  B"
 
         assertSimpleTest {
             assertThrows<IllegalArgumentException> {
-                validator.validateCarNames(input)
+                validator.checkHasNoBlank(input)
             }
         }
     }
 
     @Test
-    fun `validateCarNames 메서드 사용 시 carName 이 공백으로 작성되었을 때 예외 발생`() {
-        val input = "A, ,ABC"
+    fun `checkHasSeparator 메서드 사용 시 입력값이 separator 로 구분되어 있지 않을 때 예외 발생`() {
+        val separator = "ㅤ"   // not space, 공백 특수문자 'ㅤ'
+        val input = "A" + separator + "B"
 
         assertSimpleTest {
             assertThrows<IllegalArgumentException> {
-                validator.validateCarNames(input)
+                validator.checkHasSeparator(input, BLANK)
             }
         }
     }
 
     @Test
-    fun `validateCarNames 메서드 사용 시 carName 이 5자를 넘어설 때 예외 발생`() {
-        val input = "A,ABC,ABCDEF"
+    fun `checkHasOnlyDigit 메서드 사용 시 입력값에 정수가 아닌 다른 값이 들어있을 때 예외 발생`() {
+        val input = "12a"
 
         assertSimpleTest {
             assertThrows<IllegalArgumentException> {
-                validator.validateCarNames(input)
+                validator.checkHasOnlyDigit(input)
             }
         }
     }
 
     @Test
-    fun `validateCarNames 메서드 사용 시 carName 의 개수가 1개일 때 예외 발생`() {
-        val input = "ABC"
+    fun `checkItemsHasNoDuplication 메서드 사용 시 입력값에 중복되는 item 이 존재할 때 예외 발생`() {
+        val input = listOf(1, 2, 3, 1)
 
         assertSimpleTest {
             assertThrows<IllegalArgumentException> {
-                validator.validateCarNames(input)
+                validator.checkItemsHasNoDuplication(input)
             }
         }
     }
 
     @Test
-    fun `validateCarNames 메서드 사용 시 중복되는 carName 이 존재할 때 예외 발생`() {
-        val input = "A,B,A"
+    fun `checkNumericValueIsInBoundary 메서드 사용 시 입력값이 주어진 범위를 벗어났을 때 예외 발생`() {
+        val input = 5
 
         assertSimpleTest {
             assertThrows<IllegalArgumentException> {
-                validator.validateCarNames(input)
+                validator.checkNumericValueIsInBoundary(input, 10, 15)
             }
         }
     }
 
-    @Test
-    fun `validateCarNames 메서드 사용 시 carName 에 아무런 값을 입력하지 않았을 때 예외 발생`() {
-        val input = ""
-
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> {
-                validator.validateCarNames(input)
-            }
-        }
-    }
-
-    @Test
-    fun `validateTotalRoundNumber 메서드 사용 시 totalRoundNumber 가 문자일 때 예외 발생`() {
-        val input = "abc"
-
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> {
-                validator.validateTotalRoundNumber(input)
-            }
-        }
-    }
-
-    @Test
-    fun `validateTotalRoundNumber 메서드 사용 시 totalRoundNumber 가 정수 타입이 아닐 때 예외 발생`() {
-        val input = "1.5"
-
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> {
-                validator.validateTotalRoundNumber(input)
-            }
-        }
-    }
-
-    @Test
-    fun `validateTotalRoundNumber 메서드 사용 시 totalRoundNumber 가 1 이상의 값이 아닐 때 예외 발생`() {
-        val input = "0"
-
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> {
-                validator.validateTotalRoundNumber(input)
-            }
-        }
-    }
-
-    @Test
-    fun `validateTotalRoundNumber 메서드 사용 시 totalRoundNumber 에 아무런 값을 입력하지 않았을 때 예외 발생`() {
-        val input = ""
-
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> {
-                validator.validateTotalRoundNumber(input)
-            }
-        }
+    companion object {
+        private const val BLANK = " "
     }
 }
