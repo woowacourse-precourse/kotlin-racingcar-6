ac32b1c288425d5bb273a6c17fb7ef98abbf9a4d
Jtility <leejewon97@gmail.com>
2023-10-31 21:00:44 +0900

    feat(invalidRacingCarsCheck): create invalidRacingCarsCheck
    create invalidRacingCarsCheck and put in main

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,15 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    var racingCars = splitRacingCarName(Console.readLine())
+    val racingCars = splitRacingCarName(Console.readLine())
+    invalidRacingCarsCheck(racingCars)
+}
+
+fun invalidRacingCarsCheck(racingCars: List<String>) {
+    racingCars.forEach {
+        if (it.length > 5)
+            throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.")
+    }
 }
 
 fun splitRacingCarName(readLine: String): List<String> {
