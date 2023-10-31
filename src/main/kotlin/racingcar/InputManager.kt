b99256b98a40ac780ddc5de7a06418b72351be2a
SeongWooHong <pluto0303@naver.com>
2023-11-01 01:42:38 +0900

    feat: add exception managing in trying time

--- src/main/kotlin/racingcar/InputManager.kt
+++ src/main/kotlin/racingcar/InputManager.kt
@@ -1,9 +1,12 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import javax.print.attribute.IntegerSyntax
 import kotlin.IllegalArgumentException
+import kotlin.reflect.typeOf
 
 class InputManager {
+    private var tryTime = 0
     fun getCarName(): List<String> {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)) 기준으로 구분")
         val inputLine = Console.readLine()
@@ -17,8 +20,17 @@ class InputManager {
 
     fun getTryTime(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        val count = Console.readLine().toInt()
-        return count
+        tryTime = Console.readLine().toInt()
+        checkTryTime()
+        return tryTime
+    }
+    private fun checkTryTime() {
+        if(tryTime is Int) {
+            return;
+        }
+        else{
+            throw IllegalArgumentException("")
+        }
     }
 
 }
