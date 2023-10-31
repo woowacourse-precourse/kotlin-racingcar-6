55817b6b3fe5c9b3bb01d19193afc487682a50e6
crAy <craycoding@gmail.com>
2023-10-31 23:33:07 +0900

    feat : function makeRandomNumber, raceCar

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -1,5 +1,7 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Racing {
     fun makeCar(): List<String> {
         val inputString = Input().inputCar()
@@ -7,5 +9,18 @@ class Racing {
         val carList = inputString.split(delim)
         Validation().validCar(carList)
         return carList
+    }
+
+    fun makeRandomNumber(): Int {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        return randomNumber
+    }
+
+    fun raceCar(carResult: MutableList<Int>) {
+        for (i in carResult.indices) {
+            if (makeRandomNumber() >= 4) {
+                carResult[i]++
+            }
+        }
     }
 }
