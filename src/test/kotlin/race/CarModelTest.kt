c7023626ede16f5ed8e3a71734b7e046b2b4019b
mk43313@gmail.com <mk43313@gmail.com>
2023-10-31 16:58:33 +0900

    TEST : CarModel에 있는 함수 test

--- src/test/kotlin/race/CarModelTest.kt
+++ src/test/kotlin/race/CarModelTest.kt
@@ -1,4 +1,48 @@
 package race
 
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
 class CarModelTest {
+    private lateinit var carModel: CarModel
+
+    @BeforeEach
+    fun setUp() {
+        carModel = CarModel()
+    }
+
+    @Test
+    fun testAddCar() {
+        carModel.addCar("Car1")
+        carModel.addCar("Car2")
+
+        val garage = carModel.garage
+        assertThat(garage).containsKeys("Car1", "Car2")
+        assertThat(garage["Car1"]).isEqualTo(0)
+        assertThat(garage["Car2"]).isEqualTo(0)
+    }
+
+    @Test
+    fun testMoveCars() {
+        val moveNum = 5
+        val initialGarageSize = carModel.garage.size
+        carModel.moveCars(moveNum)
+        val finalGarageSize = carModel.garage.size
+        assertThat(finalGarageSize).isEqualTo(initialGarageSize)
+    }
+
+    @Test
+    fun testGetWinners() {
+        carModel.addCar("Car1")
+        carModel.addCar("Car2")
+        carModel.addCar("Car3")
+        carModel.garage["Car1"] = 3
+        carModel.garage["Car2"] = 5
+        carModel.garage["Car3"] = 3
+        val winners = carModel.getWinners()
+        assertThat(winners).contains("Car2")
+        assertThat(winners).doesNotContain("Car1")
+        assertThat(winners).doesNotContain("Car3")
+    }
 }
