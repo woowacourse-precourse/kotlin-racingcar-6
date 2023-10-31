f47dee8a5042b00231b12fde4c9dbb31fd9e5df4
jun <hky08067@naver.com>
2023-10-31 13:37:22 +0900

    test: refactor에 따른 변경사항 반영

--- src/test/kotlin/racingcar/RacingCarStateTest.kt
+++ src/test/kotlin/racingcar/RacingCarStateTest.kt
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import racingcar.model.Car
 import racingcar.model.MovementState
-import racingcar.model.RacingCarState
+import racingcar.domain.RacingState
 
 class RacingCarStateTest {
 
     companion object {
-        private lateinit var racingCarState: RacingCarState
+        private lateinit var racingState: RacingState
 
         @BeforeAll
         @JvmStatic
@@ -20,49 +20,57 @@ class RacingCarStateTest {
                 Car("woni", 2),
                 Car("jun", 3)
             )
-            racingCarState = RacingCarState(carList)
+            racingState = RacingState(carList)
         }
     }
 
     @Test
     fun `숫자가 4 이상일 때 움직임 나오는지`() {
-        val method = racingCarState.javaClass
-            .getDeclaredMethod("getMovementState", Int::class.java).apply {
+        val method = racingState.javaClass
+            .getDeclaredMethod("determineMovement", Int::class.java).apply {
                 isAccessible = true
             }
-        val received = method(racingCarState, 6)
+        val received = method(racingState, 6)
         val expected = MovementState.MOVE
         Assertions.assertThat(received).isEqualTo(expected)
     }
 
     @Test
     fun `숫자가 4 미만일 때 멈춤이 나오는지`() {
-        val method = racingCarState.javaClass
-            .getDeclaredMethod("getMovementState", Int::class.java).apply {
+        val method = racingState.javaClass
+            .getDeclaredMethod("determineMovement", Int::class.java).apply {
                 isAccessible = true
             }
-        val received = method(racingCarState, 3)
+        val received = method(racingState, 3)
         val expected = MovementState.STOP
         Assertions.assertThat(received).isEqualTo(expected)
     }
 
     @Test
     fun `자동차의 움직임 결과 확인`() {
-        val received = racingCarState.getMovementResult()
-        val expected = "pobi : -\n" + "woni : --\n" + "jun : ---"
+        val method = racingState.javaClass
+            .getDeclaredMethod("getMovementResult").apply {
+                isAccessible = true
+            }
+        val received = method(racingState)
+        val expected = "pobi : -\n" + "woni : --\n" + "jun : ---" + "\n"
         Assertions.assertThat(received).isEqualTo(expected)
     }
 
     @Test
     fun `자동차의 움직임 잘못된 결과 확인`() {
-        val received = racingCarState.getMovementResult()
-        val expected = "pobi -\n" + "woni : --\n" + "jun : ---"
+        val method = racingState.javaClass
+            .getDeclaredMethod("getMovementResult").apply {
+                isAccessible = true
+            }
+        val received = method(racingState)
+        val expected = "pobi -\n" + "woni : --\n" + "jun : ---" + "\n"
         Assertions.assertThat(received).isNotEqualTo(expected)
     }
 
     @Test
     fun `승자 결과 확인`() {
-        val received = racingCarState.getWinner()
+        val received = racingState.getWinner()
         val expected = "최종 우승자 : jun"
         Assertions.assertThat(received).isEqualTo(expected)
     }
