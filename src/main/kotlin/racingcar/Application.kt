d8ae6a80e1071a01d86d776bbf37e0f549652bee
wonjongin <wonggamggik@gmail.com>
2023-11-01 05:30:15 +0900

    throw함수 main함수 영역으로 가져옴

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -8,6 +8,9 @@ fun main() {
 
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carsList: List<String> = machine.makeCars()
+    if (!machine.checkCarNameUnder5(carsList)) {
+        throw IllegalArgumentException("자동차 이름이 5자를 초과합니다.")
+    }
     val carsClass: MutableList<Car> = mutableListOf()
 
     // car 선언
@@ -17,6 +20,11 @@ fun main() {
 
     println("시도할 횟수는 몇 회인가요?")
     val tryCount = machine.tryMove()
+    if (tryCount == -1) {
+        throw IllegalArgumentException("숫자를 입력하세요")
+    } else if (tryCount == -2) {
+        throw IllegalArgumentException("양의 정수를 입력하세요")
+    }
 
     println("실행 결과")
     racing.playRacing(tryCount, carsClass)
--- src/main/kotlin/racingcar/InputMachine.kt
+++ src/main/kotlin/racingcar/InputMachine.kt
@@ -7,22 +7,23 @@ class InputMachine {
 
         val cars = readln().split(",")
 
-        checkCarNameUnder5(cars)
-
         return cars
 
     }
 
-    private fun checkCarNameUnder5(cars: List<String>) {
+    fun checkCarNameUnder5(cars: List<String>): Boolean {
 
         for (car in cars) {
 
             if (car.length > 5) {
 
-                throw IllegalArgumentException("자동차 이름이 5자를 초과합니다.")
+                return false
 
             }
         }
+
+        return true
+
     }
     //
 
@@ -32,13 +33,19 @@ class InputMachine {
         val tryCount: Int
 
         try {
+
             tryCount = readln().toInt()
+
         } catch (e: NumberFormatException) {
-            throw IllegalArgumentException("숫자를 입력하세요")
+
+            return -1
+
         }
 
         if (!checkOnlyNatural(tryCount)) {
-            throw IllegalArgumentException("양의 정수를 입력하세요")
+
+            return -2
+
         }
 
         return tryCount
