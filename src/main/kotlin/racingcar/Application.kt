04935d0143f0cf3b6ed7bc27be20392c48ecc668
김진아 <c2010333@naver.com>
2023-11-01 00:55:46 +0900

    [REFACTORING] 필요없는 부분 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 
-fun main(args: Array<String>) {
+fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 
     val car = Console.readLine()!!.split(",").map { it.trim() }
