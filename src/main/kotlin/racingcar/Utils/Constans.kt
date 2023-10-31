d57179db85824d52021bb030e299c47ef02a59f4
Hogu59 <dbstjdguswkd@naver.com>
2023-10-31 20:25:52 +0900

    feat: 변동 없는 문자열 Constant파일로 분리

---
+++ src/main/kotlin/racingcar/Utils/Constans.kt
@@ -0,0 +1,13 @@
+package racingcar.Utils
+
+object Constans {
+    //Exceptions
+    const val EXCEPTION_EXCEED_FIVE_LETTERS = "Name Could not exceed 5 letters."
+    const val EXCEPTION_NAME_CONTAINS_NON_LETTER = "Name should contains only letters."
+    const val EXCEPTION_NUMBER_UNVALID = "횟수는 숫자여야 합니다."
+
+    //InputView
+    const val EXECUTION_RESULT_MENTION = "\n실행 결과"
+    const val CAR_NAME_INPUT_MENTION = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val TIME_INPUT_MENTION = "시도할 횟수는 몇 회인가요?"
+}
