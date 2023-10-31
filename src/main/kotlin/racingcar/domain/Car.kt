14083d9a99e6c739b7d553643c496b5eab61b154
Wonhui Roh <rwhui012@gmail.com>
2023-11-01 02:18:15 +0900

    refactor(racingcar): refactored the `Car` class
    - move the logic for getting the RandomNumber to be done outside of the `Car` class
    - introduce `STOP_RECORD` for clarity

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,26 +1,22 @@
 package racingcar.domain
 
-import racingcar.utils.getRandomNumber
-import racingcar.res.CarMovement.GO
-import racingcar.res.CarMovement.GO_RECORD
+import racingcar.res.CarMovement.MOVING_FORWARD
+import racingcar.res.CarMovement.MOVING_FORWARD_RECORD
 import racingcar.res.CarMovement.STOP
+import racingcar.res.CarMovement.STOP_RECORD
 
 class Car(val carName: String) {
   private var _record: String = ""
-    get() {
-      moveOrNot(getRandomNumber())
-
-      return field
-    }
 
   val record: String
     get() = _record
 
   fun moveOrNot(number: Int): Boolean {
     return if (number >= 4) {
-      this._record += GO_RECORD
-      GO
+      this._record += MOVING_FORWARD_RECORD
+      MOVING_FORWARD
     } else {
+      this._record += STOP_RECORD
       STOP
     }
   }
