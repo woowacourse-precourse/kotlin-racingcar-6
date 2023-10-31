4a61b987ebcabc6fffa71a22b1f4f72c59129ecb
getupminaaa <getupmina@gmail.com>
2023-10-31 13:50:59 +0900

    refactor: Constant 클래스 생성 및 상수 분리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,12 +1,9 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
-
-private const val CAR_NAME_MAX_LENGTH = 5
-const val RACE_START_MSG = "경주할 자동차 이름을 입력하세요."
-const val NUMBER_ATTEMPTS_MSG = "시도할 횟수는 몇 회인가요?"
-const val BASE_NUMBER = 4
-
+import racingcar.Constants.CAR_NAME_MAX_LENGTH
+import racingcar.Constants.NUMBER_ATTEMPTS_MSG
+import racingcar.Constants.RACE_START_MSG
 
 fun main() {
 
