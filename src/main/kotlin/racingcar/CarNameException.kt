67048de0a8ee0855d884f7b2a29bcbde83570e6b
K_Gs:D <rootachieve6053@gmail.com>
2023-10-31 21:16:07 +0900

    feat(CarNameException): 클래스 생성
    CarNameException 클래스를 만들었습니다.
    - 이름 중복
    - 이름에 올바르지 못한 문자
    - 길이가 0이거나 5초과
    위 경우에 대해 예외를 던지도록 했습니다.

---
+++ src/main/kotlin/racingcar/CarNameException.kt
@@ -0,0 +1,27 @@
+package racingcar
+
+class CarNameException : ExceptionCheckerInterface {
+    override fun check(input: String) {
+        val inputCharList = input.toList()
+        val nameChecker = inputCharList.all {
+            it.isLetterOrDigit() || it == ','
+        }
+        if(nameChecker.not()){
+            throw IllegalArgumentException(Const.CAR_NAME_ERR_TYPE)
+        }
+
+        val nameList = input.split(",")
+        val nameDuplicate = nameList.distinct()
+        if(nameDuplicate.size != nameList.size){
+            throw IllegalArgumentException(Const.CAR_NAME_ERR_DUP)
+        }
+
+        val nameLenChecker = nameList.all {
+            it.length in 1..5
+        }
+
+        if(nameLenChecker.not()){
+            throw IllegalArgumentException(Const.CAR_NAME_ERR_LEN)
+        }
+    }
+}
--- src/main/kotlin/racingcar/Const.kt
+++ src/main/kotlin/racingcar/Const.kt
@@ -4,4 +4,7 @@ object Const {
     const val MOVE_POINT_NUMBER = 4
 
     const val WINNER_MSG = "최총 우승자 : "
+    const val CAR_NAME_ERR_TYPE = "잘못된 문자가 들어가 있습니다."
+    const val CAR_NAME_ERR_LEN = "이름의 길이가 잘못되었습니다."
+    const val CAR_NAME_ERR_DUP = "이름이 중복되었습니다."
 }
