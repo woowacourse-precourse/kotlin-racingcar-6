1be9694c49af2e9d181c8df3edf7563e5a28c9b9
junyeoleee <117695928+junyeoleee@users.noreply.github.com>
2023-10-31 17:52:06 +0900

    자동차 객체를 생성하고 경주를 시작

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,22 +1,34 @@
+package racingcar
+
+import kotlin.random.Random
+
 fun main() {
-    val carNames = getCarNames()
-    val tryCount = getTryCount()
-    val cars = createCars(carNames)
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNames = readLine()
+    println("시도할 횟수는 몇 회인가요?")
+    val tryCount = readLine()?.toIntOrNull() ?: throw IllegalArgumentException("올바른 숫자를 입력하세요.")
+
+    val cars = carNames?.split(",")?.map { Car(it.trim()) }
+        ?: throw IllegalArgumentException("올바른 자동차 이름을 입력하세요.")
 
     race(cars, tryCount)
 }
 
-fun getCarNames(): String? {
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    return readLine()
+data class Car(val name: String, var position: Int = 0)
+
+fun race(cars: List<Car>, tryCount: Int) {
+    repeat(tryCount) {
+        println("\n시도 ${it + 1}")
+        cars.forEach { car ->
+
+            println("${car.name} : ${"-".repeat(car.position)}")
+        }
+    }
+
+    val maxPosition = cars.map { it.position }.maxOrNull()
+    val winners = cars.filter { it.position == maxPosition }.map { it.name }
+
+    println("\n최종 우승자 : ${winners.joinToString(", ")}")
 }
 
-fun getTryCount(): Int {
-    println("시도할 횟수는 몇 회인가요?")
-    return readLine()?.toIntOrNull() ?: throw IllegalArgumentException("올바른 숫자를 입력하세요.")
-}
 
-fun createCars(carNames: String?): List<Car> {
-    return carNames?.split(",")?.map { Car(it.trim()) }
-        ?: throw IllegalArgumentException("올바른 자동차 이름을 입력하세요.")
-}
