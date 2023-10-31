cc6801e8679cc713894cf8c63b03a0e5aef2108b
uush <dbtmd447@gmail.com>
2023-10-31 15:58:17 +0900

    feat: 사용자의 입력을 받는 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,4 +2,11 @@ package racingcar
 
 fun main() {
     // TODO: 프로그램 구현
+    val carsName = inputCarsName()
+    println(carsName)
+}
+
+fun inputCarsName() : String{
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    return readLine().toString()
 }
