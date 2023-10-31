f67ed8d074cf5e3c94f8e0d51056f60ef219ca19
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-10-31 22:28:46 +0900

    fix) error

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -3,14 +3,10 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
 class Car(name: String) {
-    private var name: String
+    private var name: String = name
     private var position = 0
     companion object {
         private const val MOVE = "-"
-    }
-
-    init {
-        this.name = name
     }
 
     fun getName(): String {
