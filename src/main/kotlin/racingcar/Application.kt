82befb6081e051528488988a89ab9964637e1981
김진아 <c2010333@naver.com>
2023-10-31 23:42:54 +0900

    [FEAT] 게임에 대한 결과값 출력 및 우승자 출력

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,42 +6,60 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main(arge: Array<String>) {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+
     val car = Console.readLine()!!.split(",")
     val cars = car.map { Car(it) }
 
     println("시도할 횟수는 몇 회인가요?")
     val rounds = Console.readLine()!!.toInt()
 
-    val race = Car.Race(cars, rounds)
-    race.run
+    val race = Race(cars, rounds)
+    race.run()
+
+    val winners = race.winners()
+    println("최종 우승자 : ${winners.joinToString { it.name }}")
 }
 
-class Car(val name: String, var position: Int = 0) {
-    init {
-        checkName(name)
-    }
 
-    private fun checkName(name: String) {
-        if (name.length > 5 || name.contains(",") || name.isBlank()) {
-            throw IllegalArgumentException()
+    class Car(val name: String, var position: Int = 0) {
+        init {
+            checkName(name)
+        }
+
+        private fun checkName(name: String) {
+            if (name.length > 5 || name.contains(",") || name.isBlank()) {
+                throw IllegalArgumentException()
+            }
+        }
+
+        fun move(raceNums: Int) {
+            if (raceNums >= 4) {
+                position++
+            }
+        }
+
+        override fun toString(): String {
+            return "$name : ${"-".repeat(position)}"
         }
     }
 
-    fun move(raceNums: Int) {
-        if(raceNums >= 4) {
-            position++
+    class Race(val cars: List<Car>, val rounds: Int) {
+        fun run() {
+            for (track in 0 until rounds) {
+                cars.forEach {
+                    it.move(Randoms.pickNumberInRange(0, 9))
+                    result()
+                }
+            }
         }
-    }
-    override fun toString(): String {
-        return "$name : ${"-".repeat(position)}"
-    }
-}
 
-class Race(val cars: List<Car>, val rounds: Int) {
-    fun run() {
-        for (track in 0 until rounds) {
-            cars.forEach {
-                it.move(Randoms.pickNumberInRange(0,9)) }
+        private fun result() {
+            cars.forEach { println(it) }
+            println()
+        }
+
+        fun winners(): List<Car> {
+            val maxPosition = cars.maxOf { it.position }
+            return cars.filter { it.position == maxPosition }
         }
     }
-}
