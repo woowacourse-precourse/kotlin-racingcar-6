312c2d58ec445507d82cb1b2c8cf3ca61ff401d8
hxeyexn <kck85075@kumoh.ac.kr>
2023-10-31 23:50:40 +0900

    Feat: 자동차 전진 기능

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,10 +2,19 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class Car {
+class Car(val name: String) {
+
+    private var position = 0
 
     private fun findForwardCondition(): Boolean {
         return Randoms.pickNumberInRange(0, 9) > 3
+    }
+
+    fun move() {
+        if (findForwardCondition()) {
+            position++
+        }
+        print("$name : ${"-".repeat(position)}")
     }
 
 }
