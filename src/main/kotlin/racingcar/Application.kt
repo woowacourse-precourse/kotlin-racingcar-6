f69966a08286afb35f43348cd571c45cc32c4533
Jang Hong Jun <37576725+ajang369@users.noreply.github.com>
2023-10-31 21:10:00 +0900

    feat: 자동차 이름 입력 기능 및 예외 처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,26 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    val carInput = Console.readLine()
+    val carList = carInput.split(",")
+    processExceptionCar(carInput, carList)
+
+}
+
+fun processExceptionCar(carInput: String, carList: List<String>) {
+    if (carInput.isNullOrBlank()) {
+        throw IllegalArgumentException(Const.EXCEPTION_WRONG_INPUT)
+    }
+    for (name in carList) {
+        if (name.toList().any { !it.isLetterOrDigit() }) {
+            throw IllegalArgumentException(Const.EXCEPTION_WRONG_NAME)
+        }
+
+        if (name.length > 5) {
+            throw  IllegalArgumentException(Const.EXCEPTION_NAME_LENGTH)
+        }
+    }
 }
---
+++ src/main/kotlin/racingcar/Const.kt
@@ -0,0 +1,9 @@
+package racingcar
+
+object Const {
+
+    const val EXCEPTION_WRONG_INPUT = "이름 입력이 NULL이거나 공백이 입력되었습니다."
+    const val EXCEPTION_NAME_LENGTH = "이름의 길이가 잘못되었습니다."
+    const val EXCEPTION_WRONG_NAME = "이름에 공백이나 특수문자가 들어가있습니다."
+
+}
