8fce80bedc4fc6740cbf9f9f7f2fc2066fab7d18
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-31 15:21:40 +0900

    refactor: 레이싱 참가 자동차 관리 객체 변경, Racing -> RacingManager

--- src/main/kotlin/race/Racing.kt
+++ src/main/kotlin/race/Racing.kt
@@ -1,23 +1,12 @@
 package race
 
-import car.Car
 import car.CarConfiguration.INIT_DIST
-import car.CarFactory.makeCar
 import race.RacingSystemValues.FINAL_WINNER
 import race.RacingSystemValues.WINNER_SEPARATOR
 import java.math.BigInteger
 
 class Racing {
-    private var racingCarList = listOf<Car>()
     private var firstCarDist = BigInteger(INIT_DIST)
-    fun settingRacingCar(carList: List<String>) {
-        val tempCarList = mutableListOf<Car>()
-        carList.forEach {
-            tempCarList.add(makeCar(it))
-        }
-        racingCarList = tempCarList
-    }
-
     fun start() {
         for (car in racingCarList) {
             car.start()
--- src/main/kotlin/race/RacingManager.kt
+++ src/main/kotlin/race/RacingManager.kt
@@ -2,11 +2,16 @@ package race
 
 import Controller.inputCarInformation
 import Controller.inputRacingRoundInformation
+import car.Car
+import car.CarFactory.makeCar
 import race.RacingSystemValues.COMPUTATION_OUTCOME
 import race.RacingSystemValues.INIT_RACING_ROUND
 import java.math.BigInteger
 
 class RacingManager(private val racing: Racing) {
+
+    private lateinit var racingCarList: List<Car>
+
     init {
         settingRacingCar()
         raceStart()
@@ -14,7 +19,11 @@ class RacingManager(private val racing: Racing) {
     }
 
     private fun settingRacingCar() {
-        racing.settingRacingCar(inputCarInformation())
+        val tempCarList = mutableListOf<Car>()
+        inputCarInformation().forEach {
+            tempCarList.add(makeCar(it))
+        }
+        racingCarList = tempCarList
     }
 
     private fun raceStart() {
