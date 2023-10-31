7bdd5868aa1c9082f52725b70d0deabf0b6cb103
조형석 <chs9902@naver.com>
2023-10-31 19:09:14 +0900

    test: 자동차 이름이 없을 때 테스트 추가

---
+++ src/test/kotlin/racingcar/CarNameTest.kt
@@ -0,0 +1,20 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CarNameTest :NsTest(){
+    @Test
+    fun `자동차 이름이 없을 때`() {
+        assertThrows<IllegalArgumentException> { runException(",java", "1") }
+    }
+
+
+
+    override fun runMain() {
+        main()
+    }
+}
