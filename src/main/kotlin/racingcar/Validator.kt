f718945b19d2c171bf7e1a76390cd4f670d9493d
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 02:40:04 +0900

    feat: 차량 이름이 5자리 이하인지 검사
    차량의 이름은 5자리 이하여야 한다. 5글자가 넘어갈 시,
    IllegalArgumentException을 발생시킨다.

--- src/main/kotlin/racingcar/Validator.kt
+++ src/main/kotlin/racingcar/Validator.kt
@@ -3,19 +3,25 @@ package racingcar
 class Validator {
     fun containsComma(inputString: String) {
         if (!inputString.contains(','))
-            throw IllegalArgumentException("invalid input string, there are another special characters")
+            throw IllegalArgumentException("invalid input string, there is no comma")
     }
 
     fun validateNames(carsNames: List<String>) {
         for (name in carsNames) {
             isNull(name)
-            
+            underFiveLength(name)
         }
     }
 
     private fun isNull(name: String) {
         if (name.isNullOrEmpty()) {
             throw IllegalArgumentException("invalid car's name, name is null or empty")
+        }
+    }
+
+    private fun underFiveLength(name: String) {
+        if (name.length > 5){
+            throw IllegalArgumentException("invalid car's name, length of name is over 5")
         }
     }
 }
