ba93b3a6d7cc76a1717a0c6a1fde234062819bef
Seannnn55 <sally99h@gmail.com>
2023-10-31 18:39:58 +0900

    feat: [PostGame] 우승자 판별 후 리스트 리턴하는 함수 생성

---
+++ src/main/kotlin/racingcar/PostGame.kt
@@ -0,0 +1,10 @@
+package racingcar
+
+class PostGame {
+    fun getWinner(carList:MutableList<Players>):List<Players>{
+        val winnerCar : Int = carList.maxBy {it.num}.num
+        var winnerList = carList.filter { it.num == winnerCar }
+
+        return winnerList
+    }
+}
