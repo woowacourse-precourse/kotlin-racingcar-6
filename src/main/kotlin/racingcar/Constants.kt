3c50f41691f21ead414d1027bd54a7a4b77f0baf
getupminaaa <getupmina@gmail.com>
2023-10-31 16:31:25 +0900

    refactor: 게임 시작 시 출력 문구에 "이름은 쉼표(,) 기준으로 구분)" 추가

--- src/main/kotlin/racingcar/Constants.kt
+++ src/main/kotlin/racingcar/Constants.kt
@@ -2,7 +2,7 @@ package racingcar
 
 object Constants {
     const val CAR_NAME_MAX_LENGTH = 5
-    const val RACE_START_MSG = "경주할 자동차 이름을 입력하세요."
+    const val RACE_START_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     const val NUMBER_ATTEMPTS_MSG = "시도할 횟수는 몇 회인가요?"
     const val BASE_NUMBER = 4
 }
--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -137,7 +137,7 @@ class ApplicationTest : NsTest() {
             run {
                 printRaceStart(RACE_START_MSG)
             }
-            assertThat(output()).isEqualTo("경주할 자동차 이름을 입력하세요.")
+            assertThat(output()).isEqualTo("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         }
     }
 
