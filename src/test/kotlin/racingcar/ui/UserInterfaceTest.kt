42343e5cc1ba116d4919698d2df2409d1d27c3a1
book3039 <book3039@knou.ac.kr>
2023-11-01 00:44:26 +0900

    refactor: UserInterfaceTest.kt 테스트 코드 리팩토링

--- src/test/kotlin/racingcar/ui/UserInterfaceTest.kt
+++ src/test/kotlin/racingcar/ui/UserInterfaceTest.kt
@@ -7,17 +7,15 @@ import org.junit.jupiter.api.assertThrows
 import racingcar.ui.UserInput.createNameList
 
 class UserInterfaceTest {
-    val input = "  choi  , gerrard  ,  mint  "
-    val names = listOf("choi", "gerrard", "mint")
-    val inputNames = input.createNameList()
+
     @Test
     fun `입력을 쉼표로 구분하고, 앞 뒤 공백을 제거하여 리스트를 생성한다`() {
-        assertEquals(names, inputNames)
+        assertEquals(listOf("choi", "steve", "mint"), "  choi  , steve  ,  mint  ".createNameList())
     }
 
     @Test
     fun `자동차 이름이 5자 이하인지 검사`() {
-        assertThrows<IllegalArgumentException> { InputValidator.checkNamesLength(inputNames)}
+        assertThrows<IllegalArgumentException> { InputValidator.checkNamesLength("choi, gerrard, mint".createNameList())}
     }
 
     @Test
