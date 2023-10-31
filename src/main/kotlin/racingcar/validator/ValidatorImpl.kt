893596fa9c04a9c6160b2e67520209fc5c4d46ae
lee989898 <wnsrud1352@gmail.com>
2023-10-31 16:19:00 +0900

    [feat] InputView의 값이 적절한 지 확인하는 클래스 생성

---
+++ src/main/kotlin/racingcar/validator/ValidatorImpl.kt
@@ -0,0 +1,11 @@
+package racingcar.validator
+
+class ValidatorImpl: Validator {
+    override fun nameLength(name: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun attemptsComponents(attempts: String) {
+        TODO("Not yet implemented")
+    }
+}
