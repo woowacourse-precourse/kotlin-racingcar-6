cfd9ff6ecd84f0745288f1bc05c7720afec25361
yoon <cbj0010@naver.com>
2023-10-31 14:30:13 +0900

    [Feat] go,stop판단 함수

--- src/main/kotlin/racingcar/Driver.kt
+++ src/main/kotlin/racingcar/Driver.kt
@@ -1,4 +1,21 @@
 package racingcar
 
 class Driver {
+
+    fun decideMovement() {
+
+    }
+
+    private fun checkIsGoToStop(randomNum: Int): String {
+        val num = when (randomNum) {
+            in 0..4 -> STOP
+            else -> GO
+        }
+        return num
+    }
+
+    companion object {
+        const val STOP = ""
+        const val GO = "-"
+    }
 }
