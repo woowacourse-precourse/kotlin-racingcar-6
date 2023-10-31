e9ce6f3f270dc94701af61b3e4764f7e23827356
MinJun <agvber12@gmail.com>
2023-11-01 07:06:44 +0900

    feat: 사용자로 부터 경주차의 이름 가져오기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,23 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
+private val racingCar = mutableMapOf<String, Int>() // 자동차 이름, 점수
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    inputRacingCarName()
+
+}
+
+private fun inputRacingCarName() {
+    val readLine = readLine()
+    val racingCarNameList = readLine.split(",")
+    racingCarNameList.forEach { car ->
+        if (car.length <= 5) {
+            throw IllegalArgumentException()
+        }
+
+        racingCar[car] = 0
+    }
 }
