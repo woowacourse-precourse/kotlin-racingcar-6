fce4bfd3f77d3dad6b17da19aad807a7792bbd15
wnsgml517 <wnsgml517@naver.com>
2023-10-31 22:15:50 +0900

    CarRace.kt 요구 메소드 사용 변경

--- src/main/kotlin/racingcar/CarRace.kt
+++ src/main/kotlin/racingcar/CarRace.kt
@@ -1,11 +1,11 @@
 package racingcar
 
 import java.lang.IllegalArgumentException
-
+import camp.nextstep.edu.missionutils.Console
 object CarRace {
     fun getMoveCount(): Int {
         print("시도할 횟수를 입력하세요: ")
-        val moveCount = readLine()?.toInt()
+        val moveCount = Console.readLine()?.toInt()
         if (moveCount == null || moveCount < 1) {
             throw IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.")
         }
@@ -14,7 +14,7 @@ object CarRace {
 
     fun getCarNames(): List<String> {
         print("경주할 자동차 이름을 쉼표(,)로 구분하여 입력하세요 (각 이름은 5자 이하): ")
-        val input = readLine()
+        val input = Console.readLine()
         if (input.isNullOrBlank()) {
             throw IllegalArgumentException("자동차 이름을 입력해야 합니다.")
         }
