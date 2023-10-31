7f31eb723f1d5aa497c436924db56cf3dce12476
Sihwan Kim <vkn123@naver.com>
2023-10-31 16:29:50 +0900

    test: 제출 오류로 테스트 코드 하나로 이동

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -4,8 +4,7 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeT
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.*
 
 class ApplicationTest : NsTest() {
     @Test
@@ -30,8 +29,87 @@ class ApplicationTest : NsTest() {
         main()
     }
 
+    @Test
+    fun `inputRoundCount() 숫자에 문자열 표함`() {
+        assertThrows<IllegalArgumentException>("잘못된 입력") {
+            userClient.checkIsNumber("12n")
+        }
+    }
+
+    @Test
+    fun `inputRoundCount() 공백입력`() {
+        assertThrows<IllegalArgumentException>("잘못된 입력") {
+            userClient.checkIsNumber("")
+        }
+    }
+
+    @Test
+    fun `inputRoundCount() 정상입력`() {
+        assertDoesNotThrow {
+            userClient.checkIsNumber("124")
+        }
+    }
+
+
+    @Test
+    fun `자동차 이름 5글자 초과`() {
+        assertThrows<IllegalArgumentException>("잘못된 입력") {
+            userClient.afterCheckConvertCarList("fiveOver,abc,de")
+        }
+    }
+
+    @Test
+    fun `자동차 이름 공백입력`() {
+        assertThrows<IllegalArgumentException>("자동차 개수 0 또는 1개 입력") {
+            userClient.afterCheckConvertCarList("")
+        }
+    }
+    @Test
+    fun `자동차 이름 사이 공백 입력`() {
+        assertThrows<IllegalArgumentException>("자동차 이름 공백") {
+            userClient.afterCheckConvertCarList("abc,,def")
+        }
+    }
+    @Test
+    fun `자동차 이름 한 개만 입력`() {
+        assertThrows<IllegalArgumentException>("자동차 개수 0 또는 1개 입력") {
+            userClient.afterCheckConvertCarList("five")
+        }
+    }
+
+
+    @Test
+    fun `쉼표기준 맨앞 쉼표`() {
+        assertThrows<IllegalArgumentException>("맨앞 쉼표") {
+            userClient.afterCheckConvertCarList(",asd,sdf")
+        }
+    }
+
+    @Test
+    fun `쉼표기준 맨뒤 쉼표`() {
+        assertThrows<IllegalArgumentException>("맨뒤 쉼표") {
+            userClient.afterCheckConvertCarList("asd,sdfd,sdsf,sddd,")
+        }
+    }
+
+    @Test
+    fun `쉼표기준 정상입력`() {
+        Assertions.assertEquals(
+            userClient.afterCheckConvertCarList("abc,def,ghi").map { it.name },
+            listOf(Car("abc"), Car("def"), Car("ghi")).map { it.name }
+        )
+    }
+
+
     companion object {
+        var userClient = UserClient()
+
         private const val MOVING_FORWARD = 4
         private const val STOP = 3
+        @JvmStatic
+        @BeforeAll
+        fun beforeAll(): Unit {
+            userClient = UserClient()
+        }
     }
 }
