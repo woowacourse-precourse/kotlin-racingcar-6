17c0785772cf08560c6d754d2717491dbf0aa736
Jtility <leejewon97@gmail.com>
2023-11-01 02:40:02 +0900

    refactor(RacingCar): create class RacingCar
    and create method move(), print()

---
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -0,0 +1,12 @@
+package racingcar
+
+class RacingCar(name: String) {
+    fun move() {
+        TODO("Not yet implemented")
+    }
+
+    fun print() {
+        TODO("Not yet implemented")
+    }
+
+}
