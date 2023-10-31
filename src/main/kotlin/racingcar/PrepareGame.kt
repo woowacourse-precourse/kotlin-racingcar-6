eb308f54c709d3152fa0af6eeb936ecba44c0bfa
Seannnn55 <sally99h@gmail.com>
2023-10-31 19:20:55 +0900

    refactor: [PrepareGame] 출력문 괄호 수정

--- src/main/kotlin/racingcar/PrepareGame.kt
+++ src/main/kotlin/racingcar/PrepareGame.kt
@@ -3,7 +3,7 @@ package racingcar
 class PrepareGame {
 
     fun racingCarName(): List<String> {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         var userName: String? = readLine()
         var userList: List<String> = userName!!.split(",")
 
