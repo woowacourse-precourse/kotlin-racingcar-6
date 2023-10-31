16602f9ef08770a48c197e0fd08eb59274c81a1c
lee989898 <wnsrud1352@gmail.com>
2023-10-31 11:45:42 +0900

    [feat] 0~9 사이에 랜덤 숫자를 반환하는 메서드 구현

--- src/main/kotlin/racingcar/randomnumbergenerator/RandomNumberGeneratorImpl.kt
+++ src/main/kotlin/racingcar/randomnumbergenerator/RandomNumberGeneratorImpl.kt
@@ -1,7 +1,9 @@
 package racingcar.randomnumbergenerator
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class RandomNumberGeneratorImpl : RandomNumberGenerator {
     override fun generateRandomNumber(): Int {
-        TODO("Not yet implemented")
+        return Randoms.pickNumberInRange(0, 9)
     }
 }
