456d07bfe16942c64daa08746b64c2a5ebd82848
wonjongin <wonggamggik@gmail.com>
2023-10-31 23:06:25 +0900

    1. Racing.random09() 테스트 편하게 수정 2. Racing.forwardOver4() 작성 및 테스트 완료

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,19 +5,19 @@ fun main() {
     val machine = InputMachine()
     val racing = Racing()
 
-//    val carsList : List<String> = machine.makeCars()
-//
-//    val carsClass : MutableList<Car> = mutableListOf()
-//
-//    // car 선언
-//    for (car in carsList) {
-//        carsClass.add(Car(car))
-//    }
+    val carsList : List<String> = machine.makeCars()
+
+    val carsClass : MutableList<Car> = mutableListOf()
+
+    // car 선언
+    for (car in carsList) {
+        carsClass.add(Car(car))
+    }
 //
 //    machine.checkCarNameUnder5(carsClass)
 
 //    val tryCount = machine.tryMove()
 
-    println(racing.random09())
+    racing.forwardOver4(carsClass)
 
 }
