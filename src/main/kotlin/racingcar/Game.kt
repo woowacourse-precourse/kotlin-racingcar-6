95ec391c055d5d18655bca08f802777579cbb7f2
yoon <cbj0010@naver.com>
2023-11-01 01:57:30 +0900

    [Feat] 실행횟수 예외처리 조건 추가

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -18,8 +18,8 @@ class Game {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val carList = createCars()
         println("시도할 횟수는 몇 회인가요?")
-        val tryGame = Console.readLine() ?: throw IllegalArgumentException("숫자를 입력해주세요")
-        val maxDistance = CarFactory().displyCarRace(carList, tryGame.toInt())
+        val tryGame = Console.readLine()?.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요")
+        val maxDistance = CarFactory().displyCarRace(carList, tryGame)
         val winners = findWinner(carList, maxDistance)
         println("최종 우승자 : $winners")
     }
