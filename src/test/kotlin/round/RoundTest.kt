3acbfca24c238fba668efcdd0a9661dbef9683e5
stopkite <stopkite98@gmail.com>
2023-10-31 18:17:27 +0900

    refactor: 불필요한 import 제거

--- src/test/kotlin/round/RoundTest.kt
+++ src/test/kotlin/round/RoundTest.kt
@@ -1,12 +1,10 @@
 package round
 
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import racingcar.car.Car
-import kotlin.math.exp
 
 class RoundTest {
 
