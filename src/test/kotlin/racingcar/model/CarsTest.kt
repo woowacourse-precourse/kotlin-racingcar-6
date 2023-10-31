f8834c2b4d9c82659e61f4eb40449842da9805e0
sh1mj1 <7wlgns@gmail.com>
2023-10-31 21:01:44 +0900

    test(CarsTest): test for getting winner logic
    * cars 중 가장 큰 position 의 car 를 리턴한다.
    * 만약 그것이 여럿이면 여러 car 들을 리턴한다.
    * 모든 car 가 움직이지 않고 position 이 0 이면 모든 car 를 리턴한다.

--- src/test/kotlin/racingcar/model/CarsTest.kt
+++ src/test/kotlin/racingcar/model/CarsTest.kt
@@ -1,5 +1,6 @@
 package racingcar.model
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -21,6 +22,45 @@ class CarsTest {
     fun `중복된 이름이 없으면 테스트를 통과한다`() {
         cars = Cars(listOf(Car("pobi"), Car("woni"), Car("jun")))
         assertDoesNotThrow { cars.validateDuplicate() }
+    }
+
+    @Test
+    fun `cars 들 중 가장 큰 position 을 가진 car 가 하나이면 그 이름을 리턴한다`() {
+        cars = Cars(
+            listOf(
+                Car("pobi", position = 0),
+                Car("woni", position = 1),
+                Car("jun", position = 2)
+            )
+        )
+        val result = cars.decideWinner()
+        Assertions.assertThat(result).isEqualTo(listOf("jun"))
+    }
+
+    @Test
+    fun `cars 들 중 가장 큰 position 을 가진 car 가 여럿이면 그 이름들을 리턴한다`() {
+        cars = Cars(
+            listOf(
+                Car("pobi", position = 2),
+                Car("woni", position = 2),
+                Car("jun", position = 2)
+            )
+        )
+        val result = cars.decideWinner()
+        Assertions.assertThat(result).isEqualTo(listOf("pobi", "woni", "jun"))
+    }
+
+    @Test
+    fun `cars 들의 position 이 모두 0 이면 모든 이름을 리턴한다`() {
+        cars = Cars(
+            listOf(
+                Car("pobi", position = 0),
+                Car("woni", position = 0),
+                Car("jun", position = 0)
+            )
+        )
+        val result = cars.decideWinner()
+        Assertions.assertThat(result).isEqualTo(listOf("pobi", "woni", "jun"))
     }
 
 }
