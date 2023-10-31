8c207ded7d05b2f413dc5d8600a0b4c81e2694ae
aprilgom <aprilgom@users.noreply.github.com>
2023-10-31 17:38:45 +0900

    feat($Car): setCarName with exception

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,11 @@
 package racingcar
 
+class Car(val name: String) {
+    init {
+        require(name.length <= 5){ "Car name length exceeds 5 letters" }
+    }
+}
+
 fun main() {
     // TODO: 프로그램 구현
 }
