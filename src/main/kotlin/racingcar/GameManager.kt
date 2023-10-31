754d877647b1169d65bed14d8676830743e5b7b4
Jbrochan <hyeongchanj@gmail.com>
2023-10-31 16:53:32 +0900

    feat: 시도 횟수 저장 방법 변경
    - 레이싱 시도 횟수를 GameManager 클래스의 멤버 변수에 저장하도록 변경

--- src/main/kotlin/racingcar/GameManager.kt
+++ src/main/kotlin/racingcar/GameManager.kt
@@ -2,13 +2,14 @@ package racingcar
 
 class GameManager {
     private val racingCarList = ArrayList<RacingCar>()
+    private var trial = 0
     fun runRacingGame() {
         val inputManager = InputManager()
 
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         makeRacingCarList(inputManager.getRacingCarNameList())
         println("시도할 횟수는 몇 회인가요?")
-        val trial = inputManager.getTrial()
+        trial = inputManager.getTrial()
     }
 
     private fun makeRacingCarList(racingCarNameList: List<String>) {
