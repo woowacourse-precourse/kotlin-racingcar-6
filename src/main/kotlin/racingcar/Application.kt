582428de7cfc5799796c3b64e29258a83a2b2128
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-10-31 13:03:31 +0900

    [Feat] 자동차 이름 입력 함수 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val game = RacingCarGame()
+    game.inputCarNames()
+}
+
+class RacingCarGame {
+
+    fun inputCarNames() {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        val carNames = Console.readLine()
+        val carNameList = carNames.split(',')
+    }
 }
