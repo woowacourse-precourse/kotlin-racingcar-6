1bda89b2101e5deff7265cfbeb01dcc08d85132e
Jun <ayz1070@gmail.com>
2023-10-31 23:40:18 +0900

    테스트 코드 수정

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -6,32 +6,53 @@ import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import racingcar.domain.Car
 
 class ApplicationTest : NsTest() {
-//    @Test
-//    fun `전진 정지`() {
-//        assertRandomNumberInRangeTest(
-//            {
-//                run("pobi,woni", "1")
-//                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi")
-//            },
-//            MOVING_FORWARD, STOP
-//        )
-//    }
-//
-//    @Test
-//    fun `이름에 대한 예외 처리`() {
-//        assertSimpleTest {
-//            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
-//        }
-//    }
+    @Test
+    fun `전진 정지`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni", "1")
+                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi")
+            },
+            MOVING_FORWARD, STOP
+        )
+    }
+
+    @Test
+    fun `이름에 대한 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+        }
+    }
+
+    @Test
+    fun `4이상의 숫자 입력 시 count 증가`() {
+        var car = Car("pobi")
+        var now = car.count
+        car.move(5)
+        var after = car.count
+        assertThat(now+1).isEqualTo(after)
+    }
+
+    @Test
+    fun `4미만의 숫자 입력 시 count 변화 없음`() {
+        var car = Car("pobi")
+        var now = car.count
+        car.move(3)
+        var after = car.count
+        assertThat(now).isEqualTo(after)
+    }
+
+
 
     public override fun runMain() {
         main()
     }
 
-//    companion object {
-//        private const val MOVING_FORWARD = 4
-//        private const val STOP = 3
-//    }
+    companion object {
+        private const val MOVING_FORWARD = 4
+        private const val STOP = 3
+    }
 }
