f7f8510b48f6e5e76cd91fd5f3b52b8b7c62bb47
NOGUEN <nhg1113@naver.com>
2023-11-01 02:07:08 +0900

    feat: add exception constants

---
+++ src/main/kotlin/racingcar/Constants.kt
@@ -0,0 +1,6 @@
+package racingcar
+
+const val EXCEPTION_MESSAGE_EXCEED_NAME_LENGTH = "이름의 길이가 5를 초과했습니다."
+const val EXCEPTION_MESSAGE_BLANK_NAME = "이름이 비었습니다."
+const val EXCEPTION_MESSAGE_WRONG_ROUND_NUMBER = "잘못된 라운드 값이 입력되었습니다."
+
