0886e76f88dc612dfbe90799e7fe887e9932c909
wonjongin <wonggamggik@gmail.com>
2023-11-01 03:15:30 +0900

    1. 테스트 케이스 스켈레톤 코드 생성

---
+++ src/test/kotlin/racingcar/ApplicationKtTest.kt
@@ -0,0 +1,34 @@
+package racingcar
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayNameGeneration
+import org.junit.jupiter.api.Test
+
+class ApplicationKtTest {
+    @Test
+    fun `자동차 이름이 5글자 이하면_생성`() {
+
+    }
+
+    @Test
+    fun `자동차 이름이 5글자 초과면_오류`() {
+
+    }
+
+    @Test
+    fun `이동 횟수 양의 정수이면_실행`() {
+
+    }
+
+    @Test
+    fun `이동 횟수 음의 정수이면_오류`() {
+
+    }
+
+    @Test
+    fun `이동 횟수 문자면_오류`() {
+
+    }
+
+}
