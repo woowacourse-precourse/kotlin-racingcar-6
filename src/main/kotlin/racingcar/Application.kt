fe426fa28bce4abda393e5b8358d04bfa7fed6fc
상은 <71699054+vmkmym@users.noreply.github.com>
2023-10-31 21:47:26 +0900

    <refactor> 변하지 않는 값 상수로 선언 - 마법 숫자를 읽기 쉽고 유지 관리 가능한 상수로 대체

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,12 +6,17 @@ import camp.nextstep.edu.missionutils.Randoms
 const val PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 const val PROMPT_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
 const val MAX_CAR_NAME_LENGTH = 5
+const val START_IDX = 0
+const val END_IDX = 9
+const val CONDITION = 4
+const val RESULT = "\n실행 결과"
+
 
 data class Car(val name: String) {
-    var position: Int = 0
+    var position: Int = START_IDX
 
     fun move() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+        if (Randoms.pickNumberInRange(START_IDX, END_IDX) >= CONDITION) {
             position++
         }
     }
@@ -26,7 +31,7 @@ fun main() {
     val cars = createCars(carNames)
 
     val inputAttempts = readAttempts()
-    println("\n실행 결과")
+    println(RESULT)
 
     val maxPosition = simulateRace(cars, inputAttempts)
     val winners = findWinners(cars, maxPosition)
@@ -37,7 +42,6 @@ private fun readCarNames(): List<String> {
     println(PROMPT_CAR_NAMES)
 
     val inputNames = Console.readLine()
-
     if (inputNames.split(",").any { it.length > MAX_CAR_NAME_LENGTH }) {
         throw IllegalArgumentException()
     }
@@ -49,7 +53,7 @@ private fun readAttempts(): Int {
 
     return try {
         val input = Console.readLine().toInt()
-        if (input <= 0) throw IllegalArgumentException()
+        if (input <= START_IDX) throw IllegalArgumentException()
         input
     } catch (e: NumberFormatException) {
         throw IllegalArgumentException()
@@ -61,13 +65,11 @@ private fun createCars(names: List<String>): List<Car> {
 }
 
 private fun simulateRace(cars: List<Car>, attempts: Int): Int {
-    var maxPosition = 0
+    var maxPosition = START_IDX
 
     repeat(attempts) {
         cars.forEach { it.move() }
-
         val max = cars.maxOfOrNull { it.position }
-
         if (max != null && max > maxPosition) {
             maxPosition = max
         }
