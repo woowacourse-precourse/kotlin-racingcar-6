783078c474f80ebec2aae0a2e9e1dd81efd8a49f
Jtility <leejewon97@gmail.com>
2023-11-01 04:58:00 +0900

    refactor(RacingCar): change type of member variable to access

--- src/main/kotlin/racingcar/RacingCar.kt
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -2,8 +2,9 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class RacingCar(name: String) {
-    private var mileage = 0
+class RacingCar(val name: String) {
+    var mileage = 0
+
     fun move() {
         if (Randoms.pickNumberInRange(0, 9) >= 4) {
             mileage++
