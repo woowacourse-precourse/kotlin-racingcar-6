33c99faa81d73366f9fc9621eef0dacd24b4c9e1
lee989898 <wnsrud1352@gmail.com>
2023-10-31 17:28:09 +0900

    [feat] attempt 상태 저장 및 attempt 설정 메서드 구현

--- src/main/kotlin/racingcar/attempts/AttemptsImpl.kt
+++ src/main/kotlin/racingcar/attempts/AttemptsImpl.kt
@@ -1,10 +1,15 @@
 package racingcar.attempts
 
 class AttemptsImpl : Attempts {
+    private var _attempt: Int = ZERO
     override val attempt: Int
         get() = TODO("Not yet implemented")
 
     override fun setAttempt(attempt: Int) {
-        TODO("Not yet implemented")
+        _attempt = attempt
+    }
+
+    companion object {
+        private const val ZERO = 0
     }
 }
