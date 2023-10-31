dada76aef95df9e874db8da570d269d660f70981
상은 <71699054+vmkmym@users.noreply.github.com>
2023-10-31 22:31:11 +0900

    <Refactor> 코드 구조 개선
    - createCars 함수 대신 carNames.map { Car(it) }을 사용하여 자동차 목록을 만들기
    
    - Car 클래스에서 position 속성을 직접 변경할 수 있도록 변경하고, display 함수를 통해 표시하는 로직을 포함
    
    - simulateRace 함수에서 maxPosition을 계산할 때 maxOrNull 함수를 사용하고, null 값이 발생할 경우 0을 기본값으로 사용

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,7 +3,6 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-
 const val PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 const val PROMPT_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
 const val RESULT = "\n실행 결과"
@@ -11,12 +10,11 @@ const val WINNER_MESSAGE = "최종 우승자 : "
 
 const val MAX_CAR_NAME_LENGTH = 5
 const val START_IDX = 0
-const val INITIAL_IDX = 0
 const val END_IDX = 9
 const val CONDITION = 4
 
 data class Car(val name: String) {
-    private var position: Int = INITIAL_IDX
+    var position: Int = 0
 
     fun move() {
         if (Randoms.pickNumberInRange(START_IDX, END_IDX) >= CONDITION) {
@@ -24,26 +22,24 @@ data class Car(val name: String) {
         }
     }
 
-    fun getPosition(): Int {
-        return position
-    }
+    fun display(): String = "$name : ${"-".repeat(position)}"
 }
 
 fun main() {
     val carNames = readCarNames()
-    val cars = createCars(carNames)
+    val cars = carNames.map { Car(it) }
 
-    val inputAttempts = readAttempts()
+    val attempts = readAttempts()
     println(RESULT)
 
-    val maxPosition = simulateRace(cars, inputAttempts)
-    val winners = findWinners(cars, maxPosition)
-    println(WINNER_MESSAGE+winners)
+    val maxPosition = simulateRace(cars, attempts)
+
+    val winners = cars.filter { it.position == maxPosition }.joinToString(", ") { it.name }
+    println("$WINNER_MESSAGE$winners")
 }
 
 private fun readCarNames(): List<String> {
     println(PROMPT_CAR_NAMES)
-
     val inputNames = Console.readLine()
     if (inputNames.split(",").any { it.length > MAX_CAR_NAME_LENGTH }) {
         throw IllegalArgumentException()
@@ -53,7 +49,6 @@ private fun readCarNames(): List<String> {
 
 private fun readAttempts(): Int {
     println(PROMPT_ATTEMPTS)
-
     return try {
         val input = Console.readLine().toInt()
         if (input <= START_IDX) throw IllegalArgumentException()
@@ -63,26 +58,11 @@ private fun readAttempts(): Int {
     }
 }
 
-private fun createCars(names: List<String>): List<Car> {
-    return names.map { Car(it) }
-}
-
 private fun simulateRace(cars: List<Car>, attempts: Int): Int {
-    var maxPosition = INITIAL_IDX
-
     repeat(attempts) {
         cars.forEach { it.move() }
-        val max = cars.maxOfOrNull { it.getPosition() }
-        if (max != null && max > maxPosition) {
-            maxPosition = max
-        }
-
-        cars.forEach { println("${it.name} : ${"-".repeat(it.getPosition())}") }
+        cars.forEach { println(it.display()) }
         println()
     }
-    return maxPosition
-}
-
-private fun findWinners(cars: List<Car>, maxPosition: Int): String {
-    return cars.filter { it.getPosition() == maxPosition }.joinToString(", ") { it.name }
+    return cars.map { it.position }.maxOrNull() ?: 0
 }
