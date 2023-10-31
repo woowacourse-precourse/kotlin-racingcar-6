83df223c6a1efcceed8a106d2211e302f5f53644
somin <salth0601@gmail.com>
2023-10-31 23:30:02 +0900

    style: import Constants

--- src/main/kotlin/racingcar/GameConsole.kt
+++ src/main/kotlin/racingcar/GameConsole.kt
@@ -1,21 +1,27 @@
 package racingcar
 
+import racingcar.Constants.CAR_NAME_SEPARATOR
+import racingcar.Constants.MESSAGE_ASK_CAR_NAME
+import racingcar.Constants.MESSAGE_ASK_MOVE_COUNT
+import racingcar.Constants.MESSAGE_FINAL_WINNER
+import racingcar.Constants.PROGRESS_BAR
+
 object GameConsole {
 
     fun printPosition(name: String, position: Int) {
-        println("$name : ${Constants.PROGRESS_BAR.repeat(position)}")
+        println("$name : ${PROGRESS_BAR.repeat(position)}")
     }
 
     fun printWinnerNames(winnerNames: List<String>) {
-        val winner = winnerNames.joinToString(Constants.CAR_NAME_SEPARATOR)
-        println(Constants.MESSAGE_FINAL_WINNER + winner)
+        val winner = winnerNames.joinToString(CAR_NAME_SEPARATOR)
+        println(MESSAGE_FINAL_WINNER + winner)
     }
 
     fun printCarNamePrompt() {
-        println(Constants.MESSAGE_ASK_CAR_NAME)
+        println(MESSAGE_ASK_CAR_NAME)
     }
 
     fun printMoveCountPrompt() {
-        println(Constants.MESSAGE_ASK_MOVE_COUNT)
+        println(MESSAGE_ASK_MOVE_COUNT)
     }
 }
