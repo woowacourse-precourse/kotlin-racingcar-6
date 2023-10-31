fc20eb58f56d652ca9996df1724023c05e79a84a
lee989898 <wnsrud1352@gmail.com>
2023-10-31 16:29:12 +0900

    [feat] 이름 길이가 적절한지 확인하는 메서드 구현

--- src/main/kotlin/racingcar/validator/ValidatorImpl.kt
+++ src/main/kotlin/racingcar/validator/ValidatorImpl.kt
@@ -1,11 +1,16 @@
 package racingcar.validator
 
-class ValidatorImpl: Validator {
+class ValidatorImpl : Validator {
     override fun nameLength(name: String) {
-        TODO("Not yet implemented")
+        require(name.length in MINIMUM_NAME_LENGTH..MAXIMUM_NAME_LENGTH)
     }
 
     override fun attemptsComponents(attempts: String) {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val MINIMUM_NAME_LENGTH = 1
+        private const val MAXIMUM_NAME_LENGTH = 5
     }
 }
