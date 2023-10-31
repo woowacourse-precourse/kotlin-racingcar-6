10e95212b02b80c09a45ec878f99fa77515c8164
lee989898 <wnsrud1352@gmail.com>
2023-10-31 15:52:56 +0900

    [test] 랜덤 숫자가 0~3일 경우 자동차가 멈추는지 확인하는 테스트 구현

--- src/test/kotlin/racingcar/car/CarTest.kt
+++ src/test/kotlin/racingcar/car/CarTest.kt
@@ -1,6 +1,10 @@
 package racingcar.car
 
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.`in`
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
 
@@ -9,5 +13,18 @@ class CarTest {
     @BeforeEach
     fun setUp() {
         car = CarImpl()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun `랜덤 숫자가 0~3일 경우 자동차가 멈추는지`(randomNumber: Int) {
+        car.forwardOrStop(randomNumber)
+        val result = car.currentPosition
+
+        assertThat(STOP).isEqualTo(result)
+    }
+
+    companion object {
+        private const val STOP = ""
     }
 }
