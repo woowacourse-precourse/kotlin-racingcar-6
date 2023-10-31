d4dcb9ea4b61be2fc711930a3170d0191c09bd9d
YuBeen-Park <park_ubin@naver.com>
2023-10-31 17:06:27 +0900

    [feat] car - 자동차 객체 및 출력 구현

--- src/main/kotlin/racingcar/model/RacingCar.kt
+++ src/main/kotlin/racingcar/model/RacingCar.kt
@@ -1,3 +1,12 @@
 package racingcar.model
 
-class RacingCar
+import racingcar.utils.Constants
+
+class RacingCar(
+    var carName: String,
+    var location: Int = 5,
+) {
+    override fun toString(): String {
+        return "$carName : " + Constants.CAR_LOCATION.repeat(location)
+    }
+}
--- src/main/kotlin/racingcar/utils/Constants.kt
+++ src/main/kotlin/racingcar/utils/Constants.kt
@@ -5,4 +5,8 @@ object Constants {
     const val TRIAL_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?"
     const val RESULT_MESSAGE = "실행 결과"
     const val FINAL_WINNER_MESSAGE = "최종 우승자 : "
+
+    const val CAR_LOCATION = "-"
+
+    val NAME_LENGTH_RANGE = 1..5
 }
