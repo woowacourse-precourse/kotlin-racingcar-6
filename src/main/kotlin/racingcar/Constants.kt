e9f59e97aedbae12ab2ce4cfcf062c354294c28f
Sheep1sik <bohg6799@naver.com>
2023-10-31 18:51:38 +0900

    fix: 오류 수정 테스트 4
    EXECUTION_RESULT_MESSAGE 상수 "실행결과" -> "\n실행 결과"

--- src/main/kotlin/racingcar/Constants.kt
+++ src/main/kotlin/racingcar/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     const val FORWARD_CONDITION = 4
     const val INPUT_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     const val TRY_COUNT_PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?"
-    const val EXECUTION_RESULT_MESSAGE = "실행 결과"
+    const val EXECUTION_RESULT_MESSAGE = "\n실행 결과"
     const val FINAL_WINNER_MESSAGE = "최종 우승자 : "
 
 }
