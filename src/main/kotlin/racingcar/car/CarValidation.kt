acb478ba59cb5357b46139fc0389e706c41d4f71
stopkite <stopkite98@gmail.com>
2023-10-31 15:26:43 +0900

    refactor: 유효성 검사 메세지 상수화

--- src/main/kotlin/racingcar/car/CarValidation.kt
+++ src/main/kotlin/racingcar/car/CarValidation.kt
@@ -10,32 +10,42 @@ class CarValidation {
     }
 
     private fun isInputWithinFiveChar(list: List<String>) {
-        if (list.any { it.length > 5 }) {
-            throw IllegalArgumentException("자동차 이름은 5자 이하만 입력 가능합니다.")
+        if (list.any { it.length > MAX_LENGTH }) {
+            throw IllegalArgumentException(MAX_INPUT_ERROR)
         }
     }
 
     private fun hasWhiteSpaceAtEdge(list: List<String>) {
         if (list.any { it.startsWith(' ') || it.endsWith(' ') }) {
-            throw IllegalArgumentException("이름의 시작과 끝에는 공백 입력이 불가능합니다.")
+            throw IllegalArgumentException(WHITE_SPACE_ERROR)
         }
     }
 
     private fun checkAtLeastTwoNames(list: List<String>) {
-        if (list.count() < 2) {
-            throw IllegalArgumentException("최소 2대이상의 자동차 이름이 입력되어야 게임 진행이 가능합니다.")
+        if (list.count() < MIN_COUNT) {
+            throw IllegalArgumentException(MIN_COUNT_ERROR)
         }
     }
 
     private fun checkIsStartWithComma(list: List<String>) {
         if (list.first().isEmpty()) {
-            throw IllegalArgumentException("처음에 콤마가 추가되었습니다. 콤마로 입력을 시작해서는 안됩니다.")
+            throw IllegalArgumentException(COMMA_START_ERROR)
         }
     }
 
     private fun checkIsEndWithComma(list: List<String>) {
         if (list.last().isEmpty()) {
-            throw IllegalArgumentException("콤마로 입력이 끝나서는 안됩니다.")
+            throw IllegalArgumentException(COMMA_END_ERROR)
         }
+    }
+
+    companion object {
+        const val MIN_COUNT = 2
+        const val MAX_LENGTH = 5
+        const val WHITE_SPACE_ERROR = "이름의 시작과 끝에는 공백 입력이 불가능합니다."
+        const val MIN_COUNT_ERROR = "최소 2대이상의 자동차 이름이 입력되어야 게임 진행이 가능합니다."
+        const val MAX_INPUT_ERROR = "자동차 이름은 5자 이하만 입력 가능합니다."
+        const val COMMA_START_ERROR = "처음에 콤마가 추가되었습니다. 콤마로 입력을 시작해서는 안됩니다."
+        const val COMMA_END_ERROR = "콤마로 입력이 끝나서는 안됩니다."
     }
 }
--- src/main/kotlin/racingcar/round/RoundValidation.kt
+++ src/main/kotlin/racingcar/round/RoundValidation.kt
@@ -8,13 +8,19 @@ class RoundValidation {
 
     private fun checkIsNumber(input: String) {
         if (input.toIntOrNull() == null) {
-            throw IllegalArgumentException("입력값은 숫자여야 합니다.")
+            throw IllegalArgumentException(NUMBER_INPUT_ERROR)
         }
     }
 
     private fun checkIsBiggerThanOne(input: String) {
-        if (input.toInt() < 1) {
-            throw IllegalArgumentException("입력값은 1보다 큰 값이어야 합니다.")
+        if (input.toInt() < MIN_ROUND) {
+            throw IllegalArgumentException(MIN_ROUND_ERROR)
         }
+    }
+
+    companion object {
+        const val MIN_ROUND = 1
+        const val NUMBER_INPUT_ERROR = "입력값은 숫자여야 합니다."
+        const val MIN_ROUND_ERROR = "입력값은 1보다 큰 값이어야 합니다."
     }
 }
