a0909a2c63fd280794f199668b2583229c09f316
lee989898 <wnsrud1352@gmail.com>
2023-10-31 13:07:51 +0900

    [test] Car 테스트 초기화 구현

---
+++ src/test/kotlin/racingcar/car/CarTest.kt
@@ -0,0 +1,10 @@
+package racingcar.car
+
+import racingcar.randomnumbergenerator.RandomNumberGenerator
+import racingcar.randomnumbergenerator.RandomNumberGeneratorImpl
+
+class CarTest {
+
+    private var randomNumberGenerator: RandomNumberGenerator = RandomNumberGeneratorImpl()
+    private var car: Car = CarImpl()
+}
