c5ceb230d3d52e9da1eebdd1ac8fd78c4f148fdc
V. hs Sin <cbrnt1210@naver.com>
2023-10-31 22:56:40 +0900

    move 함수에서 입력받을 때 설명을 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,6 +3,7 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun make_frame(): List<String>{
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val sentence = Console.readLine().split(",")
     for (car in sentence){
         if (car.length > 3 || car.isEmpty()){
