88c260f2f5b0d334aa9811edfcd1d18df873793d
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 01:09:57 +0900

    feat: 함수 이름 변경
    validateString -> containsComma

--- src/main/kotlin/racingcar/Validator.kt
+++ src/main/kotlin/racingcar/Validator.kt
@@ -1,7 +1,7 @@
 package racingcar
 
 class Validator {
-    fun validateString(inputString: String) {
+    fun containsComma(inputString: String) {
         if (!inputString.contains(','))
             throw IllegalArgumentException("invalid input string, there are another special characters")
     }
