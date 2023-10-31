d88b9953b4f80200ccb8186f8f477ac7729fd0ef
uush <dbtmd447@gmail.com>
2023-10-31 16:07:41 +0900

    feat: 쉼표로 구분하여 리스트에 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,10 +3,16 @@ package racingcar
 fun main() {
     // TODO: 프로그램 구현
     val carsName = inputCarsName()
-    println(carsName)
+    val res = addCarsList(carsName)
+    println(res)
 }
 
 fun inputCarsName() : String{
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     return readLine().toString()
+}
+
+fun addCarsList(carsName: String) : List<String> {
+    val splitResult = carsName.split(",").toList()
+    return splitResult
 }
