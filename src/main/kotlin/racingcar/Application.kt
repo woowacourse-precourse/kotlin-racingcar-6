788efeffdf0f5e56b3802948edc9c8f4d1494125
juseong <wjdwntjd55@naver.com>
2023-10-31 15:59:42 +0900

    Feat: 자동차 이름 입력하는 기능구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,13 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val carNames = getCarNames()
+}
+
+fun getCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNames = Console.readLine()
+    return carNames.split(",")
 }
