7286487f4ef64d9e89846d76b7367bda632163a4
LEE <tpqms775@naver.com>
2023-10-31 21:17:42 +0900

    refactor(racingcar): 주어진 횟수동안 0~9 숫자 랜덤 구하기 코드 변경

--- src/main/kotlin/racingcar/RandomNumCreator.kt
+++ src/main/kotlin/racingcar/RandomNumCreator.kt
@@ -5,13 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms
 class RandomNumCreator {
     private val inputManager = InputManager()
 
-    fun creator() : List<Int>{
-        val num = inputManager.readRepeatNum()
-        val numList: MutableList<Int> = mutableListOf()
-        for(index in 1..num){
-            val randomNum = Randoms.pickNumberInRange(0, 9)
-            numList.add(randomNum)
-        }
-        return numList
+    fun creator(): Int {
+
+        return Randoms.pickNumberInRange(0, 9)
     }
 }
