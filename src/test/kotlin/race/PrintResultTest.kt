40c41d1ea0555a40d8aa5d7c373459e0373da43e
mk43313@gmail.com <mk43313@gmail.com>
2023-10-31 16:57:39 +0900

    TEST : PrintMessage test

--- src/test/kotlin/race/PrintResultTest.kt
+++ src/test/kotlin/race/PrintResultTest.kt
@@ -1,4 +1,23 @@
 package race
 
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import org.assertj.core.api.Assertions.assertThat
+
 class PrintResultTest {
+
+    @Test
+    fun testPrintMessage() {
+        val outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+
+        val printResult = PrintResult()
+        val message = "Test Message"
+        printResult.printMessage(message)
+
+        val printedMessage = outputStream.toString().trim()
+        System.setOut(System.out)
+        assertThat(message).isEqualTo(printedMessage)
+    }
 }
