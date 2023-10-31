d8759232ca341fdd5d22181fa3d78fa4a4e37602
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-01 02:09:42 +0900

    fix) execption

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -26,6 +26,49 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    @Test
+    fun `이름이 공백 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("", "1") }
+        }
+    }
+    @Test
+    fun `반복 횟수 공백 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,kim", " ") }
+        }
+    }
+    @Test
+    fun `반복 횟수가 음수일 경우 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni,kim", "-3") }
+        }
+    }
+    @Test
+    fun `반복 횟수가 문자일 경우 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni,kim", "a") }
+        }
+    }
+    @Test
+    fun `차 이름이 중복일 경우 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,pobi,kim", "1") }
+        }
+    }
+    @Test
+    fun `이름 사이에 공백이 있을 경우 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi, pobi,kim", "1") }
+        }
+    }
+    @Test
+    fun `자동차 이름이 1글자 미만일 경우 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,kim,,lee", "1") }
+        }
+    }
+
     public override fun runMain() {
         main()
     }
