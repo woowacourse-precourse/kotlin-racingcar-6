f0656b040ccccbc81e2cb5df5501c9cc62cf47f1
ErroredPasta <ForeignServices3132@pm.me>
2023-10-31 22:51:55 +0900

    test: RaceView의 출력에 대한 테스트
    "시도할 횟수는 몇 회인가요?"같은 단순 출력 기능은 테스트하지 않음

--- src/test/kotlin/racingcar/view/RaceViewTest.kt
+++ src/test/kotlin/racingcar/view/RaceViewTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import racingcar.model.TurnNumber
+import racingcar.model.car.Car
 import racingcar.model.car.CarName
 
 class RaceViewTest {
@@ -101,4 +102,88 @@ class RaceViewTest {
         // then
         assertThat(turnNumber.number).isEqualTo(1)
     }
+
+    @Test
+    fun displayRaceResult_carsAreGiven_printCarNameAndDistanceMarkerCorrectly() {
+        // given
+        val cars = listOf(
+            createCarWithNameAndDistance(CarName("pobi"), 4),
+            createCarWithNameAndDistance(CarName("woni"), 5),
+            createCarWithNameAndDistance(CarName("jun"), 6),
+        )
+
+        // when
+        view.displayRaceResult(cars = cars)
+
+        // then
+        assertThat(outputView.capturedTexts[0]).isEqualTo("pobi : ----")
+        assertThat(outputView.capturedTexts[1]).isEqualTo("woni : -----")
+        assertThat(outputView.capturedTexts[2]).isEqualTo("jun : ------")
+    }
+
+    @Test
+    fun displayRaceResult_emptyListGiven_printNothing() {
+        // given
+        val cars = emptyList<Car>()
+
+        // when
+        view.displayRaceResult(cars = cars)
+
+        // then
+        assertThat(outputView.capturedTexts).isEmpty()
+    }
+
+    @Test
+    fun displayWinners_winnerNameIsGiven_printWinnerName() {
+        // given
+        val winnerNames = listOf(CarName("pobi"))
+
+        // when
+        view.displayWinners(winnerNames = winnerNames)
+
+        // then
+        assertThat(outputView.capturedTexts[0]).isEqualTo("최종 우승자 : pobi")
+    }
+
+    @Test
+    fun displayWinners_multipleWinnerNamesAreGiven_printWinnerNamesJoinedByComma() {
+        // given
+        val winnerNames = listOf(
+            CarName("pobi"),
+            CarName("woni"),
+            CarName("jun"),
+        )
+
+        // when
+        view.displayWinners(winnerNames = winnerNames)
+
+        // then
+        assertThat(outputView.capturedTexts[0]).isEqualTo("최종 우승자 : pobi, woni, jun")
+    }
+
+    @Test
+    fun displayWinners_emptyListGiven_onlyPrintWinnerTitle() {
+        // given
+        val winnerNames = emptyList<CarName>()
+
+        // when
+        view.displayWinners(winnerNames = winnerNames)
+
+        // then
+        assertThat(outputView.capturedTexts[0]).isEqualTo("최종 우승자 : ")
+    }
+
+    // region helper functions
+
+    private fun createCarWithNameAndDistance(name: CarName, distance: Int): Car {
+        val car = Car(name = name, randomNumberGenerator = alwaysGoForward)
+        repeat(distance) { car.tryGoForward() }
+        return car
+    }
+
+    companion object {
+        private val alwaysGoForward: (Int, Int) -> Int = { _, _ -> 9 }
+    }
+
+    // endregion
 }
