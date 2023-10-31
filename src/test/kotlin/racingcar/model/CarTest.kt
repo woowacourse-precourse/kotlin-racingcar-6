9179b7ac798091d72bedd33f95f99d23a44f46c0
sh1mj1 <7wlgns@gmail.com>
2023-10-31 18:47:14 +0900

    test(CarsTest): add test for moveForward method
    * 랜덤 수가 4 이상이면 Car 객체의 position 이 1 증가한다.

--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -1,6 +1,8 @@
 package racingcar.model
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.function.Executable
@@ -8,6 +10,11 @@ import org.junit.jupiter.api.function.Executable
 class CarTest {
 
     lateinit var car: Car
+
+    @BeforeEach
+    fun setUp() {
+        car = Car(name = "car")
+    }
 
     @Test
     fun `Car 이름이 empty 이면 예외를 던진다`() {
@@ -54,6 +61,30 @@ class CarTest {
         car = Car("abcde")
         val executable = Executable { car.validateCar() }
         assertDoesNotThrow(executable)
+    }
 
+    @Test
+    fun `생성한 랜덤 수가 4 이상이면 앞으로 나아간다`() {
+        car = Car(name = "car", position = 0)
+        car.moveForward(NumberGeneratorTest(4))
+        Assertions.assertThat(car.position).isEqualTo(1)
+    }
+
+    @Test
+    fun `생성한 랜덤 수가 3 이하이면 앞으로 나아가지 않는다`() {
+        car = Car(name = "car", position = 0)
+        car.moveForward(NumberGeneratorTest(3))
+        Assertions.assertThat(car.position).isEqualTo(0)
+    }
+
+    /**
+     * 테스트 용도의 클래스
+     *
+     * 리턴하는 RandomNumber 를 개발자가 입력하는 값으로 한다.
+      */
+    class NumberGeneratorTest(private val returnValue: Int) : NumberGenerator() {
+        override fun generateRandomNumber(): Int {
+            return returnValue
+        }
     }
 }
