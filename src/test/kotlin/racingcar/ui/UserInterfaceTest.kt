9431106c05f2f0315c8fd0012f0136213d021840
book3039 <book3039@knou.ac.kr>
2023-10-31 23:55:55 +0900

    test: 시도 횟수가 100 이하인지 검사 테스트 작성

--- src/test/kotlin/racingcar/ui/UserInterfaceTest.kt
+++ src/test/kotlin/racingcar/ui/UserInterfaceTest.kt
@@ -41,4 +41,9 @@ class UserInterfaceTest {
     fun `시도 횟수가 1 이상인지 검사_값 0`() {
         assertThrows<IllegalArgumentException> { InputValidator.isPositive(0)}
     }
+
+    @Test
+    fun `시도 횟수가 100 이하인지 검사_값 1000`() {
+        assertThrows<IllegalArgumentException> { InputValidator.isUnderHundred(1000)}
+    }
 }
