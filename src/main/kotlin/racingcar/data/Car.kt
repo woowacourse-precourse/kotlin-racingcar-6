663a9674832a7a967ed9e5a752cd0a5dd5af38e3
DoDoBest <send1684@gmail.com>
2023-10-31 22:49:43 +0900

    chore: 현재 코드에서 사용하지 않는 Car의 id 멤버변수 삭제

--- src/main/kotlin/racingcar/data/Car.kt
+++ src/main/kotlin/racingcar/data/Car.kt
@@ -1,11 +1,8 @@
 package racingcar.data
 
-import java.util.concurrent.atomic.AtomicInteger
-
 data class Car(
     val name: String,
 ) {
-    private val id: Int = numForId.getAndIncrement()
     private var _location: Int = 0
     val location: Int
         get() = _location
@@ -15,8 +12,4 @@ data class Car(
     }
 
     override fun toString(): String = "$name : ${"-".repeat(_location)}"
-
-    companion object {
-        private var numForId = AtomicInteger(0)
-    }
 }
