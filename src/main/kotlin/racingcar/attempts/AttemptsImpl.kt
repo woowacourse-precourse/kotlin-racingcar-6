f7fa04abaab9e6f61a37b11619b6d3c65fd80f1b
lee989898 <wnsrud1352@gmail.com>
2023-10-31 17:22:25 +0900

    [feat] 시도 횟수 저장하는 클래스 생성

---
+++ src/main/kotlin/racingcar/attempts/AttemptsImpl.kt
@@ -0,0 +1,10 @@
+package racingcar.attempts
+
+class AttemptsImpl : Attempts {
+    override val attempts: Int
+        get() = TODO("Not yet implemented")
+
+    override fun setAttempts(attempts: Int) {
+        TODO("Not yet implemented")
+    }
+}
