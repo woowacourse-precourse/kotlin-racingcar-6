98baa61bb854cccdaa9ae56c2f3441bedf84df88
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-01 02:17:34 +0900

    fix)3

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,24 +3,12 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-val INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-val INPUT_REPEAT = "시도할 횟수는 몇 회인가요?"
-val NOT_INT_OR_NULL = "숫자가 아니거나 null입니다."
-val INPUT_UNDER_ZERO = "입력 값에 음수가 있습니다."
-val INPUT_OVER_FIVE_OR_NULL = "이름이 5자가 넘어가거나 값이 없습니다."
-val INPUT_HAVE_SPACE = ",뒤에 공백이 있습니다."
-val INPUT_DUPLICATE = "중복된 수가 있습니다."
-val INPUT_SIZE = "2대 이상 입력해야 합니다."
-val SPACE = " "
-val MAX_LENGTH = 5
-val MOVE = "-"
-
 val cars = mutableListOf<Car>()
 val winners = mutableListOf<String>()
 fun main() {
     cars.clear()
     winners.clear()
-    println(INPUT_NAME)
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     input()
     val repeat = inputRepeat()
     processGame(repeat)
@@ -71,7 +59,7 @@ fun step(random: Int, car: Car) {
 fun printStep(car: Car) {
     var go = ""
     for(i in 0..<car.position){
-        go += MOVE
+        go += "-"
     }
     println("${car.name} : $go")
 }
@@ -83,14 +71,14 @@ fun input(){
     }
 }
 fun checkName(input: String) {
-    if (input.length > MAX_LENGTH || input.isEmpty()) {
-        throw IllegalArgumentException(INPUT_OVER_FIVE_OR_NULL)
+    if (input.length > 5 || input.isEmpty()) {
+        throw IllegalArgumentException("이름이 5자가 넘어가거나 값이 없습니다.")
     } else {
         cars.add(Car(input))
     }
 }
 fun inputRepeat(): Int {
-    println(INPUT_REPEAT)
+    println("시도할 횟수는 몇 회인가요?")
     val repeat = Console.readLine()
     checkNum(repeat)
     Console.close()
@@ -98,9 +86,9 @@ fun inputRepeat(): Int {
 }
 fun checkNum(repeat: String) {
     if (repeat.toIntOrNull() == null) {
-        throw IllegalArgumentException(NOT_INT_OR_NULL)
+        throw IllegalArgumentException("숫자가 아니거나 null입니다.")
     }
     if (repeat.toInt() < 1) {
-        throw IllegalArgumentException(INPUT_UNDER_ZERO)
+        throw IllegalArgumentException("입력 값에 음수가 있습니다.")
     }
 }
