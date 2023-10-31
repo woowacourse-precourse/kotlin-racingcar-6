a74bb5169fbf17cfe0683e4495991a3d1220adcb
lee989898 <wnsrud1352@gmail.com>
2023-10-31 17:00:03 +0900

    [feat] 시도 횟수 입력 문구 출력 메서드 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -5,9 +5,14 @@ import racingcar.car.Car
 object OutputView {
     private const val COLON = ":"
     private const val ENTER_NAMES_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    private const val HOW_MANY_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
 
     fun enterNamesOfCars() {
         println(ENTER_NAMES_OF_CARS)
+    }
+
+    fun howManyAttempts() {
+        println(HOW_MANY_ATTEMPTS)
     }
 
     fun executionResult(cars: List<Car>) {
