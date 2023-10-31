3afc34fd4bf1a5bc296aa19be5dad568a771f60f
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 03:28:15 +0900

    test: 시도할 횟수를 입력받는 테스트 코드
    정수만 입력하였을 때 정상 동작하며, 다른 문자나 빈칸이 포함되면 예외가
    발생한다.
    단, 0을 입력한 경우에 대한 예외처리가 필요하다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,6 +3,8 @@ package racingcar
 fun main() {
     // TODO: 프로그램 구현
     val user = User()
-    val list: List<String> = user.getCarNames()
-    print(list)
+//    val list: List<String> = user.getCarNames()
+//    print(list)
+    val count: Int = user.getTryCount()
+    print(count)
 }
