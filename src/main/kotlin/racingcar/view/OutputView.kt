d26d5be5e73a8aab8d38925a58b03ef550dc1c26
lee989898 <wnsrud1352@gmail.com>
2023-10-31 16:57:41 +0900

    [feat] 자동차 이름 입력 문구 출력 메서드 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -4,6 +4,11 @@ import racingcar.car.Car
 
 object OutputView {
     private const val COLON = ":"
+    private const val ENTER_NAMES_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+
+    fun enterNamesOfCars() {
+        println(ENTER_NAMES_OF_CARS)
+    }
 
     fun executionResult(cars: List<Car>) {
         for (car in cars) {
