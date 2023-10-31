845c8062c9872cf4e25b4df14f9972c25e15ddd3
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-01 02:23:32 +0900

    fix)6

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -8,6 +8,8 @@ val winners = mutableListOf<String>()
 var MAX = 0
 
 fun main() {
+    cars.clear()
+    winners.clear()
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     input()
     val repeat = inputRepeat()
