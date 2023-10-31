f7d1fdeb285fc19ef135308ff63e6b11551a991a
Wooyoung Myung <mwy3055@gmail.com>
2023-10-31 22:01:05 +0900

    feature: 자동차 이름을 입력받고, Car 객체 만들기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,30 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val input = getCarsName()
+    val cars = generateCarsFromInput(input)
+}
+
+fun generateCarsFromInput(input: String): List<Car> {
+    val tokens = input.split(",")
+    val cars = tokens.map { token ->
+        generateCarFromToken(token)
+    }
+    return cars
+}
+
+fun generateCarFromToken(token: String): Car {
+    GameUtils.checkValidInput {
+        token.length <= 5
+    }
+    return Car(token)
+}
+
+fun getCarsName(): String {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = Console.readLine()
+    GameUtils.checkValidInput { input != null }
+    return input
 }
