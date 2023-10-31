e5f4e7ce55805ff6be53b8f393fb5c7c9d4d9ee4
juseong <wjdwntjd55@naver.com>
2023-10-31 16:02:52 +0900

    Feat: 입력한 자동차 이름이 올바르게 작성했는지 확인하는 기능

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,7 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import kotlin.system.exitProcess
 
 fun main() {
     val carNames = getCarNames()
@@ -9,5 +10,13 @@ fun main() {
 fun getCarNames(): List<String> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carNames = Console.readLine()
+    checkCarNames(carNames)
     return carNames.split(",")
+}
+
+fun checkCarNames(carNames: String) {
+    val names = carNames.split(",")
+    if (names.any { it.isEmpty() || it.length > 5 }) {
+        exitProcess(0)
+    }
 }
