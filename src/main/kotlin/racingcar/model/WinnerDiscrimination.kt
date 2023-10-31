1c0476d41a44184c27264fffabce390a3c2000c8
조형석 <chs9902@naver.com>
2023-10-31 22:32:13 +0900

    fix: 우승자 판별 기능 오류 해결

--- src/main/kotlin/racingcar/model/WinnerDiscrimination.kt
+++ src/main/kotlin/racingcar/model/WinnerDiscrimination.kt
@@ -4,17 +4,18 @@ import net.bytebuddy.pool.TypePool.CacheProvider.Discriminating
 
 class WinnerDiscrimination {
     fun answer(scoreBoard: List<String>, cars: List<String>): MutableList<String> {
-        var winnerIndices = Discriminating(scoreBoard)
-        var winnerCars = mutableListOf<String>()
+        val winnerIndices = Discriminating(scoreBoard)
+        val winnerCars = mutableListOf<String>()
         for (selectedIndex in winnerIndices) {
             winnerCars.add(cars[selectedIndex])
         }
         return winnerCars
     }
-    fun Discriminating(scoreBoard: List<String>):MutableList<Int>{
+
+    fun Discriminating(scoreBoard: List<String>): MutableList<Int> {
         var winnerIndices = mutableListOf<Int>()
+        var max = 0
         for ((index, score) in scoreBoard.withIndex()) {
-            var max = 0
             if (score.length > max) {
                 max = score.length
                 winnerIndices = mutableListOf(index)
