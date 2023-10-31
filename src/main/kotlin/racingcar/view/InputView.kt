bd8286c2e523aad8d1b729aa3395bb2f758e87bb
lee989898 <wnsrud1352@gmail.com>
2023-10-31 16:41:17 +0900

    [chore] 구분자 comma를 상수화

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -4,9 +4,10 @@ import camp.nextstep.edu.missionutils.Console
 import racingcar.validator.Validator
 
 object InputView {
+    private const val COMMA = ","
 
     fun askCarToRace(validator: Validator): List<String> {
-        val cars = Console.readLine().split(",")
+        val cars = Console.readLine().split(COMMA)
 
         for (car in cars) {
             validator.nameLength(car)
