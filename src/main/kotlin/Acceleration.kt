0fa3045722f30f44b41441b4b53354c89eb34857
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-31 18:23:33 +0900

    remove: 리베이스 중복파일 제거

--- src/main/kotlin/Acceleration.kt
+++
@@ -1,8 +0,0 @@
-import camp.nextstep.edu.missionutils.Randoms
-import util.CarValue.MAX_RANDOM_VALUE
-import util.CarValue.MIN_FORWARD_VALUE
-import util.CarValue.MIN_RANDOM_VALUE
-
-abstract class Acceleration {
-    fun isGo() = Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE) >= MIN_FORWARD_VALUE
-}
