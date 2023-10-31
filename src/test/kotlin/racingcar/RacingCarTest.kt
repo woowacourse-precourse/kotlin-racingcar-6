c0fb689d88190ea5fe406903b5da9f61df27072d
Jtility <leejewon97@gmail.com>
2023-11-01 05:18:31 +0900

    test(RacingCarTest): change tests name

--- src/test/kotlin/racingcar/RacingCarTest.kt
+++ src/test/kotlin/racingcar/RacingCarTest.kt
@@ -6,20 +6,20 @@ import org.junit.jupiter.api.assertThrows
 
 class RacingCarTest {
     @Test
-    fun `쉼표를 구분자로 경주할 자동차 이름 체크`() {
+    fun `쉼표를 구분자로 경주할 자동차 이름`() {
         val result = splitRacingCarName("pobi,woni,jun")
         assertThat(result).containsExactly("pobi", "woni", "jun")
     }
 
     @Test
-    fun `자동차 이름을 잘 입력 했는지 체크`() {
+    fun `유효하지 않은 자동차 이름`() {
         assertThrows<IllegalArgumentException>("자동차 이름은 5자 이하만 가능합니다.") {
             invalidRacingCarNamesCheck(listOf("pobi", "woni", "junjunjunjunjun"))
         }
     }
 
     @Test
-    fun `횟수 입력 체크`() {
+    fun `유효하지 않은 횟수 입력`() {
         assertThrows<IllegalArgumentException>("정수를 입력하세요.") {
             invalidTryCountCheck("abc".toIntOrNull())
         }
