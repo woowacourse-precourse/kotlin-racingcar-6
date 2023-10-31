5d20bbef032661125a0dbbcaf27487bde00aa408
crAy <craycoding@gmail.com>
2023-11-01 00:23:14 +0900

    feat : add start function, update main

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -3,6 +3,22 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
 class Racing {
+
+    fun startRace() {
+        val carList = makeCar()
+        val attempt = Input().inputAttempt()
+        val carResult = MutableList(carList.size) { "" }
+        printStart()
+        for (i in 0 until attempt) {
+            race(carList, carResult)
+        }
+        printResult(carList, carResult)
+    }
+
+    fun race(carList: List<String>, carResult: MutableList<String>) {
+        raceCar(carResult)
+        printRace(carList, carResult)
+    }
     fun makeCar(): List<String> {
         val inputString = Input().inputCar()
         val delim = ","
@@ -26,6 +42,9 @@ class Racing {
 
     fun findWinner(carResult: MutableList<String>): MutableList<Int> {
         val maxLength = carResult.max()
+        if (maxLength == "") {
+            return mutableListOf<Int>()
+        }
         val maxIndexList = mutableListOf<Int>()
         for (i in carResult.indices) {
             if (carResult[i] == maxLength) {
@@ -39,15 +58,26 @@ class Racing {
         for (i in carList.indices) {
             println("${carList[i]} : ${carResult[i]}")
         }
+        println()
     }
 
     fun printResult(carList: List<String>, carResult: MutableList<String>) {
         val maxIndexList = findWinner(carResult)
+        if (maxIndexList.isEmpty()) {
+            print("우승자가 없습니다.")
+            return
+        }
+        print("최종 우승자 : ")
         for (index in maxIndexList) {
             print(carList[index])
             if (index != maxIndexList.last()) {
                 print(", ")
             }
         }
+    }
+
+    fun printStart() {
+        println()
+        println("실행 결과")
     }
 }
