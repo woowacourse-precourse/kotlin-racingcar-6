715575be05f6225e012885ee1eeff50bd613a6eb
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2023-10-31 14:39:22 +0900

    feat: test 코드 작성 🖍️ #15

--- src/main/kotlin/racingcar/domain/Constants.kt
+++ src/main/kotlin/racingcar/domain/Constants.kt
@@ -14,9 +14,8 @@ object Constants {
 
     // Error Messages
     const val EMPTY_INPUT_ERROR_MESSAGE = "입력값이 없습니다."
-    const val LENGTH_ERROR_MESSAGE = "이름은 5자 이하만 가능합니다."
+    const val LENGTH_ERROR_MESSAGE = "이름은 1자 이상 5자 이하만 가능합니다."
     const val NOT_NUMBER_ERROR_MESSAGE = "입력값이 숫자가 아닙니다."
     const val DISTINCT_ERROR_MESSAGE = "중복된 이름이 존재합니다."
-    const val EMPTY_WINNER_ERROR_MESSAGE = "우승자가 없습니다."
     const val NEGATIVE_NUMBER_ERROR_MESSAGE = "음수는 입력할 수 없습니다."
 }
