465df269c5c9c639418baa32bbb9c8c9a68a9e43
lee989898 <wnsrud1352@gmail.com>
2023-10-31 17:42:28 +0900

    [feat] 시도횟수와 자동차 현재 위치 비교하는 메서드 구현

--- src/main/kotlin/racingcar/attemptcomparator/AttemptComparatorImpl.kt
+++ src/main/kotlin/racingcar/attemptcomparator/AttemptComparatorImpl.kt
@@ -1,10 +1,8 @@
 package racingcar.attemptcomparator
 
-import racingcar.attempts.Attempts
-import racingcar.car.Car
-
 class AttemptComparatorImpl : AttemptComparator {
-    override fun compareAttemptAndCarPosition(attempts: Attempts, car: Car): Boolean {
-        TODO("Not yet implemented")
+
+    override fun compareAttemptAndCarPosition(attempts: Int, carPosition: String): Boolean {
+        return attempts == carPosition.length
     }
 }
