dd72741517b1a08287b123c34ab7961fa8d0bbbb
sh1mj1 <7wlgns@gmail.com>
2023-10-31 19:52:09 +0900

    feat(OutputGuide): print start-guide & try-count-guide, result-guide message

---
+++ src/main/kotlin/racingcar/view/OutputGuide.kt
@@ -0,0 +1,15 @@
+package racingcar.view
+
+class OutputGuide {
+    fun showStartGuide() {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    }
+
+    fun showTryCountGuide() {
+        println("시도할 횟수는 몇 회인가요?")
+    }
+
+    fun showResultGuide() {
+        println("실행 결과")
+    }
+}
