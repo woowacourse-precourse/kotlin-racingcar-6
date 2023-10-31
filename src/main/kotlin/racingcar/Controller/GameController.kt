e53a3c2ef68b555ef998927984bc82a90f43aab9
sooohi <68193613+sooohi@users.noreply.github.com>
2023-10-31 17:37:56 +0900

    redactor : 게임 진행 횟수 메서드 분리

--- src/main/kotlin/racingcar/Controller/GameController.kt
+++ src/main/kotlin/racingcar/Controller/GameController.kt
@@ -10,11 +10,12 @@ class GameController {
                 throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.")
             }
         }
-        println("시도할 횟수는 몇 회인가요?")
-        val tryCountInput = readLine()!!.toInt()
+
+        val tryCountInput = tryCount()
         if (tryCountInput <= 0) {
             throw IllegalArgumentException("시도 횟수는 0보다 커야 합니다.")
         }
+
         val carMove = mutableMapOf<String, Int>()
 
         for (i in 1..tryCountInput) {
@@ -48,5 +49,11 @@ class GameController {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val carName = readLine()
             return carName?.split(",") ?: throw IllegalArgumentException("올바르게 다시 입력해주세요")
+    }
+
+    private fun tryCount():Int{
+        println("시도할 횟수는 몇 회인가요?")
+        val tryCountInput = readLine()!!.toInt()
+        return tryCountInput
     }
 }
