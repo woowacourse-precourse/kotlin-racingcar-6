eacc84cbc2e44cdab8de335e9f38bf07398a52f7
lee989898 <wnsrud1352@gmail.com>
2023-10-31 11:43:39 +0900

    [test] 랜덤으로 생성한 숫자가 0~9사이 숫자를 반환하는지 확인하는 테스트 구현

--- src/test/kotlin/racingcar/randomnumbergenerator/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/racingcar/randomnumbergenerator/RandomNumberGeneratorTest.kt
@@ -1,6 +1,9 @@
+
 package racingcar.randomnumbergenerator
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class RandomNumberGeneratorTest {
 
@@ -9,5 +12,19 @@ class RandomNumberGeneratorTest {
     @BeforeEach
     fun setUp() {
         randomNumberGenerator = RandomNumberGeneratorImpl()
+    }
+
+    @Test
+    fun `랜덤으로 생성한 숫자가 0~9사이 숫자를 반환`() {
+        val result = randomNumberGenerator.generateRandomNumber()
+
+        assertThat(result)
+            .isGreaterThanOrEqualTo(MINIMUM_RANDOM_NUMBER)
+            .isLessThanOrEqualTo(MAXIMUM_RANDOM_NUMBER)
+    }
+
+    companion object {
+        private const val MINIMUM_RANDOM_NUMBER = 0
+        private const val MAXIMUM_RANDOM_NUMBER = 9
     }
 }
