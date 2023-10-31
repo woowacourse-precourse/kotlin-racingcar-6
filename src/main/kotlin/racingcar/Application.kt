fac3a225b2ddcdb7f200da933fbe59887bd1eb29
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-01 02:13:03 +0900

    fix)1

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,19 +3,19 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-const val INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-const val INPUT_REPEAT = "시도할 횟수는 몇 회인가요?"
-const val NOT_INT_OR_NULL = "숫자가 아니거나 null입니다."
-const val INPUT_UNDER_ZERO = "입력 값에 음수가 있습니다."
-const val INPUT_OVER_FIVE_OR_NULL = "이름이 5자가 넘어가거나 값이 없습니다."
-const val INPUT_HAVE_SPACE = ",뒤에 공백이 있습니다."
-const val INPUT_DUPLICATE = "중복된 수가 있습니다."
-const val INPUT_SIZE = "2대 이상 입력해야 합니다."
-const val SPACE = " "
-const val MAX_LENGTH = 5
-const val MOVE = "-"
+val INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+val INPUT_REPEAT = "시도할 횟수는 몇 회인가요?"
+val NOT_INT_OR_NULL = "숫자가 아니거나 null입니다."
+val INPUT_UNDER_ZERO = "입력 값에 음수가 있습니다."
+val INPUT_OVER_FIVE_OR_NULL = "이름이 5자가 넘어가거나 값이 없습니다."
+val INPUT_HAVE_SPACE = ",뒤에 공백이 있습니다."
+val INPUT_DUPLICATE = "중복된 수가 있습니다."
+val INPUT_SIZE = "2대 이상 입력해야 합니다."
+val SPACE = " "
+val MAX_LENGTH = 5
+val MOVE = "-"
 
-var cars = mutableListOf<Car>()
+val cars = mutableListOf<Car>()
 val winners = mutableListOf<String>()
 fun main() {
     println(INPUT_NAME)
