6ad261db5fac667d14c6dc52329d37d7043abd31
YuJungHyun <0703olivia@naver.com>
2023-10-31 22:58:50 +0900

    refactor(OutputView):  경주 결과 출력 메소드 간결하게 수정

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -5,16 +5,15 @@ import racingcar.model.RacingCar
 class OutputView {
     fun printResultMessage() = println("\n실행 결과")
 
-    fun printResult(racingCars: List<RacingCar>) {
-        for (car in racingCars) {
-            println("${car.name} : " + "-".repeat(car.forward))
-        }
-        println()
+    fun printResult(racingCars: List<RacingCar>) : String {
+        val result = StringBuilder()
+        for (car in racingCars)
+            result.append("${car.name} : ").append("-".repeat(car.forward)).append("\n")
+        return result.toString()
     }
 
     fun printWinnerMessage() = print("최종 우승자 : ")
 
-    fun printWinner(winnerCars: List<String>) {
-        println(winnerCars.joinToString(", "))
-    }
+    fun printWinner(winnerCars: List<String>) : String = winnerCars.joinToString(", ")
+
 }
