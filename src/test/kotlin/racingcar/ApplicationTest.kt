64719b912fd7de4d369050717f272b6f7e6acc44
Nohsein <si2238@naver.com>
2023-10-31 17:54:28 +0900

    test: 입력에 대한 예외 처리
    자동차 이름 - 중복, 빈칸, 5자 초과 시 에러
    시도 횟수 - 1이상의 숫자 아닐 시 에러

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -6,6 +6,8 @@ import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class ApplicationTest : NsTest() {
     @Test
@@ -23,6 +25,29 @@ class ApplicationTest : NsTest() {
     fun `이름에 대한 예외 처리`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " ", "pobi,,java", "pobi,java,"])
+    fun `공백 이름에 대한 예외 처리`(name: String) {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException(name, "1") }
+        }
+    }
+
+    @Test
+    fun `중복 이름에 대한 예외 처리`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,java,pobi", "1") }
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["one", "0", " "])
+    fun `시도 횟수에 대한 예외 처리`(tryNum: String) {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,java", tryNum) }
         }
     }
 
