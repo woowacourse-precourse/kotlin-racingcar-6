d7fe276fc41a7f03c168931c7e33ec850ea1ada2
jklom0326 <jklom0326@gmail.com>
2023-10-31 23:26:24 +0900

    refactor: Exception 클래스 문자열 상수화

--- src/main/kotlin/racingcar/Exception.kt
+++ src/main/kotlin/racingcar/Exception.kt
@@ -3,14 +3,21 @@ package racingcar
 class Exception {
     fun nameValidation(name: List<String>) {
         name.forEach {
-            if (it.length > 5) throw IllegalArgumentException("5글자를 초과할 수 없습니다.")
-            if (it.isEmpty()) throw IllegalArgumentException("이름이 비어있습니다.")
+            if (it.length > 5) throw IllegalArgumentException(NAME_LENGTH_EXCEPTION)
+            if (it.isEmpty()) throw IllegalArgumentException(EMPTY_NAME_EXCEPTION)
         }
     }
 
     fun roundsValidation(input: String): Int {
-        val count = input.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닙니다.")
-        if (count < 0) throw IllegalArgumentException("0보다 작은 수는 입력할 수 없습니다.")
+        val count = input.toIntOrNull() ?: throw IllegalArgumentException(NOT_NUMBER_EXCEPTION)
+        if (count < 0) throw IllegalArgumentException(NEGATIVE_INTEGER_EXCEPTION)
         return count
+    }
+
+    companion object {
+        const val NAME_LENGTH_EXCEPTION = "5글자를 초과할 수 없습니다."
+        const val EMPTY_NAME_EXCEPTION = "이름이 비어있습니다."
+        const val NOT_NUMBER_EXCEPTION = "숫자가 아닙니다."
+        const val NEGATIVE_INTEGER_EXCEPTION = "0보다 작은 수는 입력할 수 없습니다."
     }
 }
