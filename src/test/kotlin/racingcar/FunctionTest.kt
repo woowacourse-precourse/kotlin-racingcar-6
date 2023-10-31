53eb06fb0d7b27c12429e6131c5931f694b9b3c8
조형석 <chs9902@naver.com>
2023-10-31 20:58:11 +0900

    test: 우승자 판별 테스트

--- src/test/kotlin/racingcar/FunctionTest.kt
+++ src/test/kotlin/racingcar/FunctionTest.kt
@@ -3,13 +3,23 @@ package racingcar
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import racingcar.model.CarNameSeparator
+import racingcar.model.WinnerDiscrimination
 
 class FunctionTest {
     @Test
-    fun testSeparator() {
+    fun `자동차 이름 분리 테스트`() {
         val names = "kim,su,han,mu"
         val result = CarNameSeparator().separator(names)
         val expectedList = listOf("kim","su","han","mu")
         assertEquals(expectedList, result)
+    }
+
+    @Test
+    fun `우승자 판별 테스트`() {
+        val scoreBoard = listOf("-", "--", "--", "-")
+        val cars = listOf("kim","su","han","mu")
+        val result = WinnerDiscrimination().answer(scoreBoard, cars)
+        val expectedWinners = listOf("su", "han")
+        assertEquals(expectedWinners, result)
     }
 }
