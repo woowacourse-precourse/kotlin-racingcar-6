dd55d013be8fa146cf5720a55b678ec3e097e338
조형석 <chs9902@naver.com>
2023-10-31 19:14:15 +0900

    feat: 자동차 이름 공백 검사 추가

--- src/main/kotlin/racingcar/model/Validation.kt
+++ src/main/kotlin/racingcar/model/Validation.kt
@@ -8,6 +8,8 @@ class Validation {
                 throw IllegalArgumentException(Constants.ERROR_WORD_MAX)
             } else if (name.length == Constants.MIN) {
                 throw IllegalArgumentException(Constants.ERROR_WORD_MIN)
+            } else if (name.contains(Constants.SPACE_BAR)){
+                throw IllegalArgumentException(Constants.ERROR_WORD_EMPTY_SPACE)
             }
         }
     }
