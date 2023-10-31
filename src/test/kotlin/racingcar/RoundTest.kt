1d106814f18586466babef0f8ddd46866814d85b
jklom0326 <jklom0326@gmail.com>
2023-10-31 23:28:31 +0900

    test: RoundTest 테스트 분리

--- src/test/kotlin/racingcar/RoundTest.kt
+++ src/test/kotlin/racingcar/RoundTest.kt
@@ -7,13 +7,23 @@ class RoundTest {
     private val exception = Exception()
 
     @Test
-    fun `라운드 예외 테스트`() {
+    fun `라운드 입력이 정수값이 이닐 경우`() {
         val input1 = "aaa"
-        val input2 = ""
-        val input3 = "-1"
 
         assertThrows<IllegalArgumentException> { exception.roundsValidation(input1) }
+    }
+
+    @Test
+    fun `라운드 입력이 비어 있을 경우`() {
+        val input2 = ""
+
         assertThrows<IllegalArgumentException> { exception.roundsValidation(input2) }
+    }
+
+    @Test
+    fun `라운드 입력이 음수인 경우`() {
+        val input3 = "-1"
+
         assertThrows<IllegalArgumentException> { exception.roundsValidation(input3) }
     }
 }
