41e1757ff5e31bd52882df6af620e2d3b988add9
park-yina <111878820+park-yina@users.noreply.github.com>
2023-10-31 18:49:37 +0900

    예기치 못한 오류 수정하기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -11,15 +11,13 @@ fun main() {
    if (inputUser.validName(carNames)) {
       val gameCount = inputUser.tryGameCount()
       gamePlaying.outputStartMent()
-      if (inputUser.validName(carNames)) {
          for (round in 1..gameCount) {
             gameInfo = gamePlaying.generateNumber(carNames)
             gamePlaying.printPerExecutionResult(gameInfo)
          }
-      }
    }
 
-      val scoreList = gamePlaying.calculateScore(gameInfo)
-      val topScore = gamePlaying.findTopScoreList(scoreList)
-      gamePlaying.printlnTopScore(topScore)
-   }
+   val scoreList = gamePlaying.calculateScore(gameInfo)
+   val topScore = gamePlaying.findTopScoreList(scoreList)
+   gamePlaying.printlnTopScore(topScore)
+}
