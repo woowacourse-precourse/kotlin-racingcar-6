c87aff3f75681d8c96105d72ae36247244d26948
hxeyexn <kck85075@kumoh.ac.kr>
2023-10-31 23:23:31 +0900

    Feat: 사용자가 입력한 차량 이름 가져오는 기능

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -3,13 +3,19 @@ package racingcar
 class RacingGame {
 
     private val host = Host()
+    private val player = Player()
     private val validation = Validation()
 
-    fun start() {
+    fun getCarNames(): List<Car> {
         host.printRequestEnterCarName()
-        validation.isValidCarName()
-        host.printQuestionNumberOfAttempt()
-        validation.isValidNumberOfAttempt()
-    }
+        val inputCarNames = player.inputCarsName()
+        val carNames = inputCarNames.split(",")
+        val carList = mutableListOf<Car>()
+        for (name in carNames) {
+            carList.add(Car(name))
+        }
+        validation.isValidCarName(inputCarNames, carList)
 
+        return carList
+    }
 }
