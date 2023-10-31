0cb39a5e0d712526fcbeb783e1fb45bd9d1e6ae2
wnsgml517 <wnsgml517@naver.com>
2023-10-31 22:52:45 +0900

    예외처리 함수 따로 리팩토링

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,28 +1,30 @@
 package racingcar
 
 fun main() {
-        val carNames = CarRace.getCarNames()
-        val moveCount = CarRace.getMoveCount()
-        val moveCountResult = runCatching { moveCount }
+    val carNames = CarRace.getCarNames()
+    val moveCount = CarRace.getMoveCount()
+    val moveCountResult = runCatching { moveCount }
 
-        if (carNames is IllegalArgumentException) {
-            println("${(carNames as IllegalArgumentException).message} 애플리케이션을 종료합니다.")
-            return
-        }
-        moveCountResult.onFailure { e ->
-            println("오류: ${e.message}\n프로그램을 종료합니다.")
-        }
+    handleExceptions(carNames, moveCountResult)
+
+    if (carNames is IllegalArgumentException) {
+        println("${(carNames as IllegalArgumentException).message} 애플리케이션을 종료합니다.")
+        return
+    }
+    moveCountResult.onFailure { e ->
+        println("오류: ${e.message}\n프로그램을 종료합니다.")
+    }
 
 
-        val carPositions = IntArray(carNames.size)
+    val carPositions = IntArray(carNames.size)
 
-        println("\n자동차 경주 게임을 시작합니다!\n")
+    println("\n자동차 경주 게임을 시작합니다!\n")
 
-        repeat(moveCount) {
-            CarMovement.moveCars(carNames, carPositions)
-            CarResults.printCurrentPositions(carNames, carPositions)
-        }
+    repeat(moveCount) {
+        CarMovement.moveCars(carNames, carPositions)
+        CarResults.printCurrentPositions(carNames, carPositions)
+    }
 
-        val winners = CarResults.findWinners(carNames, carPositions)
-        CarResults.printWinners(winners)
+    val winners = CarResults.findWinners(carNames, carPositions)
+    CarResults.printWinners(winners)
 }
