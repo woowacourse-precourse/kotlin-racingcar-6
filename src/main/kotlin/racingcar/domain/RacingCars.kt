d96b1ffd39cf4405c6d04883cba6d5b82a89ddec
JJongmen <pjy90123@gmail.com>
2023-10-31 16:53:30 +0900

    refactor(RacingCars): 팩토리 메서드로 객체 생성하도록 수정
    우승자 판단하기 위해 차들의 목록을 입력받아 객체를 생성하려고 수정
    기존은 이름들을 생성자에 입력받아 객체를 생성하였지만
    팩토리 메서드를 통해 이름들과 차들을 각각 입력받을 수 있게됨

--- src/main/kotlin/racingcar/domain/RacingCars.kt
+++ src/main/kotlin/racingcar/domain/RacingCars.kt
@@ -2,19 +2,21 @@ package racingcar.domain
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class RacingCars(names: List<String>) {
-    private val cars: List<RacingCar>
-
+class RacingCars private constructor(private val cars: List<RacingCar>) {
     init {
-        require(names.size >= MIN_CARS_SIZE) { "자동차는 최소 ${MIN_CARS_SIZE}대 이상이어야 합니다." }
-        cars = names.map { RacingCar(it) }
+        require(cars.size >= MINIMUM_NUMBER_OF_CARS) { "자동차는 최소 ${MINIMUM_NUMBER_OF_CARS}대 이상이어야 합니다." }
     }
 
     fun move() {
         cars.forEach { it.move(Randoms.pickNumberInRange(0, 9)) }
+    fun move() = cars.forEach { it.move(Randoms.pickNumberInRange(0, 9)) }
     }
 
     companion object {
-        private const val MIN_CARS_SIZE = 2
+        private const val MINIMUM_NUMBER_OF_CARS = 2
+
+        fun fromNames(names: List<String>) = RacingCars(names.map { RacingCar(it) })
+
+        fun fromCars(cars: List<RacingCar>) = RacingCars(cars)
     }
 }
--- src/test/kotlin/racingcar/domain/RacingCarsTest.kt
+++ src/test/kotlin/racingcar/domain/RacingCarsTest.kt
@@ -1,17 +1,31 @@
 package racingcar.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class RacingCarsTest {
     @Test
-    fun `생성실패_2개 미만의 차 이름이 입력됨`() {
-        assertThrows<IllegalArgumentException> { RacingCars(mutableListOf("pobi")) }
+    fun `이름들로 생성실패_2개 미만의 차 이름이 입력됨`() {
+        assertThrows<IllegalArgumentException> { RacingCars.fromNames(mutableListOf("pobi")) }
     }
 
     @Test
     fun 생성성공() {
         assertDoesNotThrow { RacingCars(mutableListOf("pobi", "woni")) }
+    fun `이름들로 생성성공`() {
+        assertDoesNotThrow { RacingCars.fromNames(mutableListOf("pobi", "woni")) }
+    }
+
+    @Test
+    fun `차들로 생성실패_2개 미만의 차들이 입력됨`() {
+        assertThrows<IllegalArgumentException> { RacingCars.fromCars(mutableListOf(RacingCar("pobi"))) }
+    }
+
+    @Test
+    fun `차들로 생성성공`() {
+        assertDoesNotThrow { RacingCars.fromCars(mutableListOf(RacingCar("pobi"), RacingCar("woni"))) }
+    }
     }
 }
