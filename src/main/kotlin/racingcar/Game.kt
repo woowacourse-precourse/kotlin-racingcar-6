4f33eae7e7803c4e7fcd178411d4b580f48118e3
stopkite <stopkite98@gmail.com>
2023-10-31 14:30:35 +0900

    refactor: 우승자 출력 기능 클래스화 및 게임 요소 생성자로 이동

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -1,24 +1,16 @@
 package racingcar
 
-class Game {
-    private val gameInitializer = GameInitializer()
-    private val gameMatch = GameMatch()
-
+class Game(
+    private val gameInitializer: GameInitializer = GameInitializer(),
+    private val gameMatch: GameMatch = GameMatch(),
+    private val gameResultPresenter: GameResultPresenter = GameResultPresenter()
+) {
     private val cars: Cars = Cars()
     private var round: Round = Round()
 
     fun start() {
         gameInitializer.init(cars, round)
         gameMatch.start(cars, round)
-        showWinners()
-    }
-
-    private fun showWinners() {
-        print(WINNER_INFO)
-        cars.showWinners()
-    }
-
-    companion object {
-        const val WINNER_INFO = "최종 우승자 : "
+        gameResultPresenter.showWinners(cars)
     }
 }
