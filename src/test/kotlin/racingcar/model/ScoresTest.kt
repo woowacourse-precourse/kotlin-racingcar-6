0f7011df9d6dfef05b3126180a891eb579c8e554
sseung416 <choiseungyeon416@gmail.com>
2023-10-31 17:54:11 +0900

    test: Scores 클래스 테스트 코드 작성

---
+++ src/test/kotlin/racingcar/model/ScoresTest.kt
@@ -0,0 +1,28 @@
+package racingcar.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class ScoresTest {
+
+    private val scores = Scores.create(attempt = 3)
+
+    @Test
+    @DisplayName("attempt 값이 최대를 넘었을 때, 에러를 발생하는지")
+    fun addCurrentScore_overflowAttempt() {
+        assertThatThrownBy { scores.addCurrentScore(attempt = 4, score = 4) }
+            .isExactlyInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Scores.Error.OverflowAttempt.message)
+    }
+
+    @Test
+    @DisplayName("해당 주차의 점수가 잘 기록되는지 확인")
+    fun addCurrentScore() {
+        scores.addCurrentScore(3, 4)
+        scores.addCurrentScore(1, 3)
+        assertThat(scores.scoreByAttempt[3]).isEqualTo(4)
+        assertThat(scores.scoreByAttempt[1]).isEqualTo(3)
+    }
+}
