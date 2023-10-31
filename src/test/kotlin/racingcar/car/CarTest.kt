260b81f3882a2f2673aca6b46dd7d85fcd52e4ca
lee989898 <wnsrud1352@gmail.com>
2023-10-31 16:06:37 +0900

    [chore] 불필요한 import문 제거

--- src/test/kotlin/racingcar/car/CarTest.kt
+++ src/test/kotlin/racingcar/car/CarTest.kt
@@ -1,7 +1,6 @@
 package racingcar.car
 
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.`in`
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
