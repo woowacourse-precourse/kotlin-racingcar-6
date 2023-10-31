a0acc679b96802e318b285f8d634ad5d66026ba0
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-01 01:08:21 +0900

    fix)

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -25,48 +25,25 @@ class ApplicationTest : NsTest() {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
         }
     }
+
     @Test
-    fun `이름이 공백 예외 처리`() {
+    fun `반복횟수에 대한 예외 처리`() {
         assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("", "1") }
+            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "a") }
         }
     }
+
     @Test
-    fun `반복 횟수 공백 예외 처리`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi,kim", " ") }
-        }
+    fun `1명이 플레이 할 때 예외 처리`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("kss", "1")
+                assertThat(output()).contains("kss : -", "최종 우승자 : kss")
+            },
+            MOVING_FORWARD
+        )
     }
-    @Test
-    fun `반복 횟수가 음수일 경우 예외 처리`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi,woni,kim", "-3") }
-        }
-    }
-    @Test
-    fun `반복 횟수가 문자일 경우 예외 처리`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi,woni,kim", "a") }
-        }
-    }
-    @Test
-    fun `차 이름이 중복일 경우 예외 처리`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi,pobi,kim", "1") }
-        }
-    }
-    @Test
-    fun `이름 사이에 공백이 있을 경우 예외 처리`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi, pobi,kim", "1") }
-        }
-    }
-    @Test
-    fun `자동차 이름이 1글자 미만일 경우 예외 처리`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi,kim,,lee", "1") }
-        }
-    }
+
 
     public override fun runMain() {
         main()
