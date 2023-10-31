20e15c28a5d0954e8e16b2a09c970092067e38ea
MinJun <agvber12@gmail.com>
2023-11-01 07:13:51 +0900

    feat: 사용자로 부터 시도 횟수 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,12 +1,17 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console.readLine
+import camp.nextstep.edu.missionutils.Randoms
 
 private val racingCar = mutableMapOf<String, Int>() // 자동차 이름, 점수
+private var tryCount: Int = 0
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     inputRacingCarName()
+
+    println("시도할 횟수는 몇 회인가요?")
+    inputTryCount()
 
 }
 
@@ -19,5 +24,14 @@ private fun inputRacingCarName() {
         }
 
         racingCar[car] = 0
+    }
+}
+
+private fun inputTryCount() {
+    try {
+        val readLine = readLine()
+        tryCount = readLine.toInt()
+    } catch (e: Exception) {
+        throw IllegalArgumentException()
     }
 }
