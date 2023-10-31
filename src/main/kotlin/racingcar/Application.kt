550ebf24e553e4ba3f88f529f72fbb562a06d042
aprilgom <aprilgom@users.noreply.github.com>
2023-10-31 20:10:51 +0900

    feat($Car): goForwardOrStop

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,17 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Randoms
 
 class Car(val name: String) {
     var position = 0
         private set
     init {
         require(name.length <= 5){ "Car name length exceeds 5 letters" }
+    }
+    fun goForwardOrStop() {
+        val random = Randoms.pickNumberInRange(0,9)
+        if (random >= 4) {
+            position++
+        }
     }
 }
 
