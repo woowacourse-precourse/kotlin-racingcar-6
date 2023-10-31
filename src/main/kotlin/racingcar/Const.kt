f5cc39ff1769427d87d95089e3cd39fd08bf143e
K_Gs:D <rootachieve6053@gmail.com>
2023-10-31 18:18:05 +0900

    feat(Printer): class 생성
    Printer class를 생성했습니다.
    - printList 함수 : 자동차 리스트를 입력받아 각 객체를 print 넣어 호출합니다.
    - pinrt 함수 : 자동차를 받으면 해당 자동차의 위치, 이름을 출력합니다.
    - printWinner : 승리한 자동차 리스트를 받으면 해당 자동차 리스트를 양식에 맞게 출력합니다.

--- src/main/kotlin/racingcar/Const.kt
+++ src/main/kotlin/racingcar/Const.kt
@@ -2,4 +2,6 @@ package racingcar
 
 object Const {
     const val MOVE_POINT_NUMBER = 4
+
+    const val WINNER_MSG = "최총 우승자 : "
 }
---
+++ src/main/kotlin/racingcar/Printer.kt
@@ -0,0 +1,31 @@
+package racingcar
+
+import java.lang.StringBuilder
+
+class Printer {
+    fun printList(carList: List<Car>) {
+        for (item in carList) {
+            print(item)
+        }
+    }
+
+    private fun print(car: Car) {
+        val stringBuilder = StringBuilder()
+
+        stringBuilder.append(car.name + " : ")
+        for (i in 1..car.location) {
+            stringBuilder.append('-')
+        }
+        println(stringBuilder)
+    }
+
+    fun printWinner(carList: List<Car>) {
+        val stringBuilder = StringBuilder()
+        carList.map {
+            stringBuilder.append(it.name + ", ")
+        }
+        stringBuilder.trim()
+        stringBuilder.removeSuffix(",")
+        println(Const.WINNER_MSG + stringBuilder)
+    }
+}
