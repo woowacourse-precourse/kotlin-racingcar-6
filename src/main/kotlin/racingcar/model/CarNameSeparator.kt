90772b1c209e8e930c0e805c482687a7e4157a82
조형석 <chs9902@naver.com>
2023-10-31 22:33:15 +0900

    refactor: 자동차 이름 분리 기능 리팩토링

--- src/main/kotlin/racingcar/model/CarNameSeparator.kt
+++ src/main/kotlin/racingcar/model/CarNameSeparator.kt
@@ -1,18 +1,7 @@
 package racingcar.model
 
 class CarNameSeparator {
-    fun separator(names: String) :List<String>{
-        var separatedName: MutableList<String> = mutableListOf()
-        var carName = Constants.EMPTY_SPACE
-        for (word in names) {
-            if (word == Constants.COMMA) {
-                separatedName.add(carName)
-                carName = Constants.EMPTY_SPACE
-                continue
-            }
-            carName += word
-        }
-        separatedName.add(carName)
-        return separatedName
+    fun separator(names: String): List<String> {
+        return names.split(Constants.COMMA.toString())
     }
 }
