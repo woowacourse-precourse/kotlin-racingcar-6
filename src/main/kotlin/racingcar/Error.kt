18932a846bfd2cb66a8cc6311d6d5a1c03dd5899
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-10-31 17:08:10 +0900

    fix6

--- src/main/kotlin/racingcar/Error.kt
+++ src/main/kotlin/racingcar/Error.kt
@@ -7,6 +7,7 @@ class Error {
         private const val INPUT_OVER_FIVE_OR_NULL = "이름이 5자가 넘어가거나 값이 없습니다."
         private const val INPUT_HAVE_SPACE = ",뒤에 공백이 있습니다."
         private const val INPUT_DUPLICATE = "중복된 수가 있습니다."
+        private const val INPUT_SIZE = "2대 이상 입력해야 합니다."
         private const val SPACE = " "
         private const val MAX_LENGTH = 5
     }
@@ -26,10 +27,11 @@ class Error {
             checkError(cars[i])
         }
         checkDuplicate(cars)
+        checkSize(cars)
     }
 
     private fun checkError(input: String) {
-        if (input.length > MAX_LENGTH || input.length < 0) {
+        if (input.length > MAX_LENGTH || input.isEmpty()) {
             throw IllegalArgumentException(INPUT_OVER_FIVE_OR_NULL)
         }
         if (input.contains(SPACE)) {
@@ -40,6 +42,11 @@ class Error {
     private fun checkDuplicate(input: List<String>) {
         if (input.toSet().size != input.size) {
             throw IllegalArgumentException(INPUT_DUPLICATE)
+        }
+    }
+    private fun checkSize(input: List<String>){
+        if(input.size<2){
+            throw IllegalArgumentException(INPUT_SIZE)
         }
     }
 }
