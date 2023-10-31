0f1e13dcce9d22e8ec39c3962cd5bed844c9b224
sh1mj1 <7wlgns@gmail.com>
2023-10-31 21:10:36 +0900

    feat(GameController): validate cars(input) & start turn with tryCount & decide and show the result
    * 입력으로 받은 cars 의 유효성을 검증한다.
    * 입력으로 받은 시도 횟수만큼 이동을 진행하고, 결과를 보여준다.
    * 승자를 결정하고 결과를 보여준다.

--- src/main/kotlin/racingcar/controller/GameController.kt
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -2,26 +2,38 @@ package racingcar.controller
 
 import racingcar.model.Car
 import racingcar.model.Cars
-import racingcar.view.InputCars
-import racingcar.view.InputTry
-import racingcar.view.OutputGuide
+import racingcar.view.*
 
 class GameController {
     private val outputGuide = OutputGuide()
     private val inputCars = InputCars()
     private val inputTry = InputTry()
+    private val outputCars = OutputCars()
+    private val outputResult = OutputResult()
+
     private lateinit var cars: Cars
+
     fun startGame() {
         outputGuide.showStartGuide()
         cars = Cars(inputCars().map { Car(it) })
+        cars.validate()
+
         outputGuide.showTryCountGuide()
         val tryCount = inputTry()
+        startTurn(tryCount)
+        val winners = cars.decideWinner()
+        outputResult.showResult(winners)
 
     }
 
-    fun startTurn(tryCount: Int) {
-
+    private fun startTurn(tryCount: Int) {
+        outputGuide.showResultGuide()
+        for (i in 1..tryCount) {
+            cars.cars.forEach {
+                it.moveForward()
+            }
+            outputCars.showRace(cars)
+        }
     }
-
 
 }
