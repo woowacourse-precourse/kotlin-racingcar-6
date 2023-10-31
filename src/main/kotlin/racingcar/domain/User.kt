79057e8c39848970ee674273f653cee9d8bf7189
3uomlkh <cw1205@duksung.ac.kr>
2023-10-31 19:25:12 +0900

    Refactor: 시도 횟수 예외처리 메서드 분리

--- src/main/kotlin/racingcar/domain/User.kt
+++ src/main/kotlin/racingcar/domain/User.kt
@@ -6,15 +6,30 @@ class User {
     companion object {
         val name = inputName()
         val nameList = name?.split(",")
+        val carNum = nameList?.size
         val resultName = nameList?.joinToString(",")
-        val tryNum = inputTry()
+        val tryNum = inputTrial()
     }
 }
 
 fun inputName(): String? {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     return Console.readLine()
 }
 
-fun inputTry(): Int {
-    return Console.readLine().toInt()
+fun inputTrial(): Int {
+    println("시도할 횟수는 몇 회인가요?")
+    var num = Console.readLine()
+    var result = inputTrialValidation(num)
+    return result
+}
+
+fun inputTrialValidation(num: String): Int {
+    val result: Int
+    try {
+        result = num.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("문자가 아닌 숫자를 입력하세요.")
+    }
+    return result
 }
