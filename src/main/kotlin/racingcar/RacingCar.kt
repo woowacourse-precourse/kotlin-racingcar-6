09cd7008f1659995aad1b8e09a21fd0dc298202f
Jtility <leejewon97@gmail.com>
2023-11-01 04:19:40 +0900

    feat(RacingCar.move()): implement move()
    if random number greater than 4, increase member variable mileage

--- src/main/kotlin/racingcar/RacingCar.kt
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -1,8 +1,13 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class RacingCar(name: String) {
+    private var mileage = 0
     fun move() {
-        TODO("Not yet implemented")
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            mileage++
+        }
     }
 
     fun print() {
