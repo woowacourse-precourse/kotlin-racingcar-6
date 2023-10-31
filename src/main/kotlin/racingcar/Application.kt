e6380a883d0da711aecdc152b0e495b1494fd101
Jtility <leejewon97@gmail.com>
2023-10-31 21:53:53 +0900

    feat(invalidTryCountCheck): create invalidTryCountCheck
    check try count is integer and greater than 0
    put in main

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,6 +6,15 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val racingCars = splitRacingCarName(Console.readLine())
     invalidRacingCarsCheck(racingCars)
+    println("시도할 회수는 몇회인가요?")
+    invalidTryCountCheck(Console.readLine().toIntOrNull())
+}
+
+fun invalidTryCountCheck(tryCount: Int?) {
+    when {
+        tryCount == null -> throw IllegalArgumentException("정수를 입력하세요.")
+        tryCount < 1 -> throw IllegalArgumentException("1 이상의 숫자를 입력하세요.")
+    }
 }
 
 fun invalidRacingCarsCheck(racingCars: List<String>) {
