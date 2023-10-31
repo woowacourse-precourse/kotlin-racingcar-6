ec84e1088a67567f479ab56b33f704b13723813a
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 03:59:46 +0900

    feat: 자동차가 숫자를 뽑아 전진할지 정지할지 결정하는 기능 구현
    자동차가 Randoms 클래스의 pickNumberInRange()를 사용하여 0부터 9사이의
    숫자를 뽑는다.
    해당 숫자가 4 이상이면 전진하고, 아니면 정지한다.
    4 이상인 경우 해당 숫자만큼 movingDistance를 증가시킨다.

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,4 +1,11 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Randoms
 
-class Car {
+class Car(val name: String, var movingDistance: Int) {
+    fun chooseGoOrStop() {
+        val randomNumber: Int = Randoms.pickNumberInRange(0, 9)
+        if (randomNumber >= 4){
+            this.movingDistance += 1
+        }
+    }
 }
