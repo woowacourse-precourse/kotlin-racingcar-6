765ffa75861a40d0f50ff9c103d1304203d9dcf0
Seannnn55 <sally99h@gmail.com>
2023-10-31 19:00:15 +0900

    refactor: [PrepareGame] 출력 메세지 추가

--- src/main/kotlin/racingcar/PrepareGame.kt
+++ src/main/kotlin/racingcar/PrepareGame.kt
@@ -3,14 +3,16 @@ package racingcar
 class PrepareGame {
 
     fun racingCarName(): List<String> {
-        var userName: String = readLine()
-        var userList: List<String> = userName.split(",")
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+        var userName: String? = readLine()
+        var userList: List<String> = userName!!.split(",")
 
         return userList
     }
 
     fun racingNumber(): Int{
-        var raceNum: Int = readLine().toInt()
+        println("시도할 횟수는 몇 회인가요?")
+        var raceNum: Int = readLine()!!.toInt()
 
         return raceNum
     }
