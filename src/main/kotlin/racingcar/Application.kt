df4169d400281e6be8d7f239a51b0a4cf6cd0b51
wnsgml517 <wnsgml517@naver.com>
2023-10-31 22:55:18 +0900

    주석 달기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,30 +1,27 @@
 package racingcar
 
 fun main() {
+    // 자동차 이름 입력
     val carNames = CarRace.getCarNames()
+
+    // 시도 횟수 입력 및 예외 처리
     val moveCount = CarRace.getMoveCount()
     val moveCountResult = runCatching { moveCount }
-
     handleExceptions(carNames, moveCountResult)
 
-    if (carNames is IllegalArgumentException) {
-        println("${(carNames as IllegalArgumentException).message} 애플리케이션을 종료합니다.")
-        return
-    }
-    moveCountResult.onFailure { e ->
-        println("오류: ${e.message}\n프로그램을 종료합니다.")
-    }
-
-
-    val carPositions = IntArray(carNames.size)
-
+    // 게임 시작 메시지 출력
     println("\n자동차 경주 게임을 시작합니다!\n")
 
+    // 자동차 위치 배열 초기화
+    val carPositions = IntArray(carNames.size)
+
+    // 지정된 횟수만큼 게임 루프 실행
     repeat(moveCount) {
         CarMovement.moveCars(carNames, carPositions)
         CarResults.printCurrentPositions(carNames, carPositions)
     }
 
+    // 최종 우승자 찾고 출력
     val winners = CarResults.findWinners(carNames, carPositions)
     CarResults.printWinners(winners)
 }
