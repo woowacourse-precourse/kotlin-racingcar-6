ceffe1c519e245705dc9e5641b5e469f3776ca68
aprilgom <aprilgom@users.noreply.github.com>
2023-10-31 19:42:35 +0900

    feat($Car): get Car position

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,8 @@
 package racingcar
 
 class Car(val name: String) {
+    var position = 0
+        private set
     init {
         require(name.length <= 5){ "Car name length exceeds 5 letters" }
     }
