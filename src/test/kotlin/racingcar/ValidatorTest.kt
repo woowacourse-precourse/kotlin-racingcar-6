07d7c59b95c33e7a33c44456e15eefa72dd3daad
altpfwlzh <altpfwlzh@gmail.com>
2023-11-01 22:48:36 +0900

    style(test) : 예외 테스트 prefix 제거

--- src/test/kotlin/racingcar/ValidatorTest.kt
+++ src/test/kotlin/racingcar/ValidatorTest.kt
@@ -6,21 +6,21 @@ import util.Validator
 
 class ValidatorTest {
     @Test
-    fun `예외 테스트) 입력 값이 없을 경우`() {
+    fun `입력 값이 없을 경우`() {
         assertThrows<IllegalArgumentException> {
             Validator().checkInputEmpty("")
         }
     }
 
     @Test
-    fun `예외 테스트) 시도 횟수가 0번일 경우`() {
+    fun `시도 횟수가 0번일 경우`() {
         assertThrows<IllegalArgumentException> {
             Validator().checkTryNum(0.toString())
         }
     }
 
     @Test
-    fun `예외 테스트) 시도 횟수가 숫자가 아니거나 음수일 경우`() {
+    fun `시도 횟수가 숫자가 아니거나 음수일 경우`() {
         assertThrows<IllegalArgumentException> {
             Validator().checkTryNum("-100")
         }
