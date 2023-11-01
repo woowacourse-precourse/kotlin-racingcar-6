626684a04b3e3d51fcb77b18ccaeefb326b4990c
iiolo <alohalo98@naver.com>
2023-11-01 17:42:38 +0900

    feat: car data class 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,20 +7,25 @@ fun main() {
 
 }
 
+data class Car(
+    val name: String,
+    val randomNum: Int?
+)
+
 fun getCarNames(): String {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     return Console.readLine()
 }
 
-fun getValidCarNamesArray(carNames: String): Collection<String> {
-    val carNamesArray = carNames.split(",")
-    if (carNamesArray.any { it.length > 5 }) {
+fun getValidCarNamesArray(carNames: String): Collection<Car> {
+    val carNamesArray = carNames.split(",").map { Car(it,null) }
+    if (carNamesArray.any { it.name.length > 5 }) {
         throw IllegalArgumentException("자동차 이름은 5자 이하 이어야 함")
     }
     return carNamesArray
 }
 
-fun getNumberOfRacing():Int{
+fun getNumberOfRacing(): Int {
     println("시도할 횟수는 몇 회인가요?")
     return Console.readLine().toInt()
 }
