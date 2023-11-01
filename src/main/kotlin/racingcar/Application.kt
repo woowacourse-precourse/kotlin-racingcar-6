fb1768002702534355a6321d9746711b447fea90
seonghyun <skyladder8895@gmail.com>
2023-11-01 16:39:48 +0900

    fix: issue with invalid number in input

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -16,7 +16,9 @@ fun getCarNames(): List<String> {
     val userInput = Console.readLine()
     val carNames = userInput.split(",").map { it.trim() }
 
-
+    if (carNames.size < 2) {
+        throw IllegalArgumentException("자동차 경주 게임을 시작하기 위해서는 2대 이상의 자동차가 필요합니다.")
+    }
     if (carNames.any { it.length > 5 }) {
         throw IllegalArgumentException("이름은 5자 이하만 가능합니다.")
     }
@@ -29,6 +31,11 @@ fun getRounds(): Int {
     val userInput = Console.readLine()
     if (userInput.any { !it.isDigit() }) {
         throw IllegalArgumentException("입력값은 숫자여야 합니다.")
+    }
+
+    val inputNumber = userInput.toInt()
+    if (inputNumber < 1) {
+        throw IllegalArgumentException("시도할 횟수가 적절하지 않습니다.")
     }
     return userInput.toInt()
 }
