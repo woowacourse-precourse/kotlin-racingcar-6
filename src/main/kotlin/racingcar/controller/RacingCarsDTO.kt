5de13d5c5a6e94c6167d8d9c6b617de7a9ccf5db
윤동현 <donghyunyoon81@gmail.com>
2023-11-01 14:42:17 +0900

    feat: 자동차 이름이 5글자 초과일 경우 오류가 발생하는 기능 추가

--- src/main/kotlin/racingcar/controller/RacingCarsDTO.kt
+++ src/main/kotlin/racingcar/controller/RacingCarsDTO.kt
@@ -3,7 +3,12 @@ package racingcar.controller
 import racingcar.model.RacingCarGenerator
 import racingcar.model.RacingCarModel
 
-class RacingCarsDTO(val carsName:String) {
-    fun convertRacingCars() = carsName.split(",").map {carName -> RacingCarModel(carName) }.toMutableList()
+class RacingCarsDTO(val carsName: String) {
+
+    init {
+        carsName.split(",").forEach { carName -> require(carName.length <= 5) }
+    }
+
+    fun convertRacingCars() = carsName.split(",").map { carName -> RacingCarModel(carName) }.toMutableList()
 
 }
