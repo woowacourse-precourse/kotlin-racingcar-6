b43d031a19cbe66d070f09b02caee19951c94198
이아름 <souk0712@naver.com>
2023-11-01 22:54:09 +0900

    refactor: 간편한 객체 사용을 위한 코드 변경
    Car.kt:
    
    - name은 val형식으로 private을 붙일 필요 없어서 제거
    - 캡술화를 위해 외부에 접근이 불가능한 _distance와 접근 가능한 distance로 변수 분리
    - getDistanceLength()함수를 distanceLength 변수로 변경
    - equals()와 hashCode함수() 정의

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,22 +1,34 @@
 package racingcar
 
 class Car(name: String) {
-    private val name: String
-    private var distance: String
+    val name: String
+    private var _distance: String
+    val distance: String
+        get() = this._distance
+    val distanceLength: Int
+        get() = distance.length
 
     init {
         require(name.length in 1 until 6) { "이름은 1자 이상, 5자 이하만 가능합니다." }
         this.name = name
-        this.distance = ""
+        this._distance = ""
     }
 
     fun drive() {
-        this.distance = distance.plus('-')
+        this._distance = _distance.plus('-')
     }
 
-    fun getName(): String = name
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Car) return false
+        if (name != other.name) return false
+        if (distance != other.distance) return false
+        return true
+    }
 
-    fun getDistance(): String = distance
-
-    fun getDistanceLength(): Int = distance.length
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + distance.hashCode()
+        return result
+    }
 }
