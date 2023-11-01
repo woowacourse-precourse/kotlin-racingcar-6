7ed77a2f4afbdf931b27f8958a714fcf70ff5d21
songpink <jejeje04@naver.com>
2023-11-01 15:22:33 +0900

    랜덤변수 val로 다시 바꿔봄

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    var game = Randoms.pickNumberInRange(0, 9)
+    val game = Randoms.pickNumberInRange(0, 9)
     val read = Console.readLine()
     println(game)
     println(read)
