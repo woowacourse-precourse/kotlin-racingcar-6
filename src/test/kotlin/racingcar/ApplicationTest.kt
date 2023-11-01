47f3e69fd8d0a8b5d26cbc8d8adb62db26703817
acecic82 <102938al@naver.com>
2023-11-01 21:34:10 +0900

    feat (ApplicationTest): 여러 경우에 대한 테스트 코드 추가
    여러 경우에 대한 테스트 코드 추가
    
    - 단독 승리인 경우
    - 공동 우승인 경우
    - 3명 중 1,3번째가 승리한 경우
    - 혼자 경기한 경우
    - 모두 전진 혹은 스탑한 경우
    
    테스트 코드를 추가해서 기능이 문제없음을 확인했습니다.

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -25,6 +25,82 @@ class ApplicationTest : NsTest() {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
         }
     }
+    @Test
+    fun `단독 우승`() {
+        assertRandomNumberInRangeTest(
+                {
+                    run("pobi,woni", "2")
+                    assertThat(output()).contains("pobi : ", "woni : --", "최종 우승자 : woni")
+                },
+                STOP, MOVING_FORWARD, STOP, MOVING_FORWARD
+        )
+    }
+
+    @Test
+    fun `혼자 경기한 경우`() {
+        assertRandomNumberInRangeTest(
+                {
+                    run("pobi", "2")
+                    assertThat(output()).contains("pobi : ", "최종 우승자 : pobi")
+                },
+                STOP, STOP
+        )
+    }
+
+    @Test
+    fun `1, 3번째가 우승한 경우`() {
+        assertRandomNumberInRangeTest(
+                {
+                    run("pobi,woni,rilla", "3")
+                    assertThat(output()).contains("pobi : --", "woni : ", "rilla : --", "최종 우승자 : pobi, rilla")
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP, STOP
+        )
+    }
+
+    @Test
+    fun `공동 우승`() {
+        assertRandomNumberInRangeTest(
+                {
+                    run("pobi,woni", "1")
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni")
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        )
+    }
+
+    @Test
+    fun `모두 전진 실패`() {
+        assertRandomNumberInRangeTest(
+                {
+                    run("pobi,woni", "3")
+                    assertThat(output()).contains("pobi : ", "woni : ", "최종 우승자 : pobi, woni")
+                },
+                STOP, STOP, STOP, STOP, STOP, STOP
+        )
+    }
+
+    @Test
+    fun `모두 전진 성공`() {
+        assertRandomNumberInRangeTest(
+                {
+                    run("pobi,woni", "2")
+                    assertThat(output()).contains("pobi : --", "woni : --", "최종 우승자 : pobi, woni")
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        )
+    }
+
+    @Test
+    fun `전진 스탑 스탑 전진`() {
+        assertRandomNumberInRangeTest(
+                {
+                    run("pobi,woni", "2")
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni")
+                },
+                MOVING_FORWARD, STOP, STOP, MOVING_FORWARD
+        )
+    }
 
     public override fun runMain() {
         main()
