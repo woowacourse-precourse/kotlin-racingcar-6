35bcf64390ed8efa582b9f0f697d25e54d704696
HeewonP825 <7hwon@naver.com>
2023-11-01 20:07:04 +0900

    Feat(application.kt): edit user input line

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,10 +2,10 @@ package racingcar
 
 fun main() {
 
-    print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ")
+    print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
     val carNames = readLine()?.split(",")?.map { it.trim() }
 
-    print("시도할 횟수는 몇 회인가요? ")
+    print("시도할 횟수는 몇 회인가요?\n")
     val tryCount = readLine()?.toIntOrNull()
 
     if (carNames == null || tryCount == null || carNames.isEmpty()) {
@@ -17,4 +17,3 @@ fun main() {
     val racingGame = RacingGame(cars, tryCount)
     racingGame.playGame()
 }
-
