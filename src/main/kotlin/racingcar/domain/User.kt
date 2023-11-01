5df80ab51e3c16579d72e66e86deb99fb81799da
Joontae <chlwnsxo0@gmail.com>
2023-11-01 15:35:13 +0900

    feat(racing): 입력에 대한 예외 처리 추가

--- src/main/kotlin/racingcar/domain/User.kt
+++ src/main/kotlin/racingcar/domain/User.kt
@@ -3,13 +3,21 @@ package racingcar.domain
 import racingcar.domain.car.Car
 import racingcar.domain.car.Cars
 
-
 class User {
     fun askCarName(input: String, cars: Cars): Cars {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val carNameList = input.split(",")
+        val validNameRegex = "^[0-9A-Za-z가-힣]{1,5}".toRegex()
         for (carName in carNameList) {
-            if (carName.length > 5) throw IllegalArgumentException("String index out of range: 5")
+            if (carName.length > 5) {
+                throw IllegalArgumentException("차의 이름 길이가 5 이하 이어야 합니다.")
+            }
+            if (carName.isBlank()) {
+                throw IllegalArgumentException("차의 이름이 공백일 수 없습니다.")
+            }
+            if (carName.matches(validNameRegex)) {
+                throw IllegalArgumentException("차의 이름은 특수 문자를 포함할 수 없습니다.")
+            }
             cars.addCar(Car(carName))
         }
         return cars
@@ -17,6 +25,12 @@ class User {
 
     fun askNumber(count: String): Int {
         println("시도할 횟수는 몇 회인가요?")
-        return count.toIntOrNull() ?: throw IllegalArgumentException("Input is not Int")
+        if (count.toIntOrNull() == null) {
+            throw IllegalArgumentException("시도할 횟수는 정수 이어야 합니다.")
+        }
+        if (count.toIntOrNull()!! < 0) {
+            throw IllegalArgumentException("시도할 횟수는 0 이상 이어야 합니다.")
+        }
+        return count.toIntOrNull()!!
     }
 }
