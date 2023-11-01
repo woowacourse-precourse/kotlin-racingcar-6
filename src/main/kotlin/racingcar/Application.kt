0614a98c1ff6222b447b515cac77c5a024daa79e
최형우 <guddn106901@gmail.com>
2023-11-01 17:43:19 +0900

    자동차 이름, 게임 횟수 입력 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,18 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
 fun main() {
-    // TODO: 프로그램 구현
+
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val nameInput = Console.readLine()
+    val carList = nameInput.split(",");
+    for( i in 0..carList.size){
+        if(carList[i].length>5){
+            throw IllegalArgumentException("5자 이상 입력")
+            return
+        }
+    }
+
+    println("시도할 횟수는 몇 회인가요?")
+    val playTime = Console.readLine()
 }
