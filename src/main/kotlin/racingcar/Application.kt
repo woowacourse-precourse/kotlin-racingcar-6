7e266233bbd2a8fb44c2fe83b893f45c2cd124d5
iiolo <alohalo98@naver.com>
2023-11-01 17:06:24 +0900

    feat: 2번 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,7 +6,12 @@ fun main() {
 
 }
 
-fun getCarNames(){
+fun getCarNames(): String {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    Console.readLine()
+    return Console.readLine()
+}
+
+fun validateCarNames(carNames: String): Boolean{
+    val carNameArray = carNames.split(",")
+    return !carNameArray.any{it.length>5}
 }
