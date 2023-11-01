09345653f584432f766292db2bc8a4ce90f10243
yujin37 <yj061628@gmail.com>
2023-11-01 16:54:57 +0900

    refactor: Application 파일 내 클래스, 클래스 파일로 분리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,33 +2,6 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 
-class RacingCarValidator {
-    fun validateCarName(checkName: String) {
-        if(checkName.isBlank() || checkName.length > 5) {
-            throw IllegalArgumentException()
-        }
-    }
-    fun checkCars(cars:String) : List<String> {
-        val racingCars = cars.split(',')
-        for(racingCar in racingCars){
-            RacingCarValidator().validateCarName(racingCar)
-        }
-        return racingCars
-    }
-    fun checkTrying(tries: String) : Int {
-        if (tries.isBlank()) {
-            throw IllegalArgumentException()
-        }
-        try{
-            return tries.toInt()
-        } catch (e: NumberFormatException) {
-            throw IllegalArgumentException()
-        }
-    }
-    companion object {
-        val instance = RacingCarValidator()
-    }
-}
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val cars = Console.readLine()
