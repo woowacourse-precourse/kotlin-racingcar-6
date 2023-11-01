2043c859e79250c9222b774607877489e34184ad
pengcon <vpdzjs@gmail.com>
2023-11-01 19:13:20 +0900

    feat : 전진 또는 멈춤을 결정 할 랜덤 값 생성 기능 구현

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -1,6 +1,7 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 import java.lang.NumberFormatException
 
 class Racing {
@@ -19,5 +20,9 @@ class Racing {
         if (moveCount <= 0) {
             throw IllegalArgumentException("1 이상의 정수를 입력해주세요.")
         }
+    }
+
+    fun createRandomNumber(): Int{
+        return Randoms.pickNumberInRange(0, 9)
     }
 }
