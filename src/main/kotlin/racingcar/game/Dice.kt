b7d16f5b76b11e4e5bae5293c14dfaa354a4804a
PParkcode <qazwsxedc1226@gmail.com>
2023-11-01 17:05:03 +0900

    Refactor: 매직 넘버 상수로 변경
    Dice 클래스에서 랜덤 숫자를 구하는 코드에서 0, 9가 매직 넘버로 사용되어, 가독성을 높이기 위해 상수로 바꾸었다.

--- src/main/kotlin/racingcar/game/Dice.kt
+++ src/main/kotlin/racingcar/game/Dice.kt
@@ -3,7 +3,8 @@ package racingcar.game
 import camp.nextstep.edu.missionutils.Randoms
 
 const val MINIMUM_GO = 4
-
+const val START_NUM = 0
+const val END_NUM = 9
 class Dice {
 
     fun goStop(): Boolean {
@@ -12,7 +13,7 @@ class Dice {
     }
 
     private fun rollTheDice(): Int {
-        return Randoms.pickNumberInRange(0, 9)
+        return Randoms.pickNumberInRange(START_NUM, END_NUM)
     }
 
 
