835a82e96d5aab24942e172e9ad7d48c0c0c4259
wonjongin <wonggamggik@gmail.com>
2023-11-01 11:08:19 +0900

    재실행 - InputMachine.checkCarNameUnder5() 코드 매개변수 Car 클래스로 변경 제출 오류 파악 위해 되집어보는 과정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,6 +3,14 @@ package racingcar
 fun main() {
 
     val machine = InputMachine()
-    val cars : List<String> = machine.makeCars()
-    machine.checkCarNameUnder5(cars)
+    val carsList : List<String> = machine.makeCars()
+
+    val carsClass : MutableList<Car> = mutableListOf()
+
+    // car 선언
+    for (car in carsList) {
+        carsClass.add(Car(car))
+    }
+
+    machine.checkCarNameUnder5(carsClass)
 }
