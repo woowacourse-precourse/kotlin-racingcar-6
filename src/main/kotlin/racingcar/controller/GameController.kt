7f01e40a52102ca75f78f81fe23327dc2ec55d04
bak <bak4017@gmail.com>
2023-11-01 22:15:46 +0900

    refactor(racingcar): play 함수의 라운드 실행 로직 분리

--- src/main/kotlin/racingcar/controller/GameController.kt
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -3,6 +3,7 @@ package racingcar.controller
 import racingcar.constant.Constants.EXECUTION_RESULT_STRING
 import racingcar.domain.CarStatusUpdater
 import racingcar.domain.NumberGenerator
+import racingcar.domain.RoundExecutor
 import racingcar.util.Validator
 import racingcar.view.InputView
 import racingcar.view.RaceView
@@ -28,23 +29,8 @@ class GameController {
     private fun play(carNames: ArrayList<String>, attemptCount: Int) {
         println(EXECUTION_RESULT_STRING)
 
-        val carStatusUpdater = CarStatusUpdater()
-        val numberGenerator = NumberGenerator()
-        val raceView = RaceView()
-
-        carStatus.clear()
-        carStatus.putAll(carNames.associateWith { "" })
-        var currentCount = 0
-        var randomNumber: Int
-        while (currentCount < attemptCount) {
-            for (car in carStatus) {
-                randomNumber = numberGenerator.createRandomNumber()
-                carStatusUpdater.moveCarForward(carStatus, car.key, randomNumber)
-            }
-            raceView.printCarProgress(carStatus)
-
-            currentCount++
-        }
+        val roundExecutor = RoundExecutor()
+        roundExecutor.executeRounds(carNames, carStatus, attemptCount)
     }
 
     private fun printResult() {
