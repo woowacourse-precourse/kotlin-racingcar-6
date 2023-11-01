3bb907b3e7cf893397c6a5a9137dbb01eedd3c42
acecic82 <102938al@naver.com>
2023-11-01 20:01:27 +0900

    feat (Player.class): 플레이어 도메인 추가
    플레이어 도메인 추가 및 README.md 업데이트
    - 플레이어 이름
    - 승리 카운트
    - README.md 에 도메인 구현 부분 clear 표시 추가
    
    플레이어에게 필요한 기능만을 추가한 플레이어 도메인이 추가되었습니다.
    이름을 저장할 name과 승리 횟수를 저장할 count로 구성되어 있습니다.

---
+++ src/main/kotlin/domain/Player.kt
@@ -0,0 +1,12 @@
+package domain
+data class Player(
+        val name: String,
+        var winCount: Int = 0
+) {
+    fun addWinCount() {
+        winCount++
+    }
+    companion object {
+        fun toPlayer(name: String): Player = Player(name, 0)
+    }
+}
