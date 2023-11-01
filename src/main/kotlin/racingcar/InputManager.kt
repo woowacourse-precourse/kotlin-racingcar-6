31be87cecda3074f8564d3773bb0c2c97e7a0756
Jbrochan <hyeongchanj@gmail.com>
2023-11-01 15:03:53 +0900

    feat: 경주 자동차 이름 입력 유효성 검사 기능 구현
    - 사용자가 입력한 경주 자동차의 이름이 6자 이상일 경우 예외 처리

--- src/main/kotlin/racingcar/InputManager.kt
+++ src/main/kotlin/racingcar/InputManager.kt
@@ -15,11 +15,21 @@ class InputManager {
 
     // 입력된 경주 자동차 이름을 기반으로 경주 자동차 이름 리스트 반환
     fun getRacingCarNameList(): List<String> {
-        return inputRacingCarName().split(",")
+        return checkRacingCarNameValidation(inputRacingCarName().split(","))
     }
 
     // 입력된 경주 시도 횟수를 정수형으로 반환
     fun getTrial(): Int {
         return inputTrial().toInt()
+    }
+
+    // 경주 자동차 이름 길이에 대한 유효성 검사
+    private fun checkRacingCarNameValidation(racingCarNameList: List<String>): List<String> {
+        for (racingCarName in racingCarNameList) {
+            if (racingCarName.length >= 6) {
+                throw IllegalArgumentException("이름 길이 제한 오류 발생")
+            }
+        }
+        return racingCarNameList
     }
 }
