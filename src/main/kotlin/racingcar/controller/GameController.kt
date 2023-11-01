2d9cce7b7460c13deb2910148e9f96c0f36c6ac6
sh1mj1 <7wlgns@gmail.com>
2023-11-01 21:55:04 +0900

    refactor(GameController): inject dependency of view
    * InputView 와 OutputView 를 DI 하도록 변경한다.
    * 이로써 GameController 테스트를 더 쉽게 할 수 있다.

--- src/main/kotlin/racingcar/controller/GameController.kt
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -5,13 +5,11 @@ import racingcar.model.Cars
 import racingcar.view.InputView
 import racingcar.view.OutputView
 
-class GameController {
+class GameController(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView(),
+) {
 
-    // view
-    private val inputView = InputView()
-    private val outputView = OutputView()
-
-    // model
     private lateinit var cars: Cars
 
     fun startGame() {
