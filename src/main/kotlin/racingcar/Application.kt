2c28de8606f11fa960a6d823b6aba3005dd09d61
Jtility <leejewon97@gmail.com>
2023-11-01 16:58:48 +0900

    refactor(printFinalWinner): change name to finalWinner and return type
    for apart feature and test function
    winner will not null

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -11,13 +11,13 @@ fun main() {
     invalidTryCountCheck(tryCount)
     val racingCars = racingCarNames.map { RacingCar(it) }
     race(racingCars, tryCount)
-    printFinalWinner(racingCars)
+    print(finalWinner(racingCars))
 }
 
-fun printFinalWinner(racingCars: List<RacingCar>) {
-    val winner = racingCars.maxByOrNull { it.mileage }
-    val winnerNames = racingCars.filter { it.mileage == winner?.mileage }.map { it.name }
-    println("최종 우승자 : ${winnerNames.joinToString(", ")}")
+fun finalWinner(racingCars: List<RacingCar>): String {
+    val winner = racingCars.maxBy { it.mileage }
+    val winnerNames = racingCars.filter { it.mileage == winner.mileage }.map { it.name }
+    return "최종 우승자 : ${winnerNames.joinToString(", ")}"
 }
 
 fun race(racingCars: List<RacingCar>, tryCount: Int?) {
