faa2f3b194d1cc18619b0162ddf1aaa5bdf85c11
eunyeong <feijoaplz@gmail.com>
2023-11-01 09:43:18 +0900

    style: 문자열 const val 상수로 변경

--- src/main/kotlin/racingcar/Run.kt
+++ src/main/kotlin/racingcar/Run.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 
 const val INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 const val INPUT_ATTEMPT_TIMES_MESSAGE = "시도할 횟수는 몇 회인가요?"
+
 class Run {
     fun inputData(){
         println(INPUT_CAR_NAMES_MESSAGE)
