9717be86611841b98690803f30ec781c7bab6be8
EJ-pro <hsshss2522@naver.com>
2023-11-01 13:45:43 +0900

    feat(game): 무작위 이동 로직 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -24,6 +24,12 @@ class Car(val name: String) {
         println()
     }
 }
+fun Car.move() {
+    val randomNumber = Randoms.pickNumberInRange(0, 9)
+    if (randomNumber >= 4) {
+        position++
+    }
+}
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
