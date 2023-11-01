b23333ee31a0b1f0bf304de7f91362b468d40062
seonghyun <skyladder8895@gmail.com>
2023-11-01 14:14:06 +0900

    feat: validate user input for name length 5

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -13,7 +13,11 @@ fun main() {
 
 fun getCarNames(): String {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    return Console.readLine()
+    val userInput = Console.readLine()
+    if (userInput.length > 5) {
+        throw IllegalArgumentException("이름은 5자 이하만 가능합니다.")
+    }
+    return userInput
 }
 
 fun getRounds(): Int {
