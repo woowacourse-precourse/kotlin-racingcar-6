cc96b759a270cea15e7f97b5fbfd8b03887c9223
이홍진\iit80 <hong80714@gmail.com>
2023-11-01 20:15:49 +0900

    [feat] Validations.kt 예외처리 추가 자동차의 이름이 비어있을 때, 중복일 때, 한대의 이름만 입력받았을 때에 대한 오류처리

--- src/main/kotlin/racingcar/Constants.kt
+++ src/main/kotlin/racingcar/Constants.kt
@@ -5,6 +5,9 @@ object Constants {
     const val OVER_INPUT_ALARM = "5글자 이하로 이름을 지어주세요"
     const val ROUND_INPUT = "시도할 횟수는 몇 회인가요?"
     const val NOT_POSITIVE_INTEGER_ALARM = "자연수로 입력해주세요"
+    const val EMPTY_INPUT_NAME_ALARM = "자동차 이름이 비어 있습니다."
+    const val DUPLICATION_NAME_ALARM = "중복된 자동차 이름이 있습니다."
+    const val AT_LEAST_TWO_CAR_NEED_ALARM = "최소 2대 이상의 자동차가 필요합니다."
     const val EXECUTION_RESULT = "\n실행 결과"
     const val POSITION_MARK = "-"
     const val CAR_NAMES_SPLITER = ','
--- src/main/kotlin/racingcar/functions/Validations.kt
+++ src/main/kotlin/racingcar/functions/Validations.kt
@@ -7,6 +7,15 @@ fun validateCarNames(cars: List<Cars>) {
     if (cars.any { it.name.length > Constants.NAME_LENGTH_RULE }) {
         throw IllegalArgumentException(Constants.OVER_INPUT_ALARM)
     }
+    if (cars.any { it.name.isBlank() }) {
+        throw IllegalArgumentException(Constants.EMPTY_INPUT_NAME_ALARM)
+    }
+    if (cars.map { it.name }.distinct().size != cars.size) {
+        throw IllegalArgumentException(Constants.DUPLICATION_NAME_ALARM)
+    }
+    if (cars.size <= 1) {
+        throw IllegalArgumentException(Constants.AT_LEAST_TWO_CAR_NEED_ALARM)
+    }
 }
 
 fun validateRaceRound(raceRound: Int) {
