8701704e1c9dfcfb6e529dc8d58bd2e633ff139e
minuxx <minux8905@gmail.com>
2023-11-01 10:33:08 +0900

    feat(Car): 전진을 시도하는 메서드 추가

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,4 +1,16 @@
 package racingcar
 
-class Car(val name: String, val forwardCount: Int = 0) {
+import camp.nextstep.edu.missionutils.Randoms
+
+const val START_RANDOM_NUMBER = 0
+const val END_RANDOM_NUMBER = 9
+const val CAN_MOVE_FORWARD_NUMBER = 4
+
+class Car(val name: String, var forwardCount: Int = 0) {
+    fun tryMoveForward() {
+        val randomNumber = Randoms.pickNumberInRange(START_RANDOM_NUMBER, END_RANDOM_NUMBER)
+        if (randomNumber >= CAN_MOVE_FORWARD_NUMBER) {
+            forwardCount += 1
+        }
+    }
 }
