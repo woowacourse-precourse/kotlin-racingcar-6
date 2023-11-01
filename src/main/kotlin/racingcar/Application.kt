fcd363f790ceb1b200362b5e67d8abdefe91cbf9
iiolo <alohalo98@naver.com>
2023-11-01 18:47:56 +0900

    feat: 1번 기능에서 공백 유효성 검사 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -13,7 +13,9 @@ data class Car(
 
 fun getCarNames(): String {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    return Console.readLine()
+    val input = Console.readLine()
+    if (input.isBlank()) throw IllegalArgumentException("공백만 입력 불가")
+    return input
 }
 
 fun getValidCarNamesArray(carNames: String): Collection<Car> {
