277a50ef41183eff11552964d6c7045647f1e68e
pengcon <vpdzjs@gmail.com>
2023-11-01 21:21:57 +0900

    feat : 전진 결과 출력 기능 구현

--- src/main/kotlin/racingcar/Result.kt
+++ src/main/kotlin/racingcar/Result.kt
@@ -1,6 +1,16 @@
 package racingcar
 
 class Result {
+
+    fun printProgress(carNames : List<String>,carProgressList : MutableList<Int>) {
+        for (i in carNames.indices){
+            print(carNames[i]+" : ")
+            drawProgress(carProgressList[i])
+            println("")
+        }
+        println("")
+    }
+
     fun drawProgress(progress : Int){
         for (i in 0..<progress){
             print("-")
