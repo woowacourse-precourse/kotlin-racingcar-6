9d0d675c7a0dfe2f2131be0c1789ca13f4a0278b
acecic82 <102938al@naver.com>
2023-11-01 20:14:24 +0900

    feat (PlayGame): gamePlay를 위한 인터페이스 구현 및 ReadMe 파일 상태 업데이트
    인터페이스 구현 및 ReadMe 파일 상태 업데이트
    - 게임 시작, 끝, 진행을 위한 인터페이스 구현
    - ReadMe.md 파일에 상태 업데이트
    
    게임 플레이를 구현하기 위해 필요한 인터페이스 틀을 구현하였습니다.

---
+++ src/main/kotlin/service/PlayGame.kt
@@ -0,0 +1,14 @@
+package service
+
+interface PlayGame {
+    // 게임 셋팅
+    fun initGame()
+    // 플레이어 값 초기화
+    fun initPlayers(names: List<String>)
+    // 게임의 전체 진행을 위한 메서드
+    fun playGame()
+    // 게임의 각 라운드를 위한 메서드
+    fun playRound()
+    // 게임 종료시 호출될 메서드
+    fun endGame(winnerCount: Int)
+}
