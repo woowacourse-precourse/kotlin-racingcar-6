4df0233cc904bea4e58cc5abe4f987a8524145a9
JJongmen <pjy90123@gmail.com>
2023-11-01 13:15:13 +0900

    feat(Player): 시도 횟수 입력

--- src/main/kotlin/racingcar/ui/Player.kt
+++ src/main/kotlin/racingcar/ui/Player.kt
@@ -5,6 +5,20 @@ import camp.nextstep.edu.missionutils.Console
 class Player {
     fun inputCarNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-        return Console.readLine()?.split(",") ?: throw IllegalArgumentException("입력값이 없습니다.")
+        return inputWithNullCheck().split(",")
     }
+
+    fun inputTryCount(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        val tryCount = inputIntWithNullCheck()
+        if (tryCount < 1) {
+            throw IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.")
+        }
+        return tryCount
+    }
+
+    private fun inputWithNullCheck() = Console.readLine() ?: throw IllegalArgumentException("입력값이 없습니다.")
+
+    private fun inputIntWithNullCheck() =
+        inputWithNullCheck().toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닙니다.")
 }
