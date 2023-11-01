dffbb7307ac4144ff1dba371581289f4750ea8da
kkosang <87695921+kkosang@users.noreply.github.com>
2023-11-01 18:38:22 +0900

    test: 자동차 이름 길이 테스트

--- src/test/kotlin/racingcar/InputCarNameTest.kt
+++ src/test/kotlin/racingcar/InputCarNameTest.kt
@@ -12,4 +12,13 @@ class InputCarNameTest {
         val validatedNames = inputValidation.validateCarNames(names)
         Assertions.assertEquals(names, validatedNames)
     }
+
+    @Test
+    fun `자동차_이름_리스트_길이_검증`() {
+        val inputValidation = InputValidation()
+        val names = listOf("자동차1", "자동차2", "자동차3", "자동차4444")
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            inputValidation.validateCarNames(names)
+        }
+    }
 }
