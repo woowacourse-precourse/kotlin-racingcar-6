b0da4b8da2eac54ba0b1e3a981a532530ccf5a15
wonjongin <wonggamggik@gmail.com>
2023-11-01 11:10:13 +0900

    재실행 - InputMachine.tryCount() 작성 및 테스트 완료 제출 오류 파악 위해 되집어보는 과정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,14 +3,5 @@ package racingcar
 fun main() {
 
     val machine = InputMachine()
-    val carsList : List<String> = machine.makeCars()
-
-    val carsClass : MutableList<Car> = mutableListOf()
-
-    // car 선언
-    for (car in carsList) {
-        carsClass.add(Car(car))
-    }
-
-    machine.checkCarNameUnder5(carsClass)
+    val tryCount = machine.tryMove()
 }
