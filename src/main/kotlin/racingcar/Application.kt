64e4d473c05ae29e2ffc5987acb7b487893ffcc8
HeewonP825 <7hwon@naver.com>
2023-11-01 09:27:08 +0900

    Feat(application.kt): add main

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,20 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
+
+    print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ")
+    val carNames = readLine()?.split(",")?.map { it.trim() }
+
+    print("시도할 횟수는 몇 회인가요?: ")
+    val tryCount = readLine()?.toIntOrNull()
+
+    if (carNames == null || tryCount == null || carNames.isEmpty()) {
+        println("입력값이 올바르지 않습니다.")
+        return
+    }
+
+    val cars = carNames.map { Car(it) }
+    val racingGame = RacingGame(cars, tryCount)
+    racingGame.playGame()
 }
+
