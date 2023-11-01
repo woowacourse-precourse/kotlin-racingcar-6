30602fda6cfc77115dd35490df7c194eb0c18632
lee989898 <wnsrud1352@gmail.com>
2023-11-01 18:54:01 +0900

    [feat] 게임 시작하는 메서드에 시도 횟수 입력하라는 메서드 추가

--- src/main/kotlin/racingcar/controller/Controller.kt
+++ src/main/kotlin/racingcar/controller/Controller.kt
@@ -14,6 +14,7 @@ class Controller(
     fun startGame() {
         OutputView.enterNamesOfCars()
         setRacingCars(getNamesOfCar())
+        OutputView.howManyAttempts()
     }
 
     private fun getNamesOfCar(): List<String> {
