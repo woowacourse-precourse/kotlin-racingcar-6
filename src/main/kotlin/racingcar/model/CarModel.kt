77eb19693cf659d1fcf9faa204a82b16b8614d80
cucumber99 <dlsguq1@gmail.com>
2023-11-01 21:29:22 +0900

    feat: 난수 생성 기능 분리

--- src/main/kotlin/racingcar/model/CarModel.kt
+++ src/main/kotlin/racingcar/model/CarModel.kt
@@ -1,8 +1,7 @@
 package racingcar.model
-import camp.nextstep.edu.missionutils.Randoms
 
 class CarModel(val name: String, var position: Int = 0) {
     fun move() {
-        if(Randoms.pickNumberInRange(0,9) >= 4) position++
+        if(Generator.numberGenerator() >= 4) position++
     }
 }
