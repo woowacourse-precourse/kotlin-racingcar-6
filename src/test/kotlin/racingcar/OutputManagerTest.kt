b32bc32a6e4ab5b9ebc86853b4a5fb698be39d70
aprilgom <aprilgom@users.noreply.github.com>
2023-11-01 17:18:32 +0900

    test($OutputManager): test printGameStatus when normal car list

--- src/test/kotlin/racingcar/OutputManagerTest.kt
+++ src/test/kotlin/racingcar/OutputManagerTest.kt
@@ -8,4 +8,13 @@ class OutputManagerTest {
         val list = listOf("a","b","c").map {Car(it)}
         OutputManager.printWinners(list)
     }
+
+    @Test
+    fun `정상 리스트일때 printGameStatus 테스트` (){
+        val list = listOf("a","b","c").map {Car(it)}
+        repeat(3){
+            list.forEach{it.goForwardOrStop()}
+        }
+        OutputManager.printGameStatus(list)
+    }
 }
