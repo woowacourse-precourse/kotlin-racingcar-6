3846c4d7ce01b1d6654f91fdb3f7f6bd21e285c9
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-01 11:40:11 +0900

    refactor: 입력 메시지 관리 객체 변경

--- src/main/kotlin/Controller.kt
+++ src/main/kotlin/Controller.kt
@@ -1,22 +1,22 @@
+import InputValue.INPUT_CAR_MESSAGE
+import InputValue.INPUT_ROUND_MESSAGE
 import Validator.nameCheck
 import Validator.roundCheck
 import camp.nextstep.edu.missionutils.Console
 import car.Car
 import car.CarFactory.makeCar
-import race.RacingSystemValues.CAR_INPUT_MESSAGE
-import race.RacingSystemValues.TRIES_REQUEST_MESSAGE
 import java.math.BigInteger
 
 object Controller {
     fun inputCarInformation(): List<Car> {
-        println(CAR_INPUT_MESSAGE)
+        println(INPUT_CAR_MESSAGE)
         val input = Console.readLine()
         nameCheck(input)
         return makeCar(input)
     }
 
     fun inputRacingRoundInformation(): BigInteger {
-        println(TRIES_REQUEST_MESSAGE)
+        println(INPUT_ROUND_MESSAGE)
         val input = Console.readLine()
         roundCheck(input)
         return input.toBigInteger()
