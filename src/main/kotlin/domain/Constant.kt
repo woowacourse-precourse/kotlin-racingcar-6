147b7a671b58ba90175a3ac67015eb0118d6cf3d
pearhyunjin <pearhyunjin@gmail.com>
2023-11-01 12:19:11 +0900

    feat: write essential constant in constant file write basic essential text, num and notation

---
+++ src/main/kotlin/domain/Constant.kt
@@ -0,0 +1,15 @@
+package domain
+
+object Constant {
+    const val INPUT_CAR_NAME_TEXT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val INPUT_TRY_NUM_TEXT = "시도할 횟수는 몇 회인가요?"
+    const val RESULT_TEXT = "실행 결과"
+    const val FINAL_CHAMPION = "최송 우승자 : "
+
+    const val FORWARD_NOTATION = "-"
+    const val FORWARD_CONDITION_MIN = 0
+    const val FORWARD_CONDITION_MAX = 9
+    const val FORWARD_CONDITION_STANDARD = 4
+    const val CAR_NAME_MAX = 5
+    const val NAME_DIVISION_NOTATION = ","
+}
