c20eeaeb100c208df31fd705c26c39c17103fd52
seonghyun <skyladder8895@gmail.com>
2023-11-01 23:17:51 +0900

    feat: declare constants for threshold

--- src/main/kotlin/racingcar/racingcar.kt
+++ src/main/kotlin/racingcar/racingcar.kt
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms
 class Car(val name: String) {
     var position: Int = 0
 
+    companion object {
+        const val ADVANCE_THRESHOLD = 4
+    }
     fun printCarData() {
         print("$name : ")
         repeat(position) {
@@ -13,7 +16,7 @@ class Car(val name: String) {
         println("")
     }
     fun advancePosition () {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+        if (Randoms.pickNumberInRange(0, 9) >= ADVANCE_THRESHOLD) {
             position++
         }
     }
