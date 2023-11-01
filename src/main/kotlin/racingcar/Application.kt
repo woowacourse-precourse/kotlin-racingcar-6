ff8706c232b9a33772a61714d25496616f86a87c
hyun <sarahs19991115@gmail.com>
2023-11-01 22:15:55 +0900

    자동차 목록 중복 검사 추가 + 반복할 입력 횟수 입력 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,19 +6,27 @@ fun main() {
     var racingCarNames: List<String>
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     racingCarNames = getRacingCarNames()
+    println("시도할 횟수는 몇 회인가요?")
+    val trialCount: Int = Console.readLine().toInt()
 
+    println("실행 결과")
+    repeat(trialCount){
+
+    }
 }
 
 fun getRacingCarNames(): List<String> {
     val allRacingCarNames: String = Console.readLine().replace("\\s".toRegex(), "")
+    val result: List<String> = allRacingCarNames.split(",").distinct()
+
     require(isOnlyBlankValidation(allRacingCarNames) ||
-            isOnlyOneValidation(allRacingCarNames)) {
+            isOnlyOneValidation(result.toString())) {
         "경주할 자동차 이름을 두 대 이상 입력하세요"
     }
-    var result: List<String> = allRacingCarNames.split(",")
     require(isNameLongerThanFiveValidation(result)) {
         "자동차 이름은 5자 이하만 가능합니다"
     }
+
     return result
 }
 
