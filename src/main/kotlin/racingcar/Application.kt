39a1eb4b31bb143ee80482b469c5c0a00d371bbb
hyun <sarahs19991115@gmail.com>
2023-11-01 23:41:49 +0900

    매직 넘버와 문자열 처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,16 +3,33 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
+val MIN_VALUE: Int = 0
+val MAX_VALUE: Int = 9
+
+val START_MESSAGE: String = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+val TRIAL_MESSAGE: String = "시도할 횟수는 몇 회인가요?"
+val RESULT_MESSAGE: String = "실행 결과"
+val WINNER_MESSAGE: String = "최종 우승자 :"
+
+val NOT_NUMBER_ERROR_MESSAGE: String = "숫자를 입력해주세요"
+val LACK_OF_CARS_ERROR_MESSAGE: String = "경주할 자동차 이름을 두 대 이상 입력하세요"
+val TOO_LONG_CAR_NAME_ERROR_MESSAGE: String = "자동차 이름은 5자 이하만 가능합니다"
+
 fun main() {
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    startGame()
+}
+
+fun startGame() {
+
+    println(START_MESSAGE)
     var CarNames: List<String> = getCarNames()
-    println("시도할 횟수는 몇 회인가요?")
+    println(TRIAL_MESSAGE)
     val trialCount: Int? = Console.readLine().toIntOrNull()
-    require(!isNumberTrialCount(trialCount)){
-        "숫자를 입력해주세요"
+    require(isNumberTrialCountValidation(trialCount)){
+        NOT_NUMBER_ERROR_MESSAGE
     }
 
-    println("실행 결과")
+    println(RESULT_MESSAGE)
     var moveResult: MutableList<String> = MutableList(CarNames.size) { "" }
     repeat(trialCount!!){
         moveResult = runOneRound(CarNames, moveResult)
@@ -21,7 +38,7 @@ fun main() {
     }
 
     val winnerNames: String = getWinnerNames(CarNames, moveResult)
-    println("최종 우승자 : $winnerNames")
+    println("$WINNER_MESSAGE $winnerNames")
 }
 
 fun getCarNames(): List<String> {
@@ -30,10 +47,10 @@ fun getCarNames(): List<String> {
 
     require(!isOnlyBlankValidation(allRacingCarNames) &&
             !isOnlyOneValidation(result.toString())) {
-        "경주할 자동차 이름을 두 대 이상 입력하세요"
+        LACK_OF_CARS_ERROR_MESSAGE
     }
-    require(!isNameLongerThanFiveValidation(result)) {
-        "자동차 이름은 5자 이하만 가능합니다"
+    require(isNameLengthBelowFiveValidation(result)) {
+        TOO_LONG_CAR_NAME_ERROR_MESSAGE
     }
 
     return result
@@ -47,17 +64,15 @@ fun isOnlyOneValidation(pureString: String): Boolean {
     return pureString.split(",").size == 1
 }
 
-fun isNameLongerThanFiveValidation(nameList: List<String>): Boolean {
+fun isNameLengthBelowFiveValidation(nameList: List<String>): Boolean {
     return nameList.all{it.length<=5}
 }
 
-fun isNumberTrialCount(trialCount: Int?) : Boolean {
+fun isNumberTrialCountValidation(trialCount: Int?) : Boolean {
     return trialCount != null
 }
 
 fun runOneRound(CarNames: List<String>, moveResult: MutableList<String>): MutableList<String> {
-    val MIN_VALUE: Int = 0
-    val MAX_VALUE: Int = 9
     for(index in CarNames.indices) {
         val moveOrNot = Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE)
         if(moveOrNot >= 4)
