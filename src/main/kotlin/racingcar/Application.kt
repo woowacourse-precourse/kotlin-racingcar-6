1a005a3eddb3dda3059142379df2ddcf36d44bf6
eunsxx <88652027+eunsxx@users.noreply.github.com>
2023-11-01 13:32:56 +0900

    feat : Determine the condition of moving
    Function with move condition true when the value is 4 or more based on a random value from 0 to 9

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,17 +1,34 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+import org.assertj.core.util.Maps
+
 
 fun main() {
     // TODO : 자동차 이름 입력받기 (,로 구분)
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    var input: String = readLine() ?: throw IllegalArgumentException()
-    var cars = input.split(',')
+    val input: String = readLine() ?: throw IllegalArgumentException()
+    val cars = input.split(',')
+    val carMap = cars.associateWith { 0 }.toMutableMap()
 
-    println(cars)
-
+    println(carMap)
+    val keys = carMap.keys
     // TODO : 시도할 횟수 입력받기
     println("시도할 횟수는 몇 회인가요?")
-    var tries: Int = readLine()?.toInt() ?: throw IllegalArgumentException()
+    val tries: Int = readLine()?.toInt() ?: throw IllegalArgumentException()
 
     println(tries)
+
+
+    // TODO : 최종결과 출력하는 함수 만들기
 }
+
+fun moveCondition(): Boolean {
+    // TODO : 전진 조건 판별하는 함수 만들기
+    val num = Randoms.pickNumberInRange(0, 9)
+    println(num)
+
+    if (num < 4) return false
+    else return true
+}
+
