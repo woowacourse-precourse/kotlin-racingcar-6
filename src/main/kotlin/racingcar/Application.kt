ced1a43104fdc69d74fe28b4840deadd4c541e3f
EJ-pro <hsshss2522@naver.com>
2023-11-01 13:41:41 +0900

    chore(init): 자동차 경주 게임 초기 설정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,20 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNames = Console.readLine()
+
+    println("시도할 횟수는 몇 회인가요?")
+    val tryCount = Console.readLine().toIntOrNull()
+
+    println("\n실행 결과")
+
+    require(carNames != null && tryCount != null && tryCount > 0) { throw IllegalArgumentException("잘못된 입력입니다.")}
+
+    val carNameList = carNames.split(",")
+
 }
+
