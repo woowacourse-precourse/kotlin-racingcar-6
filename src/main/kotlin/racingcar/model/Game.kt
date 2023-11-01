daa366c94aab9b4f3ace5ff318b58d37750127a4
kimmandoo <mingyu5675@gmail.com>
2023-11-01 17:40:30 +0900

    refactor: 불필요한 import 제거

--- src/main/kotlin/racingcar/model/Game.kt
+++ src/main/kotlin/racingcar/model/Game.kt
@@ -1,7 +1,6 @@
 package racingcar.model
 
 import camp.nextstep.edu.missionutils.Randoms
-import racingcar.controller.InputVerification
 
 class Game(private val user: List<String>) {
     private val scoreMap = mutableMapOf<String, Int>()
