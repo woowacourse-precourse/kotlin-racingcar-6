5bf9f6d36ce30b027e2d17bebe6c465ed5d8cdaa
YuBeen-Park <park_ubin@naver.com>
2023-11-01 15:56:34 +0900

    [feat] 입력 받은 자동차 리스트에서 이름 중복 검사 구현

--- src/main/kotlin/racingcar/UserInput.kt
+++ src/main/kotlin/racingcar/UserInput.kt
@@ -10,13 +10,14 @@ class UserInput {
     fun getNameInput(): List<RacingCar> {
         println(Constants.NAME_INPUT_MESSAGE)
         val userInput = Console.readLine()
-        return when {
-            userInput.isNullOrBlank() -> throw IllegalArgumentException()
-            else -> userInput.split(',').map {
-                it.toRacingCar()
-            }
-        }
+        require(userInput.isNullOrBlank())
+        val carList = userInput.split(',').map { it.toRacingCar() }
+        require(hasDuplication(carList))
+        return carList
     }
+
+    private fun hasDuplication(list: List<RacingCar>): Boolean =
+        list.size != list.distinctBy { car -> car.carName }.size
 
     fun getTrialInput(): Int {
         println(Constants.TRIAL_INPUT_MESSAGE)
