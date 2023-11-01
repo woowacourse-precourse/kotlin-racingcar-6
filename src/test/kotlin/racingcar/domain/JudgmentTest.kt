50fe83d4f4fe907376a48e2d239e34069a735442
bak <bak4017@gmail.com>
2023-11-01 13:03:20 +0900

    refactor(racingcar): 테스트 중복제거

--- src/test/kotlin/racingcar/domain/JudgmentTest.kt
+++ src/test/kotlin/racingcar/domain/JudgmentTest.kt
@@ -5,16 +5,16 @@ import org.junit.jupiter.api.Test
 import racingcar.constant.Constants.MIN_FORWARD_THRESHOLD
 
 class JudgmentTest {
+    private val judgment = Judgment()
+
     @Test
     fun `난수가 경계값 이상일 때 전진 가능`() {
-        val judgment = Judgment()
         assertTrue(judgment.canMoveForward(MIN_FORWARD_THRESHOLD))
         assertTrue(judgment.canMoveForward(MIN_FORWARD_THRESHOLD + 1))
     }
 
     @Test
     fun `난수가 경계값 미만일 때 전진 불가능`() {
-        val judgment = Judgment()
         assertFalse(judgment.canMoveForward(MIN_FORWARD_THRESHOLD - 1))
     }
 }
