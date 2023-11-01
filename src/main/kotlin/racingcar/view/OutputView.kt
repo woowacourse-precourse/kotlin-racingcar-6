50bae537e1861460cba3be1c1b58954d7f622441
sseung416 <choiseungyeon416@gmail.com>
2023-11-01 13:26:48 +0900

    refactor: OutputView의 message format 상수 이름 변경 (~Format -> ~Template)

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -25,29 +25,30 @@ class OutputView {
     }
 
     fun printWinner(winners: List<CarName>) {
-        val winnersMessage = Message.WinnerFormat.toString().format(formatWinner(winners))
+        val winnersMessage = Message.WinnerTemplate.toString().format(formatWinner(winners))
         print(winnersMessage)
     }
 
     private fun formatRaceResults(carNameAndScoreList: List<Pair<CarName, Score>>): String =
         carNameAndScoreList.joinToString("\n") { (carName, score) ->
-            Message.RaceResultFormat.toString().format(carName, formatScore(score))
+            Message.RaceResultTemplate.toString().format(carName, formatScore(score))
         }
 
     private fun formatScore(score: Score): String = ONE_STEP.repeat(score)
 
-    private fun formatWinner(names: List<CarName>): String = names.joinToString(", ")
+    private fun formatWinner(names: List<CarName>): String = names.joinToString(WINNER_SEPARATOR)
 
     companion object {
         private const val ONE_STEP = "-"
+        private const val WINNER_SEPARATOR = ", "
     }
 
     private enum class Message(private val message: String) {
         RaceCarNames("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
         NumberOfRound("시도할 횟수는 몇 회인가요?"),
         RaceResult("실행 결과"),
-        WinnerFormat("최종 우승자 : %s"),
-        RaceResultFormat("%s : %s");
+        WinnerTemplate("최종 우승자 : %s"),
+        RaceResultTemplate("%s : %s");
 
         override fun toString() = message
     }
