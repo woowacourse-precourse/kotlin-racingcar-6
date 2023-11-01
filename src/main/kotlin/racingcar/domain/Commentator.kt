313bf97d9d4723855be727e763778079f67ccc82
Wonhui Roh <rwhui012@gmail.com>
2023-11-01 16:26:54 +0900

    refactor(racingcar): finished changing the static data in the `Commentator` class to constant values.

--- src/main/kotlin/racingcar/domain/Commentator.kt
+++ src/main/kotlin/racingcar/domain/Commentator.kt
@@ -1,11 +1,13 @@
 package racingcar.domain
 
+import racingcar.res.GuideMessage.ANNOUNCE_THE_WINNERS
+import racingcar.res.GuideMessage.SHOW_EXECUTION_RESULT
 import racingcar.utils.getRandomNumber
 
 class Commentator(private val cars: List<Car>, private val numberOfMoves: Int) {
   fun commentRace() {
     println()
-    println("실행 결과")
+    println(SHOW_EXECUTION_RESULT)
 
     repeat(numberOfMoves) {
       for (car in cars) {
@@ -20,7 +22,7 @@ class Commentator(private val cars: List<Car>, private val numberOfMoves: Int) {
     val maxRecord = cars.map{ it.record }.maxOf { it.length }
     val nameOfWinners = cars.filter { it.record.length == maxRecord }.map { it.carName }
 
-    print("최종 우승자 : ")
+    print(ANNOUNCE_THE_WINNERS)
     println(nameOfWinners.joinToString(", "))
   }
 }
