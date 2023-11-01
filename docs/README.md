c42086e027b23898586712797c81ce9e16dc7b20
dabom <dabomsong71@pukyong.ac.kr>
2023-11-01 15:42:40 +0900

    fix(Exeption) exeption for input name

--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,2 @@
 1. Create Car Class - readine, ","split -> tolist
+2. main 코드 작성(시작 문구)
--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -8,7 +8,34 @@ data class Car(val name: String, var distance: Int = 0) {
             distance++
         }
     }
-fun main() {
+    override fun toString(): String {
+        return "$name : ${"-".repeat(distance)}"
+    }
+}
+fun race(cars: List<Car>, rounds: Int) {
+    repeat(rounds) {
+        cars.forEach { it.move() }
+        cars.forEach { println(it) }
+        println()
+    }
 
+    val maxDistance = cars.maxOf { it.distance }
+    val winners = cars.filter { it.distance == maxDistance }
+
+    println("최종 우승자 : ${winners.joinToString { it.name }}")
+}
+fun main() {
+    println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)")
+    val carNames = Console.readLine().split(",")
+
+    if (carNames.any { it.length > 5 || it.any { it.isDigit() } }) {
+        throw IllegalArgumentException()
+    }
+
+    println("시도할 횟수는 몇 회인가요?")
+    val rounds = Console.readLine().toInt()
+
+    val cars = carNames.map { Car(it) }
+    race(cars, rounds)
 }
 
