74a4e709afa54f1c475ac42562d14cde3fc20999
s1lv3rrud <lek8575@naver.com>
2023-11-01 23:37:10 +0900

    feat(allocRandom): 자동차 랜덤 번호 부여

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,15 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
+fun allocRandom(carsNum: Int): Array<Int> {
+    var randomNum = Array(carsNum) { 0 }
+    for (i: Int in 0..carsNum - 1) {
+        randomNum[i] = Randoms.pickNumberInRange(0, 9)
+    }
+    return randomNum
+}
+
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     var cars = readLine()
