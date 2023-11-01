939180e8a468b4d0c0e2d4e521fd76a919a02076
gaeun <geun5744@gmail.com>
2023-11-01 16:24:22 +0900

    [TEST(CarsTest)] 우승자 목록 출력 테스트 구현

--- src/test/kotlin/racingcar/domain/CarsTest.kt
+++ src/test/kotlin/racingcar/domain/CarsTest.kt
@@ -1,5 +1,7 @@
 package racingcar.domain
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -25,6 +27,24 @@ class CarsTest {
             assertEquals(car.advanceState, 0)
         }
 
+    }
+
+    @Test
+    fun `우승자 목록 출력 테스트`() {
+        assertRandomNumberInRangeTest(
+            {
+                cars.moveCars()
+                assertThat(cars.getWinnerList())
+                    .contains("가은")
+
+            }, MOVING_FORWARD, STOP, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP
+
+        )
+    }
+
+    companion object {
+        private const val MOVING_FORWARD = 4
+        private const val STOP = 3
     }
 
 }
