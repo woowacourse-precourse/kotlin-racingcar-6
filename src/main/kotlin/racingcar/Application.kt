8675a97b12a4fb6809032dfeace0fff4a448cd15
hyun <sarahs19991115@gmail.com>
2023-11-01 23:12:16 +0900

    우승자 출력 기능 구현 + 시도 횟수 숫자 확인 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,25 +7,32 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     var CarNames: List<String> = getCarNames()
     println("시도할 횟수는 몇 회인가요?")
-    val trialCount: Int = Console.readLine().toInt()
+    val trialCount: Int? = Console.readLine().toIntOrNull()
+    require(!isNumberTrialCount(trialCount)){
+        "숫자를 입력해주세요"
+    }
 
     println("실행 결과")
     var moveResult: MutableList<String> = MutableList(CarNames.size) { "" }
-    repeat(trialCount){
+    repeat(trialCount!!){
         moveResult = runOneRound(CarNames, moveResult)
         printOneRound(CarNames, moveResult)
+        println("")
     }
+
+    val winnerNames: String = getWinnerNames(CarNames, moveResult)
+    println("최종 우승자 : $winnerNames")
 }
 
 fun getCarNames(): List<String> {
     val allRacingCarNames: String = Console.readLine().replace("\\s".toRegex(), "")
     val result: List<String> = allRacingCarNames.split(",").distinct()
 
-    require(isOnlyBlankValidation(allRacingCarNames) ||
-            isOnlyOneValidation(result.toString())) {
+    require(!isOnlyBlankValidation(allRacingCarNames) &&
+            !isOnlyOneValidation(result.toString())) {
         "경주할 자동차 이름을 두 대 이상 입력하세요"
     }
-    require(isNameLongerThanFiveValidation(result)) {
+    require(!isNameLongerThanFiveValidation(result)) {
         "자동차 이름은 5자 이하만 가능합니다"
     }
 
@@ -44,6 +51,9 @@ fun isNameLongerThanFiveValidation(nameList: List<String>): Boolean {
     return nameList.all{it.length<=5}
 }
 
+fun isNumberTrialCount(trialCount: Int?) : Boolean {
+    return trialCount != null
+}
 
 fun runOneRound(CarNames: List<String>, moveResult: MutableList<String>): MutableList<String> {
     val MIN_VALUE: Int = 0
@@ -59,4 +69,19 @@ fun runOneRound(CarNames: List<String>, moveResult: MutableList<String>): Mutabl
 fun printOneRound(CarNames: List<String>, moveResult: MutableList<String>) {
     for(index in CarNames.indices)
         println("${CarNames[index]} : ${moveResult[index]}")
+}
+
+
+fun getWinnerNames(carNames: List<String>, moveResult: MutableList<String>): String {
+    val countOfMove = moveResult.map { it.length }
+    val maxMoveLength = countOfMove.maxOrNull()
+    val winnerList = mutableListOf<String>()
+
+    for ((index, length) in countOfMove.withIndex()) {
+        if (length == maxMoveLength) {
+            winnerList.add(carNames[index])
+        }
+    }
+
+    return winnerList.joinToString(", ")
 }
