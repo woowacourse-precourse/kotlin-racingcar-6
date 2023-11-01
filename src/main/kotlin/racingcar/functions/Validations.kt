a41e96cc6db2eadccb0abd9e6a21ee4e5e159964
이홍진\iit80 <hong80714@gmail.com>
2023-11-01 20:32:26 +0900

    [refactor] 예외처리 함수 기능별로 분리 이름에 대한 예외처리를 하는 validateCarNames 함수에 대해 기능을 하나만 처리하도록 하위 기능들을 함수로 분리

--- src/main/kotlin/racingcar/functions/Validations.kt
+++ src/main/kotlin/racingcar/functions/Validations.kt
@@ -4,18 +4,10 @@ import racingcar.Cars
 import racingcar.Constants
 
 fun validateCarNames(cars: List<Cars>) {
-    if (cars.any { it.name.length > Constants.NAME_LENGTH_RULE }) {
-        throw IllegalArgumentException(Constants.OVER_INPUT_ALARM)
-    }
-    if (cars.any { it.name.isBlank() }) {
-        throw IllegalArgumentException(Constants.EMPTY_INPUT_NAME_ALARM)
-    }
-    if (cars.map { it.name }.distinct().size != cars.size) {
-        throw IllegalArgumentException(Constants.DUPLICATION_NAME_ALARM)
-    }
-    if (cars.size <= 1) {
-        throw IllegalArgumentException(Constants.AT_LEAST_TWO_CAR_NEED_ALARM)
-    }
+    validateCarNameLength(cars)
+    validateEmptyCarNames(cars)
+    validateDuplicateCarNames(cars)
+    validateMinimumCarNumber(cars)
 }
 
 fun validateRaceRound(raceRound: Int) {
@@ -23,3 +15,28 @@ fun validateRaceRound(raceRound: Int) {
         throw IllegalArgumentException(Constants.NOT_POSITIVE_INTEGER_ALARM)
     }
 }
+
+fun validateCarNameLength(cars: List<Cars>) {
+    if (cars.any { it.name.length > Constants.NAME_LENGTH_RULE }) {
+        throw IllegalArgumentException(Constants.OVER_INPUT_ALARM)
+    }
+}
+
+fun validateEmptyCarNames(cars: List<Cars>) {
+    if (cars.any { it.name.isBlank() }) {
+        throw IllegalArgumentException(Constants.EMPTY_INPUT_NAME_ALARM)
+    }
+}
+
+fun validateDuplicateCarNames(cars: List<Cars>) {
+    if (cars.map { it.name }.distinct().size != cars.size) {
+        throw IllegalArgumentException(Constants.DUPLICATION_NAME_ALARM)
+    }
+}
+
+fun validateMinimumCarNumber(cars: List<Cars>) {
+    if (cars.size <= 1) {
+        throw IllegalArgumentException(Constants.AT_LEAST_TWO_CAR_NEED_ALARM)
+    }
+}
+
