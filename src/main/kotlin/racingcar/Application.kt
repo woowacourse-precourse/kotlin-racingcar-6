d066404fb5c2b60c0113a1e7159553b2688d65bb
gillyongs <khs20010327@gmail.com>
2023-11-01 22:44:46 +0900

    feat: 사용자 입력값 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,7 @@
+// Application.kt
+
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
+    RacingGame().start()
 }
---
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -0,0 +1,15 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+class RacingGame {
+    fun start() {
+        print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ")
+        val carNames = readCarNames()
+        print("시도할 횟수는 몇 회인가요?: ")
+        val numberOfTries = readNumberOfTries()
+
+    }
+
+}
