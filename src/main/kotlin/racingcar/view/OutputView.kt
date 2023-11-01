776f93a535c42b5ebaed2c462e1044b2db6f9f8c
lee989898 <wnsrud1352@gmail.com>
2023-11-01 17:18:37 +0900

    [feat] 실행 결과 문구 출력 메서드 구현 및 원래 있던 executionResult -> executionCarResult로 메서드 명 변경

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -6,8 +6,10 @@ object OutputView {
     private const val COLON = ":"
     private const val COMMA = ","
     private const val GAP = " "
+    private const val LINE_BREAK = "\n"
     private const val ENTER_NAMES_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     private const val HOW_MANY_ATTEMPTS = "시도할 횟수는 몇 회인가요?"
+    private const val EXECUTION_RESULT = "실행 결과"
     private const val FINAL_WINNER = "최종 우승자"
 
     fun enterNamesOfCars() {
@@ -18,10 +20,15 @@ object OutputView {
         println(HOW_MANY_ATTEMPTS)
     }
 
-    fun executionResult(cars: List<Car>) {
+    fun executionResult() {
+        println("$LINE_BREAK$EXECUTION_RESULT")
+    }
+
+    fun executionCarResult(cars: List<Car>) {
         for (car in cars) {
             println("${car.name} $COLON ${car.currentPosition}")
         }
+        println()
     }
 
     fun finalWinner(winners: List<String>) {
