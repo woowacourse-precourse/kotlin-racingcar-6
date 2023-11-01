beac492f09db84f907d9731cf0f7a7eaf8a46744
정채훈 <mrchjung@peopleinnet.com>
2023-11-01 15:16:11 +0900

    feat : 자동차 이름을 입력받아 Car 객체 생성하기

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -5,7 +5,8 @@ class Game {
     fun start() {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val inputString = Console.readLine()
-        val namesList = inputString.split(",").map{ it.trim() }
+        val names = inputString.split(",").map{ it.trim() }
+        val cars: List<Car> = names.map{ Car(it) }
     }
 }
 
