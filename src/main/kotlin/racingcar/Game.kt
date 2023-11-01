aa81b7d4df686c66f6ef5489b9daf343c23e22b1
정채훈 <mrchjung@peopleinnet.com>
2023-11-01 16:38:29 +0900

    style : kotlin coding convention 에 맞게 sytle 수정

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -7,21 +7,22 @@ class Game {
     fun start() {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val inputString = Console.readLine()
-        val names = inputString.split(",").map{ it.trim() }
-        val cars: List<Car> = names.map{ Car(it) }
+        val names = inputString.split(",").map { it.trim() }
+        val cars: List<Car> = names.map { Car(it) }
 
     }
 }
 
 fun List<Car>.toStatusString(): String {
     val progressCharacter = "-"
-    return this.map { "${it.name} : ${progressCharacter.repeat(it.location)}"}.joinToString("\n")
+    return this.map { "${it.name} : ${progressCharacter.repeat(it.location)}" }.joinToString("\n")
 }
 
 class Car(val name: String) {
     init {
         if (name.length > 5) throw IllegalArgumentException("5자 이상 이름 필요")
     }
+
     var location: Int = 0
     fun randMove(): Unit {
         if (Randoms.pickNumberInRange(0, 9) >= 4) location++
