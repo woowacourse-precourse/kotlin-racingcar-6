1ed329f16ea2f8c9d4df082ff034ae33477688c8
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-01 10:09:26 +0900

    Feat: 자동차 이름 유효성 검사(자동차 이름이 공란일 때)

--- src/main/kotlin/racingcar/Validation.kt
+++ src/main/kotlin/racingcar/Validation.kt
@@ -1,28 +1,29 @@
 package racingcar
 
-import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 class Validation {
 
-    private fun isValidNumberOfCars(carsNameList: List<Car>) {
+    fun isValidNumberOfCars(carsNameList: List<Car>) {
         if (carsNameList.size < 2) {
             throw IllegalArgumentException("자동차 이름은 2대 이상 입력해 주세요.")
         }
     }
 
-    private fun checkContainsSpace(carsName: String) {
+    fun checkContainsSpace(carsName: String) {
         if (carsName.contains(" ")) {
             throw IllegalArgumentException("자동차 이름은 쉼표(,)로 구분해 주세요.")
         }
     }
 
-    private fun checkCarNameDuplication(carsNameList: List<Car>) {
-        if (carsNameList.distinct().size != carsNameList.size) {
+    fun checkCarNameDuplication(carsNameList: List<Car>) {
+        val names = carsNameList.map { it.name }
+        if (names.distinct().size != names.size) {
             throw IllegalArgumentException("중복된 이름은 작성할 수 없습니다.")
         }
     }
 
-    private fun checkCarNameLength(carsNameList: List<Car>) {
+    fun checkCarNameLength(carsNameList: List<Car>) {
         carsNameList.forEach {
             if (it.name.length > 5) {
                 throw IllegalArgumentException("자동차 이름은 5자 이하만 입력 가능합니다.")
@@ -30,9 +31,16 @@ class Validation {
         }
     }
 
+    fun checkCarNameBlank(carsNameList: List<Car>) {
+        if (carsNameList.any { it.name == "" }) {
+            throw IllegalArgumentException("자동차 이름은 공백일 수 없습니다.")
+        }
+    }
+
     fun isValidCarName(carsName: String, carList: List<Car>) {
         checkContainsSpace(carsName)
         isValidNumberOfCars(carList)
+        checkCarNameBlank(carList)
         checkCarNameDuplication(carList)
         checkCarNameLength(carList)
     }
