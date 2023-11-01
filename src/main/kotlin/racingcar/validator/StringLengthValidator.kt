422e0444c3041cdde2f7df938b64f9c2bb53ccaf
murjune <sis9221302@gmail.com>
2023-11-01 19:19:34 +0900

    feat(StringLengthValidator) : 공백 문자열 검사 추가

--- src/main/kotlin/racingcar/validator/StringLengthValidator.kt
+++ src/main/kotlin/racingcar/validator/StringLengthValidator.kt
@@ -5,16 +5,20 @@ class StringLengthValidator() : StringValidator {
     override fun validate(target: String) {
         if (target
                 .split(DELIMITER)
-                .map { it.trim() }
+                .any { it.isBlank() }
+        ) throw IllegalArgumentException(BLANK_ERROR_MESSAGE)
+        if (target
+                .split(DELIMITER)
                 .all { it.length in MIN_LENGTH..MAX_LENGTH }
                 .not()
-        ) throw IllegalArgumentException(ERROR_MESSAGE)
+        ) throw IllegalArgumentException(LENGTH_ERROR_MESSAGE)
     }
 
     companion object {
         private const val DELIMITER = ","
         private const val MIN_LENGTH = 1
         private const val MAX_LENGTH = 5
-        private const val ERROR_MESSAGE = "문자열의 길이가 1글자 이상 5글자 이하여야 합니다"
+        private const val LENGTH_ERROR_MESSAGE = "문자열의 길이가 1글자 이상 5글자 이하여야 합니다"
+        private const val BLANK_ERROR_MESSAGE = "공백 문자열은 유효하지 않습니다"
     }
 }
