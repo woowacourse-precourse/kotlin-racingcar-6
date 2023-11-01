534bc84c7b3c86251f4829ab4ff05137153a2342
SeungGu Kang <alex9366@naver.com>
2023-11-01 15:54:38 +0900

    feat: 값읍 입력받고 유효한 값인지 확인하는 클래스 생성

---
+++ src/main/kotlin/racingcar/InputView.kt
@@ -0,0 +1,40 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+    companion object ErrorMessage {
+        const val NULL: String = "빈 값 입니다."
+        const val CONTAIN_ZERO = "0은 포함될 수 없습니다."
+        const val NUMERIC = "숫자만 입력이 가능합니다."
+        const val FIVE_LETTER = "이름은 5자리 이하만 입력이 가능합니다."
+    }
+
+    fun inputName(): List<String> {
+        val list = Console.readLine().split(',').map { it.trim() }
+        checkNameFormat(list)
+        return list
+    }
+
+    fun inputCount(): Int {
+        val count = Console.readLine()
+        checkCount(count)
+        return count.toInt()
+    }
+
+    private fun checkCount(count: String) {
+        if(!isNumeric(count)) { throw IllegalArgumentException(NUMERIC) }
+        if(isZero(count)) { throw IllegalArgumentException(CONTAIN_ZERO) }
+    }
+    private fun isNumeric(count: String): Boolean = count.toIntOrNull() != null
+    private fun isZero(count: String): Boolean = count.toInt() == 0
+
+    private fun checkNameFormat(list: List<String>) {
+        for(i in list) {
+            if(!isFiveLetter(i)) { throw IllegalArgumentException(FIVE_LETTER) }
+            if(isBlank(i)) { throw IllegalArgumentException(NULL)}
+        }
+    }
+    private fun isFiveLetter(str: String): Boolean = str.length <= 5
+    private fun isBlank(str: String): Boolean = str.isBlank()
+}
