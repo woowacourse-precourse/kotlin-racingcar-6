db0a2737dd2fa5798c574bac5e2094de1ca28427
murjune <sis9221302@gmail.com>
2023-11-01 19:31:34 +0900

    fix(NumericStringValidator)  : validate 로직 함수화

--- src/main/kotlin/racingcar/validator/NumericStringValidator.kt
+++ src/main/kotlin/racingcar/validator/NumericStringValidator.kt
@@ -1,13 +1,14 @@
 package racingcar.validator
 
 class NumericStringValidator() : StringValidator {
-    override fun validate(target: String) {
-        if (target == ZERO) throw IllegalArgumentException(ZERO_ERROR_MESSAGE)
-        if (target
-                .all { it.isDigit() }
-                .not()
-        ) throw IllegalArgumentException(NUMERIC_ERROR_MESSAGE)
+    override fun validate(target: String) = when {
+        isNotNumeric(target) -> throw IllegalArgumentException(NUMERIC_ERROR_MESSAGE)
+        isZero(target) -> throw IllegalArgumentException(ZERO_ERROR_MESSAGE)
+        else -> Unit
     }
+
+    private fun isZero(target: String) = target == ZERO
+    private fun isNotNumeric(target: String) = target.all { it.isDigit() }.not()
 
     companion object {
         private const val ZERO = "0"
