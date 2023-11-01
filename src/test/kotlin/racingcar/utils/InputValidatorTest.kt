be9480f93a057b68178394cea65997a4d49b0229
junyong <junyong008@gmail.com>
2023-11-01 13:56:07 +0900

    test: InputValidator 테스트 코드 수정

--- src/test/kotlin/racingcar/utils/InputValidatorTest.kt
+++ src/test/kotlin/racingcar/utils/InputValidatorTest.kt
@@ -5,58 +5,53 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.Test
 import racingcar.game.Car
-import racingcar.utils.TypeConverter.stringToInt
-import racingcar.utils.TypeConverter.stringToCarList
 import racingcar.utils.InputValidator.validateInputCarNames
 import racingcar.utils.InputValidator.validateInputRoundCount
 
 class InputValidatorTest {
     @Test
     fun `자동차 목록 입력 검증 - 정상 입력`() {
-        val input = stringToCarList("jun,pobi,wow")
+        val input = listOf(Car("jun"), Car("pobi"), Car("wow"))
         validateInputCarNames(input)
         assertEquals(input, listOf(Car("jun"), Car("pobi"), Car("wow")))
     }
 
     @Test
     fun `자동차 목록 입력 검증 - 빈 값 예외 처리`() {
-        assertThrows<IllegalArgumentException> { validateInputCarNames(stringToCarList("")) }
+        val input = listOf(Car(""))
+        assertThrows<IllegalArgumentException> { validateInputCarNames(input) }
     }
 
     @Test
     fun `자동차 목록 입력 검증 - 이름 길이 예외 처리`() {
-        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames(stringToCarList("hi,wow,junyong,name")) }
+        val input = listOf(Car("hi"), Car("wow"), Car("junyong"), Car("name"))
+        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames(input) }
         assertThat(exception.message).contains("junyong")
     }
 
     @Test
     fun `자동차 목록 입력 검증 - 중복 이름 예외 처리`() {
-        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames(stringToCarList("hi,wow,nice,wow")) }
+        val input = listOf(Car("hi"), Car("wow"), Car("nice"), Car("wow"))
+        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames(input) }
         assertThat(exception.message).contains("wow")
     }
 
     @Test
     fun `자동차 목록 입력 검증 - 공백 포함 이름 예외 처리`() {
-        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames(stringToCarList("hi,wow,j y")) }
+        val input = listOf(Car("hi"), Car("wow"), Car("j y"))
+        val exception = assertThrows<IllegalArgumentException> { validateInputCarNames(input) }
         assertThat(exception.message).contains("j y")
     }
 
     @Test
     fun `이동할 횟수 입력 검증 - 정상 입력`() {
-        val input = stringToInt("10")
+        val input = 10
         validateInputRoundCount(input)
         assertEquals(input, 10)
     }
 
     @Test
-    fun `이동할 횟수 입력 검증 - 빈 값 예외 처리`() =
-        assertThrows<IllegalArgumentException> { validateInputRoundCount(stringToInt("")) }
-
-    @Test
-    fun `이동할 횟수 입력 검증 - 숫자 외 입력 예외 처리`() =
-        assertThrows<IllegalArgumentException> { validateInputRoundCount(stringToInt("hello")) }
-
-    @Test
-    fun `이동할 횟수 입력 검증 - 1회 미만 예외 처리`() =
-        assertThrows<IllegalArgumentException> { validateInputRoundCount(stringToInt("0")) }
+    fun `이동할 횟수 입력 검증 - 1회 미만 예외 처리`() {
+        assertThrows<IllegalArgumentException> { validateInputRoundCount(0) }
+    }
 }
