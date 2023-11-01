09c5486bb0c625b091b18f7a7f7ebb92bd08597d
gaeun <geun5744@gmail.com>
2023-11-01 16:29:01 +0900

    [TEST(Validation)] 이름 길이 체크 메서드 테스트

--- src/test/kotlin/racingcar/domain/ValidationTest.kt
+++ src/test/kotlin/racingcar/domain/ValidationTest.kt
@@ -3,6 +3,7 @@ package racingcar.domain
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import racingcar.domain.Validation.checkContainComma
+import racingcar.domain.Validation.checkNameLength
 import java.lang.IllegalArgumentException
 
 class ValidationTest {
@@ -12,6 +13,14 @@ class ValidationTest {
         val input = "가은 나은 다은"
         assertThrows<IllegalArgumentException> {
             checkContainComma(input)
+        }
+    }
+
+    @Test
+    fun `이름 길이 체크 메서드 테스트`() {
+        val input = "가은,나은,여섯글자이름".split(",")
+        assertThrows<IllegalArgumentException> {
+            checkNameLength(input)
         }
     }
 
