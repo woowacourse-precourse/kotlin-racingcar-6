15824416a19426ce55505c7977c4dee3392f29e9
JJongmen <pjy90123@gmail.com>
2023-11-01 11:26:59 +0900

    refactor(RacingCar, CarName): 차의 이름을 CarName으로 분리
    기존 RacingCar에서의 이름 검증을 CarName에게 위임
    RacingCarTest에서 하던 이름 테스트를 CarNameTest로 이동

--- src/main/kotlin/racingcar/domain/RacingCar.kt
+++ src/main/kotlin/racingcar/domain/RacingCar.kt
@@ -1,13 +1,7 @@
 package racingcar.domain
 
 class RacingCar(name: String, private var position: Int = 0) {
-    private val name: String
-
-    init {
-        val trimmedName = name.trim()
-        require(trimmedName.length in MIN_NAME_LENGTH..MAX_NAME_LENGTH) { "자동차 이름은 ${MIN_NAME_LENGTH}자 이상 ${MAX_NAME_LENGTH}자 이하만 가능합니다." }
-        this.name = trimmedName
-    }
+    private val name: CarName = CarName(name)
 
     fun move(randomNumber: Int) {
         if (randomNumber >= 4) {
@@ -18,9 +12,4 @@ class RacingCar(name: String, private var position: Int = 0) {
     fun getPosition(): Int = position
 
     override fun toString() = "$name : ${"-".repeat(position)}"
-
-    companion object {
-        private const val MIN_NAME_LENGTH = 1
-        private const val MAX_NAME_LENGTH = 5
-    }
 }
--- src/test/kotlin/racingcar/domain/RacingCarTest.kt
+++ src/test/kotlin/racingcar/domain/RacingCarTest.kt
@@ -3,32 +3,11 @@ package racingcar.domain
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
 
 class RacingCarTest {
     @Test
-    fun `생성실패_이름이 1자 미만임`() {
-        assertThrows<IllegalArgumentException> { RacingCar("") }
-    }
-
-    @Test
-    fun `생성실패_이름이 5자를 초과함`() {
-        assertThrows<IllegalArgumentException> { RacingCar("Kennen") }
-    }
-
-    @Test
-    fun `생성실패_공백 제거 후 이름이 1자 미만임`() {
-        assertThrows<IllegalArgumentException> { RacingCar("   ") }
-    }
-
-    @Test
     fun 생성성공() {
         assertDoesNotThrow { RacingCar("pobi") }
-    }
-
-    @Test
-    fun `생성성공_공백 제거 후 4자가 됨`() {
-        assertDoesNotThrow { RacingCar("  pobi  ") }
     }
 
     @Test
