9ca5f378565e27a14f61ec33a43fca7e8bfab2c3
전형선 <hsun5163@gmail.com>
2023-11-01 16:30:02 +0900

    refactor : 공백 추가.

--- src/main/kotlin/racingcar/util/GameMessage.kt
+++ src/main/kotlin/racingcar/util/GameMessage.kt
@@ -5,5 +5,5 @@ object GameMessage {
     const val INPUT_ATTEMPT_COUNT = "시도할 횟수는 몇 회인가요?"
 
     const val OUTPUT_GAME_RESULT = "실행 결과"
-    const val OUTPUT_GAME_WINNER = "최종 우승자 :"
+    const val OUTPUT_GAME_WINNER = "최종 우승자 : "
 }
