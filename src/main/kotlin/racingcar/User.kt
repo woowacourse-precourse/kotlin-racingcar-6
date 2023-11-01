68fd846af3aa90e51eb4feb25baa735f8fd5aafb
sea <sea@seas-MacBook-Pro.local>
2023-11-01 18:31:03 +0900

    사용자 시도할 횟수 입력받는 기능 추가

--- src/main/kotlin/racingcar/User.kt
+++ src/main/kotlin/racingcar/User.kt
@@ -2,6 +2,7 @@ import camp.nextstep.edu.missionutils.Console
 
 class User {
     var carNameList: List<String> = emptyList<String>()
+    var tryCount: Int = 0
 
     fun inputCarNames() {
         print("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)")
@@ -11,6 +12,11 @@ class User {
 
     fun separateCommas(names: String) {
         val carNameList = names.split(",").toList()
+    }
+
+    fun inputTryCount() {
+        print("시도할 횟수는 몇 회인가요?")
+        this.tryCount = Console.readLine().toInt()
     }
 
 }
