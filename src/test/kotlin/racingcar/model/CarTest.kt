0d3633b4a30c0fe6535594bf70d8e29a5acfb54e
sh1mj1 <7wlgns@gmail.com>
2023-11-01 20:46:51 +0900

    refactor(CarTest): refactor from multiple tests to the method using @ParameterizedTest
    * 여러 테스트 메서드를 하나의 @ParameterizedTest 로 리팩토링한다.

--- src/test/kotlin/racingcar/model/CarTest.kt
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -2,10 +2,9 @@ package racingcar.model
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.api.function.Executable
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import racingcar.config.ExceptionMessage.INVALID_CAR_NAME
 import racingcar.config.ExceptionMessage.TOO_LONG_NAME
 
@@ -13,69 +12,44 @@ class CarTest {
 
     private lateinit var car: Car
 
-    @BeforeEach
-    fun setUp() {
-        car = Car(name = "car")
-    }
-
-    @Test
-    fun `Car 이름이 empty 이면 예외를 던진다`() {
-        car = Car("")
-        assertThrows<IllegalArgumentException>(
-            INVALID_CAR_NAME
-        ) {
+    @ParameterizedTest
+    @ValueSource(strings = ["", " ", "\n"])
+    fun `Car 이름 blank 입력 예외 테스트`(input: String) {
+        car = Car(input)
+        assertThrows<IllegalArgumentException>(INVALID_CAR_NAME) {
             car.validateCar()
         }
     }
 
-    @Test
-    fun `Car 이름이 공백으로만 이루어졌다면 예외를 던진다`() {
-        car = Car(" ")
-        assertThrows<IllegalArgumentException>(
-            INVALID_CAR_NAME
-        ) {
+    @ParameterizedTest
+    @ValueSource(strings = ["abcdef", "123456", "000000"])
+    fun `Car 이름이 5자 초과라면 예외를 던진다`(input: String) {
+        car = Car(input)
+        assertThrows<IllegalArgumentException>(TOO_LONG_NAME) {
             car.validateCar()
         }
     }
 
-    @Test
-    fun `Car 이름이 개행으로만 이루어졌다면 예외를 던진다`() {
-        car = Car("\n")
-        assertThrows<IllegalArgumentException>(
-            INVALID_CAR_NAME
-        ) {
-            car.validateCar()
-        }
+    @ParameterizedTest
+    @ValueSource(strings = ["abcd", "1234", "0000"])
+    fun `Car 이름이 5자 이하 notBlank 이면 정상 실행된다`(input: String) {
+        car = Car(input)
+        assertDoesNotThrow { car.validateCar() }
     }
 
-    @Test
-    fun `Car 이름이 5자 초과라면 예외를 던진다`() {
-        car = Car("abcdef")
-        assertThrows<IllegalArgumentException>(
-            TOO_LONG_NAME
-        ) {
-            car.validateCar()
-        }
-    }
-
-    @Test
-    fun `Car 이름이 5자 이하 notBlank 이면 정상 실행된다`() {
-        car = Car("abcde")
-        val executable = Executable { car.validateCar() }
-        assertDoesNotThrow(executable)
-    }
-
-    @Test
-    fun `생성한 랜덤 수가 4 이상이면 앞으로 나아간다`() {
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6])
+    fun `생성한 랜덤 수가 4 이상이면 앞으로 나아간다`(randomNumber: Int) {
         car = Car(name = "car", position = 0)
-        car.moveForward(NumberGeneratorTest(4))
+        car.moveForward(NumberGeneratorTest(randomNumber))
         Assertions.assertThat(car.position).isEqualTo(1)
     }
 
-    @Test
-    fun `생성한 랜덤 수가 3 이하이면 앞으로 나아가지 않는다`() {
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun `생성한 랜덤 수가 3 이하이면 앞으로 나아가지 않는다`(randomNumber: Int) {
         car = Car(name = "car", position = 0)
-        car.moveForward(NumberGeneratorTest(3))
+        car.moveForward(NumberGeneratorTest(randomNumber))
         Assertions.assertThat(car.position).isEqualTo(0)
     }
 
