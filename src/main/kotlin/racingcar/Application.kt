6b60e531a35109870c6b999b24b399f5e8cfad46
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 22:25:10 +0900

    feat: 자동차 경주 게임 동작 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,14 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
     val user = User()
-//    val list: List<String> = user.getCarNames()
-//    print(list)
+
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val list: List<String> = user.getCarNames()
+
+    println("시도할 횟수는 몇 회인가요?")
     val count: Int = user.getTryCount()
-    print(count)
+
+    val gameManager: GameManager = GameManager(list, count)
+    gameManager.raceStart()
 }
