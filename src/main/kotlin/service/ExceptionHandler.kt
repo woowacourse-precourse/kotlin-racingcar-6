56ec27e0c13c594ef166567afadd85e7ad3a3515
2kjin <lkjin1202@gmail.com>
2023-11-01 18:03:20 +0900

    ♻️ refactor : 예외 사항 추가 및 IllegalArgumentException 발생 후 종료 추가 구현

--- src/main/kotlin/service/ExceptionHandler.kt
+++ src/main/kotlin/service/ExceptionHandler.kt
@@ -1,32 +1,58 @@
 package service
 
 object ExceptionHandler {
+    // 자동차 이름 입력 시 알파벳과 쉼표만 입력했는지 확인
     fun checkNames(names: String) : Boolean {
-        try {
-            for (i in names) {
-                require(i in 'A'..'Z'|| i in 'a'..'z' || i == ',') { Message.ERROR.toString() }
+        for (i in names) {
+            if (!(i in 'A'..'Z' || i in 'a'..'z' || i == ',')) {
+                throw IllegalArgumentException("[ERROR]")
             }
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-            return false
         }
         return true
     }
 
+    // 이름의 길이가 5자 이하인지 확인
     fun checkNameLength(name: String) {
-        try {
-            require(name.length <= 5) { Message.ERROR.toString() }
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
+        if (name.length > 5) {
+            throw IllegalArgumentException("[ERROR]")
         }
     }
 
+    // 시도할 회수에 숫자가 들어왔는지 확인
     fun checkValidCount(count: String): Boolean {
-        try {
-            requireNotNull(count.toIntOrNull()) { Message.ERROR.toString() }
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-            return false
+        val intValue = count.toIntOrNull()
+        if (intValue == null) {
+            throw IllegalArgumentException("[ERROR]")
+        }
+        return true
+    }
+
+    // 입력값이 없는지 확인
+    fun checkNotEmpty(value: String): Boolean {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("[ERROR]")
+        }
+        return true
+    }
+
+    // 중복된 이름 확인
+    fun checkDuplicateNames(names: String): Boolean {
+        val nameList = names.split(",")
+        val distinctNames = nameList.distinct()
+        if (nameList.size != distinctNames.size) {
+            throw IllegalArgumentException("[ERROR]")
+        }
+        return true
+    }
+
+    // 음수 확인
+    fun checkNonNegativeNumber(number: String): Boolean {
+        val intValue = number.toIntOrNull()
+        if (intValue == null) {
+            throw IllegalArgumentException("[ERROR]")
+        }
+        if (intValue < 0) {
+            throw IllegalArgumentException("[ERROR]")
         }
         return true
     }
