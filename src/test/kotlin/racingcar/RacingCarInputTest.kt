fc4439103433a51b6b5e96725936b3385fffd218
이빵주 <yungju9982@naver.com>
2023-11-01 21:45:49 +0900

    [test] 자동차 글자수 테스트코드 작성

--- src/test/kotlin/racingcar/RacingCarInputTest.kt
+++ src/test/kotlin/racingcar/RacingCarInputTest.kt
@@ -1,4 +1,21 @@
 package racingcar
 
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
 class RacingCarInputTest {
+
+    @Test
+    fun `입력된 자동차 이름이 5자 초과이면 예외가 발생한다`() {
+        // given
+        val racingCarName: List<Car> = listOf(
+            Car("abcdef"), Car("abcde")
+        )
+
+        //then
+        assertThatThrownBy {
+            RacingCarInput.validate(racingCarName)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("입력된 자동차 이름이 5자 초과입니다.")
+    }
 }
