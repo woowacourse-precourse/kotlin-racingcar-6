d49c954b8961f65f2c3719bcff6048b34d00bcf2
sh1mj1 <7wlgns@gmail.com>
2023-11-01 21:22:15 +0900

    refactor(CarsTest): refactor from multiple tests to the method using @ParameterizedTest
    * 여러 테스트 메서드를 하나의 @ParameterizedTest 로 리팩토링한다.
    * 리스트 입력으로 들어갈 데이터들을 companion object 를 사용하여 초기화한다.

--- src/test/kotlin/racingcar/model/CarsTest.kt
+++ src/test/kotlin/racingcar/model/CarsTest.kt
@@ -1,67 +1,89 @@
 package racingcar.model
 
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import racingcar.config.ExceptionMessage.DUPLICATED_NAME
+import java.util.stream.Stream
 
 class CarsTest {
-    private lateinit var cars: Cars
 
-    @Test
-    fun `중복된 이름이 있으면 예외를 던진다`() {
-        cars = Cars(listOf(Car("pobi"), Car("woni"), Car("jun"), Car("pobi")))
-        assertThrows<IllegalArgumentException>(
-            DUPLICATED_NAME
-        ) {
+    @ParameterizedTest
+    @MethodSource("provideDuplicatedCars")
+    fun `중복된 이름이 있으면 예외를 던진다`(cars: Cars) {
+        assertThrows<IllegalArgumentException>(DUPLICATED_NAME) {
             cars.validateDuplicate()
         }
     }
 
-    @Test
-    fun `중복된 이름이 없으면 테스트를 통과한다`() {
-        cars = Cars(listOf(Car("pobi"), Car("woni"), Car("jun")))
+    @ParameterizedTest
+    @MethodSource("provideUniqueCars")
+    fun `중복된 이름이 없으면 테스트를 통과한다`(cars: Cars) {
         assertDoesNotThrow { cars.validateDuplicate() }
     }
 
-    @Test
-    fun `cars 들 중 가장 큰 position 을 가진 car 가 하나이면 그 이름을 리턴한다`() {
-        cars = Cars(
-            listOf(
-                Car("pobi", position = 0),
-                Car("woni", position = 1),
-                Car("jun", position = 2)
-            )
-        )
-        val result = cars.decideWinner()
-        Assertions.assertThat(result).isEqualTo(listOf("jun"))
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    fun `가장 큰 position 을 가진 car 의 이름(들)을 리스트로 리턴한다`(data: TestData) {
+        val result = data.cars.decideWinner()
+        Assertions.assertThat(result).isEqualTo(data.expected)
     }
 
-    @Test
-    fun `cars 들 중 가장 큰 position 을 가진 car 가 여럿이면 그 이름들을 리턴한다`() {
-        cars = Cars(
-            listOf(
-                Car("pobi", position = 2),
-                Car("woni", position = 2),
-                Car("jun", position = 2)
+    companion object {
+        @JvmStatic
+        fun provideUniqueCars(): Stream<Cars> = Stream.of(
+            Cars(listOf(Car("pobi"), Car("woni"), Car("jun"))),
+            Cars(listOf(Car("pobi"), Car("woni"), Car("jun"), Car("sh1m"))),
+            Cars(listOf(Car("pobi")))
+        )
+
+        @JvmStatic
+        fun provideDuplicatedCars(): Stream<Cars> = Stream.of(
+            Cars(listOf(Car("pobi"), Car("pobi"))),
+            Cars(
+                listOf(Car("pobi"), Car("woni"), Car("jun"), Car("pobi"))
+            ),
+            Cars(
+                listOf(Car("pobi"), Car("woni"), Car("jun"), Car("jun"))
             )
         )
-        val result = cars.decideWinner()
-        Assertions.assertThat(result).isEqualTo(listOf("pobi", "woni", "jun"))
-    }
 
-    @Test
-    fun `cars 들의 position 이 모두 0 이면 모든 이름을 리턴한다`() {
-        cars = Cars(
-            listOf(
-                Car("pobi", position = 0),
-                Car("woni", position = 0),
-                Car("jun", position = 0)
+        @JvmStatic
+        fun provideTestData(): Stream<TestData> = Stream.of(
+            TestData(
+                Cars(
+                    listOf(
+                        Car("pobi", position = 0),
+                        Car("woni", position = 1),
+                        Car("jun", position = 2)
+                    )
+                ),
+                listOf("jun")
+            ),
+            TestData(
+                Cars(
+                    listOf(
+                        Car("pobi", position = 2),
+                        Car("woni", position = 2),
+                        Car("jun", position = 2)
+                    )
+                ),
+                listOf("pobi", "woni", "jun")
+            ),
+            TestData(
+                Cars(
+                    listOf(
+                        Car("pobi", position = 0),
+                        Car("woni", position = 0),
+                        Car("jun", position = 0)
+                    )
+                ),
+                listOf("pobi", "woni", "jun")
             )
         )
-        val result = cars.decideWinner()
-        Assertions.assertThat(result).isEqualTo(listOf("pobi", "woni", "jun"))
     }
-
 }
+
+data class TestData(val cars: Cars, val expected: List<String>)
