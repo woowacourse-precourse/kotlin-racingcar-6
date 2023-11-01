a754e530d64cae29bb62d99aa8b508430b5471c0
NOGUEN <nhg1113@naver.com>
2023-11-01 15:28:26 +0900

    feat: Car 모델 테스트 코드

---
+++ src/test/kotlin/test/CarTest.kt
@@ -0,0 +1,30 @@
+package test
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import racingcar.model.Car
+import racingcar.model.Name
+import racingcar.model.Position
+
+class CarTest {
+    @Test
+    fun `차 비교 테스트`() {
+        val car1 = Car(name = Name("hello"), position = Position())
+        val car2 = Car(name = Name("hello"), position = Position())
+        camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest(
+            {
+                car1.doRace()
+                car2.doRace()
+                Assertions.assertThat(car1 > car2)
+                    .isEqualTo(false)
+                car1.doRace()
+                car2.doRace()
+                Assertions.assertThat(car1 > car2)
+                    .isEqualTo(false)
+                Assertions.assertThat(car1.equals(car2))
+                    .isEqualTo(true)
+            },
+            1, 5, 5, 1
+        )
+    }
+}
