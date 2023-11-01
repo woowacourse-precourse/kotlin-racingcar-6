d85f52ea464654bfe10239dc78095abdce51cc11
lee989898 <wnsrud1352@gmail.com>
2023-11-01 18:53:24 +0900

    [feat] 레이싱에 참여하는 자동차들 추가하는 메서드 구현

--- src/main/kotlin/racingcar/controller/Controller.kt
+++ src/main/kotlin/racingcar/controller/Controller.kt
@@ -1,5 +1,7 @@
 package racingcar.controller
 
+import racingcar.car.Car
+import racingcar.car.CarImpl
 import racingcar.validator.Validator
 import racingcar.view.InputView
 import racingcar.view.OutputView
@@ -7,11 +9,20 @@ import racingcar.view.OutputView
 class Controller(
     private val validator: Validator,
 ) {
+    private val cars = ArrayList<Car>()
+
     fun startGame() {
         OutputView.enterNamesOfCars()
+        setRacingCars(getNamesOfCar())
     }
 
     private fun getNamesOfCar(): List<String> {
         return InputView.askCarToRace(validator)
+    }
+
+    private fun setRacingCars(namesOfCar: List<String>) {
+        for (name in namesOfCar) {
+            cars.add(CarImpl(name))
+        }
     }
 }
