37d9089d499f5cebcb9fb8530c93011c8550f81a
aprilgom <aprilgom@users.noreply.github.com>
2023-11-01 19:13:37 +0900

    fix($OutputManager): check input is not empty in getCarNames

--- src/main/kotlin/racingcar/InputManager.kt
+++ src/main/kotlin/racingcar/InputManager.kt
@@ -6,6 +6,7 @@ object InputManager {
     fun getCarNames() : List<String> {
         println(Resources.PLEASE_ENTER_THE_NAMES_OF_CARS)
         val input = Console.readLine()
+        require(input.isNotEmpty()) {"Wrong Car name! You must input something"}
         return input.split(",").map {
             require(it.length <= 5) {"Wrong Car name! Car name must be 5 characters or less"}
             it
