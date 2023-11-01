170e3c2981de02cd021334e8275d03625862a135
ls2hn <s2hnsu@gmail.com>
2023-11-01 19:55:15 +0900

    Refactor: locatio변수의 접근 제어자를 private으로 변경

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,7 +1,7 @@
 package racingcar
 
 class Car(private val objectName: String) { // 객체명이 printLocation()에서 사용되므로 받아 옴.
-    var location: Int = 0
+    private var location: Int = 0
 
     fun moveCar() {
         if ( goOrStop() ) {
