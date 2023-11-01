c826036895c3c661c494c75eb73bea3a7ed06c89
iiolo <alohalo98@naver.com>
2023-11-01 16:38:22 +0900

    feat: 1번 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,12 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+
+}
+
+fun getCarNames(){
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    Console.readLine()
 }
