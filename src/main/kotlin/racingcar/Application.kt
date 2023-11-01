48527cfeb59ede3bb3f3a02421fe16ece11d8481
yuhyun <dlugus@naver.com>
2023-11-01 22:55:00 +0900

    fix:carGoOrStop ->raceCars로 이름 변경 및 코드 수정+ 다른 함수들도 전반적인 내용 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,28 +2,26 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
-import racingcar.getTryNumber
-
 
 fun main() {
-    // TODO: 프로그램 구현
+    playCarRace()
 }
 
 fun playCarRace(){
-    while(true) {
-        val carName = getCarNames()
-        var tryNumberToPlayGame = getTryNumber()
-        
-        for (tryNumberToPlayGame) {
-            carGoOrStop()
-        }
-        }
+    val carNames = getCarNames()
+    val tryNumber = getTryNumber()
+
+    val raceResults = raceCars(carNames, tryNumber)
+    printRaceResults(raceResults)
+       
+    val winners = findWinners(raceResults)
+    printWinners(winners)    
     }
 
 
 
 fun getCarNames():List<String> {
-    print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ")
     val userInput = Console.readLine()
     val carNames = userInput?.split(",")?.map{it.trim()}
 
@@ -53,16 +51,41 @@ fun validateTryNumber(tryNumber: String) {
 
 
 
-fun carGoOrStop() {
-    val goOrStop = Randoms.pickNumberInRange() 
+fun raceCars(carNames: List<String>, tryNumber: Int): List<Pair<String, String>> {
+    val raceResults = mutableListOf<Pair<String, String>>()
+    
+    for (carName in carNames) {       
+        var result = ""
+        for (i in 1..tryNumber) {
+            val goOrStop = Randoms.pickNumberInRange(0, 9) 
+            if (goOrStop >= 4){
+                result += "-"
+            } 
+        }
+        raceResults.add(Pair(carName, result))
+    }
+    return raceResults
+}
 
-    if (goOrStop >= 4){
-        print("-")
+fun printRaceResults(raceResults: List<Pair<String, String>>) {
+    raceResults.forEach {(carName, result) ->
+        println("$carName : $result")
     }
 }
 
-fun printWinner(){
-    print("")
+fun findWinners(raceResults: List<Pair<String, String>>) : List<String> {
+    val maxPosition = raceResults.map{it.second.count { it =='-'}}.maxOrNull()
+
+    if (maxPosition == null) {
+        return emptyList()
+    }
+    return raceResults.filter { it.second.count{ it == '-'} == maxPosition}.map{it.first}
+}
+
+
+fun printWinners(winners: List<String){
+    val winnerString = if (winners.size>1) winners.joinToString(",")  else winners.first()
+    print("최종 우승자: $winnerString") 
 }
 
 
