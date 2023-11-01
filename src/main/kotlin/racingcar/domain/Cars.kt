f78e07ec41b7e98304e8e15ab06b30102ac15ff0
gaeun <geun5744@gmail.com>
2023-11-01 16:10:49 +0900

    [REFACTOR(Cars)] 변수 수정

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -7,12 +7,12 @@ class Cars(private val determineMove: DetermineMove) {
         get() = _carList
 
 
-    private val _carStateList = mutableListOf<Int>()
+    private val carStateList = mutableListOf<Int>()
 
     fun createCar(carNames: List<String>): List<Car> {
         carNames.forEach { name ->
             _carList.add(Car(name))
-            _carStateList.add(0)
+            carStateList.add(0)
         }
         return carList
     }
@@ -31,9 +31,9 @@ class Cars(private val determineMove: DetermineMove) {
 
     private fun getAdvanceStateList(): List<Int> {
         _carList.forEachIndexed { index, car ->
-            _carStateList[index] = car.advanceState
+            carStateList[index] = car.advanceState
         }
-        return _carStateList
+        return carStateList
     }
 
 }
