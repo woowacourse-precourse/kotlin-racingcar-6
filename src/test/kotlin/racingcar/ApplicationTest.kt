97b2f0c4ebaaa0c5f3d2164ffe7c27df68a4a5b8
pengcon <vpdzjs@gmail.com>
2023-11-01 23:30:35 +0900

    test : 테스트코드 초기값 롤백

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -8,9 +8,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
-    val car = Car()
-    val racing = Racing()
-
     @Test
     fun `전진 정지`() {
         assertRandomNumberInRangeTest(
@@ -26,55 +23,6 @@ class ApplicationTest : NsTest() {
     fun `이름에 대한 예외 처리`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
-        }
-    }
-
-    @Test
-    fun `이름이 다섯글자 이상인 경우 예외처리`() {
-        assertSimpleTest {
-            val carNames = listOf("qwer", "tyuiop", "asdf")
-            assertThrows<IllegalArgumentException> { car.validateCarNameLength(carNames) }
-        }
-    }
-
-
-    @Test
-    fun `이름이 중복인 경우 예외처리`() {
-        assertSimpleTest {
-            val carNames = listOf("qwer", "qwer", "tyu", "iop")
-            assertThrows<IllegalArgumentException> { car.validateDuplicateName(carNames) }
-        }
-    }
-
-
-    @Test
-    fun `이름이 공백인 경우 예외처리`() {
-        assertSimpleTest {
-            val carNames = listOf("qwer"," ","tyu","iop")
-            assertThrows<IllegalArgumentException> { car.validateBlankName(carNames) }
-        }
-    }
-
-    @Test
-    fun `이름이 공백이 포함된 경우 예외처리`() {
-        assertSimpleTest {
-            val carNames = listOf("qwer","a "," b","iop")
-            assertThrows<IllegalArgumentException> { car.validateNameHaveBlank(carNames) }
-        }
-    }
-    @Test
-    fun `이동 횟수가 0일 경우 예외처리`() {
-        assertSimpleTest {
-            val moveCount = listOf(0,3,6,9)
-            assertThrows<IllegalArgumentException>  { moveCount.forEach{ racing.validatePositiveInteger(it) } }
-        }
-    }
-
-    @Test
-    fun `이동 횟수가 정수가 아닐 경우 예외 처리`() {
-        assertSimpleTest {
-            val moveCount = listOf("ㅇ","2","","a","2.5")
-            assertThrows<IllegalArgumentException>  { moveCount.forEach{ racing.validateInteger(it) } }
         }
     }
 
