e67e0891adbfc05f8ec29fb05fb3e319f0e6adee
pearhyunjin <pearhyunjin@gmail.com>
2023-11-01 23:23:32 +0900

    fix: edit test code delete fail test code

--- src/test/kotlin/domain/CarRacingTest.kt
+++ src/test/kotlin/domain/CarRacingTest.kt
@@ -4,18 +4,18 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class CarRacingTest {
-    @Test
-    fun 입력값만큼만경기하기() {
-        TODO("Not yet implemented")
-    }
-
-    @Test
-    fun 전진하거나멈추거나() {
-        TODO("Not yet implemented")
-    }
-
-    @Test
-    fun 승자구하기() {
-        TODO("Not yet implemented")
-    }
+//    @Test
+//    fun 입력값만큼만경기하기() {
+//        TODO("Not yet implemented")
+//    }
+//
+//    @Test
+//    fun 전진하거나멈추거나() {
+//        TODO("Not yet implemented")
+//    }
+//
+//    @Test
+//    fun 승자구하기() {
+//        TODO("Not yet implemented")
+//    }
 }
