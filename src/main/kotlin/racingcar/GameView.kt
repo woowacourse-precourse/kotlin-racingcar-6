4b8746b3bca235690fa958f3669cdc1173715131
cucumber99 <dlsguq1@gmail.com>
2023-11-01 16:53:29 +0900

    feat: View 구현

--- src/main/kotlin/racingcar/GameView.kt
+++ src/main/kotlin/racingcar/GameView.kt
@@ -1,6 +1,16 @@
 package racingcar
+import racingcar.model.CarModel
 
 class GameView {
+    fun displayRaceStatus(cars: List<CarModel>) {
+        cars.forEach { car ->
+            println("${car.name} : ${"-".repeat(car.position)}")
+        }
+        println()
+    }
 
-
+    fun displayWinner(winners: List<CarModel>) {
+        val winnerNames = winners.map { it.name }.joinToString(", ")
+        println("최종 우승자 : $winnerNames")
+    }
 }
