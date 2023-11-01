dcdd838323aec9987654528c4e2c1711d8e7cdab
yujin37 <yj061628@gmail.com>
2023-11-01 16:06:13 +0900

    fix: 첫 메시지 오타 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -30,7 +30,7 @@ class RacingCarValidator {
     }
 }
 fun main() {
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val cars = Console.readLine()
     val racingCars = RacingCarValidator.instance.checkCars(cars)
     println("시도 할 횟수는 몇 회인가요?")
