7b2eac84000166f9e0a9ad7aa8ac72eae10278c4
윤동현 <donghyunyoon81@gmail.com>
2023-11-01 11:55:52 +0900

    feat: 라운드를 실행하는 기능 추가

--- src/main/kotlin/racingcar/controller/RoundManagement.kt
+++ src/main/kotlin/racingcar/controller/RoundManagement.kt
@@ -3,7 +3,7 @@ package racingcar.controller
 class RoundManagement(val roundCount:Int,val racingCarsManagement: RacingCarsManagement) {
 
     fun playRound(){
-
+        racingCarsManagement.cars.forEach { racingCarModel -> racingCarModel.tryToMove() }
     }
 
 }
