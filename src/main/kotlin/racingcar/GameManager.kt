4410f5f5e3ac2a0e0edac5b314ceec685447bdba
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 20:43:30 +0900

    feat: 자동차 경주를 진행하는 함수 생성
    시도 횟수만큼 자동차 경주를 진행하고 최종 결과까지 출력한다.
    각 자동차가 난수를 뽑아 전진을 시도하고, 매 라운드 결과를 보여주는
    함수와 최종 우승자를 찾아 출력하는 함수도 추가 구현 예정이다.

--- src/main/kotlin/racingcar/GameManager.kt
+++ src/main/kotlin/racingcar/GameManager.kt
@@ -9,10 +9,18 @@ class GameManager(
         createCarList(nameList)
     }
 
-    fun createCarList(nameList: List<String>) {
+    private fun createCarList(nameList: List<String>) {
         for (name in nameList) {
             this.carsList.add(Car(name, ""))
         }
     }
 
+    fun raceStart(){
+        println("실행 결과")
+        for (round in 0 until tryCount) {
+            // 각 자동차는 난수를 뽑아 전진을 시도
+            // 각 자동차의 이동 거리를 출력
+            println()
+        }
+    }
 }
