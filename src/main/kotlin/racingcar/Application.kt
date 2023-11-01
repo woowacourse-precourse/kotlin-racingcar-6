30d80528f0f42bc4f6acaeeb575d08c9132a7a16
jooyyoo <aliceyun9@naver.com>
2023-11-01 20:08:50 +0900

    [feat] 차가 움직여야하는 횟수를 저장하는 함수 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -16,9 +16,17 @@ private fun inputFirstSetting() {
     println(ConstValue.SECOND_LINE)
     val trial = Console.readLine().toString()
 
-    val numberOfTrial = validateTrialNumber(trial)
+    var numberOfTrial = validateTrialNumber(trial)
 
     println(ConstValue.RESULT)
+
+    val map = makeCarMap(carList)
+
+    do {
+        doSaveCarMove(carList, map)
+        numberOfTrial -= 1
+        println()
+    } while (numberOfTrial != 0)
 
 }
 
@@ -28,6 +36,14 @@ private fun makeCarList(carList: String): List<String> {
     } else {
         return carList.split(",")
     }
+}
+
+private fun makeCarMap(carList: List<String>): MutableMap<String, Int> {
+    val score: MutableMap<String, Int> = mutableMapOf()
+    for (car in carList) {
+        score[car] = 0
+    }
+    return score
 }
 
 private fun validateTrialNumber(num: String): Int {
@@ -45,5 +61,25 @@ private fun validateTrialNumber(num: String): Int {
     }
 }
 
+private fun doSaveCarMove(carList: List<String>, map: MutableMap<String, Int>) {
+    //println(map)
+    for (car in carList) {
+        saveCarMove(car, map)
+        //println(map)
+    }
+    //showResult(carList, map)
+}
 
+// 4이상의 수가 나올 때만 값 저장
+private fun saveCarMove(carString: String, map: MutableMap<String, Int>) {
+    val randomNum = generateRandomNumber()
+    //println(randomNum)
+    if (randomNum >= 4) {
+        map[carString] = map[carString]!! + 1
+    }
+}
+
+private fun generateRandomNumber(): Int {
+    return Randoms.pickNumberInRange(0, 9)
+}
 
