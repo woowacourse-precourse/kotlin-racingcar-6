e0e323470dfdc4dcd9a37d8b40c276bfd860895e
lee989898 <wnsrud1352@gmail.com>
2023-11-01 18:24:23 +0900

    [del] 필요없는 AttemptComparator 관련 인터페이스, 클래스 및 테스트 클래스 작세

--- src/main/kotlin/racingcar/attemptcomparator/AttemptComparator.kt
+++
@@ -1,6 +0,0 @@
-package racingcar.attemptcomparator
-
-fun interface AttemptComparator {
-
-    fun compareAttemptAndCarPosition(attempts: Int, carPosition: String): Boolean
-}
--- src/main/kotlin/racingcar/attemptcomparator/AttemptComparatorImpl.kt
+++
@@ -1,8 +0,0 @@
-package racingcar.attemptcomparator
-
-class AttemptComparatorImpl : AttemptComparator {
-
-    override fun compareAttemptAndCarPosition(attempts: Int, carPosition: String): Boolean {
-        return attempts == carPosition.length
-    }
-}
