687d052f48bdc7e481e8b961aab730b3bdd4d09c
Eddie-Pak <foalstn@naver.com>
2023-11-01 23:46:45 +0900

    fix: test실패 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -19,11 +19,11 @@ class WoowaRacing {
         }
 
         val winners = findWinners(cars)
-        println("최종 우승자: ${winners.joinToString(", ")}")
+        println("최종 우승자 : ${winners.joinToString(", ")}")
     }
 
     private fun getCarNames(): List<String> {
-        println("경주할 자동차 이름을 입력해주세요(이름은 (,)를 기준으로 구분)")
+        println("경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)")
         val input = Console.readLine()
         val carNames = input.split(",").map { it.trim() }
         if (carNames.any { it.length > 5 }) {
@@ -52,7 +52,7 @@ class WoowaRacing {
         }
 
         fun getPosition() {
-            println("$name: ${"-".repeat(position)}")
+            println("$name : ${"-".repeat(position)}")
         }
     }
 
