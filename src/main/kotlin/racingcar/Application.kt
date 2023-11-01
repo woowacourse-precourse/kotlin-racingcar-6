e0399e76b06cc7bbba1ad5affce0cfdefbe3760f
jjehdud@naver.com <jjehdud@naver.com>
2023-11-01 22:42:45 +0900

    Modification required

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -14,7 +14,54 @@ fun f_input_car_name() :List<String> {
     return car_name_list
 }
 
+fun f_try_number() : Int {
+    println("시도할 횟수는 몇 회인가요?")
+    try {
+        var try_number = Console.readLine().toInt()
+        return try_number
+    }catch (e : IllegalArgumentException) {
+        return -1
+    }
+}
+
+fun racing_garme(car_name_list : List<String>, try_number : Int) {
+    println("실행 결과")
+    var forward_list : ArrayList<Int> = arrayListOf<Int>()
+    for(i in 0..car_name_list.size - 1) {
+        forward_list.add(0)
+    }
+    for (k in 0 .. try_number) {
+        for (i in 0..car_name_list.size - 1) {
+            var random_number = Randoms.pickNumberInRange(0, 9)
+            if(random_number >= 4 ) forward_list[i]++
+            print(car_name_list[i] + " : ")
+            for(j in 0 .. forward_list[i] - 1) {
+                print("-")
+            }
+            println()
+            println()
+        }
+    }
+    var max_forward = 0
+    for(i in 0 .. car_name_list.size - 1) {
+        if(max_forward < forward_list[i]) {
+            max_forward = forward_list[i]
+        }
+    }
+    var winner : ArrayList<String> = ArrayList<String>()
+    for (i in 0 .. car_name_list.size - 1) {
+        if(max_forward == forward_list[i]) {
+            winner.add(car_name_list[i])
+        }
+    }
+    print(winner.joinToString { "," })
+}
+
 fun main() {
     // TODO: 프로그램 구현
     var car_name_list = f_input_car_name()
+
+    var try_number = f_try_number()
+
+    racing_garme(car_name_list, try_number)
 }
