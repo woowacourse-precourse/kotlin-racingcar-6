4130e92ef927867a00c5f1e962465ea1fc896a9b
윤동현 <donghyunyoon81@gmail.com>
2023-11-01 14:38:59 +0900

    feat: 숫자로 변환할때 Int로 변환이 안될 경우에 오류 발생 후 종료 기능 추가

--- src/main/kotlin/racingcar/controller/RoundDTO.kt
+++ src/main/kotlin/racingcar/controller/RoundDTO.kt
@@ -1,6 +1,11 @@
 package racingcar.controller
 
-class RoundDTO(val count:String) {
+class RoundDTO(val count: String) {
+
+    init {
+        require(count.toUIntOrNull() != null)
+    }
 
     fun convertRoundCount() = count.toInt()
+
 }
