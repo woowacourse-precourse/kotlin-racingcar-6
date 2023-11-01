4978ee0cf3ad22b04b191943d50a5da5021bd2be
ls2hn <s2hnsu@gmail.com>
2023-11-01 17:34:28 +0900

    Feat: 객체의 현위치 출력하는 인스턴스 함수 구현 - printLocation()

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -9,13 +9,14 @@ fun main() {
 //    val racingInfo = beforeRacing()  // racingInfo(rmation) : carList와 numberOfRounds로 이루어진 Pair
 //    print(racingInfo)
 //    print(goOrStop())
-    val pobiCar = Car()
-    pobiCar.moveCar()
-    pobiCar.moveCar()
-    pobiCar.moveCar()
-    pobiCar.moveCar()
+    val pobi = Car("pobi")
+    pobi.moveCar()
+    pobi.moveCar()
+    pobi.moveCar()
+    pobi.moveCar()
 
-    print(pobiCar.location) // 0,1,2,3,4 모든 경우의 값이 출력되는 걸 확인했다.
+    println(pobi.location) // 0,1,2,3,4 모든 경우의 값이 출력되는 걸 확인했다.
+    pobi.printLocation()
 
 }
 
--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,12 +1,17 @@
 package racingcar
 
-class Car {
+class Car(private val objectName: String) { // 객체명이 printLocation()에서 사용되므로 받아 옴.
     var location: Int = 0
 
     fun moveCar() {
         if ( goOrStop() ) {
             location++
         }
+    }
+
+    fun printLocation() {
+        val currentLocation = "-".repeat(location)
+        println("$objectName : $currentLocation")
     }
 
 }
