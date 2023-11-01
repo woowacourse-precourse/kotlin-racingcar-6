19ef3796532c5984f5acb604cad1898816a2f07b
이빵주 <yungju9982@naver.com>
2023-11-01 22:14:06 +0900

    [test] 자동차 변수명 수정

--- src/test/kotlin/racingcar/RacingCarInputTest.kt
+++ src/test/kotlin/racingcar/RacingCarInputTest.kt
@@ -8,13 +8,14 @@ class RacingCarInputTest {
     @Test
     fun `입력된 자동차 이름이 5자 초과이면 예외가 발생한다`() {
         // given
-        val racingCarName: List<Car> = listOf(
-            Car("abcdef"), Car("abcde")
+        val racingCarNames: List<Car> = listOf(
+            Car("abcdef"),
+            Car("abcde")
         )
 
         //then
         assertThatThrownBy {
-            RacingCarInput.validate(racingCarName)
+            RacingCarInput.validate(racingCarNames)
         }.isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("입력된 자동차 이름이 5자 초과입니다.")
     }
@@ -22,11 +23,11 @@ class RacingCarInputTest {
     @Test
     fun `입력된 자동차 개수가 1개 이하면 예외가 발생한다`() {
         // given
-        val racingCarName: List<Car> = listOf(Car("abcde"))
+        val racingCarNames: List<Car> = listOf(Car("abcde"))
 
         // then
         assertThatThrownBy {
-            RacingCarInput.validate(racingCarName)
+            RacingCarInput.validate(racingCarNames)
         }.isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("입력된 자동차의 개수가 1개 이하입니다.")
     }
@@ -34,14 +35,14 @@ class RacingCarInputTest {
     @Test
     fun `중복된 자동차 이름이 입력되면 예외가 발생한다`() {
         // given
-        val racingCarName: List<Car> = listOf(
+        val racingCarNames: List<Car> = listOf(
             Car("abcde"),
             Car("abcde")
         )
 
         // then
         assertThatThrownBy {
-            RacingCarInput.validate(racingCarName)
+            RacingCarInput.validate(racingCarNames)
         }.isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("중복되는 자동차 이름이 있습니다.")
     }
