767f96cbeb7a7871c32f1af2cb1be1cb3b5a3c78
gaeun <geun5744@gmail.com>
2023-11-01 18:50:29 +0900

    [TEST(ValidationTest)] 중복된 이름 예외 처리 테스트

--- src/test/kotlin/racingcar/domain/ValidationTest.kt
+++ src/test/kotlin/racingcar/domain/ValidationTest.kt
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import racingcar.domain.Validation.checkContainComma
 import racingcar.domain.Validation.checkNameLength
-import java.lang.IllegalArgumentException
+import racingcar.domain.Validation.checkSameName
+import kotlin.IllegalArgumentException
 
 class ValidationTest {
 
@@ -21,6 +22,14 @@ class ValidationTest {
         val input = "가은,나은,여섯글자이름".split(",")
         assertThrows<IllegalArgumentException> {
             checkNameLength(input)
+        }
+    }
+
+    @Test
+    fun `중복된 이름 예외 처리 테스트`() {
+        val input = "가은,가은,나은,다은".split(",")
+        assertThrows<IllegalArgumentException> {
+            checkSameName(input)
         }
     }
 
