7bfe39c2adb10512931bd521b6422eaeebd8f705
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-11-01 20:26:45 +0900

    [FEAT(Race)] 가장 많이 전진한 자동차 결정 기능 생성

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -9,4 +9,9 @@ class Race {
     private fun viewRace(cars: List<Car>) {
         cars.forEach { it.printCarMove() }
     }
+
+    private fun decideWinner(cars: List<Car>): List<String> {
+        val maxMove = cars.maxOf { it.moveCount }
+        return cars.filter { it.moveCount == maxMove }.map { it.name }
+    }
 }
