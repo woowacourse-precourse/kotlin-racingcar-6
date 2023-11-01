2e47189657f922e627753d0d2615ce00da9f3227
EJ-pro <hsshss2522@naver.com>
2023-11-01 13:43:42 +0900

    feat(car): Car 클래스 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,6 +2,28 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+class Car(val name: String) {
+    var position = 0
+
+    init {
+        require(name.isNotBlank() && name.length <= 5) { throw IllegalArgumentException("잘못된 이름입니다.") }
+    }
+
+    fun move() {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        if (randomNumber >= 4) {
+            position++
+        }
+    }
+
+    fun printPosition() {
+        print("$name : ")
+        repeat(position) {
+            print("-")
+        }
+        println()
+    }
+}
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
@@ -16,5 +38,6 @@ fun main() {
 
     val carNameList = carNames.split(",")
 
+    val cars = carNameList.map { Car(it.trim()) }
 }
 
