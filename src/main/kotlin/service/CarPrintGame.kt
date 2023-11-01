b776953b52d37dea5b2ecabe03bec6e22984d2a9
acecic82 <102938al@naver.com>
2023-11-01 20:08:55 +0900

    feat (PrintGame): print를 위한 인터페이스 및 클래스 구현과 ReadMe.md Update
    print를 위한 인터페이스 및 클래스 구현
    - 시작, 끝, 게임 도중으로 나누어 인터페이스 구현
    - 인터페이스에 맞게 클래스 구현
    - README.md 에 사용자에게 보여주는 부분 clear 표시 추가
    
    사용자에게 게임 상태를 보여주기 위한 클래스가 추가되었습니다.

---
+++ src/main/kotlin/service/CarPrintGame.kt
@@ -0,0 +1,31 @@
+package racingcar.servie
+
+import service.PrintGame
+
+
+class CarPrintGame : PrintGame {
+    override fun printInit() {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    }
+
+    override fun tryCount() {
+        println("시도할 횟수는 몇 회인가요?")
+    }
+
+    override fun printGame(playerName: String, goCount: Int) {
+        print("$playerName : ")
+        for (loop in 0..<goCount) {
+            print("-")
+        }
+        println()
+    }
+
+    override fun printWinner(names: List<String>) {
+        print("최종 우승자 : ")
+        //마지막에서 한명을 제외하고 ", " 형식으로 뽑기위해서 -2를 사용한다
+        for (loop in 0..<names.lastIndex) {
+            print("${names[loop]}, ")
+        }
+        print(names.last())
+    }
+}
