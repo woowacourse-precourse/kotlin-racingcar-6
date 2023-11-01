2eb53bf3c0451c91a074f3cd8bbc201178cceca4
pengcon <vpdzjs@gmail.com>
2023-11-01 22:41:57 +0900

    refactor : 정수 확인 기능 따로 함수로 작성

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -2,24 +2,30 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
-import java.lang.NumberFormatException
 
 class Racing {
 
     fun enterMoveCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        val moveCount = Console.readLine()
+        val num = Console.readLine()
         println("")
-        return try {
-            moveCount!!.toInt()
-        } catch (e: NumberFormatException){
-            throw IllegalArgumentException("정수를 입력해 주세요.")
-        }
+
+        val moveCount = validateInteger(num)
+        validatePositiveInteger(moveCount)
+        return moveCount
     }
 
     fun validatePositiveInteger(moveCount: Int) {
         if (moveCount <= 0) {
             throw IllegalArgumentException("1 이상의 정수를 입력해주세요.")
+        }
+    }
+
+    fun validateInteger(num: String): Int {
+        return try {
+            num.toInt()
+        } catch (e: NumberFormatException){
+            throw IllegalArgumentException("정수를 입력해 주세요.")
         }
     }
 
