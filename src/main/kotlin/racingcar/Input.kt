62038cd0ad2434ec0176a95e3a9c3daf144d4f1a
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-11-01 20:23:56 +0900

    [FEAT(Input)] 이동 횟수 입력 기능 생성

--- src/main/kotlin/racingcar/Input.kt
+++ src/main/kotlin/racingcar/Input.kt
@@ -8,4 +8,9 @@ class Input {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         return readLine().split(",").map { Car(it) }
     }
+
+    fun inputMoveTimes() : Int {
+        println("시도할 횟수는 몇 회인가요?")
+        return readLine().toInt()
+    }
 }
