2970b81413c65fa97487a027e708cbad739ffd82
pengcon <vpdzjs@gmail.com>
2023-11-01 21:33:42 +0900

    feat : 실행을 위한 기능들 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,15 +1,27 @@
 package racingcar
 
+import racingcar.Result
+
 fun main() {
     // TODO: 프로그램 구현
     val car: Car = Car()
+    val racing = Racing()
+    val result = Result()
     val carNames = car.createCar()
 
     car.validateCarNameLength(carNames)
     car.validateDuplicateName(carNames)
     car.validateBlankName(carNames)
+    var carProgressList = car.createCarProgressList(carNames)
 
-    val racing = Racing()
     val moveCount = racing.enterMoveCount()
-    racing.validatePositiveInteger(moveCount)
+
+    println("실행 결과")
+    for (i in 0..<moveCount) {
+        carProgressList = racing.determineProgress(carProgressList)
+        result.printProgress(carNames,carProgressList)
+    }
+
+    val winnerList=result.printWinner(carNames,carProgressList)
+    print("최종 우승자 : "+winnerList)
 }
