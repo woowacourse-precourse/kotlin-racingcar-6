ef7e9d19cfe876fd23b657f9035ee88fde4a1455
songpink <jejeje04@naver.com>
2023-11-01 15:23:56 +0900

    IDE에서 push하면 안되는건가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,8 +4,8 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    val game = Randoms.pickNumberInRange(0, 9)
+    val gamee = Randoms.pickNumberInRange(0, 9)
     val read = Console.readLine()
-    println(game)
+    println(gamee)
     println(read)
 }
