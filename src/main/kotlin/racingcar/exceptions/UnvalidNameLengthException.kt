47972294d227a37966252fdf4179a0e3c61cd3d0
NOGUEN <nhg1113@naver.com>
2023-11-01 16:07:40 +0900

    fix: 커밋 이동시 생긴 충돌 해결

--- src/main/kotlin/racingcar/exceptions/UnvalidNameLengthException.kt
+++ src/main/kotlin/racingcar/exceptions/UnvalidNameLengthException.kt
@@ -3,3 +3,4 @@ package racingcar.exceptions
 import racingcar.EXCEPTION_MESSAGE_UNVALID_NAME_LENGTH
 
 class UnvalidNameLengthException : IllegalArgumentException(EXCEPTION_MESSAGE_UNVALID_NAME_LENGTH)
+
