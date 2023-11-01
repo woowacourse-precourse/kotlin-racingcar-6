d683f6eabda70414af334df5bc0f7718b2bccd71
Wonhui Roh <rwhui012@gmail.com>
2023-11-01 16:44:25 +0900

    refactor(racingcar): edited some constant values
    - add `SHOW_EXECUTION_RESULT`, `ANNOUNCE_THE_WINNERS`
    - rename `EMPTY_CAR_NAME_ERRORMESSAGE` to `BLANK_CAR_NAME_ERRORMESSAGE`, as the condition that throw the exception changed
    - rename `UNDER_5_LETTERS_CAR_NAME_ERRORMESSAGE` to `OVER_5_LETTERS_CAR_NAME_ERRORMESSAGE` for clarity

--- src/main/kotlin/racingcar/res/values.kt
+++ src/main/kotlin/racingcar/res/values.kt
@@ -10,11 +10,13 @@ object CarMovement {
 object GuideMessage {
   const val ENTER_CAR_NAMES_GUIDEMESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
   const val ENTER_NUMBER_OF_MOVES_GUIDEMESSAGE = "시도할 횟수는 몇 회인가요?"
+  const val SHOW_EXECUTION_RESULT = "실행 결과"
+  const val ANNOUNCE_THE_WINNERS = "최종 우승자 : "
 }
 
 object ErrorMessage {
-  const val EMPTY_CAR_NAME_ERRORMESSAGE = "자동차 이름을 입력해주세요."
-  const val UNDER_5_LETTERS_CAR_NAME_ERRORMESSAGE = "자동차 이름을 5자 이하로 작성해주세요."
+  const val BLANK_CAR_NAME_ERRORMESSAGE = "자동차 이름을 입력해주세요."
+  const val OVER_5_LETTERS_CAR_NAME_ERRORMESSAGE = "자동차 이름을 5자 이하로 작성해주세요."
   const val INDISTINCTIVE_CAR_NAME_ERRORMESSAGE = "자동차 이름들을 구별되게 지어주세요."
 
   const val NOT_PROPER_NUMBER_OF_MOVES_ERRORMESSAGE = "올바른 횟수를 입력해주세요."
