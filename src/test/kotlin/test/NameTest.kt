e9aa7980a4ab5846c3cd51c79e47e495af9c2271
NOGUEN <nhg1113@naver.com>
2023-11-01 13:17:24 +0900

    test: Name 모델 테스크 코드

---
+++ src/test/kotlin/test/NameTest.kt
@@ -0,0 +1,39 @@
+package test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import racingcar.EXCEPTION_MESSAGE_BLANK_NAME
+import racingcar.EXCEPTION_MESSAGE_UNVALID_NAME_LENGTH
+import racingcar.model.Name
+
+
+class NameTest {
+    @Test
+    fun `이름 길이 유효성 체크 테스트`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            val name = Name("aaaaaaa")
+
+            name.checkValidateName()
+        }
+        assertEquals(EXCEPTION_MESSAGE_UNVALID_NAME_LENGTH, exception.message)
+    }
+
+    @Test
+    fun `이름 공백 체크 테스트`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            val name = Name("")
+
+            name.checkValidateName()
+        }
+        assertEquals(EXCEPTION_MESSAGE_BLANK_NAME, exception.message)
+        //assertThat(result).containsExactly("1", "2")
+    }
+
+    @Test
+    fun `getNameString 체크 테스트`() {
+        val name = Name("hello")
+        val result = name.getNameString()
+        assertThat(result).contains("hello")
+    }
+}
