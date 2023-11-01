167a671f3fc2254319865ff28d5329c3137884f0
murjune <sis9221302@gmail.com>
2023-11-01 18:48:28 +0900

    fix/test(NumericStringValidtorTest) : 좀 더 의미있는 이름으로 변수 수정

--- src/test/kotlin/racingcar/validator/NumericStringValidatorTest.kt
+++ src/test/kotlin/racingcar/validator/NumericStringValidatorTest.kt
@@ -11,12 +11,12 @@ class NumericStringValidatorTest {
         val numericWithLetterString = "123a"
         val validator: StringValidator = NumericStringValidator()
 
-        val result1 = runCatching { validator.validate(numericString) }.exceptionOrNull()
-        val result2 = runCatching { validator.validate(numericWithLetterString) }.exceptionOrNull()
+        val validResult = runCatching { validator.validate(numericString) }.exceptionOrNull()
+        val inValidResult = runCatching { validator.validate(numericWithLetterString) }.exceptionOrNull()
 
-        assertThat(result1).isNull()
-        assertThat(result2).hasSameClassAs(IllegalArgumentException())
-        assertThat(result2!!.message).isEqualTo("문자열은 숫자여야 합니다")
+        assertThat(validResult).isNull()
+        assertThat(inValidResult).hasSameClassAs(IllegalArgumentException())
+        assertThat(inValidResult!!.message).isEqualTo("문자열은 숫자여야 합니다")
         assertThrows<IllegalArgumentException>("문자열은 숫자여야 합니다") {
             validator.validate(numericWithLetterString)
         }
