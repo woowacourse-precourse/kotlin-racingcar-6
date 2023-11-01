e75304846df47731c698f2864d33237df998b915
ErroredPasta <ForeignServices3132@pm.me>
2023-11-01 20:30:58 +0900

    fix: LESS_THAN_MIN_TURN_NUMBER_EXCEPTION에서 메시지가 MIN_TURN_NUMBER 이상이어야 한다고 수정

--- src/main/kotlin/racingcar/model/TurnNumber.kt
+++ src/main/kotlin/racingcar/model/TurnNumber.kt
@@ -8,6 +8,6 @@ value class TurnNumber(val number: Int) {
 
     companion object {
         const val MIN_TURN_NUMBER = 0
-        const val LESS_THAN_MIN_TURN_NUMBER_EXCEPTION = "Turn number must be greater than $MIN_TURN_NUMBER"
+        const val LESS_THAN_MIN_TURN_NUMBER_EXCEPTION = "Turn number must be greater than or equal to $MIN_TURN_NUMBER"
     }
 }
