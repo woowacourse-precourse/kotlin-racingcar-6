0057f7872c2f09893ed1fb668ddcfb6feb92d3a2
iiolo <alohalo98@naver.com>
2023-11-01 18:46:09 +0900

    refactor: if문 한줄로 정리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console
 
 
 fun main() {
-
 }
 
 data class Car(
@@ -19,17 +18,13 @@ fun getCarNames(): String {
 
 fun getValidCarNamesArray(carNames: String): Collection<Car> {
     val carNamesArray = carNames.split(",").map { Car(it, 0) }
-    if (carNamesArray.any { it.name.length > 5 }) {
-        throw IllegalArgumentException("자동차 이름은 5자 이하 이어야 함")
-    }
+    if (carNamesArray.any { it.name.length > 5 }) throw IllegalArgumentException("자동차 이름은 5자 이하 이어야 함")
     return carNamesArray
 }
 
 fun getNumberOfRacing(): Int {
     println("시도할 횟수는 몇 회인가요?")
-    if (!Console.readLine().matches(Regex("\\d"))){
-        throw IllegalArgumentException("숫자로 입력해야 함")
-    }
+    if (!Console.readLine().matches(Regex("\\d"))) throw IllegalArgumentException("숫자로 입력해야 함")
     return Console.readLine().toInt()
 }
 
