ecb7ac989596384dec0496a0308e4abeef78e92c
전형선 <hsun5163@gmail.com>
2023-11-01 11:36:17 +0900

    feat : 난수 발생기 구현.

---
+++ src/main/kotlin/racingcar/RandomNumberGenerator.kt
@@ -0,0 +1,11 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+object RandomNumberGenerator {
+    private const val minNumber = 0
+    private const val maxNumber = 9
+
+    fun run(): Int{
+        return Randoms.pickNumberInRange(minNumber, maxNumber)
+    }
+}
