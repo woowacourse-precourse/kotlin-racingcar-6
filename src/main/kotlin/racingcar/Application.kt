0112fa4e0103c52fb21309799cd1e6f6d4c32a66
j2j2 <fineimagination@gmail.com>
2023-11-01 15:41:16 +0900

    refactor(racingcar): 코딩 컨벤션 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,18 +5,20 @@ import racingcar.domain.Report
 import racingcar.domain.User
 
 fun main() {
-    val userInput = User()
-    var racingCars = userInput.namingCar()
-    val totalAttempt = userInput.attemptNumber()
+    val user = User()
+    var racingCars = user.namingCar()
+    val totalAttempt = user.attemptNumber()
     val racing = Race()
     val reporter = Report()
+
     println("\n실행 결과")
-    for (attemptIndex in 1..totalAttempt){
+
+    for (attemptIndex in 1..totalAttempt) {
         racingCars = racing.racing(racingCars)
         reporter.status(racingCars)
         println()
     }
-    val winner = reporter.checkWinner(racingCars)
-    val result = reporter.raceResult(winner)
 
+    val winner = reporter.checkWinner(racingCars)
+    reporter.raceResult(winner)
 }
--- src/main/kotlin/racingcar/domain/Report.kt
+++ src/main/kotlin/racingcar/domain/Report.kt
@@ -1,7 +1,6 @@
 package racingcar.domain
 
 class Report {
-
     fun status(racingCars: Map<String, Int>) {
         racingCars.forEach { (name, value) ->
             println("$name : ${"-".repeat(value)}")
@@ -10,12 +9,15 @@ class Report {
 
     fun checkWinner(racingCars: Map<String, Int>): String {
         val maxCount = racingCars.values.maxOrNull() ?: return ""
-        if (maxCount == 0) return ""
-        val winners = racingCars.filter { it.value == maxCount }.keys
-        return winners.joinToString(", ")
+        return if (maxCount == 0) {
+            ""
+        } else {
+            val winners = racingCars.filter { it.value == maxCount }.keys
+            winners.joinToString(", ")
+        }
     }
 
-    fun raceResult(winner: String) :Boolean{
+    fun raceResult(winner: String): Boolean {
         return if (winner.isEmpty()) {
             print("모두 출발하지 못해, 우승자가 없습니다")
             false
