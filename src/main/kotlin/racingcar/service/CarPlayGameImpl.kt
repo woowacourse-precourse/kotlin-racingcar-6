cef2f53703bf2769c5f6af4a44d9ef39a02e158c
acecic82 <102938al@naver.com>
2023-11-01 20:32:26 +0900

    feat (CarPlayGameImpl): 게임 진행을 할 수 있는 기능 구현 및 readMe Update
    게임 진행을 할 수 있는 기능 구현 및 readMe Update
    
    - 실제 게임이 진행되는 playGame 구현
    - round 수만큼 게임 진행
    - 매 round 마다 진행되는 게임 구현
    - readMe Update
    
    라운드 진행 시 랜덤으로 숫자를 뽑아 플레이어가 전진이 가능한지 멈춰야 하는지
    판단하여 실제 전진시키거나 멈추도록 기능을 구현하였습니다.

--- src/main/kotlin/racingcar/service/CarPlayGameImpl.kt
+++ src/main/kotlin/racingcar/service/CarPlayGameImpl.kt
@@ -1,7 +1,9 @@
 package racingcar.service
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 import racingcar.domain.Player
+import kotlin.math.max
 
 class CarPlayGameImpl(
         private val printGame: PrintGame
@@ -26,17 +28,39 @@ class CarPlayGameImpl(
     }
 
     override fun playGame() {
+        initGame()
+
+        for (loop in 0..<round) {
+            playRound()
+            println()
+        }
     }
 
     override fun playRound() {
+        players.forEach { player ->
+            // 1 : go 0 : stop
+            if (canIGo()) player.addWinCount()
+            printGame.printGame(player.name, player.winCount)
+        }
     }
 
     override fun endGame(winnerCount: Int) {
     }
 
+    // 5자 이하의 이름인지 확인하는 메서드
     private fun validCheck(name: String) {
         require(name.length < 6) {
             "이름의 길이는 5자이하 여야 합니다."
         }
+    }
+
+    // 랜덤으로 값을 뽑아 전진이 가능한지 확인하는 메서드
+    // 4 이상 부터 전진한다.
+    private fun canIGo(): Boolean {
+        return Randoms.pickNumberInRange(0, 9) > RUN_OR_STOP_STANDARD
+    }
+
+    companion object {
+        const val RUN_OR_STOP_STANDARD = 3
     }
 }
