6c9870527f3adce18cb5a6b7c0af3935326f3ac8
eunsxx <88652027+eunsxx@users.noreply.github.com>
2023-11-01 12:38:45 +0900

    feat : input from the user
    The ability to receive input from the user about the car names and the number of attempts to race

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
+
 fun main() {
-    // TODO: 프로그램 구현
+    // TODO : 자동차 이름 입력받기 (,로 구분)
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    var input: String = readLine() ?: throw IllegalArgumentException()
+    var cars = input.split(',')
+
+    println(cars)
+
+    // TODO : 시도할 횟수 입력받기
+    println("시도할 횟수는 몇 회인가요?")
+    var tries: Int = readLine()?.toInt() ?: throw IllegalArgumentException()
+
+    println(tries)
 }
