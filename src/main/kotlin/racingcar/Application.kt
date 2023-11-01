bab011ac14a9d7ec3a2247d6d5a3d828b347e424
songpink <jejeje04@naver.com>
2023-11-01 15:09:38 +0900

    Randoms모듈 체크(val로 쓰면 안되는가?)

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
 
     val carGame = Console.readLine()
-    var test = Randoms.pickNumberInRange(0, 9)
+    val test = Randoms.pickNumberInRange(0, 9)
 
     println(carGame)
     println(test)
