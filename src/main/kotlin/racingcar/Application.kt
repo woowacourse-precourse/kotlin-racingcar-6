629be4def63e4efbb3a8fdcd5b8ba2da31ce347c
DJ_Lumiere <djminstar573@gmail.com>
2023-11-01 16:59:43 +0900

    Update Application.kt
    CarNameConstraints, CarMovementConstraints, RacingCarGameMessages object 생성
    RaceCar의 움직임을 위한 moveDistance, randomNumberGenerator, filterMovableNumber 함수 생성 및 수정
    코드 리팩토링

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,9 +3,28 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-class RaceCar(private val name: String, var position: Int = 0) {
+object CarNameConstraints {
+    const val VALID_MAXIMUM_LENGTH = 5
+}
+
+object CarMovementConstraints {
+    const val RANDOM_NUMBER_GENERATOR_MINIMUM_VALUE = 0
+    const val RANDOM_NUMBER_GENERATOR_MAXIMUM_VALUE = 9
+    const val CAR_MOVEMENT_THRESHOLD = 4
+    const val MOVEMENT_UNIT = 1
+    const val NO_MOVEMENT = 0
+}
+
+object RacingCarGameMessages {
+    const val RACECAR_NAME_QUERY = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val RACECAR_TURN_COUNT_QUERY = "시도할 횟수는 몇 회인가요?"
+    const val GAME_RESULT_TITLE = "실행 결과"
+    const val FINAL_WINNER_PREFIX = "최종 우승자 : "
+}
+
+class RaceCar(val name: String, var position: Int = 0) {
     init {
-        require((name.length <= 5)) { "Invalid Car Name." }
+        require((name.length <= CarNameConstraints.VALID_MAXIMUM_LENGTH)) { "Invalid Car Name." }
     }
 
     override fun toString(): String {
@@ -14,8 +33,23 @@ class RaceCar(private val name: String, var position: Int = 0) {
 
 }
 
+fun filterMovableNumber(target: Int): Int {
+    if (target < CarMovementConstraints.CAR_MOVEMENT_THRESHOLD) {
+        return CarMovementConstraints.NO_MOVEMENT
+    }
+    return CarMovementConstraints.MOVEMENT_UNIT
+}
+
+fun randomNumberGenerator(): Int {
+    return Randoms.pickNumberInRange(
+        CarMovementConstraints.RANDOM_NUMBER_GENERATOR_MINIMUM_VALUE,
+        CarMovementConstraints.RANDOM_NUMBER_GENERATOR_MAXIMUM_VALUE
+    )
+}
+
 fun moveDistance(): Int {
-    return (Randoms.pickNumberInRange(0, 9).compareTo(4)).compareTo(-1)
+    val randomNumber = randomNumberGenerator()
+    return filterMovableNumber(randomNumber)
 }
 
 private fun RaceCar.move() {
@@ -23,7 +57,7 @@ private fun RaceCar.move() {
 }
 
 fun parseCarName(): List<RaceCar> {
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    println(RacingCarGameMessages.RACECAR_NAME_QUERY)
     return Console.readLine().split(",").map { RaceCar(it.trim()) }
 }
 
@@ -32,7 +66,7 @@ fun isNonnegativeInt(target: String): Boolean {
 }
 
 fun parseTurnCount(): Int {
-    println("시도할 횟수는 몇 회인가요?")
+    println(RacingCarGameMessages.RACECAR_TURN_COUNT_QUERY)
     val turnCountString = Console.readLine()
     require(isNonnegativeInt(turnCountString)) { "Invalid Turn Count." }
     return turnCountString.toInt()
@@ -52,11 +86,11 @@ fun decideWinners(raceCars: List<RaceCar>): List<String> {
 fun main() {
     val raceCars = parseCarName()
     val turnCount = parseTurnCount()
-    println("\n실행 결과")
+    println("\n" + RacingCarGameMessages.GAME_RESULT_TITLE)
     for (i in 1..turnCount) {
         turnProcess(raceCars)
         println(raceCars.joinToString("\n", postfix = "\n"))
     }
     val winners = decideWinners(raceCars)
-    println(winners.joinToString(", ", "최종 우승자 : "))
+    println(winners.joinToString(", ", RacingCarGameMessages.FINAL_WINNER_PREFIX))
 }
