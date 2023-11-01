67f846df8b8764eae84532ca26e06ba3d6546b16
ls2hn <s2hnsu@gmail.com>
2023-11-01 14:53:03 +0900

    Feat: Pair(자동차 이름리스트, 시도횟수) 입력받기 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     // TODO: 프로그램 구현
+
+    val racingInfo = beforeRacing()  // racingInfo(rmation) : carList와 numberOfRounds로 이루어진 Pair
+    print(racingInfo)
+
 }
+
+fun beforeRacing(): Pair<List<String>, Int> {
+
+    val carList: List<String> = listOf(Console.readLine())
+    val numberOfRounds = Console.readLine().toInt()
+
+    return Pair(carList, numberOfRounds)
+}
+
