1961f43dedc9c48ccc6fdbe6739048b87ab00045
cucumber99 <dlsguq1@gmail.com>
2023-11-01 16:48:24 +0900

    feat: 게임 모델 구현

--- src/main/kotlin/racingcar/model/GameModel.kt
+++ src/main/kotlin/racingcar/model/GameModel.kt
@@ -1,4 +1,15 @@
 package racingcar.model
 
-class GameModel {
+class GameModel(names: List<String>, private val attempts: Int) {
+    private val cars: List<CarModel> = names.map { CarModel(it) }
+    fun race() {
+        repeat(attempts) {
+            cars.forEach{ it.move() }
+        }
+    }
+
+    fun getWinners(): List<CarModel> {
+        val maxPosition = cars.maxOf { it.position }
+        return cars.filter { it.position == maxPosition }
+    }
 }
