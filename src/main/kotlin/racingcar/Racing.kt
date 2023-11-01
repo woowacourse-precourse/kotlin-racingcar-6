10fa3879aa4fd04d58467c66ec69e70e84f6a1b5
pengcon <vpdzjs@gmail.com>
2023-11-01 18:51:29 +0900

    refactor : enterMoveCount 수정

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -1,11 +1,17 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import java.lang.NumberFormatException
 
 class Racing {
 
-    fun enterMoveCount(): String {
+    fun enterMoveCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        return Console.readLine()
+        val moveCount = Console.readLine()
+        return try {
+            moveCount!!.toInt()
+        } catch (e: NumberFormatException){
+            throw IllegalArgumentException("정수를 입력해 주세요.")
+        }
     }
 }
