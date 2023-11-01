929c5f5788dfa68698f4919b716741d82f3460af
songpink <jejeje04@naver.com>
2023-11-01 15:25:48 +0900

    다시 되돌려봄

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,11 +1,7 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
-
 fun main() {
-    val game = Randoms.pickNumberInRange(0, 9)
-    val read = Console.readLine()
-    println(game)
-    println(read)
+
+    val carGame = RacingCarGame()
+    carGame.gameStart()
 }
