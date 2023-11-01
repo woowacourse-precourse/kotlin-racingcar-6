c4952845db13459015bcaed23f06989043b72e8b
yuseong01 <dog2655@naver.com>
2023-11-01 22:47:42 +0900

    feat: "사용자로부터 자동차이름 입력받는 기능 구현"
    경주할 자동차 이름을 입력받고 쉼표(,)기준으로 각각의 자동차를 분류, 5자 이내로 입력하도록 처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
 
 fun main() {
-    // TODO: 프로그램 구현
+    val carNames = inputCarNames()
+}
+
+fun inputCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = readLine() ?: ""
+    val carNames = input.split(",").map { it.trim() }
+
+    if (carNames.any { it.length > 5 }) {
+        throw IllegalArgumentException("자동차 이름은 5자 이하로 입력해야 합니다.")
+    }
+
+    return carNames
 }
