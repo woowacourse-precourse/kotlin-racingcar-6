6959f16ff1a74ce751750efbf8f52e1f77483990
songpink <jejeje04@naver.com>
2023-11-01 15:21:43 +0900

    Console 재테스트

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,8 +1,11 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     var game = Randoms.pickNumberInRange(0, 9)
+    val read = Console.readLine()
     println(game)
+    println(read)
 }
