90b39c654503be2df14f4216fec9ac0017b797df
민감자 <mouse0429@naver.com>
2023-11-01 22:11:52 +0900

    feat(Game): 유저로부터 시도할 횟수를 입력 받는 기능

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 
 class Game {
     private lateinit var carList: List<Car>
+    private var tryNumber: Int = 0
 
     private enum class ProgressMessage(val message: String) {
         RequestCarName("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
@@ -20,16 +21,22 @@ class Game {
         getCarList()
 
         ProgressMessage.RequestTryNumber.show()
+        getTryNumber()
+
         ProgressMessage.DescribeNextResult.show()
     }
 
-    private fun initCarList(inputCarList: List<Car>) {
-        this.carList = inputCarList
-    }
-
+    private fun initCarList(inputCarList: List<Car>) { this.carList = inputCarList }
+    private fun initTryNumber(inputNumber: Int) { this.tryNumber = inputNumber }
     private fun getCarList() {
         val carList = Console.readLine().split(",")
         verifyCarList(carList)
         initCarList(carList.map { Car(it) })
+    }
+
+    private fun getTryNumber() {
+        val tryNumber = Console.readLine()
+        verifyTryNumber(tryNumber)
+        initTryNumber(tryNumber.toInt())
     }
 }
