401637e6dfc05a2435088fde1ab19df2678e7ecb
yuhyun <dlugus@naver.com>
2023-11-01 22:20:06 +0900

    chore: getCarNames()의 carNames 선언 변경

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,17 +2,32 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+import racingcar.getTryNumber
+
 
 fun main() {
     // TODO: 프로그램 구현
 }
 
+fun playCarRace(){
+    while(true) {
+        val carName = getCarNames()
+        var tryNumberToPlayGame = getTryNumber()
+        
+        for (tryNumberToPlayGame) {
+            carGoOrStop()
+        }
+        }
+    }
+
+
+
 fun getCarNames():List<String> {
     print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val userInput = Console.readLine()
-    val carName = userInput.split(",")
+    val carNames = userInput?.split(",")?.map{it.trim()}
 
-    return carName
+    return carNames
 }
 
 fun vaildationCarNames(userInput: String){
@@ -21,7 +36,7 @@ fun vaildationCarNames(userInput: String){
     }
 }
 
-fun getTryNumber() {
+fun getTryNumber():Int {
     print("시도할 횟수는 몇 회인가요?")
     val tryNumber = Console.readLine()
 
