8af69550078c21e47a185cd1b494a493dad22d7d
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-01 11:36:24 +0900

    test: 레이싱 테스트 코드 추가

---
+++ src/test/kotlin/lst1995kotlintest/RacingTest.kt
@@ -0,0 +1,38 @@
+package lst1995kotlintest
+
+import Validator.nameCheck
+import car.Car
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import race.Racing
+
+class RacingTest {
+    private var carList = listOf<Car>()
+    private var racing = Racing()
+    private var car = listOf<Car>()
+    private var singleRacing = Racing()
+
+    @BeforeEach
+    fun setUp() {
+        carList = nameCheck(listOf<String>("유재석", "하하", "김종국", "송지효", "전소민"))
+        car = nameCheck(listOf<String>("유재석"))
+        singleRacing.start(car)
+    }
+
+    @Test
+    fun `레이싱 출발 검사`() {
+        assertThat("${racing.start(carList)}")
+            .contains("유재석 : ")
+            .contains("하하 : ")
+            .contains("김종국 : ")
+            .contains("송지효 : ")
+            .contains("전소민 : ")
+            .contains("\n")
+    }
+
+    @Test
+    fun `우승자 출력 검사`() {
+        assertThat("${singleRacing.findWinner(car)}").isEqualTo("최종 우승자 : 유재석")
+    }
+}
