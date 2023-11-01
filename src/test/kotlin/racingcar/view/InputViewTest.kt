c4900bf77974cd0b39ef7c51053ed59faf806668
sh1mj1 <7wlgns@gmail.com>
2023-11-01 20:30:28 +0900

    refactor(InputViewTest): refactor from multiple tests to the method using @ParameterizedTest
    * 여러 테스트 메서드를 하나의 @ParameterizedTest 로 리팩토링한다.

--- src/test/kotlin/racingcar/view/InputViewTest.kt
+++ src/test/kotlin/racingcar/view/InputViewTest.kt
@@ -2,73 +2,45 @@ package racingcar.view
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 import racingcar.config.ExceptionMessage.INVALID_INTEGER
 
 class InputViewTest {
     private lateinit var inputView: InputView
-    private lateinit var inputCars: String
-    private lateinit var inputTryCount: String
 
     @BeforeEach
     fun setUp() {
         inputView = InputView()
     }
 
-    // test inputCars
-    @Test
-    fun `입력 값은 쉼표(,)를 기준으로 리스트로 반환한다`() {
-        inputCars = "pobi,woni,jun"
-        val inputList = listOf("pobi", "woni", "jun")
-        val result = inputView.inputCars(inputCars)
-        Assertions.assertThat(result).isEqualTo(inputList)
+    // inputCars test
+    @ParameterizedTest
+    @CsvSource(
+        "pobi,woni,jun : pobi,woni,jun",
+        "pobi, woni , jun : pobi,woni,jun", delimiter = ':'
+    )
+    fun `입력 값은 쉼표(,)를 기준으로 앞 뒤 공백을 제거한 값의 리스트로 반환한다`(input: String, expected: String) {
+        val result = inputView.inputCars(input)
+        Assertions.assertThat(result).isEqualTo(expected.split(','))
     }
 
-    @Test
-    fun `입력 값이 쉼표(,) 앞 뒤로 공백이 있다면 이를 제거하고 리스트로 반환한다`() {
-        inputCars = "pobi, woni , jun"
-        val inputList = listOf("pobi", "woni", "jun")
-        val result = inputView.inputCars(inputCars)
-        Assertions.assertThat(result).isEqualTo(inputList)
-    }
-
-    // test inputTryCount
-    @Test
-    fun `숫자가 아닌 문자열을 입력하면 예외를 던진다`() {
-        inputTryCount = "abc"
-        assertThrows<IllegalArgumentException>(
-            INVALID_INTEGER
-        ) {
-            inputView.inputTryCount(inputTryCount)
+    // inputTryCount test
+    @ParameterizedTest
+    @ValueSource(strings = ["abc", "-1", "${Integer.MAX_VALUE + 1}"])
+    fun `숫자 입력 예외 테스트`(input: String) {
+        assertThrows<IllegalArgumentException>(INVALID_INTEGER) {
+            inputView.inputTryCount(input)
         }
     }
 
-    @Test
-    fun `0 이하의 숫자를 입력하면 예외를 던진다`() {
-        inputTryCount = "-1"
-        assertThrows<IllegalArgumentException>(
-            INVALID_INTEGER
-        ) {
-            inputView.inputTryCount(inputTryCount)
-        }
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "4", "${Integer.MAX_VALUE}"])
+    fun `Int 형 자연수를 문자열로 입력했을 시 해당 숫자를 Int 형으로 반환한다`(input: String) {
+        val result = inputView.inputTryCount(input)
+        val expected = Integer.parseInt(input)
+        Assertions.assertThat(result).isEqualTo(expected)
     }
-
-    @Test
-    fun `Int 형의 최대값보다 큰 숫자를 입력하면 예외를 던진다`() {
-        inputTryCount = "${Integer.MAX_VALUE + 1}" // 테스트를 위해 overflow 를 일부러 발생시킨다.
-        assertThrows<IllegalArgumentException>(
-            INVALID_INTEGER
-        ) {
-            inputView.inputTryCount(inputTryCount)
-        }
-    }
-
-    @Test
-    fun `Int 형 자연수를 문자열로 입력했을 시 해당 숫자를 Int 형으로 반환한다`() {
-        inputTryCount = "${Integer.MAX_VALUE}"
-        val result = inputView.inputTryCount(inputTryCount)
-        Assertions.assertThat(result).isEqualTo(Integer.MAX_VALUE)
-    }
-
 }
