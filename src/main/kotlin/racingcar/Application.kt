13dd8f80adbc08d10d71286b5879c3eb92cb4da4
songpink <jejeje04@naver.com>
2023-11-01 15:01:38 +0900

    Console.close() 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,9 +6,9 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
 
     val carGame = Console.readLine()
+    val test = Randoms.pickNumberInRange(0, 9)
 
     println(carGame)
+    println(test)
     Console.close()
-
-
 }
