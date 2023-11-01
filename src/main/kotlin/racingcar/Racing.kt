78dd3b71a306bdec71919bd2c66904955bfa9cc6
wonjongin <wonggamggik@gmail.com>
2023-11-01 13:01:41 +0900

    재실행 - Racing#playRacing() 기능 목록 일부 수정1 제출 오류 파악 위해 되집어보는 과정

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms
 class Racing {
 
     fun playRacing(tryCount: Int, cars: MutableList<Car>) {
-        for (count in 0..<tryCount) {
+        for (count in 1..tryCount) {
             forwardOver4(cars)
         }
     }
@@ -15,6 +15,7 @@ class Racing {
     private fun random09(): Int {
 
         return Randoms.pickNumberInRange(0, 9)
+
     }
 
     private fun forwardOver4(cars: MutableList<Car>) {
