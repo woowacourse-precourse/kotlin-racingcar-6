7518cc7505c1cd1e71a41dca98a80b120aef4fd6
LEE <tpqms775@naver.com>
2023-11-01 20:00:01 +0900

    refactor(racingcar): try-catch문 삭제

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,12 +1,9 @@
 package racingcar
 
 fun main() {
-    try {
+
         // TODO: 프로그램 구현
         val racingManager = RacingManager()
         racingManager.startGame()
         racingManager.findWinner()
-    } catch (e: IllegalArgumentException) {
-        println("\nError : ${e.message}")
-    }
 }
