e5cca556ed9b94afd6571f63b8a75fdeaf10ca49
seonm9119 <seonm9119@gmail.com>
2023-11-01 10:32:10 +0900

    main: prototype design v1.0.0

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,29 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
-    // TODO: 프로그램 구현
+
+
+    // TODO: feat: input
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val names = readLine().split(',')
+
+    println("시도할 횟수는 몇 회인가요?")
+    val chance = readLine().toInt()
+
+    println("\n실행 결과")
+
+    repeat(chance){
+
+        // TODO : feat: race
+        println("race function call")
+        println()
+    }
+
+
+    // TODO : feat: output
+    println("최종 우승자")
+
+
+
 }
