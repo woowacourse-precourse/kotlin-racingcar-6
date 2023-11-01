23429828038a66ea37f21fe67bee74373a53ec13
pearhyunjin <pearhyunjin@gmail.com>
2023-11-01 22:42:52 +0900

    fix: delete unnecessary comments delete unnecessary comments

--- src/main/kotlin/domain/Car.kt
+++ src/main/kotlin/domain/Car.kt
@@ -1,15 +1,7 @@
 package domain
 
-import camp.nextstep.edu.missionutils.Console
-import domain.Constant.INPUT_CAR_NAME_TEXT
-import domain.Constant.NAME_DIVISION_NOTATION
-
 class Car(private var name: String) {
     var moveCount = 0
-
-    fun moveCount(): Int {
-        return moveCount
-    }
 
     fun moveForward(): Int {
         moveCount++
@@ -20,7 +12,7 @@ class Car(private var name: String) {
         return moveCount
     }
 
-    // 해결하는데 오래 걸린 부분
+    // 해결하는데 오래 걸린 부분 -> 객체가 저장된 위치를 반환하고 있었기 때문에 객체의 정보를 받기 위해서 사용
     override fun toString(): String {
         return "${name}"
     }
