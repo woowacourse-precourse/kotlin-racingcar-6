9d3c449634219133ca4ea35880bef9aecc2ca747
이빵주 <yungju9982@naver.com>
2023-11-01 22:09:35 +0900

    [test] 이동 횟수 범위 테스트코드 작성

--- src/test/kotlin/racingcar/MovingCountInputTest.kt
+++ src/test/kotlin/racingcar/MovingCountInputTest.kt
@@ -1,4 +1,19 @@
 package racingcar
 
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
 class MovingCountInputTest {
+
+    @Test
+    fun `입력된 이동 횟수가 0 이하라면 예외가 발생한다`() {
+        // given
+        val countInput = "0"
+
+        // then
+        assertThatThrownBy {
+            MoveCountInput.getMoveCount(countInput)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("입력된 횟수가 0 이하입니다.")
+    }
 }
