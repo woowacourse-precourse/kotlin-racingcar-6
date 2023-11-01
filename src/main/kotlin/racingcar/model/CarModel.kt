97a19a79bc25ce35bf11d4999c5f5c1df392c9b5
cucumber99 <dlsguq1@gmail.com>
2023-11-01 16:32:34 +0900

    feat: 자동차 모델 구현

--- src/main/kotlin/racingcar/model/CarModel.kt
+++ src/main/kotlin/racingcar/model/CarModel.kt
@@ -1,4 +1,8 @@
 package racingcar.model
+import camp.nextstep.edu.missionutils.Randoms
 
-class CarModel {
+class CarModel(val name: String, var position: Int = 0) {
+    fun move() {
+        if(Randoms.pickNumberInRange(0,9) >= 4) position++
+    }
 }
