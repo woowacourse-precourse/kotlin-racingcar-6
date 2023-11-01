77dd67b61092d3dbdc166ee509491dbd61e1abd7
SeungGu Kang <alex9366@naver.com>
2023-11-01 15:53:47 +0900

    feat: 게임의 초기값을 설정하고 게임 시작을 관리하는 클래스 생성

---
+++ src/main/kotlin/racingcar/GameController.kt
@@ -0,0 +1,41 @@
+package racingcar
+
+class GameController(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView(),
+) {
+    private lateinit var nameList: List<String>
+    private var car: MutableList<Car> = mutableListOf()
+    private var count: Int? = null
+
+    fun runGame() {
+        setGame()
+        Racing(outputView, count!!, car)
+        gameResult()
+    }
+
+    private fun setCount(): Int = inputView.inputCount()
+
+    private fun setCar() {
+        for(i in nameList) { car.add(Car(name = i)) }
+    }
+
+    private fun setGame() {
+        outputView.startCommand()
+        nameList = inputView.inputName()
+        outputView.getCountCommand()
+        count = setCount()
+        setCar()
+    }
+
+    private fun setWinner(): String {
+        val highestScore = car.maxOfOrNull { it.score }
+        val highestScoreCars = car.filter { it.score == highestScore }
+        return highestScoreCars.joinToString(separator = ",") { it.name }
+    }
+
+    private fun gameResult() {
+        val winners = setWinner()
+        outputView.winnerOutput(winners)
+    }
+}
