f3a5f3b904704e2d1f2f122672f15df53b7dec38
Junyoung-WON <wjyo60@gmail.com>
2023-11-01 17:33:49 +0900

    feat: 게임 관리자 클래스의 생성자와 자동차 리스트를 생성하는 함수 구현
    - 생성자는 차량 이름 리스트와 시도 횟수를 받아, 자동차 리스트를 생성한다.
    - 자동차 객체를 생성하여 리스트에 추가하는 함수 createCarList를 작성하였다.

--- src/main/kotlin/racingcar/GameManager.kt
+++ src/main/kotlin/racingcar/GameManager.kt
@@ -1,4 +1,18 @@
 package racingcar
 
-class GameManager {
+class GameManager(
+    val nameList: List<String>,
+    val tryCount: Int,
+    var carsList: MutableList<Car>
+) {
+    constructor(nameList: List<String>, tryCount: Int) : this(nameList, tryCount, mutableListOf()) {
+        createCarList(nameList)
+    }
+
+    fun createCarList(nameList: List<String>) {
+        for (name in nameList) {
+            this.carsList.add(Car(name, ""))
+        }
+    }
+
 }
