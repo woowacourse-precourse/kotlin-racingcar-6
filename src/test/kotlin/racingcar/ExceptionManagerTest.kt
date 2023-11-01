4c978a7f81cb46b0ad8db8dd9eab3f5bf7b9b75e
LEE <tpqms775@naver.com>
2023-11-01 20:04:17 +0900

    refactor(racingcar):ExceptionManagerTest테스트함수 이름 변경

--- src/test/kotlin/racingcar/ExceptionManagerTest.kt
+++ src/test/kotlin/racingcar/ExceptionManagerTest.kt
@@ -8,13 +8,13 @@ class ExceptionManagerTest {
     private val exceptionManager = ExceptionManager()
 
     @Test
-    fun testValidNames() {
+    fun `정상적인 입력을 하였을 때`() {
         val validNames = listOf("fdgdf", "woni", "lee")
         assertNotNull(exceptionManager.nameException(validNames))
     }
 
     @Test
-    fun testNameLengthException() {
+    fun `이름을 5자 이상 입력했을때 예외처리`() {
         val invalidNames = listOf("pobi", "woni", "pob2124124124")
         assertThrows<IllegalArgumentException>("이름을 5자 이하로 입력해주세요") {
             exceptionManager.nameException(invalidNames)
@@ -22,7 +22,7 @@ class ExceptionManagerTest {
     }
 
     @Test
-    fun testNameLastEmptyException() {
+    fun `마지막 이름을 (,)뒤에 입력하지 않았을 때`() {
         val invalidNames = listOf("Alice", "Bob", "")
         assertThrows<IllegalArgumentException>("마지막 이름을 입력해주세요.") {
             exceptionManager.nameException(invalidNames)
@@ -30,13 +30,13 @@ class ExceptionManagerTest {
     }
 
     @Test
-    fun testValidRepeatNum() {
+    fun `정상적인 값 입력시`() {
         val validNum = "3"
         assertNotNull(exceptionManager.repeatException(validNum))
     }
 
     @Test
-    fun testInvalidRepeatNum() {
+    fun `1이상의 숫자를 입력하지 않았을 때 예외처리`() {
         val invalidNum = "0"
         assertThrows<IllegalArgumentException>("1 이상의 숫자를 입력해주세요") {
             exceptionManager.repeatException(invalidNum)
@@ -44,14 +44,14 @@ class ExceptionManagerTest {
     }
 
     @Test
-    fun testNonNumericRepeatNum() {
+    fun `숫자대신 문자를 입력했을 때 예외처리`() {
         val nonNumericNum = "abc"
         assertThrows<IllegalArgumentException>("숫자를 입력해주세요.") {
             exceptionManager.repeatException(nonNumericNum)
         }
     }
     @Test
-    fun testMinusNumericRepeatNum() {
+    fun `음수를 입력했을 때 예외처리`() {
         val nonNumericNum = "-1"
         assertThrows<IllegalArgumentException>("1 이상의 숫자를 입력해주세요") {
             exceptionManager.repeatException(nonNumericNum)
