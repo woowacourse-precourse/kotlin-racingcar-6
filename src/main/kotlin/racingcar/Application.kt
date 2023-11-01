c2aef312712695716c127be00238ad9252245f03
hyun <sarahs19991115@gmail.com>
2023-11-01 22:42:00 +0900

    자동차 n회 동안 전진/정지 구현 + 실행 결과 출력

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,21 +1,23 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    var racingCarNames: List<String>
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    racingCarNames = getRacingCarNames()
+    var CarNames: List<String> = getCarNames()
     println("시도할 횟수는 몇 회인가요?")
     val trialCount: Int = Console.readLine().toInt()
 
     println("실행 결과")
+    var moveResult: MutableList<String> = MutableList(CarNames.size) { "" }
     repeat(trialCount){
-
+        moveResult = runOneRound(CarNames, moveResult)
+        printOneRound(CarNames, moveResult)
     }
 }
 
-fun getRacingCarNames(): List<String> {
+fun getCarNames(): List<String> {
     val allRacingCarNames: String = Console.readLine().replace("\\s".toRegex(), "")
     val result: List<String> = allRacingCarNames.split(",").distinct()
 
@@ -40,4 +42,21 @@ fun isOnlyOneValidation(pureString: String): Boolean {
 
 fun isNameLongerThanFiveValidation(nameList: List<String>): Boolean {
     return nameList.all{it.length<=5}
+}
+
+
+fun runOneRound(CarNames: List<String>, moveResult: MutableList<String>): MutableList<String> {
+    val MIN_VALUE: Int = 0
+    val MAX_VALUE: Int = 9
+    for(index in CarNames.indices) {
+        val moveOrNot = Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE)
+        if(moveOrNot >= 4)
+            moveResult[index] += "-"
+    }
+    return moveResult
+}
+
+fun printOneRound(CarNames: List<String>, moveResult: MutableList<String>) {
+    for(index in CarNames.indices)
+        println("${CarNames[index]} : ${moveResult[index]}")
 }
