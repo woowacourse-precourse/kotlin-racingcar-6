45850997c26523e060f63593ed384eec7c80657d
cucumber99 <dlsguq1@gmail.com>
2023-11-01 17:33:20 +0900

    refactor: View 클래스의 메소드 접근성 수정

--- src/main/kotlin/racingcar/GameView.kt
+++ src/main/kotlin/racingcar/GameView.kt
@@ -7,14 +7,14 @@ class GameView(private val validate: InputValidate) {
         const val INPUT_CAR_NAME_MESSAGE = "경주할 자동차의 이름들을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         const val INPUT_ATTEMPT_NUMBER_MESSAGE = "시도할 횟수는 몇 회인가요?"
     }
-    private fun getCarNames(): List<String> {
+    fun getCarNames(): List<String> {
         println(INPUT_CAR_NAME_MESSAGE)
         val cars = Console.readLine().split(",")
         validate.validateCarNames(cars)
         return cars
     }
 
-    private fun getAttempts(): Int {
+    fun getAttempts(): Int {
         println(INPUT_ATTEMPT_NUMBER_MESSAGE)
         val attempts = Console.readLine()
         return validate.validateAttempts(attempts)
