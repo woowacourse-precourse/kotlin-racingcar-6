9d16aa696be1b45b8c48311b1f946645cc53269b
seonm9119 <seonm9119@gmail.com>
2023-11-01 11:44:25 +0900

    feat: inputException v2.0.0

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,12 +1,31 @@
 package racingcar
 import camp.nextstep.edu.missionutils.Console.readLine
 
+fun inputException(inputs: List<String>): List<String> {
+
+    val names = inputs.distinct().toMutableList()
+    names.removeAll{it.trim().isEmpty()}
+
+    if (names.size < 2)
+        throw IllegalArgumentException()
+
+    for (name in names){
+        if (name.length > 5)
+            throw IllegalArgumentException()
+    }
+
+    return names
+
+}
+
+
 fun main() {
 
 
     // TODO: feat: input
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    val names = readLine().split(',')
+    val inputs = readLine().split(',')
+    val names = inputException(inputs)
 
     println("시도할 횟수는 몇 회인가요?")
     val chance = readLine().toInt()
