79256e712a4e98afb4a57ccbc91f970f1c7403eb
jooyyoo <aliceyun9@naver.com>
2023-11-01 18:49:21 +0900

    [feat] 게임 시작 전 메세지와 입력값 받기 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,49 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    inputFirstSetting()
 }
+
+private fun inputFirstSetting() {
+    println(ConstValue.FIRST_LINE)
+    val carString = Console.readLine().toString()
+
+    val carList = makeCarList(carString)
+
+    println(ConstValue.SECOND_LINE)
+    val trial = Console.readLine().toString()
+
+    val numberOfTrial = validateTrialNumber(trial)
+
+    println(ConstValue.RESULT)
+
+}
+
+private fun makeCarList(carList: String): List<String> {
+    if (carList.isEmpty()) {
+        throw IllegalArgumentException("입력값이 없습니다.")
+    } else {
+        return carList.split(",")
+    }
+}
+
+private fun validateTrialNumber(num: String): Int {
+    if (num.isEmpty()) {
+        throw IllegalArgumentException("입력값이 없습니다.")
+    }
+    try {
+        val inputNumber = num.toInt()
+        if (inputNumber <= 0) {
+            throw IllegalArgumentException("입력값이 0 이하입니다")
+        }
+        return inputNumber
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("입력값이 숫자가 아닙니다")
+    }
+}
+
+
+
