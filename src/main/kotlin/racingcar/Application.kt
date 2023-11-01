56004bc6f229e00bf19de8b9b3df08213c822348
최형우 <guddn106901@gmail.com>
2023-11-01 18:02:48 +0900

    이름을 바탕으로 car class 인스턴스화 시켜주기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,13 +6,19 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val nameInput = Console.readLine()
     val carList = nameInput.split(",");
-    for( i in 0..carList.size){
+    val carlistSize = carList.size
+    for( i in 0..carlistSize){
         if(carList[i].length>5){
             throw IllegalArgumentException("5자 이상 입력")
             return
         }
     }
 
+    val carInfo = Array<Car?>(carlistSize){null}
+    for(i in 0..carlistSize-1){
+        carInfo[i] = Car(carList[i])
+    }
+
     println("시도할 횟수는 몇 회인가요?")
-    val playTime = Console.readLine()
+    val playTime = Console.readLine().toInt()
 }
