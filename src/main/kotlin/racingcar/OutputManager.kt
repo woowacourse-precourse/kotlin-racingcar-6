2d3f28965c07d976d3f25827a690c760dbd842c6
aprilgom <aprilgom@users.noreply.github.com>
2023-11-01 18:51:17 +0900

    style($OutputManager): modify indent of buildGameStatusString

--- src/main/kotlin/racingcar/OutputManager.kt
+++ src/main/kotlin/racingcar/OutputManager.kt
@@ -6,13 +6,13 @@ object OutputManager {
         print(buildGameStatusString(cars))
         println()
     }
-    private fun buildGameStatusString(cars: List<Car>) = cars.joinToString("\n"){
-            val distanceBar = "-".repeat(it.position)
-            "${it.name} : $distanceBar"
-        }
-
+    private fun buildGameStatusString(cars: List<Car>) = cars.joinToString("\n") {
+        val distanceBar = "-".repeat(it.position)
+        "${it.name} : $distanceBar"
+    }
     fun printWinners(winners: List<Car>) {
         println(buildWinnersString(winners))
     }
     private fun buildWinnersString(winners:List<Car>): String = Resources.WINNER_IS + winners.joinToString(", ") { it.name }
+
 }
