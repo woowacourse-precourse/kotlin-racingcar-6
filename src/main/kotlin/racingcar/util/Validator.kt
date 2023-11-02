db617d1e6649e8fbfa7257922d7ccb3a20686f1a
lh99j <grand2181@gmail.com>
2023-11-02 14:16:27 +0900

    feat: 사용자 입력 널값 검증 기능 구현

--- src/main/kotlin/racingcar/util/Validator.kt
+++ src/main/kotlin/racingcar/util/Validator.kt
@@ -13,4 +13,10 @@ object Validator {
         val validation = numberList.toSet()
         if (validation.size != numberList.size) throw IllegalArgumentException("중복된 자동차의 이름이 존재합니다.")
     }
+
+    fun validateNotNull(numberList: List<String>) {
+        numberList.forEach {
+            if (it.trim().isEmpty()) throw IllegalArgumentException("자동차 이름에 널값이 존재합니다.")
+        }
+    }
 }
