d8174b1e224b04704a270020756b9858bf00cdcf
lh99j <grand2181@gmail.com>
2023-11-02 13:56:53 +0900

    feat: 사용자의 입력 가져오는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,8 +1,11 @@
 package racingcar
 
+import racingcar.view.InputView
 import racingcar.view.OutputView
 
 fun main() {
     val outputView = OutputView()
+    val inputView = InputView()
     outputView.printInputCarNameMessage()
+    val userInput = inputView.getUserInput()
 }
---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,7 @@
+package racingcar.view
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+    fun getUserInput(): String = Console.readLine()
+}
