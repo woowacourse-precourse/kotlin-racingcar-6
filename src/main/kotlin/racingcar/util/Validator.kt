8251585f2d0fa57af7eb81cffa826c3ab0c3f779
lh99j <grand2181@gmail.com>
2023-11-02 14:14:23 +0900

    feat: 사용자 입력 중복 검증 기능 구현

--- src/main/kotlin/racingcar/util/Validator.kt
+++ src/main/kotlin/racingcar/util/Validator.kt
@@ -8,4 +8,9 @@ object Validator {
             if (it.length > 5) throw IllegalArgumentException("자동차의 이름이 5보다 깁니다.")
         }
     }
+
+    fun validateUnique(numberList: List<String>) {
+        val validation = numberList.toSet()
+        if (validation.size != numberList.size) throw IllegalArgumentException("중복된 자동차의 이름이 존재합니다.")
+    }
 }
