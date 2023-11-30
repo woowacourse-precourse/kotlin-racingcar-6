169e34eebd747e927cdbccbe5ea8ffb20ef28dbd
sh1mj1 <7wlgns@gmail.com>
2023-11-30 17:30:59 +0900

    feat(OutputView.showTotalWinner): 실행 결과를 알려주는 기능을 구현한다

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,18 +1,29 @@
 package racingcar.view
 
+import racingcar.model.Car
 import racingcar.model.Cars
 
 class OutputView {
 
     fun showResultHeader() = println(TRY_RESULT_HEADER)
+
     fun showTryResult(cars: Cars) {
-        repeat(cars.cars.size) {
-            val car = cars.cars[it]
-            println("${car.name} : " + "-".repeat(car.position))
+        cars.cars.forEach {
+            println("${it.name} : " + "-".repeat(it.position))
         }
+        println()
+    }
+
+    fun showTotalWinner(winners: List<Car>) {
+        print(TOTAL_WINNER_GUIDE)
+        repeat(winners.size - 1) {
+            print("${winners[it].name}, ")
+        }
+        println(winners.last().name)
     }
 
     companion object {
         const val TRY_RESULT_HEADER = "실행 결과"
+        const val TOTAL_WINNER_GUIDE = "최종 우승자 : "
     }
 }
--- src/test/kotlin/racingcar/view/OutputViewTest.kt
+++ src/test/kotlin/racingcar/view/OutputViewTest.kt
@@ -18,10 +18,20 @@ class OutputViewTest {
         val cars = Cars(
             listOf(
                 Car("pobi", 1),
-                Car("woni", 2),
+                Car("woni", 3),
                 Car("jun", 3),
             )
         )
         outputView.showTryResult(cars)
     }
+
+    @Test
+    fun `최종 우승자를 보여주는 메서드 출력 확인용 테스트`() {
+        val winners = listOf(
+            Car("woni", 3),
+            Car("jun", 3),
+        )
+        outputView.showTotalWinner(winners)
+    }
+
 }
