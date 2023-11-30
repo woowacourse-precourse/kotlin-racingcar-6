d3fdf524073e3b7b5ed2969a8c6526f22fbe8da2
sh1mj1 <7wlgns@gmail.com>
2023-11-30 14:46:23 +0900

    feat(Car): 자동차에 대한 유효성 검증과 테스트를 구현한다
    * 공백 이름을 허용하지 않는다.
    * 자동차의 이름은 5자 이하만 가능하다.

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -3,4 +3,15 @@ package racingcar.model
 data class Car(
     val name: String,
     val position: Int
-)
+) {
+    init {
+        require(name.isNotBlank()) { BLANK_NAME }
+        require(name.length <= NAME_MAX_LENGTH) { TOO_LONG_NAME }
+    }
+
+    companion object {
+        private const val NAME_MAX_LENGTH = 5
+        const val TOO_LONG_NAME = "자동차의 이름은 ${NAME_MAX_LENGTH}자 이하만 가능합니다."
+        const val BLANK_NAME = "자동차의 이름은 공백일 수 없습니다."
+    }
+}
---
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -0,0 +1,39 @@
+package racingcar.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import racingcar.model.Car.Companion.BLANK_NAME
+import racingcar.model.Car.Companion.TOO_LONG_NAME
+
+class CarTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["123456", "abcdef", "심지 자동차", "πøˆoi9"])
+    fun `자동차의 이름이 5자 초과이면 예외를 던진다`(carName: String) {
+        assertThrowsWithMessage(carName, TOO_LONG_NAME)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", "  ", "\n"])
+    fun `자동차의 이름이 공백이나 빈 값이면 예외를 던진다`(carName: String) {
+        assertThrowsWithMessage(carName, BLANK_NAME)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["a", " a ", "pobi", "woni", "jun", "simji"])
+    fun `정상적인 자동차의 이름은 통과한다`(carName: String) {
+        assertDoesNotThrow { Car(carName, 0) }
+    }
+
+
+    private fun assertThrowsWithMessage(carName: String, message: String) {
+        val exception = assertThrows<IllegalArgumentException> {
+            Car(carName, 0)
+        }
+        assertThat(exception.message).isEqualTo(message)
+    }
+
+}
